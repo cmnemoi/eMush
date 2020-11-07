7910752392fc92727907a618ced7903ee65b6383
A7 <atar.inscription@gmail.com>
2020-11-07 02:32:18 +0100

    Add char panel part 2
    move back the scss into the Vue components (because i'm an indecisive idiot)
    finish adding the char panel elements

--- App/src/components/Game/CharPanel.vue
+++ App/src/components/Game/CharPanel.vue
@@ -3,8 +3,8 @@
 
     <div class="char-sheet">
       <img class="avatar" src="@/assets/images/char/stephen_seagull.jpg" alt="avatar">
+
       <ul class="statuses"></ul>
-      <ul class="skills"></ul>
 
       <div class="health-points">
         <div class="life">
@@ -62,13 +62,54 @@
         </ul>
       </div>
     </div>
+    
+    <div class="column">
+      <ul class="skills">
+        <li><img src="@/assets/images/skills/cook.png" alt="cook"></li>
+        <li><img src="@/assets/images/skills/sturdy.png" alt="sturdy"></li>
+        <li><img src="@/assets/images/skills/opportunist.png" alt="opportunist"></li>
+        <li><img src="@/assets/images/skills/sturdy.png" alt="sturdy"></li>
+      </ul>
 
-    <div class="action-sheet">
-      <ul class="interactions"></ul>
-      <div class="action-points">
-        <div class="actions"></div>
-        <div class="movements"></div>
-        <div class="specials"></div>
+      <div class="actions-sheet">
+        <img src="@/assets/images/pam.png" alt="pam">
+        <div class="action-points">
+          <div class="actions">
+            <ul>
+              <li class="full"></li>
+              <li class="full"></li>
+              <li class="full"></li>
+              <li class="full"></li>
+              <li class="empty"></li>
+              <li class="empty"></li>
+              <li class="empty"></li>
+              <li class="empty"></li>
+              <li class="empty"></li>
+              <li class="empty"></li>
+              <li class="empty"></li>
+              <li class="empty"></li>
+            </ul>
+          </div>
+          <div class="movements">
+            <ul>
+              <li class="full"></li>
+              <li class="full"></li>
+              <li class="full"></li>
+              <li class="full"></li>
+              <li class="full"></li>
+              <li class="full"></li>
+              <li class="full"></li>
+              <li class="full"></li>
+              <li class="full"></li>
+              <li class="empty"></li>
+              <li class="empty"></li>
+              <li class="empty"></li>
+            </ul>
+          </div>
+        </div>
+        <ul class="specials">
+          <li><img src="@/assets/images/pa_cook.png">x6</li>
+        </ul>
       </div>
     </div>
 
@@ -83,5 +124,261 @@ export default {
 }
 </script>
 
-<style scoped>
+<style lang="scss" scoped>
+
+
+
+.char-panel {
+  flex-direction: row;
+
+  & .char-sheet {
+    width: 176px;
+    min-height: 459px;
+    padding: 5px;
+    border-top-left-radius: 4px;
+    background: rgba(54,76,148,.35);
+
+    & .avatar {
+      width: 166px;
+      height: auto;
+    }
+
+    & .health-points {
+      flex-direction: row;
+      margin: 3px 0;
+
+      & .life, & .morale {
+        flex-direction: row;
+        align-items: center;
+        margin-right: 3px;
+
+        & p, & ul {
+          display: flex;
+          flex-direction: row;
+          align-items: center;
+          border: 1px solid #4077b5;
+          border-radius: 3px;
+          background: rgba(58,106,171,1);
+          box-shadow: 0 0 4px 1px inset rgba(28,29,56,1);
+        }
+
+        & p {
+
+          min-width: 24px;
+          height: 15px;
+          margin: 1px;
+          padding-right: 1px;
+          font-size: .5em;
+          letter-spacing: .03em;
+          border-right-width: 0;
+
+          
+          & img {
+            width: 11px;
+            height: 13px;
+            margin-right: 1px;
+          }
+        }
+
+        & ul {
+          /*min-width: 59px;*/
+          height: 11px;
+          border-radius: 2px;
+          margin-left: -2px;
+          border-left-width: 0;
+
+          & li {
+            width: 3px;
+            height: 5px;
+            margin-right: 1px;
+            background: rgba(138,170,44,1);
+            box-shadow: 1px 1px 0 0 inset rgba(255,255,255,.7);
+
+            &.empty {
+              background: rgba(37,72,137,1);
+              box-shadow: 1px 1px 0 0 inset rgba(78,154,255,.7);
+            }
+          }
+        }
+      }
+    }
+
+    & .inventory {
+      display: flex;
+      flex-direction: row;
+
+      & li {
+        width: 54px;
+        height: 54px;
+        margin-right: 1px;
+        border: 1px solid #4077b5;
+        border-radius: 3px;
+        background: rgba(58,106,171,1);
+        box-shadow: 0 0 8px 2px inset rgba(17,56,128,1);
+      }
+    }
+
+    & .interactions {
+      margin-top: 12px;
+
+      & > p {
+        margin: 0 0 4px 0;
+        font-size: .83em;
+        letter-spacing: .03em;
+        font-variant: small-caps;
+      }
+
+      & ul {
+        display: flex;
+        flex-direction: column;
+
+        & li a {
+          display: flex;
+          flex-direction: row;
+          align-items: center;
+          justify-content: center;
+          min-height: 19px;
+          width: auto;
+          margin: 1px;
+          padding-bottom: 2px;
+          border-radius: 1px;
+          color: white ;
+          font-size: .83em;
+          font-weight: 700;
+          letter-spacing: .03em;
+          text-decoration: none;
+          font-variant: small-caps;
+          text-align: center;
+          text-shadow: 0 0 4px #15273C, 0 0 4px #15273C;
+          box-shadow: 0 0 0 1px rgba(12,18,137,1);
+          background: #003cc3;
+          background: linear-gradient(0deg, #003cc3 2%, #0074df 6%, #0074df 46%, #008ee5 54%, #008ee5 94%, #499ce5 96%);
+
+          & img {
+            padding: 0 .3em;
+          }
+
+          &:hover{
+            background: #003cc3;
+            background: linear-gradient(0deg, #0061D5 2%, #00A0E9 6%, #00A0E9 46%, #00B3ED 54%, #00B3ED 94%, #40B5EC 96%);
+          }
+        }
+        & li.crossed a {
+          opacity: .5;
+          text-decoration: line-through;
+        }
+      }
+    }
+  }
+
+  & .column {
+    justify-content: space-between;
+
+    & .skills {
+      display: flex;
+      flex-direction: column;
+      min-width: 32px;
+      float: right;
+
+      & li {
+
+        display: flex;
+        align-items: center;
+        /* justify-content: center; */
+        width: 30px;
+        height: 34px;
+        padding-right: 3px;
+        margin-bottom: 7px;
+        background: transparent url('~@/assets/images/skills/skillblock_gold.png') center left no-repeat;
+        border-left: 1px solid #191A53;
+
+        &:nth-child(1) {
+          background: transparent url('~@/assets/images/skills/skillblock.png') center left no-repeat;
+        }
+        &:nth-child(2) {
+          background: transparent url('~@/assets/images/skills/skillblock_once.png') center left no-repeat;
+        }
+
+      }
+    }
+
+    & .actions-sheet {
+      align-items: center;
+      justify-content: flex-start;
+      width: 28px;
+      min-height: 134px;
+      padding: 5px 5px 5px 0;
+      border-top-right-radius: 4px;
+      background: rgba(54,76,148,.35);
+
+      & > img { margin: 3px; }
+
+      & .action-points {
+        flex-direction: row;
+
+        & > div {
+          border-radius: 3px;
+          background: rgb(17,18,48);
+          background: linear-gradient(0deg, rgba(17,18,48,1) 0%, rgba(53,56,152,1) 100%);
+
+          & ul {
+            $border: 1px;
+            display: block;
+            flex-direction: column;
+            align-items: center;
+            margin: 1px;
+            padding: 1px;
+            border-radius: 2px;
+            border: 1px solid;
+            border-image-slice: 1;
+            border-width: 1px;
+            border-image-source: linear-gradient(0deg, rgba(13,44,115,1) 0%, rgba(89,164,216,1) 100%);
+            background: rgb(17,18,48);
+            background: linear-gradient(0deg, rgba(17,18,48,1) 0%, rgba(53,56,152,1) 100%);
+
+            & li {
+              width: 5px;
+              height: 6px;
+              border-bottom: 1px solid black;
+              background: rgba(138,170,44,1);
+              box-shadow: 0 -1px 0 0 inset rgba(0,0,0,.4);
+            }
+          }
+        }
+
+        & .movements ul li {
+          background: rgb(0,255,228);
+          background: linear-gradient(135deg, rgba(255,255,255,1) 5%, rgba(0,255,228,1) 20%);
+
+          &.empty {
+            background: rgb(14,62,56);
+            background: linear-gradient(135deg, rgba(18,85,106,1) 5%, rgba(14,62,56,1) 20%);
+          }
+        }
+
+        & .actions ul li {
+          background: rgb(255,85,153);
+          background: linear-gradient(135deg, rgba(255,255,255,1) 5%, rgba(255,85,153,1) 20%);
+
+          &.empty {
+            background: rgb(64,0,0);
+            background: linear-gradient(135deg, rgba(77,17,32,1) 5%, rgba(64,0,0,1) 20%);
+          }
+        }
+      }
+
+      & .specials li {
+        display: flex;
+        flex-direction: row;
+        align-items: baseline;
+        margin: 2px 0;
+        font-size: .65em;
+        font-weight: 700;
+
+        & img { margin-right: -3px; }
+      }
+    }
+  }
+}
+
 </style>
--- App/src/components/Game/GameBanner.vue
+++ App/src/components/Game/GameBanner.vue
@@ -71,5 +71,175 @@ export default {
 }
 </script>
 
-<style scoped>
+<style  lang="scss" scoped>
+
+.titles, .title, .game-banner, .daedalus-banner, .character-banner, .daedalus-players, .cycle-time {
+  flex-direction: row;
+
+}
+
+p, a, li, span {
+  font-size: .85em;
+  letter-spacing: .03em;
+  font-variant: small-caps;
+}
+
+.game-banner {
+  margin-bottom: 8px;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: baseline;
+}
+
+.character-banner {
+  display: flex;
+  flex-direction: row;
+
+  & .in-game-level {
+
+    --bezel-size: 11.5px;
+
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+    width: 52px;
+    height: 35px;
+    font-size: 17pt;
+    border: 1px solid rgba(4,21,156,1);
+    border-radius: 4px;
+    background: rgb(0,60,195);
+    background: linear-gradient(0deg, rgba(0,60,195,1) 2%, rgba(0,116,223,1) 6%, rgba(0,116,223,1) 46%, rgba(0,142,229,1) 54%, rgba(0,142,229,1) 94%, rgba(73,156,229,1) 96%);
+
+    clip-path: polygon(
+      var(--bezel-size) 0,
+      100% 0,
+      100% 100%,
+      0 100%,
+      0 var(--bezel-size)
+    );
+
+    margin: auto;
+    padding-right: 4px;
+    font-family: Days-One;
+    color: white;
+    text-decoration: none;
+    text-shadow: 0 0 4px #15273C;
+  }
+
+  & .in-game-level-progress {
+    position: relative;
+    width: 11px;
+    height: 33px;
+    z-index: 2;
+    margin: 1px;
+    margin-left: -10px;
+    border-radius: 4px;
+    border: 1px solid #4077B5;
+    background: #4077B5;
+    box-shadow: 0 0 5px 1px #15273C inset, 0 0 0 1px #234164;
+
+    & > div {
+      position: relative;
+      width: 100%;
+      height: 100%;
+      border-radius: 3px;
+      background: rgb(128,72,112);
+      background: linear-gradient(90deg, rgba(255,137,204,1) 0%, rgba(255,137,204,1) 25%, rgba(213,38,89,1) 94%, rgba(128,18,47,1) 100%);
+      box-shadow: 0 0 2px 1px rgba(71,10,26,.7) inset;
+    }
+
+    &::after {
+      content: "";
+      position: absolute;
+      bottom: 2px;
+      width: 9px;
+      height: 11px;
+      background : transparent url('~@/assets/images/levelup_arrow_ingame.png') no-repeat center; 
+    }
+  }
+
+  & .character-info {
+    display: flex;
+    justify-content: center;
+    margin-left: 6px;
+
+    & .character-name {
+      margin-bottom: 1px;
+      font-size: .91em;
+      font-weight: 700;
+      text-transform: uppercase;
+    }
+
+    & .title {
+      display: flex;
+      flex-direction: row;
+      align-items: center;
+
+      & .title-name {
+        margin-left: 4px;
+        font-size: .68em;
+        font-weight: bold;
+        font-variant: small-caps;
+        white-space: nowrap;
+        color: rgba(255,255,255,0.75);
+      }
+    }
+  }
+}
+
+
+
+.daedalus-banner {
+  align-items: center;
+
+  & > div {
+    margin: 0 .8em;
+    display:flex;
+    align-items: center;
+  }
+
+  & .daedalus-alarms {
+    padding: 3px 6px 1px 6px;
+    max-height: 25px;
+    border: 1px solid red;
+    border-radius: 3px;
+    background: rgba(58,106,171,1);
+    box-shadow: 0 0 5px 1px inset rgba(28,29,56,1);
+
+    animation: alarms-border-color .85s ease-in-out infinite; /* keyframes at the end of the doc */
+
+    color: #ff4e64;
+    text-shadow: 0 0 2px rgba(0,0,0,1), 0 0 2px rgba(0,0,0,1); /* twice the same shadow */
+    font-weight: bold;
+
+    & li:not(:last-child) {
+      margin-right: 10px;
+    }
+  }
+
+  & .daedalus-info li {
+    display: flex;
+    align-items: center;
+  
+      &:not(:last-child) {
+      padding-right: 1em;
+    }
+
+    & img {
+      padding: 2px 0 0 4px;
+    }
+  }
+
+  & .cycle-time img {
+    margin-right: .4em;
+  }
+}
+
+@keyframes alarms-border-color {
+  0%   {border: 1px solid #ff4e64 ;}
+  50%  {border: 1px solid rgba(58,106,171,1) ;}
+  100% {border: 1px solid #ff4e64 ;}
+}
+
 </style>

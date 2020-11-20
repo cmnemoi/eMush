6f2a40cb740fef47cf06738bfac97811e2304fa7
Sylvain Breton <sylvain.breton.6@gmail.com>
2020-11-20 07:33:17 +0100

    added files

---
+++ Api/src/Action/Actions/Hide.php
@@ -0,0 +1,105 @@
+<?php
+
+namespace Mush\Action\Actions;
+
+use Mush\Action\ActionResult\ActionResult;
+use Mush\Action\ActionResult\Success;
+use Mush\Action\Entity\ActionParameters;
+use Mush\Action\Enum\ActionEnum;
+use Mush\Action\Service\SuccessRateServiceInterface;
+use Mush\Game\Entity\GameConfig;
+use Mush\Game\Service\GameConfigServiceInterface;
+use Mush\Game\Service\RandomServiceInterface;
+use Mush\Item\Entity\GameItem;
+use Mush\Item\Service\GameItemServiceInterface;
+use Mush\Player\Entity\Player;
+use Mush\Player\Service\PlayerServiceInterface;
+use Mush\RoomLog\Enum\VisibilityEnum;
+use Mush\RoomLog\Service\RoomLogServiceInterface;
+use Mush\Status\Enum\ItemStatusEnum;
+use Mush\Status\Service\StatusServiceInterface;
+use Symfony\Component\EventDispatcher\EventDispatcherInterface;
+
+class Hide extends Action
+{
+    protected const NAME = ActionEnum::REPAIR;
+
+    private GameItem $gameItem;
+
+    private RoomLogServiceInterface $roomLogService;
+    private GameItemServiceInterface $gameItemService;
+    private PlayerServiceInterface $playerService;
+    private GameConfig $gameConfig;
+
+    public function __construct(
+        EventDispatcherInterface $eventDispatcher,
+        RoomLogServiceInterface $roomLogService,
+        GameItemServiceInterface $gameItemService,
+        PlayerServiceInterface $playerService,
+        RandomServiceInterface $randomService,
+        SuccessRateServiceInterface $successRateService,
+        StatusServiceInterface $statusService,
+        GameConfigServiceInterface $gameConfigService
+    ) {
+        parent::__construct($randomService, $successRateService, $eventDispatcher, $statusService);
+
+        $this->roomLogService = $roomLogService;
+        $this->gameItemService = $gameItemService;
+        $this->playerService = $playerService;
+        $this->randomService = $randomService;
+        $this->successRateService = $successRateService;
+        $this->gameConfig = $gameConfigService->getConfig();
+
+        $this->actionCost->setActionPointCost(1);
+    }
+
+    public function loadParameters(Player $player, ActionParameters $actionParameters)
+    {
+        if (!($item = $actionParameters->getItem()) && !($item = $actionParameters->getDoor())) {
+            throw new \InvalidArgumentException('Invalid item parameter');
+        }
+
+        $this->player = $player;
+        $this->gameItem = $item;
+    }
+
+    public function canExecute(): bool
+    {
+        //Check that the item is reachable
+        return $this->gameItem->isBroken() &&
+            $this->player->canReachItem($this->gameItem)
+        ;
+    }
+
+    protected function applyEffects(): ActionResult
+    {
+        $modificator = 1; //@TODO: skills, wrench
+
+        $response = $this->makeAttempt($this->gameItem->getBrokenRate(), $modificator);
+
+        if ($response instanceof Success) {
+            $this->gameItem->removeStatus($this->gameItem->getStatusByName(ItemStatusEnum::BROKEN));
+            $this->gameItemService->persist($this->gameItem);
+        }
+
+        $this->playerService->persist($this->player);
+
+        return $response;
+    }
+
+    protected function createLog(ActionResult $actionResult): void
+    {
+        $this->roomLogService->createPlayerLog(
+            ActionEnum::REPAIR,
+            $this->player->getRoom(),
+            $this->player,
+            VisibilityEnum::PUBLIC,
+            new \DateTime('now')
+        );
+    }
+
+    public function getActionName(): string
+    {
+        return self::NAME;
+    }
+}

21efa190ce09502d010cd98bc1541c68bc471fd7
Tishwa <solen.leroux.c@gmail.com>
2021-01-05 22:44:11 +0100

    add bunch of rules

--- App/.eslintrc.js
+++ App/.eslintrc.js
@@ -1,14 +1,25 @@
 module.exports = {
     "root": true,
     "env": {
-      "node": true
+        "node": true
     },
     "extends": [
-      "plugin:vue/vue3-recommended",
-      "eslint:recommended"
+        "plugin:vue/vue3-recommended",
+        "eslint:recommended"
     ],
     "parserOptions": {
-      "parser": "babel-eslint"
+        "parser": "babel-eslint"
     },
-    "rules": {}
+    "rules": {
+        "indent": ["warn", 4],
+        "vue/html-indent": ["warn", 4],
+        "vue/script-indent": ["warn", 4],
+        "vue/max-attributes-per-line": ["warn", { "singleline": 3 }],
+        "comma-dangle": ["warn", "never"],
+        "eol-last": ["warn", "always"],
+        "no-trailing-spaces": "warn",
+        "no-var": "warn",
+        "object-curly-spacing": ["warn", "always"],
+        "semi": ["warn", "always"]
+    }
 }

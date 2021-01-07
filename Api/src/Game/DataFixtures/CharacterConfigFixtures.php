1edb5b6cbaf18476f2c2daff4c3147a4e6dc0650
Sylvain Breton <sylvain.breton.6@gmail.com>
2021-01-07 08:57:13 +0100

    merge comment

--- Api/src/Game/DataFixtures/CharacterConfigFixtures.php
+++ Api/src/Game/DataFixtures/CharacterConfigFixtures.php
@@ -24,10 +24,15 @@ class CharacterConfigFixtures extends Fixture implements DependentFixtureInterfa
         $hitAction = $this->getReference(ActionsFixtures::HIT_DEFAULT);
         $hideAction = $this->getReference(ActionsFixtures::HIDE_DEFAULT);
         $searchAction = $this->getReference(ActionsFixtures::SEARCH_DEFAULT);
-        $searchAction = $this->getReference(ActionsFixtures::SABOTAGE_DEFAULT);
-        $searchAction = $this->getReference(ActionsFixtures::EXTRACT_SPORE);
-        $searchAction = $this->getReference(ActionsFixtures::INFECT_PLAYER);
-        $defaultActions = new ArrayCollection([$hitAction, $hideAction, $searchAction]);
+        $sabotageAction = $this->getReference(ActionsFixtures::SABOTAGE_DEFAULT);
+        $extractSporeAction = $this->getReference(ActionsFixtures::EXTRACT_SPORE);
+        $infectAction = $this->getReference(ActionsFixtures::INFECT_PLAYER);
+        $defaultActions = new ArrayCollection([$hitAction,
+                                               $hideAction,
+                                               $searchAction,
+                                               $sabotageAction,
+                                               $infectAction,
+                                               $extractSporeAction]);
 
         $andie = new CharacterConfig();
         $andie

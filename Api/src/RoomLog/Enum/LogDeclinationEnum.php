097f074e4e83d652127a8435899db6bb0f8712ff
Sylvain Breton <sylvain.breton.6@gmail.com>
2021-01-09 18:52:49 +0100

    corrected some logs

--- Api/src/RoomLog/Enum/LogDeclinationEnum.php
+++ Api/src/RoomLog/Enum/LogDeclinationEnum.php
@@ -6,7 +6,18 @@ use Mush\Action\Enum\ActionEnum;
 
 class LogDeclinationEnum
 {
-    public static function getAll(): array
+    public const REPAIR_FAIL_P1 = 'repair_fail_part1';
+    public const REPAIR_FAIL_P2 = 'repair_fail_part2';
+
+
+    public static function getAllParts(): array
+    {
+        return [
+            ActionLogEnum::REPAIR_SUCCESS => [self::REPAIR_FAIL_P1, self::REPAIR_FAIL_P2],
+        ];
+    }
+
+    public static function getAllVersions(): array
     {
         return [
             ActionLogEnum::CONSUME_SUCCESS => [
@@ -64,22 +75,88 @@ class LogDeclinationEnum
                 'shred_4' => 1,
             ],
             ActionEnum::RETRIEVE_OXYGEN => [
-                'retrieve_oxygen_1' => 1,
-                'retrieve_oxygen_2' => 1,
-                'retrieve_oxygen_3' => 1,
-                'retrieve_oxygen_4' => 1,
-                'retrieve_oxygen_5' => 1,
-                'retrieve_oxygen_6' => 1,
-                'retrieve_oxygen_7' => 1,
-                'retrieve_oxygen_8' => 1,
-                'retrieve_oxygen_9' => 1,
-                'retrieve_oxygen_10' => 1,
+                'retrieve_oxygen_v1' => 1,
+                'retrieve_oxygen_v2' => 1,
+                'retrieve_oxygen_v3' => 1,
+                'retrieve_oxygen_v4' => 1,
+                'retrieve_oxygen_v5' => 1,
+                'retrieve_oxygen_v6' => 1,
+                'retrieve_oxygen_v7' => 1,
+                'retrieve_oxygen_v8' => 1,
+                'retrieve_oxygen_v9' => 1,
+                'retrieve_oxygen_v10' => 1,
+            ],
+            self::REPAIR_FAIL_P1 => [
+                'repair_fail_p1_v1' => 1,
+                'repair_fail_p1_v2' => 1,
+                'repair_fail_p1_v3' => 1,
+                'repair_fail_p1_v4' => 1,
+                'repair_fail_p1_v5' => 1,
+                'repair_fail_p1_v6' => 1,
+                'repair_fail_p1_v7' => 1,
+                'repair_fail_p1_v8' => 1,
+                'repair_fail_p1_v9' => 1,
+                'repair_fail_p1_v10' => 1,
+            ],
+            self::REPAIR_FAIL_P2 => [
+                'repair_fail_p2_v1' => 1,
+                'repair_fail_p2_v2' => 1,
+                'repair_fail_p2_v3' => 1,
+                'repair_fail_p2_v4' => 1,
+                'repair_fail_p2_v5' => 1,
+                'repair_fail_p2_v6' => 1,
+                'repair_fail_p2_v7' => 1,
+                'repair_fail_p2_v8' => 1,
+                'repair_fail_p2_v9' => 1,
+                'repair_fail_p2_v10' => 1,
+                'repair_fail_p2_v11' => 1,
+                'repair_fail_p2_v12' => 1,
+                'repair_fail_p2_v13' => 1,
+                'repair_fail_p2_v14' => 1,
+                'repair_fail_p2_v15' => 1,
+                'repair_fail_p2_v16' => 1,
+                'repair_fail_p2_v17' => 1,
+                'repair_fail_p2_v18' => 1,
+                'repair_fail_p2_v19' => 1,
+                'repair_fail_p2_v20' => 1,
+                'repair_fail_p2_v21' => 1,
+                'repair_fail_p2_v22' => 1,
+                'repair_fail_p2_v23' => 1,
+                'repair_fail_p2_v24' => 1,
+                'repair_fail_p2_v25' => 1,
+                'repair_fail_p2_v26' => 1,
+                'repair_fail_p2_v27' => 1,
+                'repair_fail_p2_v28' => 1,
+                'repair_fail_p2_v29' => 1,
+                'repair_fail_p2_v30' => 1,
+                'repair_fail_p2_v31' => 1,
+                'repair_fail_p2_v32' => 1,
+                'repair_fail_p2_v33' => 1,
+                'repair_fail_p2_v34' => 1,
+                'repair_fail_p2_v35' => 1,
+                'repair_fail_p2_v36' => 1,
+                'repair_fail_p2_v37' => 1,
+                'repair_fail_p2_v38' => 1,
+                'repair_fail_p2_v39' => 1,
+                'repair_fail_p2_v40' => 1,
+                'repair_fail_p2_v41' => 1,
+                'repair_fail_p2_v42' => 1,
+                'repair_fail_p2_v43' => 1,
+                'repair_fail_p2_v44' => 1,
+                'repair_fail_p2_v45' => 1,
+                'repair_fail_p2_v46' => 1,
+                'repair_fail_p2_v47' => 1,
             ],
         ];
     }
 
     public static function getDeclination(string $key): ?array
     {
-        return self::getAll()[$key] ?? null;
+        return self::getAllVersions()[$key] ?? null;
+    }
+
+    public static function getComposed(string $key): ?array
+    {
+        return self::getAllParts()[$key] ?? null;
     }
 }

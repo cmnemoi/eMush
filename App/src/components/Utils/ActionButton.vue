d2b0784c36aa627c3b412c5ae86ac9d76d40286f
Tishwa <solen.leroux.c@gmail.com>
2021-01-09 12:45:03 +0100

    create ActionButton component and use it

---
+++ App/src/components/Utils/ActionButton.vue
@@ -0,0 +1,21 @@
+<template>
+    <a class="action-button" href="#">
+        <span v-if="action.movementPointCost > 0">{{ action.movementPointCost }}<img src="@/assets/images/pm.png" alt="mp"></span>
+        <span v-else-if="action.actionPointCost > 0">{{ action.actionPointCost }}<img src="@/assets/images/pa.png" alt="ap"></span>
+        {{ action.name }}
+    </a>
+</template>
+
+<script>
+export default {
+    props: {
+        action: Object
+    }
+};
+</script>
+
+<style lang="scss" scoped>
+.action-button {
+    @include button-style();
+}
+</style>

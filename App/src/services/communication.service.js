57f794edbe8675e63456bb00e4a12445d6012975
Tishwa <solen.leroux.c@gmail.com>
2021-01-12 20:21:56 +0100

    move roomLogs fetching to channel service

--- App/src/services/communication.service.js
+++ App/src/services/communication.service.js
@@ -1,9 +1,11 @@
 import ApiService from "@/services/api.service";
 import { Channel } from "@/entities/Channel";
 import { Message } from "@/entities/Message";
-import { ROOM_LOG, TIPS } from '@/enums/communication.enum';
+import { RoomLog } from "@/entities/RoomLog";
+import { PRIVATE, PUBLIC, ROOM_LOG, TIPS } from '@/enums/communication.enum';
 
-const CHANNELS_ENDPOINT = process.env.VUE_APP_API_URL+'channel';
+const CHANNELS_ENDPOINT = process.env.VUE_APP_API_URL + 'channel';
+const ROOM_LOGS_ENDPOINT = process.env.VUE_APP_API_URL + 'room-log';
 
 const CommunicationService = {
 
@@ -38,15 +40,51 @@ const CommunicationService = {
     },
 
     loadMessages: async (channel) => {
-        const messagesData = await ApiService.get(CHANNELS_ENDPOINT + '/' + channel.id + '/message');
-
-        let messages = [];
-        if (messagesData.data) {
-            messagesData.data.forEach((data) => {
-                messages.push((new Message()).load(data));
-            });
+        switch (channel.scope) {
+        case PRIVATE:
+        case PUBLIC:
+            return loadChannelMessages();
+        case ROOM_LOG:
+            return loadRoomLogs();
+        default:
+            return [];
         }
-        return messages;
+
+        async function loadChannelMessages() {
+            const messagesData = await ApiService.get(CHANNELS_ENDPOINT + '/' + channel.id + '/message');
+
+            let messages = [];
+            if (messagesData.data) {
+                messagesData.data.forEach((data) => {
+                    messages.push((new Message()).load(data));
+                });
+            }
+            return messages;
+        }
+
+        async function loadRoomLogs() {
+            const result = await ApiService.get(ROOM_LOGS_ENDPOINT);
+
+            const logs = [];
+            if (result.data) {
+                const days = result.data;
+                Object.keys(days).map((day) => {
+                    Object.keys(days[day]).map((cycle) => {
+                        let roomLogs = [];
+                        days[day][cycle].forEach((value) => {
+                            let roomLog = (new RoomLog()).load(value);
+                            roomLogs.push(roomLog);
+                        });
+                        logs.push({
+                            "day": day,
+                            "cycle": cycle,
+                            roomLogs
+                        });
+                    });
+                });
+            }
+            return logs;
+        }
     },
 
     sendMessage: async (channel, text, parent) => {

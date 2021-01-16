a0be74c2f09c958f554f2a41d52afa20c29eaa4f
Tishwa <solen.leroux.c@gmail.com>
2021-01-16 15:04:13 +0100

    create MiniMap component

--- App/src/components/Game/Ship/ShipPanel.vue
+++ App/src/components/Game/Ship/ShipPanel.vue
@@ -30,30 +30,8 @@
                     </p>
                 </div>
             </div>
-        </div> <!-- PLACEHOLDER -->
-        <div v-if="! loading" class="map-container">
-            <div class="map">
-                <img src="@/assets/images/shipmap.svg">
-                <ul class="crew-position">
-                    <li class="self" />
-                    <li />
-                    <li />
-                    <li />
-                    <li />
-                    <li />
-                    <li />
-                    <li />
-                    <li />
-                    <li />
-                    <li />
-                    <li />
-                    <li />
-                    <li />
-                    <li />
-                    <li />
-                </ul>
-            </div>
         </div>
+        <MiniMap v-if="! loading" />
         <RoomInventoryPanel v-if="! loading" :items="room.items" />
         <p v-if="loading" class="loading">
             Loading...
@@ -62,6 +40,7 @@
 </template>
 
 <script>
+import MiniMap from "@/components/Game/Ship/MiniMap";
 import RoomInventoryPanel from "@/components/Game/Ship/RoomInventoryPanel";
 import ActionButton from "@/components/Utils/ActionButton";
 import { Room } from "@/entities/Room";
@@ -72,6 +51,7 @@ export default {
     name: "ShipPanel",
     components: {
         ActionButton,
+        MiniMap,
         RoomInventoryPanel
     },
     props: {
@@ -126,59 +106,6 @@ export default {
         font-size: 10px;
         font-weight: 700;
     }
-
-    & .map-container {
-        align-self: flex-end;
-        z-index: 5;
-        bottom: 200px;
-        width: 240px;
-        height: 200px;
-        background: #070724;
-
-        @include corner-bezel(0, 18px, 0);
-
-        transform: scale(0.5);
-        transform-origin: bottom left;
-        transition: transform 0.5s;
-
-        &:hover {
-            transform: scale(1);
-            transition: transform 0.6s 0.2s;
-        }
-
-        & .map {
-            width: 184px;
-            height: 96px;
-            margin: auto;
-            transform: rotate(30deg);
-        }
-
-        & .crew-position {
-            position: absolute;
-
-            li {
-                position: absolute;
-                width: 4px;
-                height: 4px;
-                background: #f88;
-
-                @for $i from 1 through 16 { // randomize crew position, for testing only
-                    &:nth-child(#{$i}) {
-                        left: random(156) + 10px;
-                        top: random(52) + 18px;
-                    }
-                }
-
-                &.self {
-                    z-index: 2;
-                    width: 6px;
-                    height: 6px;
-                    background: #889c28;
-                    animation: self-position-color 1.1s infinite;
-                }
-            }
-        }
-    }
 }
 
 /* PROVISIONAL SHIP INTERACTIONS */
@@ -209,13 +136,6 @@ export default {
     .door-action-button {
         max-width: 120px;
     }
-}
-
-@keyframes self-position-color {
-    0% { background: #ff0; }
-    39% { background: #ff0; }
-    40% { background: #889c28; }
-    100% { background: #889c28; }
 }
 
 </style>

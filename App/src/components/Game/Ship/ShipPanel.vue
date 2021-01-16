7a798324bc4d8fe7946b308ae5070c5cabba4529
Tishwa <solen.leroux.c@gmail.com>
2021-01-16 15:25:34 +0100

    create TextualInterface component

--- App/src/components/Game/Ship/ShipPanel.vue
+++ App/src/components/Game/Ship/ShipPanel.vue
@@ -1,36 +1,16 @@
 <template>
-    <div class="ship-panel" @click="clickOnTarget(getPlayer, $event)">
+    <div class="ship-panel">
         <p v-if="! loading" class="room">
             {{ room.name }}
         </p>
-        <div v-if="! loading" class="ship-view">
-            <div class="textual">
-                <h1>Doors</h1>
-                <div v-for="door in room.doors" :key="door.id" class="door">
-                    <p>{{ door.direction }} :</p>
-                    <ActionButton
-                        v-for="(action, key) in door.actions"
-                        :key="key"
-                        class="door-action-button"
-                        :action="action"
-                        @click="executeDoorAction(door, action)"
-                    />
-                </div>
-
-                <h1>Equipment</h1>
-                <div v-for="(equipment,key) in room.equipments" :key="key">
-                    <p @click="clickOnTarget(equipment, $event); $event.stopPropagation()">
-                        {{ equipment.name }}
-                    </p>
-                </div>
-                <h1>Players</h1>
-                <div v-for="(player,key) in room.players" :key="key">
-                    <p @click="clickOnTarget(player, $event)">
-                        {{ player.characterValue }}
-                    </p>
-                </div>
-            </div>
-        </div>
+        <TextualInterface
+            v-if="! loading"
+            class="ship-view"
+            :room="room"
+            @clickOnDoor="executeDoorAction"
+            @clickOnTarget="setTarget"
+            @clickOnNothing="setTarget(getPlayer)"
+        />
         <MiniMap v-if="! loading" />
         <RoomInventoryPanel v-if="! loading" :items="room.items" />
         <p v-if="loading" class="loading">
@@ -42,7 +22,7 @@
 <script>
 import MiniMap from "@/components/Game/Ship/MiniMap";
 import RoomInventoryPanel from "@/components/Game/Ship/RoomInventoryPanel";
-import ActionButton from "@/components/Utils/ActionButton";
+import TextualInterface from "@/components/Game/Ship/TextualInterface";
 import { Room } from "@/entities/Room";
 import ActionService from "@/services/action.service";
 import { mapActions, mapGetters } from "vuex";
@@ -50,9 +30,9 @@ import { mapActions, mapGetters } from "vuex";
 export default {
     name: "ShipPanel",
     components: {
-        ActionButton,
         MiniMap,
-        RoomInventoryPanel
+        RoomInventoryPanel,
+        TextualInterface
     },
     props: {
         room: Room
@@ -64,13 +44,13 @@ export default {
         ])
     },
     methods: {
-        async executeDoorAction(door, action) {
+        async executeDoorAction({ door, action }) {
             this.setLoading();
             await ActionService.executeDoorAction(door, action);
             await this.reloadPlayer();
         },
-        clickOnTarget: function (target, event) {
-            this.selectTarget({ target:target }); event.stopPropagation();
+        setTarget(target) {
+            this.selectTarget({ target });
         },
         ...mapActions('player', [
             'reloadPlayer',
@@ -105,36 +85,6 @@ export default {
         font-family: 'Pixel-Square';
         font-size: 10px;
         font-weight: 700;
-    }
-}
-
-/* PROVISIONAL SHIP INTERACTIONS */
-
-.textual {
-    overflow: auto;
-    padding: 18px 12px;
-    font-size: 0.83em;
-
-    h1,
-    h2,
-    h3 {
-        color: #cf1830;
-        font-size: 1.5em;
-        font-variant: small-caps;
-        margin: 12px 0 4px 0;
-    }
-
-    p {
-        margin: 12px 0 4px 0;
-        font-weight: 700;
-    }
-
-    & > *:last-child {
-        margin-bottom: 188px;
-    }
-
-    .door-action-button {
-        max-width: 120px;
     }
 }
 

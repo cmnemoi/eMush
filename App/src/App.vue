52552a08d495fc507b2ef584a49b76970917dcfc
Tishwa <solen.leroux.c@gmail.com>
2021-01-19 20:33:26 +0100

    add translation buttons

--- App/src/App.vue
+++ App/src/App.vue
@@ -2,6 +2,7 @@
     <div class="main-container">
         <Banner />
         <router-view />
+        <Footer />
         <ErrorPopup />
     </div>
 </template>
@@ -10,12 +11,14 @@
 
 import Banner from "@/components/Banner";
 import ErrorPopup from "@/components/ErrorPopup";
+import Footer from "@/components/Footer";
 
 export default {
     name: 'App',
     components: {
         Banner,
-        ErrorPopup
+        ErrorPopup,
+        Footer
     }
 };
 </script>
---
+++ App/src/components/Footer.vue
@@ -0,0 +1,38 @@
+<template>
+    <div class="footer">
+        <button
+            v-for="(language, key) in supportedLanguages"
+            :key="key"
+            class="language-button"
+            @click="() => setUserLanguage(language)"
+        >
+            {{ language }}
+        </button>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            supportedLanguages: ["en", "es", "fr"]
+        };
+    }
+};
+</script>
+
+<style lang="scss" scoped>
+
+.footer {
+    flex-direction: row;
+    justify-content: flex-end;
+    padding: 0 20px;
+}
+
+.language-button {
+    cursor: pointer;
+
+    @include button-style();
+}
+
+</style>

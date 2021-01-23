228f15147d5cc4676f48f9bfbec1fa0ffe4400c1
Sylvain Breton <sylvain.breton.6@gmail.com>
2021-01-23 19:10:26 +0100

    syntax

--- Api/src/Equipment/Repository/GameEquipmentRepository.php
+++ Api/src/Equipment/Repository/GameEquipmentRepository.php
@@ -3,11 +3,7 @@
 namespace Mush\Equipment\Repository;
 
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
-use Doctrine\Common\Collections\ArrayCollection;
-use Doctrine\Common\Collections\Collection;
 use Doctrine\Persistence\ManagerRegistry;
-use Mush\Daedalus\Entity\Daedalus;
-use Mush\Equipment\Entity\Door;
 use Mush\Equipment\Entity\GameEquipment;
 
 class GameEquipmentRepository extends ServiceEntityRepository

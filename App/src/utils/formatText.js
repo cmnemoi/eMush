f160a149052b93371a59a137f8c8aad0eaf64c2f
Tishwa <solen.leroux.c@gmail.com>
2021-01-24 18:50:49 +0100

    add test suit and test file

--- App/src/utils/formatText.js
+++ App/src/utils/formatText.js
@@ -1,8 +1,23 @@
-export function formatText(text) {
+const helpers = {
+    computeImageHtml(key) {
+        switch(key) {
+        case "pa":
+            return `<img src="${require("@/assets/images/pa.png")}" alt="pa">`;
+        case "pm":
+            return `<img src="${require("@/assets/images/pm.png")}" alt="pm">`;
+        default:
+            throw Error(`Unexpected key for replaced image: ${key}`);
+        }
+    }
+};
+
+Object.assign(module.exports, { formatText, helpers });
+
+function formatText(text) {
     let formattedText = text;
     formattedText = formattedText.replaceAll(/\*\*(.*)\*\*/g, '<strong>$1</strong>');
     formattedText = formattedText.replaceAll(/\*(.*)\*/g, '<em>$1</em>');
-    formattedText = formattedText.replaceAll(/:pa:/g, '<img src="'+require("@/assets/images/pa.png")+'" alt="pa">');
-    formattedText = formattedText.replaceAll(/:pm:/g, '<img src="'+require("@/assets/images/pm.png")+'" alt="pm">');
+    formattedText = formattedText.replaceAll(/:pa:/g, helpers.computeImageHtml("pa"));
+    formattedText = formattedText.replaceAll(/:pm:/g, helpers.computeImageHtml("pm"));
     return formattedText;
 }

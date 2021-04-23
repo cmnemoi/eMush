5502ee40a02741a4ad2becb2aa60ae30809488e4
Guillaume Le Roux <guillaume.le.roux@hotmail.fr>
2021-04-23 13:57:28 +0200

    Fix messages and alert path

--- App/src/enums/alerts.enum.js
+++ App/src/enums/alerts.enum.js
@@ -8,7 +8,7 @@ const BROKEN_EQUIPMENTS = 'broken.equipments';
 export const AlertsIcons = {
         [NO_ALERT]: require('@/assets/images/alerts/infoalert.png'),
         [LOW_OXYGEN]: require('@/assets/images/alerts/o2alert.png'),
-        [LOW_HULL]: require('@/assets/images/alerts/shield.png'),
+        [LOW_HULL]: require('@/assets/images/shield.png'),
         [NUMBER_FIRE]: require('@/assets/images/alerts/fire.png'),
         [BROKEN_DOORS]: require('@/assets/images/alerts/door.png'),
         [BROKEN_EQUIPMENTS]: require('@/assets/images/alerts/broken.png'),

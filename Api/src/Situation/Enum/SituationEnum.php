0789e51478e85cd8f86f50e00212a487e68775aa
Sylvain Breton <sylvain.breton.6@gmail.com>
2021-05-22 10:38:35 +0200

    WIP on situation module

---
+++ Api/src/Situation/Enum/SituationEnum.php
@@ -0,0 +1,29 @@
+<?php
+
+
+namespace Mush\Situation\Enum;
+
+
+class ActionLogEnum
+{
+    public const LOW_HULL = 'low_hull';
+    public const LOW_OXYGEN = 'low_oxygen';
+
+    public const COMMUNICATIONS_DOWN = 'communications_down';
+    public const GRAVITY_BROKEN = 'gravity_broken';
+    public const LOST_CREWMATE = 'lost_crewmate';
+    public const OUTCAST = 'outcast';
+    public const FAMINE = 'famine';
+
+    public const EQUIPMENT_BROKEN = 'equipment_broken';
+    public const DOOR_BROKEN = 'door_broken';
+    public const FIRE = 'fire';
+    public const HUNTER = 'hunter';
+
+    public const SHIELD = 'shield';
+    public const MAGNETIC_NET = 'magnetic_net';
+    public const TRAVEL = 'travel';
+    public const IN_ORBIT = 'in_orbit';
+
+    public const NO_ALERT = 'no_alert';
+}

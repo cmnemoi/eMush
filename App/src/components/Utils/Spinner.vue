2beae99c15bd31523926266aa504b10445acfd8d
A7 <atar.inscription@gmail.com>
2021-05-23 22:46:28 +0200

    Rework spinner design
    Is it overkill ? Probably. Is it worth it ? Absolutely.

--- App/src/components/Utils/Spinner.vue
+++ App/src/components/Utils/Spinner.vue
@@ -1,6 +1,35 @@
 <template>
     <div v-show="loading" class="spinner_overlay">
-        <div class="spinner" />
+        <div class="spinner-container">
+            <span>Loading...</span>
+            <div class="spinner">
+                <div class="top">
+                    <img src="@/assets/images/floor-tiles.gif" />
+                </div>
+                <div class="topleft">
+                    <img src="@/assets/images/floor-tiles.gif" />
+                </div>
+                <div class="topright">
+                    <img src="@/assets/images/floor-tiles.gif" />
+                </div>
+                <div class="left">
+                    <img src="@/assets/images/floor-tiles.gif" />
+                </div>
+                <div class="right">
+                    <img src="@/assets/images/floor-tiles.gif" />
+                </div>
+                <div class="bottomleft">
+                    <img src="@/assets/images/floor-tiles.gif" />
+                </div>
+                <div class="bottomright">
+                    <img class="cat" src="@/assets/images/char/body/cat.png" />
+                    <img src="@/assets/images/floor-tiles.gif" />
+                </div>
+                <div class="bottom">
+                    <img src="@/assets/images/floor-tiles.gif" />
+                </div>
+            </div>
+        </div>
     </div>
 </template>
 
@@ -16,62 +45,119 @@ export default {
 };
 </script>
 
-<style>
-/* Center the loader */
+<style  lang="scss" scoped>
 
-.spinner_overlay {
-
+.spinner_overlay { //dark background
     position: fixed;
     left: 0;
     top: 0;
     width: 100%;
     height: 100%;
-    background-color: #000;
-    opacity: 0.5;
+    align-items: center;
+    justify-content: center;
+    background-color: rgba(15, 15, 67, .75);
     z-index: 100;
 }
 
-.spinner {
-    position: absolute;
-    left: 50%;
-    top: 50%;
-    z-index: 1000;
-    width: 120px;
-    height: 120px;
-    margin: -76px 0 0 -76px;
-    border: 16px solid #f3f3f3;
-    border-radius: 50%;
-    border-top: 16px solid #3498db;
-    -webkit-animation: spin 2s linear infinite;
-    animation: spin 2s linear infinite;
+.spinner-container {
+    align-items: center;
+    width: 96px;
+
+    span {
+        text-align: center;
+        margin-bottom: 2em;
+        font-variant: small-caps;
+        font-weight: bold;
+        letter-spacing: .02em;
+    }
 }
 
-@-webkit-keyframes spin {
-    0% { -webkit-transform: rotate(0deg); }
-    100% { -webkit-transform: rotate(360deg); }
-}
-
-@keyframes spin {
-    0% { transform: rotate(0deg); }
-    100% { transform: rotate(360deg); }
-}
-
-/* Add animation to "page content" */
-.animate-bottom {
+.spinner { //moving tiles
     position: relative;
-    -webkit-animation-name: animatebottom;
-    -webkit-animation-duration: 1s;
-    animation-name: animatebottom;
-    animation-duration: 1s
+    z-index: 100;
+    width: 32px;
+
+    div {
+        position: absolute;
+        overflow: hidden;
+        width: 32px;
+        padding-top: 7px;
+        animation: movingtile 4s cubic-bezier(0.25, 0, 0.15, 1) infinite;
+        animation-fill-mode: both;
+    }
+
+    img { width: 192px; }
+
+    img.cat {
+        position: absolute;
+        z-index: 1;
+        top: 0;
+        left: 4px;
+        width: 22px;
+    }
+
+    .top {
+        top: -16px;
+        animation-delay: 0;
+        img { transform: translateX(calc(5*-32px)); }
+    }
+    
+    .topleft {
+        left: -16px; top: -8px;
+        animation-delay: 0.5s;
+        img { transform: translateX(calc(0*-32px)); }
+    }
+
+    .topright {
+        left:  16px; top: -8px;
+        animation-delay: 3.5s;
+        img { transform: translateX(calc(1*-32px)); }
+    }
+
+    .left {
+        left: -32px;
+        animation-delay: 1s;
+        img { transform: translateX(calc(2*-32px)); }
+    }
+
+    .right {
+        left:  32px;
+        animation-delay: 3s;
+        img { transform: translateX(calc(4*-32px)); }
+    }
+
+    .bottomleft {
+        left: -16px; top:  8px;
+        animation-delay: 1.5s;
+        img { transform: translateX(calc(1*-32px)); }
+    }
+
+    .bottomright {
+        left:  16px; top:  8px;
+        animation-delay: 2.5s;
+        img { transform: translateX(calc(0*-32px)); }
+    }
+
+    .bottom {
+        top:  16px;
+        animation-delay: 2s;
+        img { transform: translateX(calc(3*-32px)); }
+    }
 }
 
-@-webkit-keyframes animatebottom {
-    from { bottom:-100px; opacity:0 }
-    to { bottom:0px; opacity:1 }
+@keyframes movingtile {
+    0%, 5% {
+        opacity: 0;
+        transform: translateY(28px);
+    }
+    45%, 60% {
+        opacity: 1;
+        transform: translateY(0);
+    }
+    85%, 100% {
+        opacity: 0;
+        transform: translateY(-8px);
+    }
 }
 
-@keyframes animatebottom {
-    from{ bottom:-100px; opacity:0 }
-    to{ bottom:0; opacity:1 }
-}
 </style>

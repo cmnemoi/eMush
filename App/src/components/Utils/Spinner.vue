93d95d41f1a378e44836bcdb0a59569caa1da476
A7 <atar.inscription@gmail.com>
2021-05-24 18:09:39 +0200

    Rework spinner pt2

--- App/src/components/Utils/Spinner.vue
+++ App/src/components/Utils/Spinner.vue
@@ -3,31 +3,12 @@
         <div class="spinner-container">
             <span>Loading...</span>
             <div class="spinner">
-                <div class="top">
-                    <img src="@/assets/images/floor-tiles.gif" />
-                </div>
-                <div class="topleft">
-                    <img src="@/assets/images/floor-tiles.gif" />
-                </div>
-                <div class="topright">
-                    <img src="@/assets/images/floor-tiles.gif" />
-                </div>
-                <div class="left">
-                    <img src="@/assets/images/floor-tiles.gif" />
-                </div>
-                <div class="right">
-                    <img src="@/assets/images/floor-tiles.gif" />
-                </div>
-                <div class="bottomleft">
-                    <img src="@/assets/images/floor-tiles.gif" />
-                </div>
-                <div class="bottomright">
-                    <img class="cat" src="@/assets/images/char/body/cat.png" />
-                    <img src="@/assets/images/floor-tiles.gif" />
-                </div>
-                <div class="bottom">
-                    <img src="@/assets/images/floor-tiles.gif" />
-                </div>
+                <img src="@/assets/images/floor-tile.gif" />
+                <img src="@/assets/images/floor-tile.gif" />
+                <img src="@/assets/images/floor-tile.gif" />
+                <img src="@/assets/images/floor-tile.gif" />
+                <img src="@/assets/images/floor-tile.gif" />
+                <img class="cat" src="@/assets/images/char/body/cat.png" />
             </div>
         </div>
     </div>
@@ -55,17 +36,17 @@ export default {
     height: 100%;
     align-items: center;
     justify-content: center;
-    background-color: rgba(15, 15, 67, .75);
+    background-color: rgba(15, 15, 67, .6);
     z-index: 100;
 }
 
 .spinner-container {
     align-items: center;
-    width: 96px;
+    // width: 96px;
 
     span {
         text-align: center;
-        margin-bottom: 2em;
+        margin-bottom: .4em;
         font-variant: small-caps;
         font-weight: bold;
         letter-spacing: .02em;
@@ -75,73 +56,28 @@ export default {
 .spinner { //moving tiles
     position: relative;
     z-index: 100;
-    width: 32px;
+    flex-direction: row;
+    align-items: flex-start;
 
-    div {
-        position: absolute;
-        overflow: hidden;
+    img:not(.cat) {
         width: 32px;
-        padding-top: 7px;
-        animation: movingtile 4s cubic-bezier(0.25, 0, 0.15, 1) infinite;
-        animation-fill-mode: both;
+        margin-left: -16px;
+        animation: movingtile 4s cubic-bezier(0.25, 0, 0.15, 1) infinite both;
     }
 
-    img { width: 192px; }
-
-    img.cat {
+    img.cat { // cat is put on 4th tile
         position: absolute;
-        z-index: 1;
-        top: 0;
-        left: 4px;
+        left: 37px;
+        top: 27px;
         width: 22px;
+        animation: movingtile 4s 2s cubic-bezier(0.25, 0, 0.15, 1) infinite both;
     }
 
-    .top {
-        top: -16px;
-        animation-delay: 0;
-        img { transform: translateX(calc(5*-32px)); }
-    }
-    
-    .topleft {
-        left: -16px; top: -8px;
-        animation-delay: 0.5s;
-        img { transform: translateX(calc(0*-32px)); }
-    }
-
-    .topright {
-        left:  16px; top: -8px;
-        animation-delay: 3.5s;
-        img { transform: translateX(calc(1*-32px)); }
-    }
-
-    .left {
-        left: -32px;
-        animation-delay: 1s;
-        img { transform: translateX(calc(2*-32px)); }
-    }
-
-    .right {
-        left:  32px;
-        animation-delay: 3s;
-        img { transform: translateX(calc(4*-32px)); }
-    }
-
-    .bottomleft {
-        left: -16px; top:  8px;
-        animation-delay: 1.5s;
-        img { transform: translateX(calc(1*-32px)); }
-    }
-
-    .bottomright {
-        left:  16px; top:  8px;
-        animation-delay: 2.5s;
-        img { transform: translateX(calc(0*-32px)); }
-    }
-
-    .bottom {
-        top:  16px;
-        animation-delay: 2s;
-        img { transform: translateX(calc(3*-32px)); }
+    @for $i from 1 through 5 {
+        img:nth-child(#{$i}) {
+            margin-top: calc(#{$i} * 8px);
+            animation-delay: calc(#{$i} * .5s);
+        }
     }
 }
 

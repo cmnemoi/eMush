7f5ec528e9e8fcf9c17704d5f3cedc77e35da667
Biocat <seriial.guigui@gmail.com>
2021-06-04 12:18:38 +0200

    Add the tooltip to the action buttons. Still need some style

--- App/src/components/Utils/ActionButton.vue
+++ App/src/components/Utils/ActionButton.vue
@@ -1,18 +1,29 @@
 <template>
-    <a class="action-button" href="#">
-        <span v-if="action.movementPointCost > 0">{{ action.movementPointCost }}<img src="@/assets/images/pm.png" alt="mp"></span>
-        <span v-else-if="action.actionPointCost > 0">{{ action.actionPointCost }}<img src="@/assets/images/pa.png" alt="ap"></span>
-        <span v-if="action.canExecute">{{ action.name }}</span>
-        <span v-else><s>{{ action.name }}</s></span>
-        <span v-if="action.successRate < 100" class="success-rate"> ({{ action.successRate }}%)</span>
-    </a>
+    <Tooltip>
+        <template v-slot:tooltip-trigger>
+            <a class="action-button" href="#">
+                <span v-if="action.movementPointCost > 0">{{ action.movementPointCost }}<img src="@/assets/images/pm.png" alt="mp"></span>
+                <span v-else-if="action.actionPointCost > 0">{{ action.actionPointCost }}<img src="@/assets/images/pa.png" alt="ap"></span>
+                <span v-if="action.canExecute">{{ action.name }}</span>
+                <span v-else><s>{{ action.name }}</s></span>
+                <span v-if="action.successRate < 100" class="success-rate"> ({{ action.successRate }}%)</span>
+            </a>
+        </template>
+        <template v-slot:tooltip-content>
+            <h1>{{ action.name }}</h1>
+            <p>{{ action.description}}</p>
+        </template>
+    </Tooltip>
 </template>
 
 <script>
+import Tooltip from "@/components/Utils/ToolTip";
+
 export default {
     props: {
         action: Object
-    }
+    },
+    components: {Tooltip}
 };
 </script>
 

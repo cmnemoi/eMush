bc4d674b928afc9f9609fd322ea81dab4d3bc490
Sylvain Breton <sylvain.breton.6@gmail.com>
2021-06-06 19:16:50 +0200

    added the action

---
+++ Api/src/Action/Actions/OpenCapsule.php
@@ -0,0 +1,96 @@
+<?php
+
+namespace Mush\Action\Actions;
+
+use Mush\Action\ActionResult\ActionResult;
+use Mush\Action\ActionResult\Success;
+use Mush\Action\Entity\ActionParameter;
+use Mush\Action\Enum\ActionEnum;
+use Mush\Action\Enum\ActionImpossibleCauseEnum;
+use Mush\Action\Service\ActionServiceInterface;
+use Mush\Action\Validator\Reach;
+use Mush\Action\Validator\Status;
+use Mush\Equipment\Entity\GameEquipment;
+use Mush\Equipment\Enum\ItemEnum;
+use Mush\Equipment\Enum\ReachEnum;
+use Mush\Equipment\Event\EquipmentEvent;
+use Mush\Equipment\Service\GameEquipmentServiceInterface;
+use Mush\Game\Service\RandomServiceInterface;
+use Mush\Player\Service\PlayerServiceInterface;
+use Mush\RoomLog\Enum\VisibilityEnum;
+use Mush\Status\Enum\EquipmentStatusEnum;
+use Symfony\Component\EventDispatcher\EventDispatcherInterface;
+use Symfony\Component\Validator\Constraint;
+use Symfony\Component\Validator\Mapping\ClassMetadata;
+use Symfony\Component\Validator\Validator\ValidatorInterface;
+
+class OpenCapsule extends AbstractAction
+{
+    private static array $capsuleContent = [
+        ItemEnum::FUEL_CAPSULE => 1,
+        ItemEnum::OXYGEN_CAPSULE => 1,
+        ItemEnum::METAL_SCRAPS => 1,
+        ItemEnum::PLASTIC_SCRAPS => 1
+    ];
+
+
+    protected string $name = ActionEnum::OPEN;
+
+    private GameEquipmentServiceInterface $gameEquipmentService;
+    private  RandomServiceInterface $randomService;
+
+    public function __construct(
+        EventDispatcherInterface $eventDispatcher,
+        ActionServiceInterface $actionService,
+        ValidatorInterface $validator,
+        GameEquipmentServiceInterface $gameEquipmentService,
+        RandomServiceInterface $randomService,
+    ) {
+        parent::__construct(
+            $eventDispatcher,
+            $actionService,
+            $validator
+        );
+
+        $this->gameEquipmentService = $gameEquipmentService;
+        $this->randomService = $randomService;
+    }
+
+
+    protected function support(?ActionParameter $parameter): bool
+    {
+        return $parameter instanceof GameEquipment;
+    }
+
+    public static function loadValidatorMetadata(ClassMetadata $metadata): void
+    {
+        $metadata->addConstraint(new Reach(['reach' => ReachEnum::ROOM, 'groups' => ['visibility']]));
+    }
+
+    protected function applyEffects(): ActionResult
+    {
+        /** @var GameEquipment $parameter */
+        $parameter = $this->parameter;
+
+
+        //remove the space capsule
+        $equipmentEvent = new EquipmentEvent($parameter, VisibilityEnum::HIDDEN, new \DateTime());
+        $this->eventDispatcher->dispatch($equipmentEvent, EquipmentEvent::EQUIPMENT_DESTROYED);
+
+
+        //Get the content
+        $contentName = $this->randomService->getSingleRandomElementFromProbaArray(self::$capsuleContent);
+
+        $productEquipment = $this
+            ->gameEquipmentService
+            ->createGameEquipmentFromName($contentName, $this->player->getDaedalus())
+        ;
+        $equipmentEvent = new EquipmentEvent($productEquipment, VisibilityEnum::HIDDEN, new \DateTime());
+        $equipmentEvent->setPlayer($this->player);
+        $this->eventDispatcher->dispatch($equipmentEvent, EquipmentEvent::EQUIPMENT_CREATED);
+
+        $this->gameEquipmentService->persist($productEquipment);
+
+        return new Success();
+    }
+}

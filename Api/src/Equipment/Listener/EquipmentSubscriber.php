47b7f801832c5a524a502382cb6738e6f43dd3c2
Sylvain Breton <sylvain.breton.6@gmail.com>
2021-06-07 12:13:04 +0200

    moved the log creation of equipmentSubscriber to roomLog module

--- Api/src/Equipment/Listener/EquipmentSubscriber.php
+++ Api/src/Equipment/Listener/EquipmentSubscriber.php
@@ -3,14 +3,11 @@
 namespace Mush\Equipment\Listener;
 
 use Error;
-use Mush\Equipment\Entity\Door;
 use Mush\Equipment\Entity\GameEquipment;
 use Mush\Equipment\Entity\GameItem;
 use Mush\Equipment\Event\EquipmentEvent;
 use Mush\Equipment\Service\GameEquipmentServiceInterface;
 use Mush\Game\Entity\GameConfig;
-use Mush\RoomLog\Enum\LogEnum;
-use Mush\RoomLog\Enum\VisibilityEnum;
 use Mush\RoomLog\Service\RoomLogServiceInterface;
 use Mush\Status\Enum\EquipmentStatusEnum;
 use Mush\Status\Service\StatusServiceInterface;
@@ -56,16 +53,6 @@ class EquipmentSubscriber implements EventSubscriberInterface
             $equipment->setPlayer($player);
         } else {
             $equipment->setPlace($player->getPlace());
-            $this->roomLogService->createLog(
-                LogEnum::OBJECT_FELT,
-                $player->getPlace(),
-                VisibilityEnum::PUBLIC,
-                'event_log',
-                $player,
-                $equipment,
-                null,
-                $event->getTime()
-            );
         }
 
         $this->gameEquipmentService->persist($equipment);
@@ -89,46 +76,13 @@ class EquipmentSubscriber implements EventSubscriberInterface
 
         $this->statusService->createCoreStatus(EquipmentStatusEnum::BROKEN, $equipment);
         $this->gameEquipmentService->persist($equipment);
-
-        if ($equipment instanceof Door) {
-            $rooms = $equipment->getRooms()->toArray();
-        } else {
-            $rooms = [$equipment->getCurrentPlace()];
-        }
-
-        foreach ($rooms as $room) {
-            $this->roomLogService->createLog(
-                LogEnum::EQUIPMENT_BROKEN,
-                $room,
-                $event->getVisibility(),
-                'event_log',
-                null,
-                $equipment,
-                null,
-                $event->getTime()
-            );
-        }
     }
 
     public function onEquipmentDestroyed(EquipmentEvent $event): void
     {
         $equipment = $event->getEquipment();
 
-        $place = $equipment->getCurrentPlace();
-        $equipment->removeLocation();
-
         $this->gameEquipmentService->delete($equipment);
-
-        $this->roomLogService->createLog(
-            LogEnum::EQUIPMENT_DESTROYED,
-            $place,
-            $event->getVisibility(),
-            'event_log',
-            null,
-            $equipment,
-            null,
-            $event->getTime()
-        );
     }
 
     private function getGameConfig(GameEquipment $gameEquipment): GameConfig

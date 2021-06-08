7d87b8f9b9dce8dca8d0572e713fa02cb2a0f982
Sylvain Breton <sylvain.breton.6@gmail.com>
2021-06-08 13:48:48 +0200

    added messages for dismantled shower

--- Api/src/Equipment/Repository/GameEquipmentRepository.php
+++ Api/src/Equipment/Repository/GameEquipmentRepository.php
@@ -5,10 +5,12 @@ namespace Mush\Equipment\Repository;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Doctrine\ORM\Query\Expr\Join;
 use Doctrine\Persistence\ManagerRegistry;
+use Mush\Daedalus\Entity\Daedalus;
 use Mush\Equipment\Criteria\GameEquipmentCriteria;
 use Mush\Equipment\Entity\EquipmentConfig;
 use Mush\Equipment\Entity\GameEquipment;
 use Mush\Equipment\Entity\GameItem;
+use Mush\Game\Entity\GameConfig;
 use Mush\Place\Entity\Place;
 use Mush\Player\Entity\Player;
 
@@ -65,6 +67,28 @@ class GameEquipmentRepository extends ServiceEntityRepository
                 call_user_func_array([$queryBuilder->expr(), 'andX'], $types)
             );
         }
+
+        return $queryBuilder->getQuery()->getResult();
+    }
+
+    public function findByNameAndDaedalus(string $name, Daedalus $daedalus): array
+    {
+        $queryBuilder = $this->createQueryBuilder('gameEquipment');
+
+        $queryBuilder
+            ->leftJoin(EquipmentConfig::class, 'equipmentConfig', Join::WITH, 'equipmentConfig = gameEquipment.equipment')
+            ->leftJoin(GameConfig::class, 'gameConfig', Join::WITH, 'gameConfig = equipmentConfig.gameConfig')
+            ->leftJoin(
+                Daedalus::class,
+                'daedalus',
+                Join::WITH,
+                'daedalus.gameConfig = gameConfig.id'
+            )
+            ->where($queryBuilder->expr()->eq('daedalus', ':daedalus'))
+            ->andWhere($queryBuilder->expr()->eq('gameEquipment.name', ':name'))
+            ->setParameter(':daedalus', $daedalus)
+            ->setParameter(':name', $name)
+        ;
 
         return $queryBuilder->getQuery()->getResult();
     }

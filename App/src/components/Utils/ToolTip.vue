4b2f876abde7887b34ed917584ceb1ac0f649fd0
Guillaume Le Roux <guillaume.le.roux@hotmail.fr>
2021-06-18 22:35:34 +0200

    Remove warnings and put Tippy config inside ToolTip component

--- App/src/components/Utils/ToolTip.vue
+++ App/src/components/Utils/ToolTip.vue
@@ -1,29 +1,33 @@
 <template>
-    <tippy theme="mush"
-        hideOnClick=false
-        allowHTML=false
-        maxWidth=280
-        followCursor=true
-        inlinePositioning=true
-        placement=bottom-start
-        duration="[null, 50]"
-        class="tippy-tooltip">
-            <slot name='tooltip-trigger' />
-            <template #content>
-                <slot name='tooltip-content' :formatContent="formatContent"></slot>
-            </template>
-    </tippy>
+    <Tippy
+        theme="mush"
+        :hide-on-click="false"
+        :allowHTML="true"
+        :max-width="280"
+        :follow-cursor="true"
+        :inline-positioning="true"
+        placement="bottom-start"
+        :duration="[null, 50]"
+        class="tippy-tooltip"
+    >
+        <slot name="tooltip-trigger" />
+        <template #content>
+            <slot name="tooltip-content" :formatContent="formatContent"></slot>
+        </template>
+    </Tippy>
 </template>
 
 <script>
 import { formatText } from "@/utils/formatText";
+import { Tippy } from 'vue-tippy';
 
-    export default {
+export default {
     name: "Tooltip",
+    components: { Tippy },
     methods:{
         formatContent(value) {
             return !value ? '' : formatText(value.toString());
-            }
-        },
+        }
     }
+};
 </script>

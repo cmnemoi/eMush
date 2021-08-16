f695bc44fbac6e8cb39337a199ef07415d92395c
A7 <atar.inscription@gmail.com>
2021-08-16 15:57:31 +0200

    Revert main page display

--- App/src/components/Game/GameContent.vue
+++ App/src/components/Game/GameContent.vue
@@ -7,7 +7,7 @@
             </div>
             <div class="game-content">
                 <CharPanel :player="player" />
-                <TradingTerminal />
+                <ShipPanel :room="player.room" :player="player" />
                 <CommsPanel :day="player.daedalus.day" :cycle="player.daedalus.cycle" />
             </div>
             <ProjectsPanel />
@@ -22,7 +22,7 @@
 <script lang="ts">
 import BannerPanel from "@/components/Game/BannerPanel.vue";
 import CharPanel from "@/components/Game/CharPanel.vue";
-import TradingTerminal from "@/components/Game/TradingTerminal.vue";
+import ShipPanel from "@/components/Game/Ship/ShipPanel.vue";
 import CommsPanel from "@/components/Game/Communications/CommsPanel.vue";
 import ProjectsPanel from "@/components/Game/ProjectsPanel.vue";
 import { mapActions, mapState } from "vuex";
@@ -37,7 +37,7 @@ export default defineComponent ({
         Purgatory,
         BannerPanel,
         CharPanel,
-        TradingTerminal,
+        ShipPanel,
         CommsPanel,
         ProjectsPanel
     },

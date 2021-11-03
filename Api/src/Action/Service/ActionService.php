1f889d04e6ea70f9ec43d43b4c8d91c53e3b0eae
Sylvain Breton <sylvain.breton.6@gmail.com>
2021-11-03 17:40:40 +0100

    fixed pa pm conversion issue

--- Api/src/Action/Service/ActionService.php
+++ Api/src/Action/Service/ActionService.php
@@ -40,7 +40,7 @@ class ActionService implements ActionServiceInterface
         }
 
         if (($movementPointCost = $this->getTotalMovementPointCost($player, $action, $parameter)) > 0) {
-            $missingMovementPoints = $action->getActionCost()->getMovementPointCost() - $player->getMovementPoint();
+            $missingMovementPoints = $movementPointCost - $player->getMovementPoint();
 
             if ($missingMovementPoints > 0) {
                 $movementPointGain = $this->getMovementPointConversionGain($player, true);

93f1a142695cffa11afd183069ec9038c3cc3316
Sami MOURAD <mourads2001@gmail.com>
2022-09-28 05:22:21 +0200

    debug log

--- Api/src/Equipment/Entity/GameItem.php
+++ Api/src/Equipment/Entity/GameItem.php
@@ -15,14 +15,18 @@ class GameItem extends GameEquipment
 
     public function getHolder(): ?EquipmentHolderInterface
     {
-        codecept_debug($this->player);
-        codecept_debug($this->place);
+        if ($this->place === null && $this->player === null) {
+            codecept_debug("non");
+        }
         return $this->player ?: $this->place;
     }
 
     public function setHolder(?EquipmentHolderInterface $holder): static
     {
+
+
         if ($holder === null) {
+            codecept_debug("non non");
             $this->place = null;
             $this->player = null;
 

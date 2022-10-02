e23ff69e642a58b8754fa3641d6322cb7208cc80
Evian <icdk5b4zf@relay.firefox.com>
2022-10-02 05:56:47 +0000

    fix: disease update hotfixes

--- Api/src/Player/Enum/PlayerVariableEnum.php
+++ Api/src/Player/Enum/PlayerVariableEnum.php
@@ -28,6 +28,7 @@ class PlayerVariableEnum
             self::MOVEMENT_POINT => ':pm:',
             self::HEALTH_POINT => ':hp:',
             self::MORAL_POINT => ':pmo:',
+            self::SATIETY => ':hungry:',
         ];
     }
 }

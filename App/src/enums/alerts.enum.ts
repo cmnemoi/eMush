50dcbac1f9023a85d7653f006378bcf8a0ad8b7d
SylvainBreton <sylvain.breton.6@gmail.com>
2022-11-10 05:59:14 +0000

    fix alert icons

--- App/src/enums/alerts.enum.ts
+++ App/src/enums/alerts.enum.ts
@@ -5,6 +5,7 @@ const FIRES = 'fires';
 const BROKEN_DOORS = 'broken_doors';
 const BROKEN_EQUIPMENTS = 'broken_equipments';
 const NO_GRAVITY = 'no_gravity';
+const HUNGER = 'hunger';
 
 export const AlertsIcons: {[index: string]: string} = {
     [NO_ALERT]: require('@/assets/images/alerts/infoalert.png'),
@@ -13,6 +14,7 @@ export const AlertsIcons: {[index: string]: string} = {
     [FIRES]: require('@/assets/images/alerts/fire.png'),
     [BROKEN_DOORS]: require('@/assets/images/alerts/door.png'),
     [BROKEN_EQUIPMENTS]: require('@/assets/images/alerts/broken.png'),
-    [NO_GRAVITY]: require('@/assets/images/alerts/simulator.png')
+    [NO_GRAVITY]: require('@/assets/images/alerts/simulator.png'),
+    [HUNGER]: require('@/assets/images/alerts/hunger.png')
 }
 ;

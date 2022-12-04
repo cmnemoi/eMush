0eddf5116d6208844ff7b2303e9c71a90541ae9a
Evian <icdk5b4zf@relay.firefox.com>
2022-12-04 19:04:16 +0000

    New Crowdin updates

--- App/src/enums/thanks.ts
+++ App/src/enums/thanks.ts
@@ -1,3 +1,3 @@
-export const developers = ['A7', 'Simpkin', 'Breut', 'Charles-Meldhine "Evian" Madi Mnemoi', 'Tishwa', 'Gowoons', 'Sami / EaCOS'];
+export const developers = ['A7', 'Simpkin', 'Breut', 'Charles-Meldhine "Evian" Madi Mnemoi', 'Tishwa', 'Gowoons', 'Sami / EaCOS', 'zasfree'];
 
-export const helpers = ['rackhaml', 'MoUoA', 'Haux49', 'Demurgos', 'Biosha', 'zasfree'];
+export const helpers = ['rackhaml', 'MoUoA', 'Haux49', 'Demurgos', 'Biosha'];

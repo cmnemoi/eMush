952495a5694efc2842bacaf11a8793d4f4413fd2
Evian <charlesmeldhine.madimnemoi@gmail.com>
2023-02-02 09:15:48 +0100

    feat: add setters for CreatedAt and UpdatedAt

--- Api/src/MetaGame/Entity/News.php
+++ Api/src/MetaGame/Entity/News.php
@@ -53,9 +53,19 @@ class News
         return $this->createdAt;
     }
 
+    public function setCreatedAt(\DateTime $createdAt): void
+    {
+        $this->createdAt = $createdAt;
+    }
+
     public function getUpdatedAt(): \DateTime
     {
         return $this->updatedAt;
+    }
+
+    public function setUpdatedAt(\DateTime $updatedAt): void
+    {
+        $this->updatedAt = $updatedAt;
     }
 
     public function getFrenchTitle(): string

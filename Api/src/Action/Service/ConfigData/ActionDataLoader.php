4f22e138a21730ec6340eee52f2b18dcc76cdb20
Evian <icdk5b4zf@relay.firefox.com>
2023-02-05 17:09:28 +0100

    refactor: merge useless methods into one

--- Api/src/Action/Service/ConfigData/ActionDataLoader.php
+++ Api/src/Action/Service/ConfigData/ActionDataLoader.php
@@ -38,8 +38,8 @@ class ActionDataLoader extends ConfigDataLoader
                 ->setTarget($actionData['target'])
                 ->setScope($actionData['scope'])
             ;
-            $this->setActionVisibility($action, $actionData['visibilities']);
             $this->setActionVariables($action, $actionData);
+            $this->setActionVisibilities($action, $actionData['visibilities']);
 
             $this->entityManager->persist($action);
         }
@@ -47,57 +47,23 @@ class ActionDataLoader extends ConfigDataLoader
         $this->entityManager->flush();
     }
 
-    private function setActionVisibility(Action $action, array $visibilityData): void
+    private function setActionVariables(Action $action, array $actionData): void
+    {
+        $actionIsSuperDirty = $actionData['percentageDirtiness']['min_value'] !== 0;
+
+        $action->setActionCost($actionData['actionPoint']['value']);
+        $action->setCriticalRate($actionData['percentageCritical']['value']);
+        $action->setDirtyRate($actionData['percentageDirtiness']['value'], $actionIsSuperDirty);
+        $action->setInjuryRate($actionData['percentageInjury']['value']);
+        $action->setMoralCost($actionData['moralPoint']['value']);
+        $action->setMovementCost($actionData['movementPoint']['value']);
+        $action->setSuccessRate($actionData['percentageSuccess']['value']);
+    }
+
+    private function setActionVisibilities(Action $action, array $visibilityData): void
     {
         foreach ($visibilityData as $visibilityType => $visibility) {
             $action->setVisibility($visibilityType, $visibility);
         }
-    }
-
-    private function setActionVariables(Action $action, array $actionData): void
-    {
-        $this->setActionActionCost($action, $actionData['actionPoint']);
-        $this->setActionMovementCost($action, $actionData['movementPoint']);
-        $this->setActionMoralCost($action, $actionData['moralPoint']);
-        $this->setActionCriticalRate($action, $actionData['percentageCritical']);
-        $this->setActionDirtyRate($action, $actionData['percentageDirtiness']);
-        $this->setActionInjuryRate($action, $actionData['percentageInjury']);
-        $this->setActionSuccessRate($action, $actionData['percentageSuccess']);
-    }
-
-    private function setActionActionCost(Action $action, array $actionCostData): void
-    {
-        $action->setActionCost($actionCostData['value']);
-    }
-
-    private function setActionCriticalRate(Action $action, array $criticalRateData): void
-    {
-        $action->setCriticalRate($criticalRateData['value']);
-    }
-
-    private function setActionDirtyRate(Action $action, array $dirtinessRateData): void
-    {
-        $isSuperDirty = $dirtinessRateData['min_value'] !== 0;
-        $action->setDirtyRate($dirtinessRateData['value'], $isSuperDirty);
-    }
-
-    private function setActionInjuryRate(Action $action, array $injuryRateData): void
-    {
-        $action->setInjuryRate($injuryRateData['value']);
-    }
-
-    private function setActionMoralCost(Action $action, array $moralCostData): void
-    {
-        $action->setMoralCost($moralCostData['value']);
-    }
-
-    private function setActionMovementCost(Action $action, array $movementCostData): void
-    {
-        $action->setMovementCost($movementCostData['value']);
-    }
-
-    private function setActionSuccessRate(Action $action, array $successRateData): void
-    {
-        $action->setSuccessRate($successRateData['value']);
     }
 }

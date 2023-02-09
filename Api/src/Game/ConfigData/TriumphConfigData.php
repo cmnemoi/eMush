989ea9e4567bd9c72ce7e7ffd2a42c53155f35f6
Evian <icdk5b4zf@relay.firefox.com>
2023-02-09 17:37:54 +0100

    refactor: move data to ConfigData files

---
+++ Api/src/Game/ConfigData/TriumphConfigData.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace Mush\Game\ConfigData;
+
+use Mush\Game\Enum\VisibilityEnum;
+
+class TriumphConfigData
+{
+    public static array $dataArray = [
+        ['name' => 'alien_science', 'triumph' => 16, 'is_all_crew' => false, 'team' => VisibilityEnum::PUBLIC],
+        ['name' => 'expedition', 'triumph' => 3, 'is_all_crew' => false, 'team' => VisibilityEnum::PUBLIC],
+        ['name' => 'super_nova', 'triumph' => 20, 'is_all_crew' => true, 'team' => VisibilityEnum::PUBLIC],
+        ['name' => 'first_starmap', 'triumph' => 6, 'is_all_crew' => true, 'team' => VisibilityEnum::PUBLIC],
+        ['name' => 'next_starmap', 'triumph' => 1, 'is_all_crew' => true, 'team' => VisibilityEnum::PUBLIC],
+        ['name' => 'cycle_mush', 'triumph' => -2, 'is_all_crew' => true, 'team' => VisibilityEnum::MUSH],
+        ['name' => 'starting_mush', 'triumph' => 120, 'is_all_crew' => false, 'team' => VisibilityEnum::MUSH],
+        ['name' => 'cycle_mush_late', 'triumph' => -3, 'is_all_crew' => false, 'team' => VisibilityEnum::MUSH],
+        ['name' => 'conversion', 'triumph' => 8, 'is_all_crew' => false, 'team' => VisibilityEnum::MUSH],
+        ['name' => 'infection', 'triumph' => 1, 'is_all_crew' => false, 'team' => VisibilityEnum::MUSH],
+        ['name' => 'humanocide', 'triumph' => 3, 'is_all_crew' => false, 'team' => VisibilityEnum::MUSH],
+        ['name' => 'chun_dead', 'triumph' => 7, 'is_all_crew' => true, 'team' => VisibilityEnum::MUSH],
+        ['name' => 'sol_return_mush', 'triumph' => 8, 'is_all_crew' => true, 'team' => VisibilityEnum::MUSH],
+        ['name' => 'eden_mush', 'triumph' => 32, 'is_all_crew' => true, 'team' => VisibilityEnum::MUSH],
+        ['name' => 'cycle_human', 'triumph' => 1, 'is_all_crew' => false, 'team' => VisibilityEnum::PUBLIC],
+        ['name' => 'cycle_inactive', 'triumph' => 0, 'is_all_crew' => false, 'team' => VisibilityEnum::PUBLIC],
+        ['name' => 'new_planet_orbit', 'triumph' => 5, 'is_all_crew' => true, 'team' => VisibilityEnum::PUBLIC],
+        ['name' => 'sol_contact', 'triumph' => 8, 'is_all_crew' => true, 'team' => VisibilityEnum::PUBLIC],
+        ['name' => 'small_research', 'triumph' => 3, 'is_all_crew' => true, 'team' => VisibilityEnum::PUBLIC],
+        ['name' => 'standard_research', 'triumph' => 6, 'is_all_crew' => true, 'team' => VisibilityEnum::PUBLIC],
+        ['name' => 'brilliant_research', 'triumph' => 16, 'is_all_crew' => true, 'team' => VisibilityEnum::PUBLIC],
+        ['name' => 'sol_return', 'triumph' => 20, 'is_all_crew' => true, 'team' => VisibilityEnum::PUBLIC],
+        ['name' => 'sol_mush_intruder', 'triumph' => -10, 'is_all_crew' => true, 'team' => VisibilityEnum::PUBLIC],
+        ['name' => 'hunter_killed', 'triumph' => 1, 'is_all_crew' => false, 'team' => VisibilityEnum::PUBLIC],
+        ['name' => 'mushicide', 'triumph' => 3, 'is_all_crew' => false, 'team' => VisibilityEnum::PUBLIC],
+        ['name' => 'rebel_wolf', 'triumph' => 8, 'is_all_crew' => true, 'team' => VisibilityEnum::PUBLIC],
+        ['name' => 'nice_surgery', 'triumph' => 5, 'is_all_crew' => false, 'team' => VisibilityEnum::PUBLIC],
+        ['name' => 'eden_crew_alive', 'triumph' => 1, 'is_all_crew' => true, 'team' => VisibilityEnum::PUBLIC],
+        ['name' => 'eden_alien_plant', 'triumph' => 1, 'is_all_crew' => true, 'team' => VisibilityEnum::PUBLIC],
+        ['name' => 'eden_gender', 'triumph' => 4, 'is_all_crew' => true, 'team' => VisibilityEnum::PUBLIC],
+        ['name' => 'eden', 'triumph' => 6, 'is_all_crew' => true, 'team' => VisibilityEnum::PUBLIC],
+        ['name' => 'eden_cat', 'triumph' => 4, 'is_all_crew' => true, 'team' => VisibilityEnum::PUBLIC],
+        ['name' => 'eden_cat_dead', 'triumph' => -4, 'is_all_crew' => true, 'team' => VisibilityEnum::PUBLIC],
+        ['name' => 'eden_cat_mush', 'triumph' => -8, 'is_all_crew' => true, 'team' => VisibilityEnum::PUBLIC],
+        ['name' => 'eden_disease', 'triumph' => -4, 'is_all_crew' => true, 'team' => VisibilityEnum::PUBLIC],
+        ['name' => 'eden_engineers', 'triumph' => 6, 'is_all_crew' => false, 'team' => VisibilityEnum::PUBLIC],
+        ['name' => 'eden_biologist', 'triumph' => 3, 'is_all_crew' => false, 'team' => VisibilityEnum::PUBLIC],
+        ['name' => 'eden_mush_intruder', 'triumph' => -16, 'is_all_crew' => true, 'team' => VisibilityEnum::PUBLIC],
+        ['name' => 'eden_by_pregnant', 'triumph' => 8, 'is_all_crew' => true, 'team' => VisibilityEnum::PUBLIC],
+        ['name' => 'eden_computed', 'triumph' => 4, 'is_all_crew' => true, 'team' => VisibilityEnum::PUBLIC],
+        ['name' => 'anathem', 'triumph' => 8, 'is_all_crew' => false, 'team' => VisibilityEnum::PUBLIC],
+        ['name' => 'pregnancy', 'triumph' => 8, 'is_all_crew' => false, 'team' => VisibilityEnum::PUBLIC],
+        ['name' => 'all_pregnant', 'triumph' => 2, 'is_all_crew' => true, 'team' => VisibilityEnum::PUBLIC],
+    ];
+}

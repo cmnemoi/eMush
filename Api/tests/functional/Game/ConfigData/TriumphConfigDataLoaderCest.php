28543a97c31e2310b0ea00ad797514ae7f94eaef
Evian <icdk5b4zf@relay.firefox.com>
2023-02-09 22:44:13 +0100

    feat: load gameConfig child configs

--- Api/tests/functional/Game/ConfigData/TriumphConfigDataLoaderCest.php
+++ Api/tests/functional/Game/ConfigData/TriumphConfigDataLoaderCest.php
@@ -4,7 +4,6 @@ namespace Mush\Tests\functional\Triumph\ConfigData;
 
 use App\Tests\FunctionalTester;
 use Mush\Game\ConfigData\TriumphConfigDataLoader;
-use Mush\Game\Entity\GameConfig;
 use Mush\Game\Entity\TriumphConfig;
 use Mush\Game\Enum\TriumphEnum;
 use Mush\Game\Enum\VisibilityEnum;
@@ -20,13 +19,6 @@ class TriumphConfigDataLoaderCest
 
     public function testloadConfigsData(FunctionalTester $I)
     {
-        $I->haveInRepository(GameConfig::class, [
-            'name' => 'default',
-        ]);
-        $defaultGameConfig = $I->grabEntityFromRepository(GameConfig::class, [
-            'name' => 'default',
-        ]);
-
         $this->triumphConfigDataLoader->loadConfigsData();
 
         $I->seeInRepository(TriumphConfig::class, [
@@ -38,16 +30,10 @@ class TriumphConfigDataLoaderCest
 
         // check that we've loaded all the triumph configs
         $I->seeNumRecords(43, TriumphConfig::class);
-        // check that we've associated successfully the triumph configs with the default game config
-        $I->assertCount(43, $defaultGameConfig->getTriumphConfig());
     }
 
     public function testloadConfigsDataConfigAlreadyExists(FunctionalTester $I)
     {
-        $I->haveInRepository(GameConfig::class, [
-            'name' => 'default',
-        ]);
-
         $I->haveInRepository(TriumphConfig::class, [
             'name' => TriumphEnum::ALIEN_SCIENCE,
             'triumph' => 16,

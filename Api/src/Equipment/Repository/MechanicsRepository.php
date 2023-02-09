4ace0882f5497a6a7c393d9f43edb79996468f4a
Evian <icdk5b4zf@relay.firefox.com>
2023-02-09 14:30:30 +0100

    style: apply linter fixes

--- Api/src/Equipment/Repository/MechanicsRepository.php
+++ Api/src/Equipment/Repository/MechanicsRepository.php
@@ -4,7 +4,7 @@ namespace Mush\Equipment\Repository;
 
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Doctrine\Persistence\ManagerRegistry;
-use Mush\Equipment\Entity\EquipmentMechanic as Mechanics;
+use Mush\Equipment\Entity\EquipmentMechanic;
 
 /**
  * @template-extends ServiceEntityRepository<EquipmentMechanic>
@@ -13,6 +13,6 @@ class MechanicsRepository extends ServiceEntityRepository
 {
     public function __construct(ManagerRegistry $registry)
     {
-        parent::__construct($registry, Mechanics::class);
+        parent::__construct($registry, EquipmentMechanic::class);
     }
 }

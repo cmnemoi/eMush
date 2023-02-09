a95107271dd97834a0706221126a2aa143738304
Evian <icdk5b4zf@relay.firefox.com>
2023-02-09 22:44:13 +0100

    feat: load gameConfig child configs

--- Api/src/Game/ConfigData/TriumphConfigDataLoader.php
+++ Api/src/Game/ConfigData/TriumphConfigDataLoader.php
@@ -3,35 +3,24 @@
 namespace Mush\Game\ConfigData;
 
 use Doctrine\ORM\EntityManagerInterface;
-use Mush\Game\Entity\GameConfig;
 use Mush\Game\Entity\TriumphConfig;
-use Mush\Game\Repository\GameConfigRepository;
 use Mush\Game\Repository\TriumphConfigRepository;
 
 class TriumphConfigDataLoader extends ConfigDataLoader
 {
     private EntityManagerInterface $entityManager;
-    private GameConfigRepository $gameConfigRepository;
     private TriumphConfigRepository $triumphConfigRepository;
 
     public function __construct(
         EntityManagerInterface $entityManager,
-        GameConfigRepository $gameConfigRepository,
         TriumphConfigRepository $triumphConfigRepository)
     {
         $this->entityManager = $entityManager;
-        $this->gameConfigRepository = $gameConfigRepository;
         $this->triumphConfigRepository = $triumphConfigRepository;
     }
 
     public function loadConfigsData(): void
     {
-        /** @var GameConfig $defaultGameConfig */
-        $defaultGameConfig = $this->gameConfigRepository->findOneBy(['name' => 'default']);
-        if ($defaultGameConfig == null) {
-            throw new \Exception('Default game config not found');
-        }
-
         foreach (TriumphConfigData::$dataArray as $triumphConfigData) {
             $triumphConfig = $this->triumphConfigRepository->findOneBy(['name' => $triumphConfigData['name']]);
 
@@ -45,9 +34,6 @@ class TriumphConfigDataLoader extends ConfigDataLoader
                 ;
 
                 $this->entityManager->persist($triumphConfig);
-                if (!$defaultGameConfig->getTriumphConfig()->contains($triumphConfig)) {
-                    $defaultGameConfig->addTriumphConfig($triumphConfig);
-                }
             }
         }
         $this->entityManager->flush();

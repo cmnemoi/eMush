9b02a6ecacb654ef425813cc7e8c9e37ce8f5341
Evian <icdk5b4zf@relay.firefox.com>
2023-02-14 10:54:37 +0000

    chore: update . reset.sh so it always generates a migration when run

--- Api/reset.sh
+++ Api/reset.sh
@@ -1,7 +1,19 @@
 #!/bin/bash
 
+function generate-test-database
+{
+    echo 'Creating a database for functional tests...'
+    php bin/console doctrine:database:create --env=test --if-not-exists
+    php bin/console doctrine:schema:drop --full-database --env=test -f
+    php bin/console doctrine:schema:update --env=test -f
+}
+
 composer install
-php bin/console doctrine:database:create --env=test --if-not-exists
-php bin/console doctrine:schema:drop --full-database --env=test -f
-php bin/console doctrine:schema:update --env=test --force
-php bin/console mush:migrate --dev
+generate-test-database
+if  [[ $1 = "--init" ]]; then
+    php bin/console doctrine:schema:drop --full-database -f
+    php bin/console mush:migrate --dev
+else
+    php bin/console doctrine:migrations:diff --no-interaction
+    php bin/console mush:migrate --dev
+fi

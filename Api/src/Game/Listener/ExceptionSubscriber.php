9685038e16708460776fc71f9b662b39cb2f03c0
Evian <icdk5b4zf@relay.firefox.com>
2023-02-16 21:48:58 +0100

    chore: trying to send mails to GitLab Service Desk (not working)

--- Api/src/Game/Listener/ExceptionSubscriber.php
+++ Api/src/Game/Listener/ExceptionSubscriber.php
@@ -6,14 +6,18 @@ use Psr\Log\LoggerInterface;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 use Symfony\Component\HttpKernel\Event\ExceptionEvent;
 use Symfony\Component\HttpKernel\KernelEvents;
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\Mime\Email;
 
 class ExceptionSubscriber implements EventSubscriberInterface
 {
     private LoggerInterface $logger;
+    private MailerInterface $mailer;
 
-    public function __construct(LoggerInterface $logger)
+    public function __construct(LoggerInterface $logger, MailerInterface $mailer)
     {
         $this->logger = $logger;
+        $this->mailer = $mailer;
     }
 
     public static function getSubscribedEvents(): array
@@ -29,5 +33,20 @@ class ExceptionSubscriber implements EventSubscriberInterface
         $this->logger->error($exception->getMessage(), [
             'trace' => $exception->getTraceAsString(),
         ]);
+
+        // TODO: does not work
+        $email = (new Email())
+            ->from('hello@example.com')
+            ->to('contact-project+eternaltwin-mush-mush-19328996-issue-@incoming.gitlab.com')
+            //->cc('cc@example.com')
+            //->bcc('bcc@example.com')
+            //->replyTo('fabien@example.com')
+            //->priority(Email::PRIORITY_HIGH)
+            ->subject('Time for Symfony Mailer!')
+            ->text('Sending emails is fun again!')
+            ->html('<p>See Twig integration for better HTML integration!</p>');
+
+        $this->mailer->send($email);
     }
+
 }

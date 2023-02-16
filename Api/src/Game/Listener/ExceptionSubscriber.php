cef2754a07b898163f2306dfe678ac17c5bb4402
Evian <icdk5b4zf@relay.firefox.com>
2023-02-16 20:03:25 +0100

    chore: add an ExceptionSubscriber which listens to exceptions and logs them with the stack trace

---
+++ Api/src/Game/Listener/ExceptionSubscriber.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Mush\Game\Listener;
+
+use Psr\Log\LoggerInterface;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\HttpKernel\Event\ExceptionEvent;
+use Symfony\Component\HttpKernel\KernelEvents;
+
+class ExceptionSubscriber implements EventSubscriberInterface
+{
+    private LoggerInterface $logger;
+
+    public function __construct(LoggerInterface $logger)
+    {
+        $this->logger = $logger;
+    }
+
+    public static function getSubscribedEvents(): array
+    {
+        return [
+            KernelEvents::EXCEPTION => 'onKernelException',
+        ];
+    }
+
+    public function onKernelException(ExceptionEvent $event): void
+    {
+        $exception = $event->getThrowable();
+        $this->logger->error($exception->getMessage(), [
+            'trace' => $exception->getTraceAsString(),
+        ]);
+    }
+}

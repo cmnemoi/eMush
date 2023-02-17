d00b771fd75509f33af78ce669a16d75a6cece35
Evian <icdk5b4zf@relay.firefox.com>
2023-02-18 00:25:53 +0100

    create a UserService->findUserDaedaluses to recover user daedaluses

--- Api/src/User/Repository/UserRepository.php
+++ Api/src/User/Repository/UserRepository.php
@@ -4,6 +4,8 @@ namespace Mush\User\Repository;
 
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Doctrine\Persistence\ManagerRegistry;
+use Mush\Daedalus\Entity\DaedalusInfo;
+use Mush\Player\Entity\PlayerInfo;
 use Mush\User\Entity\User;
 use Symfony\Bridge\Doctrine\Security\User\UserLoaderInterface;
 use Symfony\Component\Security\Core\User\UserInterface;
@@ -30,5 +32,22 @@ class UserRepository extends ServiceEntityRepository implements UserLoaderInterf
         $user = $this->findOneBy(['userId' => $identifier]);
 
         return $user instanceof User ? $user : null;
+    }
+
+    public function findUserDaedaluses(User $user): array
+    {
+        $qb = $this->createQueryBuilder('user');
+
+        $qb
+            ->select('daedalus_info')
+            ->leftJoin(PlayerInfo::class, 'player_info', 'WITH', 'player_info.user = user.id')
+            ->leftJoin('player_info.player', 'player')
+            ->leftJoin('player.daedalus', 'daedalus')
+            ->leftJoin(DaedalusInfo::class, 'daedalus_info', 'WITH', 'daedalus_info.id = daedalus.id')
+            ->where($qb->expr()->eq('user.id', ':user_id'))
+            ->setParameter('user_id', $user->getId())
+        ;
+
+        return $qb->getQuery()->getResult();
     }
 }

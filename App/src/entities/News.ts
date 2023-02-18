9bdabd4347922037c17caf6d0e536525084525d4
Evian <icdk5b4zf@relay.firefox.com>
2023-02-18 19:46:51 +0000

    feat(admin): moderators can now write news

---
+++ App/src/entities/News.ts
@@ -0,0 +1,57 @@
+export class News {
+    public iri: string|null;
+    public id: number|null;
+    public frenchTitle: string|null;
+    public englishTitle: string|null;
+    public spanishTitle: string|null;
+    public frenchContent: string|null;
+    public englishContent: string|null;
+    public spanishContent: string|null;
+
+    constructor() {
+        this.iri = null;
+        this.id = null;
+        this.frenchTitle = null;
+        this.englishTitle = null;
+        this.spanishTitle = null;
+        this.frenchContent = null;
+        this.englishContent = null;
+        this.spanishContent = null;
+    }
+    load(object: any): News {
+        if (typeof object !== "undefined") {
+            this.iri = object['@id'];
+            this.id = object.id;
+            this.frenchTitle = object.frenchTitle;
+            this.englishTitle = object.englishTitle;
+            this.spanishTitle = object.spanishTitle;
+            this.frenchContent = object.frenchContent;
+            this.englishContent = object.englishContent;
+        }
+        
+        return this;
+    }
+    jsonEncode(): object {
+        const data : any = {
+            'id': this.id,
+            'frenchTitle': this.frenchTitle,
+            'englishTitle': this.englishTitle,
+            'spanishTitle': this.spanishTitle,
+            'frenchContent': this.frenchContent,
+            'englishContent': this.englishContent,
+            'spanishContent': this.spanishContent,
+        };
+
+        return data;
+    }
+    decode(jsonString : string): News {
+        if (jsonString) {
+            const object = JSON.parse(jsonString);
+            this.load(object);
+        }
+
+        return this;
+    }
+
+
+}
---
+++ App/src/services/news.service.ts
@@ -0,0 +1,53 @@
+import ApiService from "@/services/api.service";
+import urlJoin from "url-join";
+import store from "@/store";
+import { News } from "@/entities/News";
+
+// @ts-ignore
+const NEWS_ENDPOINT = urlJoin(process.env.VUE_APP_API_URL, "news");
+
+const NewsService = {
+    createNews: async(news: News): Promise<News | null> => {
+        store.dispatch('gameConfig/setLoading', { loading: true });
+
+        const newsRecord: Record<string, any> = news.jsonEncode();
+        
+        const newsData = await ApiService.post(NEWS_ENDPOINT, newsRecord)
+            .finally(() => (store.dispatch('gameConfig/setLoading', { loading: false })));
+                
+        let createdNews = null;
+        if (newsData.data) {
+            createdNews = (new News()).load(newsData.data);
+        }
+
+        return createdNews;
+
+    },
+    loadNews: async(newsId: number): Promise<News | null> => {
+        store.dispatch('gameConfig/setLoading', { loading: true });
+        
+        const newsData = await ApiService.get(NEWS_ENDPOINT + '/' + newsId)
+            .finally(() => (store.dispatch('gameConfig/setLoading', { loading: false })));
+        
+        let news = null;
+        if (newsData.data) {
+            news = (new News()).load(newsData.data);
+        }
+
+        return news;
+    },
+    updateNews: async(news: News): Promise<News | null> => {
+        store.dispatch('gameConfig/setLoading', { loading: true });
+        
+        const newsData = await ApiService.put(NEWS_ENDPOINT + '/' + news.id, news)
+            .finally(() => (store.dispatch('gameConfig/setLoading', { loading: false })));
+                
+        let updatedNews = null;
+        if (newsData.data) {
+            updatedNews = (new News()).load(newsData.data);
+        }
+
+        return updatedNews;
+    }
+};
+export default NewsService;

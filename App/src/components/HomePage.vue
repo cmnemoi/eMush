e541291c3e0e459b8724ae41025800bf07a557d7
Evian <icdk5b4zf@relay.firefox.com>
2023-02-19 18:36:22 +0100

    add border to trailer

--- App/src/components/HomePage.vue
+++ App/src/components/HomePage.vue
@@ -86,6 +86,11 @@ export default defineComponent ({
         margin-left: auto;
         margin-right: auto;
         margin-top: 1em;
+
+        video {
+            border: 1px solid #26378c;
+            box-shadow: 0px 0px 3px 3px rgba(0,0,0,0.5);
+        }
     }
 
     p {

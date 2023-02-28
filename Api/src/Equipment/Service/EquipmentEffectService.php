d80eb7d51c9096776504d63f9ed05491ebceba0e
SylvainBreton <sylvain.breton.6@gmail.com>
2023-02-28 18:21:57 +0000

    fix: upped the chance to get action/movement/moral effects on drugs

--- Api/src/Equipment/Service/EquipmentEffectService.php
+++ Api/src/Equipment/Service/EquipmentEffectService.php
@@ -4,6 +4,7 @@ namespace Mush\Equipment\Service;
 
 use Mush\Daedalus\Entity\Daedalus;
 use Mush\Equipment\Entity\ConsumableEffect;
+use Mush\Equipment\Entity\Mechanics\Drug;
 use Mush\Equipment\Entity\Mechanics\Plant;
 use Mush\Equipment\Entity\Mechanics\Ration;
 use Mush\Equipment\Entity\PlantEffect;
@@ -89,32 +90,55 @@ class EquipmentEffectService implements EquipmentEffectServiceInterface
         $consumableEffect
             ->setDaedalus($daedalus)
             ->setRation($ration)
-            ->setActionPoint(
-                intval($this->randomService->getSingleRandomElementFromProbaArray($ration->getActionPoints()))
-            )
-            ->setMovementPoint(
-                intval($this->randomService->getSingleRandomElementFromProbaArray($ration->getMovementPoints()))
-            )
-            ->setHealthPoint(
-                intval($this->randomService->getSingleRandomElementFromProbaArray($ration->getHealthPoints()))
-            )
-            ->setMoralPoint(
-                intval($this->randomService->getSingleRandomElementFromProbaArray($ration->getMoralPoints()))
-            )
             ->setSatiety($ration->getSatiety())
         ;
+
+        if ($ration instanceof Drug) {
+            $consumableEffect = $this->createDrugEffects($consumableEffect, $ration);
+        } else {
+            $consumableEffect
+                ->setActionPoint(
+                    intval($this->randomService->getSingleRandomElementFromProbaArray($ration->getActionPoints()))
+                )
+                ->setMovementPoint(
+                    intval($this->randomService->getSingleRandomElementFromProbaArray($ration->getMovementPoints()))
+                )
+                ->setHealthPoint(
+                    intval($this->randomService->getSingleRandomElementFromProbaArray($ration->getHealthPoints()))
+                )
+                ->setMoralPoint(
+                    intval($this->randomService->getSingleRandomElementFromProbaArray($ration->getMoralPoints()))
+                );
+        }
 
         return $consumableEffect;
     }
 
-//    private function createDrugSpecialEffect(ConsumableEffect $consumableEffect, Drug $drug): ConsumableEffect
-//    {
-//        // if the ration is a drug 1 to 4 diseases are cured with 100% chances
-//        $curesNumber = intval($this->randomService->getSingleRandomElementFromProbaArray($drug->getDrugEffectsNumber()));
-//        $consumableEffect
-//            ->setCures(array_fill_keys($this->randomService->getRandomElements($drug->getCures(), $curesNumber), 100))
-//        ;
-//
-//        return $consumableEffect;
-//    }
+    private function createDrugEffects(ConsumableEffect $consumableEffect, Drug $drug): ConsumableEffect
+    {
+        // if the ration is a drug either action point gain or movement point gain
+        if ($this->randomService->isSuccessful(50)) {
+            $consumableEffect
+                ->setActionPoint(
+                    intval($this->randomService->getSingleRandomElementFromProbaArray($drug->getActionPoints()))
+                );
+        } else {
+            $consumableEffect->setMovementPoint(
+                intval($this->randomService->getSingleRandomElementFromProbaArray($drug->getMovementPoints()))
+            );
+        }
+
+        // if the ration is a drug either health point gain or moral point gain
+        if ($this->randomService->isSuccessful(50)) {
+            $consumableEffect->setHealthPoint(
+                intval($this->randomService->getSingleRandomElementFromProbaArray($drug->getHealthPoints()))
+            );
+        } else {
+            $consumableEffect->setMoralPoint(
+                intval($this->randomService->getSingleRandomElementFromProbaArray($drug->getMoralPoints()))
+            );
+        }
+
+        return $consumableEffect;
+    }
 }

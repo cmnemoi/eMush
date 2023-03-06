72d2447179671e90016af37c887a23df977cc340
Evian <icdk5b4zf@relay.firefox.com>
2023-03-06 14:16:04 +0100

    test: add a test for the fixed bugs

--- Api/tests/functional/Place/Event/RoomEventCest.php
+++ Api/tests/functional/Place/Event/RoomEventCest.php
@@ -5,15 +5,19 @@ namespace Mush\Tests\Place\Event;
 use App\Tests\FunctionalTester;
 use Doctrine\Common\Collections\ArrayCollection;
 use Mush\Communication\Entity\Channel;
+use Mush\Communication\Entity\Message;
 use Mush\Communication\Enum\ChannelScopeEnum;
+use Mush\Communication\Enum\NeronMessageEnum;
 use Mush\Daedalus\Entity\Daedalus;
 use Mush\Daedalus\Entity\DaedalusInfo;
 use Mush\Daedalus\Entity\Neron;
 use Mush\Equipment\Entity\Config\EquipmentConfig;
 use Mush\Equipment\Entity\Config\ItemConfig;
+use Mush\Equipment\Entity\Door;
 use Mush\Equipment\Entity\GameEquipment;
 use Mush\Equipment\Entity\GameItem;
 use Mush\Equipment\Enum\EquipmentEnum;
+use Mush\Equipment\Event\EquipmentEvent;
 use Mush\Game\Entity\DifficultyConfig;
 use Mush\Game\Entity\GameConfig;
 use Mush\Game\Entity\LocalizationConfig;
@@ -116,7 +120,7 @@ class RoomEventCest
 
         $room->setDaedalus($daedalus);
 
-        $statusEvent = new StatusEvent(StatusEnum::FIRE, $room, [EventEnum::NEW_CYCLE], $time);
+        $statusEvent = new StatusEvent(StatusEnum::FIRE, $room, [EventEnum::NEW_CYCLE, StatusEnum::FIRE], $time);
         $this->eventService->callEvent($statusEvent, StatusEvent::STATUS_APPLIED);
 
         $I->assertEquals(1, $room->getStatuses()->count());
@@ -126,6 +130,17 @@ class RoomEventCest
 
         $I->assertEquals($room, $fireStatus->getOwner());
         $I->assertEquals(StatusEnum::FIRE, $fireStatus->getName());
+
+        $I->seeInRepository(Message::class, [
+            'neron' => $neron,
+            'message' => NeronMessageEnum::CYCLE_FAILURES,
+            'channel' => $channel,
+        ]);
+        $I->seeInRepository(Message::class, [
+            'neron' => $neron,
+            'message' => NeronMessageEnum::NEW_FIRE,
+            'channel' => $channel,
+        ]);
     }
 
     public function testTremor(FunctionalTester $I)
@@ -170,7 +185,7 @@ class RoomEventCest
 
         // apply tremor on rooms with players
         $rooms->map(function (Place $room) use ($time) {
-            $roomEvent = new RoomEvent($room, [EventEnum::NEW_CYCLE], $time);
+            $roomEvent = new RoomEvent($room, [EventEnum::NEW_CYCLE, RoomEvent::TREMOR], $time);
             $this->eventService->callEvent($roomEvent, RoomEvent::TREMOR);
         });
 
@@ -268,7 +283,7 @@ class RoomEventCest
 
         $I->haveInRepository($tabulatrix);
 
-        $roomEvent = new RoomEvent($room, [EventEnum::NEW_CYCLE], $time);
+        $roomEvent = new RoomEvent($room, [EventEnum::NEW_CYCLE, RoomEvent::ELECTRIC_ARC], $time);
         $this->eventService->callEvent($roomEvent, RoomEvent::ELECTRIC_ARC);
 
         $I->assertEquals(7, $player->getHealthPoint());
@@ -280,6 +295,217 @@ class RoomEventCest
             'daedalusInfo' => $daedalusInfo,
             'log' => LogEnum::ELECTRIC_ARC,
             'visibility' => VisibilityEnum::PUBLIC,
+        ]);
+        $I->dontSeeInRepository(Message::class, [
+            'neron' => $neron,
+            'message' => NeronMessageEnum::CYCLE_FAILURES,
+            'channel' => $channel,
+        ]);
+        $I->dontSeeInRepository(Message::class, [
+            'neron' => $neron,
+            'message' => NeronMessageEnum::BROKEN_EQUIPMENT,
+            'channel' => $channel,
+        ]);
+    }
+
+    public function testEquipmentBreak(FunctionalTester $I)
+    {
+        $statusConfig = new StatusConfig();
+        $statusConfig->setStatusName(EquipmentStatusEnum::BROKEN)
+            ->buildName(GameConfigEnum::TEST)
+        ;
+        $I->haveInRepository($statusConfig);
+
+        $time = new \DateTime();
+        /** @var DifficultyConfig $difficultyConfig */
+        $difficultyConfig = $I->have(DifficultyConfig::class);
+        /** @var GameConfig $gameConfig */
+        $gameConfig = $I->have(GameConfig::class, [
+            'difficultyConfig' => $difficultyConfig,
+            'statusConfigs' => new ArrayCollection([$statusConfig]),
+        ]);
+
+        $neron = new Neron();
+        $neron->setIsInhibited(true);
+        $I->haveInRepository($neron);
+
+        /** @var Daedalus $daedalus */
+        $daedalus = $I->have(Daedalus::class);
+        /** @var LocalizationConfig $localizationConfig */
+        $localizationConfig = $I->have(LocalizationConfig::class, ['name' => GameConfigEnum::TEST]);
+        $daedalusInfo = new DaedalusInfo($daedalus, $gameConfig, $localizationConfig);
+        $daedalusInfo->setNeron($neron);
+        $I->haveInRepository($daedalusInfo);
+
+        $channel = new Channel();
+        $channel
+            ->setDaedalus($daedalusInfo)
+            ->setScope(ChannelScopeEnum::PUBLIC)
+        ;
+        $I->haveInRepository($channel);
+
+        /** @var Place $room */
+        $room = $I->have(Place::class, ['daedalus' => $daedalus]);
+
+        /** @var CharacterConfig $characterConfig */
+        $characterConfig = $I->have(CharacterConfig::class);
+        /** @var Player $player */
+        $player = $I->have(Player::class, ['daedalus' => $daedalus, 'place' => $room]);
+        $player->setPlayerVariables($characterConfig);
+        /** @var User $user */
+        $user = $I->have(User::class);
+        $playerInfo = new PlayerInfo($player, $user, $characterConfig);
+
+        $I->haveInRepository($playerInfo);
+        $player->setPlayerInfo($playerInfo);
+        $I->refreshEntities($player);
+
+        /** @var EquipmentConfig $equipmentConfig */
+        $equipmentConfig = $I->have(EquipmentConfig::class, ['isBreakable' => true, 'gameConfig' => $gameConfig]);
+        /** @var ItemConfig $itemConfig */
+        $itemConfig = $I->have(ItemConfig::class, ['gameConfig' => $gameConfig]);
+        /** @var EquipmentConfig $tabulatrix */
+        $tabulatrixConfig = $I->have(EquipmentConfig::class, ['isBreakable' => true, 'gameConfig' => $gameConfig, 'name' => EquipmentEnum::TABULATRIX]);
+
+        $gameEquipment = new GameEquipment($room);
+        $gameEquipment
+            ->setEquipment($equipmentConfig)
+            ->setName('some name')
+        ;
+        $I->haveInRepository($gameEquipment);
+
+        $gameItem = new GameItem($room);
+        $gameItem
+            ->setEquipment($itemConfig)
+            ->setName('some other name')
+        ;
+        $I->haveInRepository($gameItem);
+
+        $tabulatrix = new GameEquipment($room);
+        $tabulatrix
+            ->setEquipment($tabulatrixConfig)
+            ->setName(EquipmentEnum::TABULATRIX)
+        ;
+        $I->haveInRepository($tabulatrix);
+
+        $equipment = new ArrayCollection([$gameEquipment, $tabulatrix]);
+
+        foreach ($equipment as $item) {
+            $statusEvent = new StatusEvent(
+                EquipmentStatusEnum::BROKEN,
+                $item,
+                [EventEnum::NEW_CYCLE, EquipmentEvent::EQUIPMENT_BROKEN],
+                new \DateTime()
+            );
+            $statusEvent->setVisibility(VisibilityEnum::PUBLIC);
+            $this->eventService->callEvent($statusEvent, StatusEvent::STATUS_APPLIED);
+        }
+
+        $I->assertTrue($gameEquipment->isBroken());
+        $I->assertFalse($gameItem->isBroken());
+        $I->assertTrue($tabulatrix->isBroken());
+
+        $I->seeInRepository(Message::class, [
+            'neron' => $neron,
+            'message' => NeronMessageEnum::CYCLE_FAILURES,
+            'channel' => $channel,
+        ]);
+        $I->seeInRepository(Message::class, [
+            'neron' => $neron,
+            'message' => NeronMessageEnum::BROKEN_EQUIPMENT,
+            'channel' => $channel,
+        ]);
+    }
+
+    public function testDoorBreak(FunctionalTester $I)
+    {
+        $statusConfig = new StatusConfig();
+        $statusConfig->setStatusName(EquipmentStatusEnum::BROKEN)
+            ->buildName(GameConfigEnum::TEST)
+        ;
+        $I->haveInRepository($statusConfig);
+
+        $time = new \DateTime();
+        /** @var DifficultyConfig $difficultyConfig */
+        $difficultyConfig = $I->have(DifficultyConfig::class);
+        /** @var GameConfig $gameConfig */
+        $gameConfig = $I->have(GameConfig::class, [
+            'difficultyConfig' => $difficultyConfig,
+            'statusConfigs' => new ArrayCollection([$statusConfig]),
+        ]);
+
+        $neron = new Neron();
+        $neron->setIsInhibited(true);
+        $I->haveInRepository($neron);
+
+        /** @var Daedalus $daedalus */
+        $daedalus = $I->have(Daedalus::class);
+        /** @var LocalizationConfig $localizationConfig */
+        $localizationConfig = $I->have(LocalizationConfig::class, ['name' => GameConfigEnum::TEST]);
+        $daedalusInfo = new DaedalusInfo($daedalus, $gameConfig, $localizationConfig);
+        $daedalusInfo->setNeron($neron);
+        $I->haveInRepository($daedalusInfo);
+
+        $channel = new Channel();
+        $channel
+            ->setDaedalus($daedalusInfo)
+            ->setScope(ChannelScopeEnum::PUBLIC)
+        ;
+        $I->haveInRepository($channel);
+
+        /** @var Place $room */
+        $room = $I->have(Place::class, ['daedalus' => $daedalus]);
+        /** @var Place $room2 */
+        $room2 = $I->have(Place::class, ['daedalus' => $daedalus, 'name' => 'room2']);
+
+        /** @var CharacterConfig $characterConfig */
+        $characterConfig = $I->have(CharacterConfig::class);
+        /** @var Player $player */
+        $player = $I->have(Player::class, ['daedalus' => $daedalus, 'place' => $room]);
+        $player->setPlayerVariables($characterConfig);
+        /** @var User $user */
+        $user = $I->have(User::class);
+        $playerInfo = new PlayerInfo($player, $user, $characterConfig);
+
+        $I->haveInRepository($playerInfo);
+        $player->setPlayerInfo($playerInfo);
+        $I->refreshEntities($player);
+
+        /** @var EquipmentConfig $equipmentConfig */
+        $equipmentConfig = $I->have(EquipmentConfig::class, ['isBreakable' => true, 'gameConfig' => $gameConfig]);
+
+        $gameEquipment = new Door($room);
+        $gameEquipment
+            ->setEquipment($equipmentConfig)
+            ->setName('some name')
+        ;
+        $I->haveInRepository($gameEquipment);
+
+        $room->addDoor($gameEquipment);
+        $room2->addDoor($gameEquipment);
+
+        $I->refreshEntities($room, $room2, $gameEquipment);
+
+        $statusEvent = new StatusEvent(
+            EquipmentStatusEnum::BROKEN,
+            $gameEquipment,
+            [EventEnum::NEW_CYCLE, EquipmentEvent::DOOR_BROKEN],
+            new \DateTime()
+        );
+        $statusEvent->setVisibility(VisibilityEnum::HIDDEN);
+        $this->eventService->callEvent($statusEvent, StatusEvent::STATUS_APPLIED);
+
+        $I->assertTrue($gameEquipment->isBroken());
+
+        $I->dontSeeInRepository(Message::class, [
+            'neron' => $neron,
+            'message' => NeronMessageEnum::CYCLE_FAILURES,
+            'channel' => $channel,
+        ]);
+        $I->dontSeeInRepository(Message::class, [
+            'neron' => $neron,
+            'message' => NeronMessageEnum::BROKEN_EQUIPMENT,
+            'channel' => $channel,
         ]);
     }
 }

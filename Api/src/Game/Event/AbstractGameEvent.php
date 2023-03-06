5c1c49481886f9760ccceb5cd2894349c7813ef8
Evian <icdk5b4zf@relay.firefox.com>
2023-03-06 21:30:28 +0000

    fix: NERON doesn't announce equipment broken by electric arcs anymore

--- Api/src/Game/Event/AbstractGameEvent.php
+++ Api/src/Game/Event/AbstractGameEvent.php
@@ -70,6 +70,20 @@ class AbstractGameEvent extends Event
         return in_array($tag, $this->tags);
     }
 
+    // TODO: might want to create a `tagConstaints` property instead
+    /**
+     * Returns true if the event has all the tags in the array.
+     * If `$all` is false, returns true if the event has at least one of the tags.
+     */
+    public function haveTags(array $tags, bool $all = true): bool
+    {
+        if ($all) {
+            return count(array_intersect($tags, $this->tags)) === count($tags);
+        }
+
+        return count(array_intersect($tags, $this->tags)) > 0;
+    }
+
     public function mapLog(array $map): ?string
     {
         $logs = array_intersect_key($map, array_flip($this->tags));

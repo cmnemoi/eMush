0eb01f052a3d4018f6d2c787d10f65c470a16fe7
Sylvain Breton <sylvain.breton.6@gmail.com>
2023-03-07 21:54:49 +0100

    fix: spoiled food now correctly give diseases

---
+++ Api/tests/functional/Disease/Listener/EatSpoiledFoodCest.php
@@ -0,0 +1,136 @@
+<?php
+
+namespace functional\Action\Actions;
+
+use App\Tests\FunctionalTester;
+use Doctrine\Common\Collections\ArrayCollection;
+use Mush\Action\Actions\Consume;
+use Mush\Action\Entity\Action;
+use Mush\Action\Enum\ActionEnum;
+use Mush\Action\Enum\ActionScopeEnum;
+use Mush\Daedalus\Entity\Daedalus;
+use Mush\Daedalus\Entity\DaedalusInfo;
+use Mush\Equipment\Entity\Config\EquipmentConfig;
+use Mush\Equipment\Entity\ConsumableEffect;
+use Mush\Equipment\Entity\GameItem;
+use Mush\Equipment\Entity\Mechanics\Ration;
+use Mush\Equipment\Enum\GameRationEnum;
+use Mush\Game\Entity\GameConfig;
+use Mush\Game\Entity\LocalizationConfig;
+use Mush\Game\Enum\GameConfigEnum;
+use Mush\Game\Enum\LanguageEnum;
+use Mush\Game\Enum\VisibilityEnum;
+use Mush\Place\Entity\Place;
+use Mush\Player\Entity\Config\CharacterConfig;
+use Mush\Player\Entity\Player;
+use Mush\Player\Entity\PlayerInfo;
+use Mush\Status\Entity\Config\StatusConfig;
+use Mush\Status\Entity\Status;
+use Mush\Status\Enum\EquipmentStatusEnum;
+use Mush\User\Entity\User;
+
+class ConsumeActionCest
+{
+    private Consume $consumeAction;
+
+    public function _before(FunctionalTester $I)
+    {
+        $this->consumeAction = $I->grabService(Consume::class);
+    }
+
+    public function testConsume(FunctionalTester $I)
+    {
+        $gameConfig = $I->grabEntityFromRepository(GameConfig::class, ['name' => GameConfigEnum::DEFAULT]);
+
+        /** @var Daedalus $daedalus */
+        $daedalus = $I->have(Daedalus::class);
+        $localizationConfig = $I->grabEntityFromRepository(LocalizationConfig::class, ['name' => LanguageEnum::FRENCH]);
+
+        $daedalusInfo = new DaedalusInfo($daedalus, $gameConfig, $localizationConfig);
+        $I->haveInRepository($daedalusInfo);
+
+        /** @var Place $room */
+        $room = $I->have(Place::class, ['daedalus' => $daedalus]);
+
+        /** @var CharacterConfig $characterConfig */
+        $characterConfig = $I->have(CharacterConfig::class);
+        /** @var Player $player */
+        $player = $I->have(Player::class, ['daedalus' => $daedalus,
+            'place' => $room,
+        ]);
+        $player->setPlayerVariables($characterConfig);
+        $player
+            ->setActionPoint(5)
+            ->setHealthPoint(5)
+            ->setMoralPoint(5)
+            ->setMovementPoint(5)
+        ;
+        $I->flushToDatabase($player);
+
+        /** @var User $user */
+        $user = $I->have(User::class);
+        $playerInfo = new PlayerInfo($player, $user, $characterConfig);
+
+        $I->haveInRepository($playerInfo);
+        $player->setPlayerInfo($playerInfo);
+        $I->refreshEntities($player);
+
+        $consumeActionEntity = new Action();
+        $consumeActionEntity
+            ->setActionName(ActionEnum::CONSUME)
+            ->setScope(ActionScopeEnum::CURRENT)
+            ->buildName(GameConfigEnum::TEST)
+        ;
+        $I->haveInRepository($consumeActionEntity);
+
+        $ration = new Ration();
+        $ration
+            ->setActions(new ArrayCollection([$consumeActionEntity]))
+            ->setName(GameRationEnum::STANDARD_RATION . '_' . GameConfigEnum::TEST)
+        ;
+        $I->haveInRepository($ration);
+
+        $effect = new ConsumableEffect();
+        $effect
+            ->setSatiety(1)
+            ->setActionPoint(2)
+            ->setMovementPoint(3)
+            ->setMoralPoint(4)
+            ->setHealthPoint(5)
+            ->setDaedalus($daedalus)
+            ->setRation($ration)
+        ;
+        $I->haveInRepository($effect);
+
+        /** @var EquipmentConfig $equipmentConfig */
+        $equipmentConfig = $I->have(EquipmentConfig::class, [
+            'mechanics' => new ArrayCollection([$ration]),
+            'name' => GameRationEnum::STANDARD_RATION,
+        ]);
+        $I->haveInRepository($equipmentConfig);
+
+        $gameConfig->addEquipmentConfig($equipmentConfig);
+        $I->refreshEntities($gameConfig);
+
+        $statusConfig = new StatusConfig();
+        $statusConfig
+            ->setStatusName(EquipmentStatusEnum::DECOMPOSING)
+            ->setVisibility(VisibilityEnum::PUBLIC)
+            ->buildName(GameConfigEnum::TEST)
+        ;
+        $I->haveInRepository($statusConfig);
+
+        $gameItem = new GameItem($room);
+        $gameItem
+            ->setEquipment($equipmentConfig)
+            ->setName('ration')
+        ;
+        $I->haveInRepository($gameItem);
+
+        $status = new Status($gameItem, $statusConfig);
+        $I->haveInRepository($status);
+
+        $this->consumeAction->loadParameters($consumeActionEntity, $player, $gameItem);
+        $this->consumeAction->execute();
+    }
+}

de81e8e29e364e137d5de31ccc27b680e7390994
ReNacK <pierre.chiriot@telecomnancy.net>
2023-03-10 00:45:41 +0100

    chore: Add new command for filling a daedalus

---
+++ Api/src/Game/Command/FillDaedalusCommand.php
@@ -0,0 +1,196 @@
+<?php
+
+namespace Mush\Game\Command;
+
+use Mush\Player\Entity\Config\CharacterConfig;
+use Mush\Player\Repository\CharacterConfigRepository;
+use Symfony\Component\BrowserKit\Cookie;
+use Symfony\Component\Console\Attribute\AsCommand;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Style\SymfonyStyle;
+use Symfony\Component\HttpClient\HttpClient;
+use Symfony\Contracts\HttpClient\HttpClientInterface;
+
+#[AsCommand(
+    name: 'mush:fill-daedalus',
+    description: 'Fill a new Daedalus',
+    hidden: false
+)]
+class FillDaedalusCommand extends Command
+{
+    private HttpClientInterface $httpClient;
+    private CharacterConfigRepository $characterConfigRepository;
+    private string $identityServerUri;
+    private string $eMushApiUri;
+
+    private const OPTION_NUMBER = 'number';
+    private const OPTION_CHAO_FINOLA = 'chao_finola';
+    private const OPTION_ANDIE_DEREK = 'andie_derek';
+    private const OPTION_DAEDALUS_ID = 'daedalus_id';
+
+    public function __construct(HttpClientInterface $httpClient, CharacterConfigRepository $characterConfigRepository)
+    {
+        parent::__construct();
+        $this->httpClient = $httpClient;
+        $this->characterConfigRepository = $characterConfigRepository;
+        $this->identityServerUrl = $_ENV['IDENTITY_SERVER_URI'];
+        $this->eMushApiUri = $_ENV['EMUSH_BASE_URI'];
+    }
+
+    protected function configure(): void
+    {
+        $this->addOption($this::OPTION_NUMBER, null, InputOption::VALUE_OPTIONAL, 'Number of member to board ?', 16);
+        $this->addOption($this::OPTION_CHAO_FINOLA, null, InputOption::VALUE_OPTIONAL, 'Accept Chao and Finola on board', false);
+        $this->addOption($this::OPTION_ANDIE_DEREK, null, InputOption::VALUE_OPTIONAL, 'Accept Andie and Derek on board ?', false);
+        $this->addOption($this::OPTION_DAEDALUS_ID, null, InputOption::VALUE_OPTIONAL, 'Daedalus id ?', 1);
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        $io = new SymfonyStyle($input, $output);
+        $daedalusId = $input->getOption($this::OPTION_DAEDALUS_ID);
+        $numberOfMemberToBoard = $input->getOption($this::OPTION_NUMBER);
+        if ($numberOfMemberToBoard < 1 || $numberOfMemberToBoard > 16) {
+            $io->error($this::OPTION_NUMBER . ' should be between 1 and 16');
+
+            return Command::INVALID;
+        }
+        $io->info("$numberOfMemberToBoard character will be added to daedalus $daedalusId");
+
+        $isChaoAndFinola = $input->getOption($this::OPTION_CHAO_FINOLA) === null;
+        $isAndieAndDerek = $input->getOption($this::OPTION_ANDIE_DEREK) === null;
+        if ($isAndieAndDerek && $isChaoAndFinola) {
+            $io->error($this::OPTION_CHAO_FINOLA . ' and ' . $this::OPTION_ANDIE_DEREK . ' are mutually exclusive');
+
+            return Command::INVALID;
+        }
+
+        if ($isChaoAndFinola) {
+            $io->info('Andie and Derek wont be added to daedalus');
+        }
+        if ($isAndieAndDerek) {
+            $io->info('Chao and Finola wont be added to daedalus');
+        }
+
+        $io->title('Filling Daedalus...');
+
+        /** @var CharacterConfig[] $allCharacter */
+        $allCharacter = $this->characterConfigRepository->findAll();
+
+        $count = 0;
+        foreach ($allCharacter as $character) {
+            $name = $character->getName();
+
+            $io->info($name . ' on boarding ...');
+
+            if ($isAndieAndDerek && $this->isChaoOrFinola($name)) {
+                $io->info("$name not allowed on daedalus, skipping ...");
+                continue;
+            }
+            if ($isChaoAndFinola && $this->isAndieOrDerek($name)) {
+                $io->info("$name not allowed on daedalus, skipping ...");
+                continue;
+            }
+
+            $hasJoin = true;
+            try {
+                $loginFailed = false;
+
+                try {
+                    $tryToLoginRequest = $this->httpClient->request(
+                        'PUT',
+                        "$this->identityServerUrl/api/v1/auth/self",
+                        ['json' => ['login' => $name, 'password' => '31323334353637383931']]
+                    );
+                    $result = [];
+                    parse_str($tryToLoginRequest->getHeaders()['set-cookie'][0], $result);
+                    $sid = explode(';', $result['sid'])[0];
+                } catch (\Exception $e) {
+                    $loginFailed = true;
+                }
+
+                if ($loginFailed) {
+                    $createETUserResponse = $this->httpClient->request(
+                        'POST',
+                        "$this->identityServerUrl/api/v1/users",
+                        ['json' => ['username' => "$name", 'display_name' => "$name", 'password' => '31323334353637383931']]
+                    );
+                    $result = [];
+                    parse_str($createETUserResponse->getHeaders()['set-cookie'][0], $result);
+                    $sid = explode(';', $result['sid'])[0];
+                } else {
+                }
+                $client = HttpClient::create([
+                    'headers' => [
+                        'Cookie' => new Cookie('sid', $sid, strtotime('+1 day')),
+                    ],
+                ]);
+                $getTokenETResponse = $client->request(
+                    'GET',
+                    "$this->identityServerUrl/oauth/authorize?access_type=offline&response_type=code&redirect_uri=http://localhost:8080/oauth/callback&client_id=emush@clients&scope=base&state=http://localhost:8081/token",
+                    ['max_redirects' => 0]
+                );
+                $location = $getTokenETResponse->getHeaders(false)['location'];
+                $queryResult = [];
+                parse_str(parse_url($location[0])['query'], $queryResult);
+                $tokenET = $queryResult['code'];
+                $fistTokenApiResponse = $client->request(
+                    'GET',
+                    "$this->eMushApiUri/oauth/callback?code=$tokenET&state=http://localhost:8081/token",
+                    ['max_redirects' => 0]
+                );
+                $location = $fistTokenApiResponse->getHeaders(false)['location'];
+                parse_str(parse_url($location[0])['query'], $queryResult);
+                $fistTokenApi = $queryResult['code'];
+                $realTokenApiResponse = $client->request(
+                    'POST',
+                    "$this->eMushApiUri/oauth/token",
+                    ['json' => ['grant_type' => 'authorization_code', 'code' => $fistTokenApi]]
+                );
+                $token = json_decode($realTokenApiResponse->getContent())->token;
+                $userId = json_decode(base64_decode(explode('.', $token)[1]))->userId;
+
+                $joinDaedalusResponse = $this->httpClient->request(
+                    'POST',
+                    "$this->eMushApiUri/api/v1/player",
+                    [
+                        'json' => ['user' => $userId, 'daedalus' => $daedalusId, 'character' => $name],
+                        'headers' => ['Authorization' => "Bearer $token"],
+                    ],
+                );
+                ++$count;
+                $io->info($name . ' joined Daedalus !');
+            } catch (\Exception $e) {
+                $io->warning("$name cannot join Daedalus. Probably already boarded. Skipping ...");
+                continue;
+            }
+
+            if ($this->isAndieOrDerek($name)) {
+                $isAndieAndDerek = true;
+            }
+            if ($this->isChaoOrFinola($name)) {
+                $isChaoAndFinola = true;
+            }
+
+            if ($count == $numberOfMemberToBoard) {
+                break;
+            }
+        }
+        $io->info("$count member joined the Daedalus.");
+
+        return Command::SUCCESS;
+    }
+
+    public function isAndieOrDerek(string $name): bool
+    {
+        return $name == 'andie' || $name == 'derek';
+    }
+
+    public function isChaoOrFinola(string $name): bool
+    {
+        return $name == 'chao' || $name == 'finola';
+    }
+}

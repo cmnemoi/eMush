f5f1cea3fc06260582c1f4e1a149848b48924425
Evian <icdk5b4zf@relay.firefox.com>
2023-03-26 21:51:57 +0200

    chore: add HunterConfigData

--- Api/src/Hunter/Enum/HunterEnum.php
+++ Api/src/Hunter/Enum/HunterEnum.php
@@ -5,38 +5,10 @@ namespace Mush\Hunter\Enum;
 use Doctrine\Common\Collections\ArrayCollection;
 
 class HunterEnum
-{
-    public const ASTEROID = 'asteroid';
-    public const DICE = 'dice';
-    public const HUNTER = 'hunter';
-    public const SPIDER = 'spider';
-    public const TRAX = 'trax';
-
-    public static function getNormalModeHunters(): ArrayCollection
-    {
-        return new ArrayCollection([
-            self::HUNTER,
-        ]);
-    }
-
-    public static function getHardModeHunters(): ArrayCollection
-    {
-        return new ArrayCollection([
-            self::ASTEROID,
-            self::HUNTER,
-            self::SPIDER,
-            self::TRAX,
-        ]);
-    }
-
-    public static function getVeryHardModeHunters(): ArrayCollection
-    {
-        return new ArrayCollection([
-            self::ASTEROID,
-            self::DICE,
-            self::HUNTER,
-            self::SPIDER,
-            self::TRAX,
-        ]);
-    }
+{   
+    public const ASTEROID = "asteroid";
+    public const DICE = "dice";
+    public const HUNTER = "hunter";
+    public const SPIDER = "spider";
+    public const TRAX = "trax";
 }

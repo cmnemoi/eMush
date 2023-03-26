e45763bf90ccb4f6cb0b598280495b89a5dcf93f
Evian <icdk5b4zf@relay.firefox.com>
2023-03-26 21:54:07 +0200

    style: apply linter fixes

--- Api/src/Hunter/Enum/HunterEnum.php
+++ Api/src/Hunter/Enum/HunterEnum.php
@@ -5,10 +5,10 @@ namespace Mush\Hunter\Enum;
 use Doctrine\Common\Collections\ArrayCollection;
 
 class HunterEnum
-{   
-    public const ASTEROID = "asteroid";
-    public const DICE = "dice";
-    public const HUNTER = "hunter";
-    public const SPIDER = "spider";
-    public const TRAX = "trax";
+{
+    public const ASTEROID = 'asteroid';
+    public const DICE = 'dice';
+    public const HUNTER = 'hunter';
+    public const SPIDER = 'spider';
+    public const TRAX = 'trax';
 }

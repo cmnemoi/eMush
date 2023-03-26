601cafc421741b5cc626fa43696caca826c21f12
Evian <icdk5b4zf@relay.firefox.com>
2023-03-26 21:51:57 +0200

    chore: add HunterConfigData

--- Api/src/Hunter/Enum/HunterEnum.php
+++ Api/src/Hunter/Enum/HunterEnum.php
@@ -3,10 +3,10 @@
 namespace Mush\Hunter\Enum;
 
 class HunterEnum
-{
-    public const ASTEROID = 'asteroid';
-    public const DICE = 'dice';
-    public const HUNTER = 'hunter';
-    public const SPIDER = 'spider';
-    public const TRAX = 'trax';
+{   
+    public const ASTEROID = "asteroid";
+    public const DICE = "dice";
+    public const HUNTER = "hunter";
+    public const SPIDER = "spider";
+    public const TRAX = "trax";
 }

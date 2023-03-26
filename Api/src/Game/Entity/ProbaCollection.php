fef769a50507a0899c44b67375d8aefaef63c340
Evian <icdk5b4zf@relay.firefox.com>
2023-03-26 16:44:48 +0200

    feat: add a ProbaCollection class

---
+++ Api/src/Game/Entity/ProbaCollection.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Mush\Game\Entity;
+
+use Doctrine\Common\Collections\ArrayCollection;
+
+/**
+ * @template-extends ArrayCollection<string|int, int>
+ */
+class ProbabilitiesCollection extends ArrayCollection
+{
+    public function getItemProbability(string|int $key): int
+    {
+        return $this->get($key);
+    }
+
+    public function setItemProbability(string|int $key, int $value): static
+    {
+        $this->set($key, $value);
+
+        return $this;
+    }
+
+    public function getProbabilities(): array
+    {
+        return $this->toArray();
+    }
+}

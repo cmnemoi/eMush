ec330ee4038fc6f807d5a5656b816f4e2b9f1945
Evian <icdk5b4zf@relay.firefox.com>
2023-03-28 09:43:37 +0200

    chore: create hunter events

---
+++ Api/src/Hunter/Event/AbstractHunterEvent.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Mush\Hunter\Event;
+
+use Mush\Daedalus\Entity\Daedalus;
+use Mush\Game\Event\AbstractGameEvent;
+
+abstract class AbstractHunterEvent extends AbstractGameEvent
+{
+    protected Daedalus $daedalus;
+
+    public function __construct(Daedalus $daedalus, array $tags, \DateTime $time)
+    {
+        parent::__construct($tags, $time);
+
+        $this->daedalus = $daedalus;
+    }
+
+    public function getDaedalus(): Daedalus
+    {
+        return $this->daedalus;
+    }
+}

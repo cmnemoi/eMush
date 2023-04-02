c672aaf31db283bf7d3a1b8f4ca2ad9cac08f4c2
Evian <icdk5b4zf@relay.firefox.com>
2023-04-02 10:58:06 +0200

    feat: new hunters can spawn at cycle change with 20% chance

--- Api/src/Hunter/Listener/HunterCycleSubscriber.php
+++ Api/src/Hunter/Listener/HunterCycleSubscriber.php
@@ -2,18 +2,27 @@
 
 namespace Mush\Hunter\Listener;
 
+use Mush\Game\Service\EventServiceInterface;
+use Mush\Game\Service\RandomServiceInterface;
 use Mush\Hunter\Event\HunterCycleEvent;
+use Mush\Hunter\Event\HunterPoolEvent;
 use Mush\Hunter\Service\HunterServiceInterface;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 
 class HunterCycleSubscriber implements EventSubscriberInterface
 {
+    private EventServiceInterface $eventService;
     private HunterServiceInterface $hunterService;
+    private RandomServiceInterface $randomService;
 
     public function __construct(
-        HunterServiceInterface $hunterService
+        EventServiceInterface $eventService,
+        HunterServiceInterface $hunterService,
+        RandomServiceInterface $randomService
     ) {
+        $this->eventService = $eventService;
         $this->hunterService = $hunterService;
+        $this->randomService = $randomService;
     }
 
     public static function getSubscribedEvents(): array
@@ -25,7 +34,20 @@ class HunterCycleSubscriber implements EventSubscriberInterface
 
     public function onNewCycle(HunterCycleEvent $event): void
     {
-        $attackingHunters = $event->getDaedalus()->getAttackingHunters();
+        $daedalus = $event->getDaedalus();
+        $attackingHunters = $daedalus->getAttackingHunters();
         $this->hunterService->makeHuntersShoot($attackingHunters);
+
+        $this->hunterService->updateDaedalusHunterPoints($daedalus);
+
+        if ($this->randomService->isSuccessful(20)) {
+            $unpoolHunterEvent = new HunterPoolEvent(
+                $daedalus,
+                tags: $event->getTags(),
+                time: $event->getTime()
+            );
+
+            $this->eventService->callEvent($unpoolHunterEvent, HunterPoolEvent::UNPOOL_HUNTERS);
+        }
     }
 }

b518c3690049ca8bc55e9d23486ee382c3db0e72
Evian <icdk5b4zf@relay.firefox.com>
2023-04-09 12:52:43 +0200

    update HunterConfig admin page

--- App/src/entities/Config/HunterConfig.ts
+++ App/src/entities/Config/HunterConfig.ts
@@ -1,18 +1,19 @@
+import { StatusConfig } from "./StatusConfig";
+
 export class HunterConfig {
     public iri: string|null;
     public id: number|null;
     public name: string|null;
     public hunterName: string|null;
     public initialHealth: number|null;
-    public initialCharge: number|null;
-    public initialArmor: number|null;
-    public minDamage: number|null;
-    public maxDamage: number|null;
+    public damageRange: Map<number, number>|null;
     public hitChance: number|null;
     public dodgeChance: number|null;
     public drawCost: number|null;
     public maxPerWave: number|null;
     public drawWeight: number|null;
+    public spawnDifficulty: number|null;
+    public initialStatuses: StatusConfig[]|null;
 
     constructor() {
         this.iri = null;
@@ -20,15 +21,14 @@ export class HunterConfig {
         this.name = null;
         this.hunterName = null;
         this.initialHealth = null;
-        this.initialCharge = null;
-        this.initialArmor = null;
-        this.minDamage = null;
-        this.maxDamage = null;
+        this.damageRange = new Map<number, number>();
         this.hitChance = null;
         this.dodgeChance = null;
         this.drawCost = null;
         this.maxPerWave = null;
         this.drawWeight = null;
+        this.spawnDifficulty = null;
+        this.initialStatuses = [];
     }
     load(object:any) : HunterConfig {
         if (typeof object !== "undefined") {
@@ -37,33 +37,51 @@ export class HunterConfig {
             this.name = object['name'];
             this.hunterName = object['hunterName'];
             this.initialHealth = object['initialHealth'];
-            this.initialCharge = object['initialCharge'];
-            this.initialArmor = object['initialArmor'];
-            this.minDamage = object['minDamage'];
-            this.maxDamage = object['maxDamage'];
             this.hitChance = object['hitChance'];
             this.dodgeChance = object['dodgeChance'];
             this.drawCost = object['drawCost'];
             this.maxPerWave = object['maxPerWave'];
             this.drawWeight = object['drawWeight'];
+            this.spawnDifficulty = object['spawnDifficulty'];
+            if (typeof object.damageRange !== 'undefined') {
+                for (const [key, value] of Object.entries(object.damageRange)) {
+                    if (typeof key === 'string' && typeof value === 'number') {
+                        this.damageRange?.set(Number(key), value);
+                    }
+                }
+            }
+            if (typeof object.initialStatuses !== "undefined") {
+                const initialStatuses : StatusConfig[] = [];
+                object.initialStatuses.forEach((initialStatusesData: any) => {
+                    const statusConfig = (new StatusConfig()).load(initialStatusesData);
+                    initialStatuses.push(statusConfig);
+                });
+                this.initialStatuses = initialStatuses;
+            }
         }
         return this;
     }
     jsonEncode() : any {
+        const damageRange : object = {};
+        this.damageRange?.forEach((value, key) => {
+            // @ts-ignore
+            damageRange[key] = value;
+        });
+        const initialStatuses : string[] = [];
+        this.initialStatuses?.forEach(statusConfig => (typeof statusConfig.iri === 'string' ? initialStatuses.push(statusConfig.iri) : null));
         return {
             "id": this.id,
             "name": this.name,
             "hunterName": this.hunterName,
             "initialHealth": this.initialHealth,
-            "initialCharge": this.initialCharge,
-            "initialArmor": this.initialArmor,
-            "minDamage": this.minDamage,
-            "maxDamage": this.maxDamage,
+            "damageRange": damageRange,
             "hitChance": this.hitChance,
             "dodgeChance": this.dodgeChance,
             "drawCost": this.drawCost,
             "maxPerWave": this.maxPerWave,
             "drawWeight": this.drawWeight,
+            "spawnDifficulty": this.spawnDifficulty,
+            "initialStatuses": initialStatuses,
         };
     }
     decode(jsonString : string): HunterConfig {

6864401930b8bd70e99fa5a9a10db496ab90e95a
Evian <icdk5b4zf@relay.firefox.com>
2023-05-26 14:02:45 +0200

    test: add unit test

---
+++ Api/tests/unit/Action/Actions/TakeoffActionTest.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace Mush\Test\Action\Actions;
+
+use Mockery;
+use Mush\Action\ActionResult\Success;
+use Mush\Action\Actions\Takeoff;
+use Mush\Action\Enum\ActionEnum;
+use Mush\Daedalus\Entity\Daedalus;
+use Mush\Equipment\Entity\GameEquipment;
+use Mush\Equipment\Enum\EquipmentEnum;
+use Mush\Place\Entity\Place;
+use Mush\Place\Service\PlaceServiceInterface;
+use Mush\Player\Service\PlayerServiceInterface;
+
+class TakeoffActionTest extends AbstractActionTest
+{
+    /** @var PlayerServiceInterface|Mockery\Mock */
+    private PlayerServiceInterface $playerService;
+    /** @var PlaceServiceInterface|Mockery\Mock */
+    private PlaceServiceInterface $placeService;
+
+    /**
+     * @before
+     */
+    public function before()
+    {
+        parent::before();
+
+        $this->actionEntity = $this->createActionEntity(ActionEnum::TAKEOFF, 2, 0);
+
+        $this->playerService = \Mockery::mock(PlayerServiceInterface::class);
+        $this->placeService = \Mockery::mock(PlaceServiceInterface::class);
+
+        $this->action = new Takeoff(
+            $this->eventService,
+            $this->actionService,
+            $this->validator,
+            $this->playerService,
+            $this->placeService,
+        );
+    }
+
+    /**
+     * @after
+     */
+    public function after()
+    {
+        \Mockery::close();
+    }
+
+    public function testExecute()
+    {
+        $roomStart = new Place();
+        $roomEnd = new Place();
+        $patroller = new GameEquipment($roomStart);
+        $patroller->setName(EquipmentEnum::PATROL_SHIP);
+
+        $this->playerService->shouldReceive('persist');
+
+        $player = $this->createPlayer(new Daedalus(), $roomStart);
+
+        $this->action->loadParameters($this->actionEntity, $player, $patroller);
+
+        $this->placeService->shouldReceive('findByNameAndDaedalus')->andReturn($roomEnd);
+
+        $this->actionService->shouldReceive('applyCostToPlayer')->andReturn($player);
+        $result = $this->action->execute();
+
+        $this->assertInstanceOf(Success::class, $result);
+        $this->assertEquals($player->getPlace(), $roomEnd);
+
+        $this->actionService->shouldReceive('applyCostToPlayer')->andReturn($player);
+
+        $this->eventService->shouldReceive('callEvent')->times(3);
+        $result = $this->action->execute();
+
+        $this->assertInstanceOf(Success::class, $result);
+        $this->assertEquals($player->getPlace(), $roomEnd);
+    }
+}

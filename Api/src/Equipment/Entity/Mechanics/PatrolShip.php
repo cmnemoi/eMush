0ae696ef811e685b06ccd79daec64cccfba5ecd0
Evian <icdk5b4zf@relay.firefox.com>
2023-06-30 19:57:14 +0200

    refactor: create PatrolShip entity and update DB schema accordingly

--- Api/src/Equipment/Entity/Mechanics/PatrolShip.php
+++ Api/src/Equipment/Entity/Mechanics/PatrolShip.php
@@ -5,15 +5,28 @@ namespace Mush\Equipment\Entity\Mechanics;
 use Doctrine\ORM\Mapping as ORM;
 use Mush\Equipment\Entity\EquipmentMechanic;
 use Mush\Equipment\Enum\EquipmentMechanicEnum;
+use Mush\Game\Entity\ProbaCollection;
 
 #[ORM\Entity]
 class PatrolShip extends EquipmentMechanic
-{   
-    #[ORM\Column(type: 'array', nullable: false, default: [])]
+{
+    #[ORM\Column(type: 'array', nullable: true)]
     private array $collectScrapNumber;
-    
-    #[ORM\Column(type: 'array', nullable: false, default: [])]
-    private array $collectScrapPilotDamage;
+
+    #[ORM\Column(type: 'array', nullable: true)]
+    private array $collectScrapPlayerDamage;
+
+    #[ORM\Column(type: 'string', nullable: false, options: ['default' => ''])]
+    private string $dockingPlace = '';
+
+    #[ORM\Column(type: 'array', nullable: false, options: ['default' => '[]'])]
+    private array $failedManoeuvreDaedalusDamage = [];
+
+    #[ORM\Column(type: 'array', nullable: false, options: ['default' => '[]'])]
+    private array $failedManoeuvrePatrolShipDamage = [];
+
+    #[ORM\Column(type: 'array', nullable: false, options: ['default' => '[]'])]
+    private array $failedManoeuvrePlayerDamage = [];
 
     public function getMechanics(): array
     {
@@ -23,4 +36,95 @@ class PatrolShip extends EquipmentMechanic
         return $mechanics;
     }
 
+    public function getCollectScrapNumber(): ?ProbaCollection
+    {
+        return new ProbaCollection($this->collectScrapNumber);
+    }
+
+    public function setCollectScrapNumber(array|ProbaCollection $collectScrapNumber): static
+    {
+        if ($collectScrapNumber instanceof ProbaCollection) {
+            $collectScrapNumber = $collectScrapNumber->toArray();
+        }
+
+        $this->collectScrapNumber = $collectScrapNumber;
+
+        return $this;
+    }
+
+    public function getCollectScrapPlayerDamage(): ?ProbaCollection
+    {
+        return new ProbaCollection($this->collectScrapPlayerDamage);
+    }
+
+    public function setCollectScrapPlayerDamage(array|ProbaCollection $collectScrapPlayerDamage): static
+    {
+        if ($collectScrapPlayerDamage instanceof ProbaCollection) {
+            $collectScrapPlayerDamage = $collectScrapPlayerDamage->toArray();
+        }
+
+        $this->collectScrapPlayerDamage = $collectScrapPlayerDamage;
+
+        return $this;
+    }
+
+    public function getDockingPlace(): string
+    {
+        return $this->dockingPlace;
+    }
+
+    public function setDockingPlace(string $dockingPlace): static
+    {
+        $this->dockingPlace = $dockingPlace;
+
+        return $this;
+    }
+
+    public function getFailedManoeuvreDaedalusDamage(): ProbaCollection
+    {
+        return new ProbaCollection($this->failedManoeuvreDaedalusDamage);
+    }
+
+    public function setFailedManoeuvreDaedalusDamage(array|ProbaCollection $failedManoeuvreDaedalusDamage): static
+    {
+        if ($failedManoeuvreDaedalusDamage instanceof ProbaCollection) {
+            $failedManoeuvreDaedalusDamage = $failedManoeuvreDaedalusDamage->toArray();
+        }
+
+        $this->failedManoeuvreDaedalusDamage = $failedManoeuvreDaedalusDamage;
+
+        return $this;
+    }
+
+    public function getFailedManoeuvrePatrolShipDamage(): ProbaCollection
+    {
+        return new ProbaCollection($this->failedManoeuvrePatrolShipDamage);
+    }
+
+    public function setFailedManoeuvrePatrolShipDamage(array|ProbaCollection $failedManoeuvrePatrolShipDamage): static
+    {
+        if ($failedManoeuvrePatrolShipDamage instanceof ProbaCollection) {
+            $failedManoeuvrePatrolShipDamage = $failedManoeuvrePatrolShipDamage->toArray();
+        }
+
+        $this->failedManoeuvrePatrolShipDamage = $failedManoeuvrePatrolShipDamage;
+
+        return $this;
+    }
+
+    public function getFailedManoeuvrePlayerDamage(): ProbaCollection
+    {
+        return new ProbaCollection($this->failedManoeuvrePlayerDamage);
+    }
+
+    public function setFailedManoeuvrePlayerDamage(array|ProbaCollection $failedManoeuvrePlayerDamage): static
+    {
+        if ($failedManoeuvrePlayerDamage instanceof ProbaCollection) {
+            $failedManoeuvrePlayerDamage = $failedManoeuvrePlayerDamage->toArray();
+        }
+
+        $this->failedManoeuvrePlayerDamage = $failedManoeuvrePlayerDamage;
+
+        return $this;
+    }
 }

e007a8347dc83a7db4f78f6a94c259a49617b6ac
Evian <icdk5b4zf@relay.firefox.com>
2023-07-01 21:31:18 +0200

    style: apply linter fixes

--- Api/src/Action/Validator/HasEquipment.php
+++ Api/src/Action/Validator/HasEquipment.php
@@ -3,13 +3,14 @@
 namespace Mush\Action\Validator;
 
 /**
- * Validates that a player has (not) a specific equipment on reach
- * @param string $reach reach required (inventory, shelve, room, shelve_not_hidden)
- * @param array $equipments list of equipment required
- * @param bool $all if true, all the equipment are required, if false, any of them (default: true)
- * @param bool $contains if true, the equipment must be present, if false, they must not be present (default: true)
- * @param bool $checkIfOperational if true, checks that the equipment is currently working (default: false)
- * @param string $target either player or parameter (default: player)
+ * Validates that a player has (not) a specific equipment on reach.
+ *
+ * @param string $reach              reach required (inventory, shelve, room, shelve_not_hidden)
+ * @param array  $equipments         list of equipment required
+ * @param bool   $all                if true, all the equipment are required, if false, any of them (default: true)
+ * @param bool   $contains           if true, the equipment must be present, if false, they must not be present (default: true)
+ * @param bool   $checkIfOperational if true, checks that the equipment is currently working (default: false)
+ * @param string $target             either player or parameter (default: player)
  */
 class HasEquipment extends ClassConstraint
 {

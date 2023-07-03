ad7a93030de6a06b1cffed547e4dc816b57712a1
Evian <icdk5b4zf@relay.firefox.com>
2023-07-03 21:11:02 +0200

    feat: progress in SpaceBattleView rendering

--- App/src/components/Game/SpaceBattleView.vue
+++ App/src/components/Game/SpaceBattleView.vue
@@ -1,20 +1,20 @@
 <template>
-    <div class="space-battle">
-        <div class="sub-space-battle left">
-            <div class="turrets left">
-                <div class="turret" v-for="(turret, key) in player?.spaceBattle?.turrets" :key="key">
+    <div class="space-battle-container">
+        <div class="space-battle left">
+            <div class="fighters left">
+                <div :class="player?.room?.key === turret.name ? 'fighter green' : 'fighter'" id="turret" v-for="(turret, key) in player?.spaceBattle?.turrets" :key="turret.id">
                     <p>{{ turret.charges }} charges</p>
                 </div>
             </div>
-            <div class="patrol-ships right">
-                <div class="patrol-ship" v-for="(patrolShip, key) in player?.spaceBattle?.patrolShips" :key="key">
+            <div class="fighters right">
+                <div class="fighter" id="patrolship" v-for="(patrolShip, key) in player?.spaceBattle?.patrolShips" :key="patrolShip.id">
                     <p>{{ patrolShip.pilot }}</p>
                 </div>
             </div>
         </div>
-        <div :class="player?.isInAPatrolShip() ? 'sub-space-battle left' : 'sub-space-battle right'">
-            <div :class="player?.isInAPatrolShip() ? 'hunters left' : 'hunters right'">
-                <div class="hunter" v-for="(hunter, key) in player?.spaceBattle?.hunters" :key="key">
+        <div :class="player?.isInAPatrolShip() ? 'space-battle left' : 'space-battle right'">
+            <div :class="player?.isInAPatrolShip() ? 'fighters left' : 'fighters right'">
+                <div class="fighter" id="hunter" v-for="(hunter, key) in player?.spaceBattle?.hunters" :key="hunter.id">
                     <p>{{ hunter.name }}</p>
                 </div>
             </div>
@@ -38,40 +38,50 @@ export default defineComponent({
 
 <style lang="scss" scoped>
 
-.space-battle {
+.space-battle-container {
     position: absolute;
     width: 100%;
     height: 100%;
     align-self: flex-start;
+    margin-top: 30px;
 
-    .sub-space-battle {
+    .space-battle {
         position: absolute;
         width: 20%;
-    }
 
-    .patrol-ships {
-        position: absolute;
-        background-color: green;
-    }
+        .fighters {
+            position: absolute;
 
-    .turrets {
-        position: absolute;
-        background-color: blue;
-    }
+            .fighter {
+                width: 65px;
+                height: 45px;
+                margin: 1px;
+                background-color: #122270;
+                border: solid;
+                border-color: #396AAF;
+                border-width: 1px;
+            }
 
-    .hunters {
-        position: absolute;
-        background-color: red;
+            #hunter {
+                width: 40px;
+                height: 40px;
+            }
+        }
     }
 
     .left {
         align-self: flex-start;
+        margin-left: 8px;
     }
 
     .right {
         align-self: flex-end;
     }
-
+    
+    .green {
+        background-color: #48A209 !important;
+        border-color: #C1EA57 !important;
+    }
 }
 
 

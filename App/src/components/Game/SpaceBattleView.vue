7e3de453efe850411e6c0c52bf269f526ba8be48
Evian <icdk5b4zf@relay.firefox.com>
2023-07-07 21:56:58 +0200

    refactor: complete refactor

--- App/src/components/Game/SpaceBattleView.vue
+++ App/src/components/Game/SpaceBattleView.vue
@@ -1,62 +1,49 @@
 <template>
-    <div class="space-battle-container">
-        <div class="space-battle left">
-            <div class="fighters left">
-                <div :class="isPlayerInRoom(turret.name) ? 'fighter green' : 'fighter'"
-                     id="turret-container"
-                     v-for="(turret, key) in player?.spaceBattle?.turrets"
-                     :key="key">
-                    <img v-if="turret.occupiers?.length > 0"
-                         class="player-body"
-                         :src="getPlayerCharacterBodyByName(getTurretOccupier(turret))"
-                         :alt="getTurretOccupier(turret)">
-                    <span class="turret-charges">
-                        <img class="turret-img" :src="require('@/assets/images/turret.png')" alt="turret">
-                        <span>
-                            {{ turret.charges }}
-                            <img class="charges-img" :src="require('@/assets/images/status/charge.png')" alt="charges">
-                        </span>
-                    </span>
-                </div>
+    <div class="daedalus-fighters-container">
+        <div :class="['fighter-container', { 'green': isPlayerInRoom(turret.name) }]" v-for="(turret, key) in player?.spaceBattle?.turrets" :key="key">
+            <div class="sub-child gray">
+                <img v-if="!turretIsEmpty(turret)"
+                    class="player-body"
+                    :src="getPlayerCharacterBodyByName(getTurretOccupier(turret))"
+                    :alt="getTurretOccupier(turret)">
             </div>
-            <div class="fighters right">
-                <div :class="isPlayerInRoom(patrolShip.name) ? 'fighter green' : 'fighter'"
-                     id="patrolship-container"
-                     v-for="(patrolShip, key) in player?.spaceBattle?.patrolShips"
-                     :key="key">
-                    <img
-                        class="player-body"
-                        :src="getPlayerCharacterBodyByName(patrolShip.pilot)"
-                        :alt="patrolShip.pilot">
-                    <div class="patrol-ship-data">
-                        <img class="patrol-ship-img" :src="require('@/assets/images/patrol_ship.png')" alt="patrol ship">
-                        <span class="patrol-ship-armor">
-                            {{ patrolShip.armor }}
-                            <img class="armor-img" :src="require('@/assets/images/shield.png')" alt="armor">
-                        </span>
-                        <span class="patrol-ship-charges">
-                            {{ patrolShip.charges }}
-                            <img class="charges-img" :src="require('@/assets/images/status/charge.png')" alt="charge">
-                        </span>
-                    </div>
+            <div class="sub-child lightcoral">
+                <div class="sub-child-inner antiquewhite">
+                    <img class="turret-img" :src="require('@/assets/images/spaceBattleTurret.png')" alt="turret">
+                </div>
+                <div class="sub-child-inner antiquewhite">
+                    <p class="quantity">{{ turret.charges }}</p>
+                    <img class="charges-img" :src="require('@/assets/images/status/charge.png')" alt="charges">
                 </div>
             </div>
         </div>
-        <div class="space-battle right">
-            <div class="fighters right">
-                <div class="fighter"
-                     id="hunter-container"
-                     v-for="(hunter, key) in player?.spaceBattle?.hunters"
-                     :key="key">
-                    <img :src="getHunterImage(hunter)" alt="hunter">
-                    <span class="hunter-health">
-                        {{ hunter.health }}
-                        <img class="armor-img" :src="require('@/assets/images/shield.png')" alt="armor">
-                    </span>
+        <div :class="['fighter-container', { 'green': isPlayerInRoom(patrolShip.name) }]" v-for="(patrolShip, key) in player?.spaceBattle?.patrolShips" :key="key">
+            <div class="sub-child gray">
+                <img
+                class="player-body"
+                :src="getPlayerCharacterBodyByName(patrolShip.pilot)"
+                :alt="patrolShip.pilot">
+            </div>
+            <div class="sub-child lightcoral">
+                <div class="sub-child-inner antiquewhite">
+                    <img class="patrol-ship-img" :src="require('@/assets/images/patrol_ship.png')" alt="patrol ship">
+                </div>
+                <div class="sub-child-inner antiquewhite">
+                    <p class="quantity">{{ patrolShip.armor }}</p>
+                    <img class="armor-img" :src="require('@/assets/images/shield.png')" alt="armor">
+                    <p class="quantity">{{ patrolShip.charges }}</p>
+                    <img class="charges-img" :src="require('@/assets/images/status/charge.png')" alt="charge">
                 </div>
             </div>
         </div>
+  </div>
+  <div class="hunters-container">
+    <div class="hunter-container" v-for="(hunter, key) in player?.spaceBattle?.hunters" :key="key">
+        <img :src="getHunterImage(hunter)" alt="hunter">
+        <p class="quantity">{{ hunter.health }}</p>
+        <img class="armor-img" :src="require('@/assets/images/shield.png')" alt="armor">
     </div>
+  </div>
 </template>
 
 <script lang="ts">
@@ -66,8 +53,6 @@ import { Player } from '@/entities/Player';
 import { Hunter } from '@/entities/Hunter';
 import { SpaceBattleTurret } from '@/entities/SpaceBattleTurret';
 import { defineComponent } from 'vue';
-
-
 
 export default defineComponent({
     name: 'SpaceBattleView',
@@ -103,99 +88,143 @@ export default defineComponent({
             if (roomKey === undefined) return false;
             return this.player?.room?.key === roomKey;
         },
+        turretIsEmpty(turret: SpaceBattleTurret) : boolean {
+            if (turret.occupiers === null) return true;
+            return turret?.occupiers?.length === 0;
+        },
     }
 });
 
 </script>
 
 <style lang="scss" scoped>
-.space-battle-container {
-    position: absolute;
-    width: 100%;
-    height: 100%;
-    align-self: flex-start;
-    margin-top: 30px;
-    font-size: 10px;
-    font-weight: 700;
 
-    .space-battle {
+    .daedalus-fighters-container {
         position: absolute;
-        width: 35%;
-
-        .fighters {
-            position: absolute;
-
-            .fighter {
-                width: 65px;
-                height: 45px;
-                margin: 1px;
-                background-color: $slightlyDeepBlue;
-                border: solid;
-                border-color: $greyBlue;
-                border-width: 1px;
-                
-                @include corner-bezel(0, 5px, 0, 0);
-
-                .player-body {
-                    position: absolute;
-                    width: 15px;
-                    height: 40px;
-                    align-self: flex-start;
-                }
-            }
-
-            #turret-container {
-
-                .turret-charges {
-                    position: absolute;
-                    align-self: flex-end;
-                    display: flex;
-                    flex-direction: column;
-                    align-items: center;
-                    width: 45px;
-
-                    .turret-img {
-                        width: 35px;
-                        height: 25px;
-                    }
-                }
-            }
-
-            #hunter-container {
-                width: 40px;
-                height: 40px;
-                display: flex;
-                flex-direction: column;
-                align-items: center;
-
-                @include corner-bezel(3px, 0);
-            }
-        }
+        top: 25px;
+        left: 15px;
+        width: 129px;
+        height: 303px;
+        display: flex;
+        flex-direction: column;
+        flex-wrap: wrap;
+        padding: 1px;
+    }
+    .fighter-container {
+        width: 60px;
+        height: 48px;
+        background-color: $slightlyDeepBlue;
+        margin: 1px;
+        border: solid;
+        border-color: $greyBlue;
+        padding: 2px;
+        border-width: 1px;
+        border-top-right-radius: 10px;
+        display: flex;
+        flex-direction: row;
+        flex-wrap: wrap;
     }
 
-    .left {
-        align-self: flex-start;
-        margin-left: 8px;
-    }
-
-    .right {
-        align-self: flex-end;
-    }
-    
-    .green {
+    .fighter-container.green {
         background-color: $deepGreen !important;
         border-color: $green !important;
     }
 
-    .player-body {
-        position: absolute;
-        width: 15px;
-        height: 40px;
-        align-self: flex-start;
+    .sub-child {
+        flex: 1;
     }
-}
 
+    .sub-child.gray {
+        width: 20px;
+        // background-color: gray;
+    }
 
+    .sub-child.lightcoral {
+        width: calc(100% - 20px);
+        height: calc(100% - 0px);
+        // background-color: lightcoral;
+        flex: 2;
+    }
 
+    .sub-child-inner {
+        width: calc(100% - 4px);
+        height: calc(50% - 2px);
+        margin: 2px;
+        flex-direction: row;
+        justify-content: center;
+        align-items: center;
+    }
 
+    // .sub-child-inner.antiquewhite {
+    //     background-color: antiquewhite;
+    // }
+
+    .player-body {
+        width: fit-content;
+        height: fit-content;
+    }
+    .armor-img {
+        width: 10px;
+        height: 10px;
+    }
+    .charges-img {
+        width: 10px;
+        height: 10px;
+    }
+    .turret-img {
+        margin-top: 7px;
+    }
+    .patrol-ship-img {
+        width: 50px;
+        height: 50px;
+        clip-path: circle(50%);
+        transform: scaleX(-1);
+    }
+    .quantity {
+        width: fit-content;
+        height: fit-content;
+        margin: 0;
+        font-weight: 700;
+        font-size: 10px;
+        padding: 1px;
+    }
+    
+    .patrol-ship-stats {
+        width: 100%;
+        height: 100%;
+        display: flex;
+        flex-direction: row;
+    }
+
+    .hunters-container {
+        position: absolute;
+        top: 25px;
+        left: 341px;
+        width: 85px;
+        height: 303px;
+        display: flex;
+        flex-direction: column;
+        flex-wrap: wrap;
+        align-content: end;
+        padding: 1px;
+    }
+
+    .hunter-container {
+        width: 40px;
+        height: 40px;
+        background-color: $slightlyDeepBlue;
+        margin: 1px;
+        border: solid;
+        border-color: $greyBlue;
+        padding: 2px;
+        border-width: 1px;
+        border-top-left-radius: 5px;
+        display: flex;
+        flex-direction: row;
+        flex-wrap: wrap;
+        justify-content: center;
+        align-items: center;
+    }
+
+    
 </style>

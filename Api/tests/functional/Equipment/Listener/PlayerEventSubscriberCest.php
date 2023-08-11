12db19fb44046fc463a2e20f5984c625b9c35db7
Evian <icdk5b4zf@relay.firefox.com>
2023-08-12 00:37:01 +0200

    tests: add tests to check if patrolship is destroyed in space battle (currently only after collect scrap action)

---
+++ Api/tests/functional/Equipment/Listener/PlayerEventSubscriberCest.php
@@ -0,0 +1,79 @@
+<?php
+
+declare(strict_types=1);
+
+namespace functional\Equipment\Listener;
+
+use App\Tests\AbstractFunctionalTest;
+use App\Tests\FunctionalTester;
+use Mush\Action\Enum\ActionEnum;
+use Mush\Equipment\Entity\Config\EquipmentConfig;
+use Mush\Equipment\Entity\GameEquipment;
+use Mush\Equipment\Enum\EquipmentEnum;
+use Mush\Equipment\Listener\PlayerEventSubscriber;
+use Mush\Place\Enum\RoomEnum;
+use Mush\Player\Enum\EndCauseEnum;
+use Mush\Player\Event\PlayerEvent;
+
+final class PlayerEventSubscriberCest extends AbstractFunctionalTest
+{
+    private PlayerEventSubscriber $playerEventSubscriber;
+
+    public function _before(FunctionalTester $I): void
+    {
+        parent::_before($I);
+        $this->playerEventSubscriber = $I->grabService(PlayerEventSubscriber::class);
+    }
+
+    public function testOnDeathPlayerPatrolShipIsDestroyedIfCauseIsCollectScrap(FunctionalTester $I): void
+    {
+        // given
+        $pasiphaePlace = $this->createExtraPlace(RoomEnum::PASIPHAE, $I, $this->daedalus);
+
+        $patrolShipConfig = $I->grabEntityFromRepository(EquipmentConfig::class, ['equipmentName' => EquipmentEnum::PASIPHAE]);
+        $patrolShip = new GameEquipment($pasiphaePlace);
+        $patrolShip
+            ->setName(EquipmentEnum::PASIPHAE)
+            ->setEquipment($patrolShipConfig)
+        ;
+        $I->haveInRepository($patrolShip);
+        $this->player1->setPlace($pasiphaePlace);
+
+        // when
+        $deathPlayerEvent = new PlayerEvent(
+            $this->player1,
+            [ActionEnum::COLLECT_SCRAP],
+            new \DateTime()
+        );
+        $this->playerEventSubscriber->onDeathPlayer($deathPlayerEvent);
+
+        // then
+        $I->dontSeeInRepository(GameEquipment::class, ['name' => EquipmentEnum::PASIPHAE]);
+    }
+
+    public function testOnDeathPlayerPatrolShipIsNotDestroyedIfCauseIsNotCollectScrap(FunctionalTester $I): void
+    {
+        // given
+        $pasiphaePlace = $this->createExtraPlace(RoomEnum::PASIPHAE, $I, $this->daedalus);
+
+        $patrolShipConfig = $I->grabEntityFromRepository(EquipmentConfig::class, ['equipmentName' => EquipmentEnum::PASIPHAE]);
+        $patrolShip = new GameEquipment($pasiphaePlace);
+        $patrolShip
+            ->setName(EquipmentEnum::PASIPHAE)
+            ->setEquipment($patrolShipConfig)
+        ;
+        $I->haveInRepository($patrolShip);
+        $this->player1->setPlace($pasiphaePlace);
+
+        // when
+        $deathPlayerEvent = new PlayerEvent(
+            $this->player1,
+            [EndCauseEnum::ALLERGY],
+            new \DateTime()
+        );
+        $this->playerEventSubscriber->onDeathPlayer($deathPlayerEvent);
+
+        // then
+        $I->seeInRepository(GameEquipment::class, ['name' => EquipmentEnum::PASIPHAE]);
+    }
+}

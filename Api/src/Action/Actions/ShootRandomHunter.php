ea1cdb173d351492af619671f175d8794fef6067
Evian <icdk5b4zf@relay.firefox.com>
2023-08-12 21:18:21 +0200

    fix: for random shooting, check that the weapon is charged

--- Api/src/Action/Actions/ShootRandomHunter.php
+++ Api/src/Action/Actions/ShootRandomHunter.php
@@ -5,8 +5,11 @@ declare(strict_types=1);
 namespace Mush\Action\Actions;
 
 use Mush\Action\Enum\ActionEnum;
+use Mush\Action\Enum\ActionImpossibleCauseEnum;
+use Mush\Action\Validator\Charged;
 use Mush\Equipment\Entity\GameEquipment;
 use Mush\RoomLog\Entity\LogParameterInterface;
+use Symfony\Component\Validator\Mapping\ClassMetadata;
 
 class ShootRandomHunter extends ShootHunter
 {
@@ -15,5 +18,11 @@ class ShootRandomHunter extends ShootHunter
     protected function support(?LogParameterInterface $parameter): bool
     {
         return $parameter instanceof GameEquipment;
+    }
+
+    public static function loadValidatorMetadata(ClassMetadata $metadata): void
+    {
+        parent::loadValidatorMetadata($metadata);
+        $metadata->addConstraint(new Charged(['groups' => ['execute'], 'message' => ActionImpossibleCauseEnum::UNLOADED_WEAPON]));
     }
 }

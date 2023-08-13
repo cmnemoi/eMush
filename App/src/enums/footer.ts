d976076e23144249278a802822236ad0a90577af
Evian <icdk5b4zf@relay.firefox.com>
2023-08-13 15:19:18 +0200

    chore: update developpers

--- App/src/enums/footer.ts
+++ App/src/enums/footer.ts
@@ -1,5 +1,5 @@
-export const developers = ['A7', 'Simpkin', 'Breut', 'Charles-Meldhine "Evian" Madi Mnemoi', 'Biosha', 'Tishwa', 'Gowoons', 'Sami / EaCOS', 'zasfree', 'MoUoA'];
+export const developers = ['Simpkin', 'Breut', 'Charles-Meldhine "Evian" Madi Mnemoi', 'zasfree'];
 
-export const helpers = ['rackhaml', 'Haux49', 'Demurgos'];
+export const helpers = ['A7', 'rackhaml', 'Haux49', 'Demurgos', 'Biosha', 'Tishwa', 'Gowoons', 'Sami / EaCOS', 'MoUoA'];
 
 export const crowdin = "https://eternaltwin.crowdin.com/multilingual/c9f4ef84da7d855637e201101992f6ed/all?languages=fr,es,en&filter=basic&value=0";

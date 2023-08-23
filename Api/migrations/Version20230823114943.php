967776a8b66d6911c4d92fec470de415d5ff74ed
Sylvain Breton <sylvain.breton.6@gmail.com>
2023-08-23 13:50:35 +0200

    fix: fixed migration files

--- Api/migrations/Version20230823114943.php
+++ Api/migrations/Version20230823114943.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20230823113610 extends AbstractMigration
+final class Version20230823114943 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -20,12 +20,13 @@ final class Version20230823113610 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-
+        $this->addSql('ALTER TABLE abstract_modifier_config ALTER priority TYPE VARCHAR(255)');
     }
 
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
         $this->addSql('CREATE SCHEMA public');
+        $this->addSql('ALTER TABLE abstract_modifier_config ALTER priority TYPE INT');
     }
 }

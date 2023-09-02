5f2ae5648169d17cfdbb7caf6df66fbfe25baab2
Evian <icdk5b4zf@relay.firefox.com>
2023-09-02 16:42:38 +0200

    fix: only listen to patrol ship armor events to destroy patrolships

--- Api/src/Equipment/Listener/StatusEventSubscriber.php
+++ Api/src/Equipment/Listener/StatusEventSubscriber.php
@@ -7,6 +7,7 @@ namespace Mush\Equipment\Listener;
 use Mush\Equipment\Entity\GameEquipment;
 use Mush\Equipment\Service\GameEquipmentServiceInterface;
 use Mush\Player\Entity\Player;
+use Mush\Status\Enum\EquipmentStatusEnum;
 use Mush\Status\Event\StatusEvent;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 
@@ -28,7 +29,12 @@ class StatusEventSubscriber implements EventSubscriberInterface
     }
 
     public function onStatusChargeUpdated(StatusEvent $statusEvent)
-    {
+    {   
+        // only handle patrol ship armor status
+        if ($statusEvent->getStatusName() !== EquipmentStatusEnum::PATROL_SHIP_ARMOR) {
+            return;
+        }
+        
         /** @var GameEquipment $patrolShip */
         $patrolShip = $statusEvent->getStatusHolder();
         /** @var Player $patrolShipPilot */

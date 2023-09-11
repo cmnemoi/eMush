14f59441ce63caad6328b9f07888ccdf71d990ea
Sylvain Breton <sylvain.breton.6@gmail.com>
2023-09-11 19:03:43 +0200

    fix: fixed difficultyConfiggDataLoaderCest

--- Api/tests/functional/Game/ConfigData/DifficultyConfigDataLoaderCest.php
+++ Api/tests/functional/Game/ConfigData/DifficultyConfigDataLoaderCest.php
@@ -20,22 +20,22 @@ class DifficultyConfigDataLoaderCest
         $this->difficultyConfigDataLoader->loadConfigsData();
 
         $I->seeInRepository(DifficultyConfig::class, [
-            'name' => 'difficultyConfig_test',
-            'equipmentBreakRate' => 0,
-            'doorBreakRate' => 0,
-            'equipmentFireBreakRate' => 0,
-            'startingFireRate' => 0,
-            'propagatingFireRate' => 0,
-            'hullFireDamageRate' => 0,
-            'tremorRate' => 0,
-            'electricArcRate' => 0,
-            'metalPlateRate' => 0,
-            'panicCrisisRate' => 0,
+            'name' => 'default',
+            'equipmentBreakRate' => 30,
+            'doorBreakRate' => 40,
+            'equipmentFireBreakRate' => 30,
+            'startingFireRate' => 2,
+            'propagatingFireRate' => 30,
+            'hullFireDamageRate' => 20,
+            'tremorRate' => 5,
+            'electricArcRate' => 5,
+            'metalPlateRate' => 5,
+            'panicCrisisRate' => 5,
             'startingHuntersNumberOfTruceCycles' => 2,
         ]);
 
         // check that we've loaded all the difficulty configs
-        $I->seeNumRecords(1, DifficultyConfig::class);
+        $I->seeNumRecords(2, DifficultyConfig::class);
     }
 
     public function testLoadConfigsDataConfigAlreadyExists(FunctionalTester $I)

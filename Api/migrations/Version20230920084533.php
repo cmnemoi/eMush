2f9b6ed916eeda2450e6645a54a37294f3a27f33
Evian <icdk5b4zf@relay.firefox.com>
2023-09-20 15:08:53 +0200

    fix: fix migration

--- Api/migrations/Version20230920084533.php
+++ Api/migrations/Version20230920084533.php
@@ -27,7 +27,6 @@ final class Version20230920084533 extends AbstractMigration
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
-        $this->addSql('CREATE SCHEMA public');
         $this->addSql('ALTER TABLE config_daedalus DROP init_combustion_chamber_fuel');
         $this->addSql('ALTER TABLE config_daedalus DROP max_combustion_chamber_fuel');
     }

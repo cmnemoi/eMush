c7162825187bd88c02a616e94aeb5e0f942478b5
Evian <icdk5b4zf@relay.firefox.com>
2023-10-02 19:50:12 +0200

    test: add first tests for the action

---
+++ Api/tests/functional/Action/Actions/AdvanceDaedalusCest.php
@@ -0,0 +1,90 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Mush\Tests\functional\Action\Actions;
+
+use Mush\Action\Actions\AdvanceDaedalus;
+use Mush\Action\Entity\Action;
+use Mush\Action\Enum\ActionEnum;
+use Mush\Daedalus\Enum\DaedalusStatusEnum;
+use Mush\Equipment\Entity\Config\EquipmentConfig;
+use Mush\Equipment\Entity\GameEquipment;
+use Mush\Equipment\Enum\EquipmentEnum;
+use Mush\Place\Entity\Place;
+use Mush\Place\Enum\RoomEnum;
+use Mush\Status\Entity\Config\StatusConfig;
+use Mush\Status\Enum\PlayerStatusEnum;
+use Mush\Status\Service\StatusServiceInterface;
+use Mush\Tests\AbstractFunctionalTest;
+use Mush\Tests\FunctionalTester;
+
+final class AdvanceDaedalusCest extends AbstractFunctionalTest
+{
+    private Action $advanceDaedalusConfig;
+    private AdvanceDaedalus $advanceDaedalusAction;
+    private GameEquipment $commandTerminal;
+    private Place $bridge;
+    private StatusServiceInterface $statusService;
+
+    public function _before(FunctionalTester $I): void
+    {
+        parent::_before($I);
+
+        $this->advanceDaedalusConfig = $I->grabEntityFromRepository(Action::class, ['name' => ActionEnum::ADVANCE_DAEDALUS]);
+        $this->advanceDaedalusAction = $I->grabService(AdvanceDaedalus::class);
+        $this->statusService = $I->grabService(StatusServiceInterface::class);
+        $this->bridge = $this->createExtraPlace(RoomEnum::BRIDGE, $I, $this->daedalus);
+
+        $I->grabEntityFromRepository(StatusConfig::class, ['statusName' => DaedalusStatusEnum::TRAVELING]);
+
+        // given there is a command terminal in the bridge
+        $commandTerminalConfig = $I->grabEntityFromRepository(EquipmentConfig::class, ['equipmentName' => EquipmentEnum::COMMAND_TERMINAL]);
+        $this->commandTerminal = new GameEquipment($this->bridge);
+        $this->commandTerminal
+            ->setName(EquipmentEnum::COMMAND_TERMINAL)
+            ->setEquipment($commandTerminalConfig)
+        ;
+        $I->haveInRepository($this->commandTerminal);
+
+        // given the player is on the bridge
+        $this->player->changePlace($this->bridge);
+
+        // given the player is focused on the command terminal
+        $this->statusService->createStatusFromName(
+            statusName: PlayerStatusEnum::FOCUSED,
+            holder: $this->player,
+            tags: [],
+            time: new \DateTime(),
+            target: $this->commandTerminal
+        );
+    }
+
+    public function testAdvanceDaedalusNotVisibleIfPlayerIsNotFocusedOnCommandTerminal(FunctionalTester $I): void
+    {
+        // given player is not focused on the command terminal
+        $this->statusService->removeStatus(
+            statusName: PlayerStatusEnum::FOCUSED,
+            holder: $this->player,
+            tags: [],
+            time: new \DateTime(),
+        );
+
+        // when player tries to advance daedalus
+        $this->advanceDaedalusAction->loadParameters($this->advanceDaedalusConfig, $this->player, $this->commandTerminal);
+        $this->advanceDaedalusAction->execute();
+
+        // then the action is not visible
+        $I->assertFalse($this->advanceDaedalusAction->isVisible());
+    }
+
+    public function testAdvanceDaedalusSuccessCreatesADaedalusTravelingStatus(FunctionalTester $I): void
+    {
+        // when player tries to advance daedalus
+        $this->advanceDaedalusAction->loadParameters($this->advanceDaedalusConfig, $this->player, $this->commandTerminal);
+        $this->advanceDaedalusAction->execute();
+
+        // then the player has a daedalus traveling status
+        $I->assertTrue($this->daedalus->hasStatus(DaedalusStatusEnum::TRAVELING));
+    }
+}

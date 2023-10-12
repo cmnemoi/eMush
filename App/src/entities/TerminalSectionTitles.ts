4c03297861091192b884d318e650f857e14714a9
Evian <icdk5b4zf@relay.firefox.com>
2023-10-12 16:11:51 +0200

    Update front entities to manipulate normalized infos.
    (cherry picked from commit 2854fc3e51a851f02232d9a750981f98fb36cd4c)

--- App/src/entities/TerminalSectionTitles.ts
+++ App/src/entities/TerminalSectionTitles.ts
@@ -2,12 +2,16 @@ export class TerminalSectionTitles {
     public orientateDaedalus: string|null = null;
     public moveDaedalus: string|null = null;
     public generalInformations: string|null = null;
+    public orientation: string|null = null;
+    public distance: string|null = null;
 
     public load(object: any): TerminalSectionTitles {
         if (object) {
             this.orientateDaedalus = object['orientate_daedalus'];
             this.moveDaedalus = object['move_daedalus'];
             this.generalInformations = object['general_informations'];
+            this.orientation = object['orientation'];
+            this.distance = object['distance'];
         }
         return this;
     }

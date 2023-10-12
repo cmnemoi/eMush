413e532ed74737fc8a138e48bd07eb94504457d1
Evian <icdk5b4zf@relay.firefox.com>
2023-10-12 16:12:09 +0200

    Display normalized infos in Astro Terminal.

--- App/src/components/Game/Terminals/AstroTerminal.vue
+++ App/src/components/Game/Terminals/AstroTerminal.vue
@@ -1,30 +1,31 @@
 <template>
     <div class="planet-container">
-        <section class="planet">
-            <h3>Goulidon</h3>
+        <section
+            v-for="planet in planets"
+            :key="planet.id"
+            class="planet" 
+        >
+            <h3>{{ planet.name }}</h3>
             <div class="card">
-                <img src="@/assets/images/astro/planet_0_small.png">
-                <p><span>Direction</span> East</p>
-                <p><span>Fuel required</span> 3</p>
+                <img :src="getPlanetImage(planet)">
+                <p><span>{{ terminal.sectionTitles.orientation }}</span> {{ planet.orientation }}</p>
+                <p><span>{{ terminal.sectionTitles.distance }}</span> {{ planet.distance }}</p>
             </div>
             <div class="analysis">
                 <ul>
-                    <li><img src="@/assets/images/astro/cold.png"></li>
-                    <li><img src="@/assets/images/astro/ocean.png"></li>
-                    <li><img src="@/assets/images/astro/forest.png"></li>
-                    <li><img src="@/assets/images/astro/cold.png"></li>
-                    <li><img src="@/assets/images/astro/ocean.png"></li>
-                    <li><img src="@/assets/images/astro/unknown.png"></li>
-                    <li><img src="@/assets/images/astro/unknown.png"></li>
-                    <li><img src="@/assets/images/astro/unknown.png"></li>
-                    <li><img src="@/assets/images/astro/forest.png"></li>
-                    <li><img src="@/assets/images/astro/cold.png"></li>
-                    <li><img src="@/assets/images/astro/cold.png"></li>
+                    <li v-for="sector in planet.sectors" :key="sector.id">
+                        <Tippy tag="img" :src="getSectorImage(sector.key)">
+                            <template #content>
+                                <h1 v-html="formatText(sector.name)" />
+                                <p v-html="formatText(sector.description)" />
+                            </template>
+                        </Tippy>
+                    </li>
                 </ul>
                 <div class="actions">
                     <button>
                         <span class="cost">2<img src="@/assets/images/pa.png" alt="ap"></span>
-                        <span>Analysis</span>
+                        <span>Analyse</span>
                     </button>
                     <button class="delete">
                         <img src="@/assets/images/bin.png">
@@ -32,20 +33,24 @@
                 </div>
             </div>
         </section>
-
-        <section class="planet unknown">
+        <section
+            v-for="i in numberOfUnknownPlanets"
+            :key="i"
+            class="planet unknown"
+        >
             <h3>???</h3>
             <div class="card">
                 <img src="@/assets/images/astro/planet_unknown.png">
             </div>
             <div class="analysis">
                 <!-- <p>Scanning impossible...</p> -->
-                <p>80% <img src="@/assets/images/astro/thermosensors.png"></p>
+                <p>{{ scanAction.successRate }}% <img src="@/assets/images/astro/thermosensors.png"></p>
                 <div class="actions">
-                    <button>
-                        <span class="cost">2<img src="@/assets/images/pa.png" alt="ap"></span>
-                        <span>Scan (80%)</span>
-                    </button>
+                    <ActionButton
+                        :key="scanAction.key"
+                        :action="scanAction"
+                        @click="executeTargetAction(target, scanAction)"
+                    />
                 </div>
             </div>
         </section>
@@ -53,16 +58,81 @@
 </template>
 
 <script lang="ts">
+import { Planet } from "@/entities/Planet";
 import { Terminal } from "@/entities/Terminal";
 import { defineComponent } from "vue";
+import { ActionEnum } from "@/enums/action.enum";
+import { Action } from "@/entities/Action";
+import { formatText } from "@/utils/formatText";
+import { mapActions } from "vuex";
+import ActionButton from "@/components/Utils/ActionButton.vue";
 
 export default defineComponent ({
     name: "AstroTerminal",
+    components : {
+        ActionButton
+    },
     props: {
         terminal: {
             type: Terminal,
             required: true
+        },
+    },
+    computed: {
+        scanAction(): Action {
+            const action = this.terminal?.actions.find(action => action.key === ActionEnum.SCAN);
+            if (!action) throw new Error(`No ${ActionEnum.SCAN} action found for terminal ${this.terminal?.key}`);
+
+            return action;
+        },
+        target(): Terminal {
+            return this.terminal;
+        },
+        planets(): Planet[] {
+            const planets = this.terminal?.infos.planets;
+            if (!planets) throw new Error(`No planets found for terminal ${this.terminal?.key}`);
+
+            return planets;
+        },
+        numberOfUnknownPlanets(): number {
+            const maxDiscoverablePlanets = this.terminal.infos?.maxDiscoverablePlanets;
+            if (!maxDiscoverablePlanets) {
+                throw new Error(`No maxDiscoverablePlanets found for terminal ${this.terminal?.key}`);
+            }
+
+            return maxDiscoverablePlanets - this.planets.length;
         }
+    },
+    methods: {
+        ...mapActions({
+            'executeAction': 'action/executeAction',
+        }),
+        async executeTargetAction(target: Terminal, action: Action): Promise<void> {
+            if (action.canExecute) {
+                await this.executeAction({ target, action });
+            }
+        },
+        getPlanetSeedFromName(name: string): number {
+            return name.split('').reduce((acc, char) => acc + char.charCodeAt(0), 0);
+        },
+        getPlanetImage(planet: Planet): string {
+            const id = this.getPlanetSeedFromName(planet.name) % this.numberOfPlanetImages;
+            return require(`@/assets/images/astro/planet_${id}_small.png`);
+        },
+        getSectorImage(sector: string): string {
+            return require(`@/assets/images/astro/${sector}.png`);
+        },
+        formatText(text: string | null): string {
+            if (!text)
+                return '';
+            return formatText(text);
+        }
+    },
+    data() {
+        return {
+            ActionEnum,
+            numberOfPlanetImages: 5,
+        };
     },
 });
 </script>

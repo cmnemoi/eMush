4e4d0610e044c1de2d7386de8563270c29200078
Evian <icdk5b4zf@relay.firefox.com>
2023-10-14 12:25:48 +0200

    Trying to fix issues, leading to other issues.

--- App/vite.config.js
+++ App/vite.config.js
@@ -6,6 +6,9 @@ import vue from '@vitejs/plugin-vue'
 const filename = fileURLToPath(import.meta.url);
 const pathSegments = path.dirname(filename);
 
+console.log(pathSegments);
+console.log(import.meta.env.VITE_URL);
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],

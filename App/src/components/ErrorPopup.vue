f1324f58ccbfcb0b548a69005edf62f36f15e4af
A7 <atar.inscription@gmail.com>
2023-10-15 10:16:32 +0000

    chore: Create confirm popup component

--- App/src/components/ErrorPopup.vue
+++ App/src/components/ErrorPopup.vue
@@ -1,16 +1,20 @@
 <template>
     <PopUp :is-open="isError()" @close="clearError">
         <h1 class="title">{{ $t(['errors.status', errorStatus].join('.')) }}</h1>
-        <span v-if="isWorkingServerError()">{{ $t('errors.reportToDevs') }}</span>
-        <span v-else>{{ $t('errors.problem') }}</span>
-        <div class="details">
-            <span v-if="error.request.method">method: {{ error.request.method.toUpperCase() }}</span>
-            <span v-if="error.request.url">url: {{ error.request.url }}</span>
-            <span v-if="error.request.params">params: {{ error.request.params }}</span>
-            <span v-if="error.response.details">details: {{ $t(['errors', error.response.details].join('.')) }}</span>
-            <span v-if="error.response.class">class: {{ error.response.class }}</span>
-            <span><br></span>
-            <span v-html="$t('errors.consultCommunity')"></span>
+        <div class="message">
+            <img class="neron-img" src="@/assets/images/neron_eye.gif" alt="Neron">
+            <div>
+                <p v-if="isWorkingServerError()">{{ $t('errors.reportToDevs') }}</p>
+                <p v-else>{{ $t('errors.problem') }}</p>
+                <div class="code">
+                    <span v-if="error.request.method">method: {{ error.request.method.toUpperCase() }}</span>
+                    <span v-if="error.request.url">url: {{ error.request.url }}</span>
+                    <span class="details" v-if="error.request.params">params: <strong>{{ error.request.params }}</strong></span>
+                    <span class="details" v-if="error.response.details">details: <strong>{{ $t(['errors', error.response.details].join('.')) }}</strong></span>
+                    <span class="details" v-if="error.response.class">class: <strong>{{ error.response.class }}</strong></span>
+                </div>
+                <p v-html="$t('errors.consultCommunity')"></p>
+            </div>
         </div>
     </PopUp>
 </template>
@@ -57,12 +61,30 @@ export default defineComponent ({
 }
 
 .title {
-    margin-top: 0;
+    font-size: 1.5em;
+    margin-bottom: 0.5em;
 }
 
-.details {
-    margin-top: 16px;
-    font-size: .65em;
+p { font-size: 1.2em; }
+
+.message {
+    flex-direction: row;
+    align-items: flex-start;
+    gap: 0.8em;
+
+    img { margin: 1em 0; }
+}
+
+.code {
+    padding: 0.8em 1.4em;
+    border: 1px solid $red;
+    border-radius: 4px;
+    background-color: #222b6b;
+    line-height: 1.4em;
+
+    .details { margin-top: 0.4em; }
+
+    strong { letter-spacing: 0.03em; }
 }
 
 </style>
--- App/src/components/Utils/PopUp.vue
+++ App/src/components/Utils/PopUp.vue
@@ -30,7 +30,7 @@ export default defineComponent ({
 <style lang="scss" scoped>
 .modal-background {
     position: fixed;
-    background: transparentize(#09092d, 0.4);
+    background: transparentize(#09092d, 0.5);
     top: 0;
     right: 0;
     bottom: 0;
@@ -40,18 +40,35 @@ export default defineComponent ({
 
     .modal-box {
         min-width: 400px;
+        max-width: 740px;
         position: absolute;
         top: 50%;
         left: 50%;
         transform: translate(-50%, -50%);
-        padding: 2em;
-        font-size: 1.1em;
-        background-color: #191a4c;
-        border-radius: 3px;
-        border: 1px solid #3965fb;
-        box-shadow:
-            0 0 0 1px #191a4c,
-            0 0 5px 1px rgba(57, 101, 251, 0.7),
+        padding: 1.6em 2em;
+        background-color: rgba(35, 37, 100, .9);
+        box-shadow: 
+            inset 0 0 12px 3px #3965fb, inset 0 0 0 2px #3965fb;
+
+            &::after { // angle blue triangles decoration
+            content: "";
+            position: absolute;
+            z-index: -1;
+            left: 0;
+            right: 0;
+            top: 0;
+            bottom: 0;
+            margin: 3px;
+            border: 5px solid transparent;
+            border-image: url('~@/assets/images/ToolTip-corners.gif') 50% round;
+        };
+    }
+
+    &::v-deep h1 {
+        font-size: 1.5em;
+        text-transform: uppercase;
+        text-align: center;
+        margin: 0;
     }
 }
 
@@ -63,7 +80,7 @@ export default defineComponent ({
     top: 0;
     padding: 12px;
     color: transparentize(white, 0.4);
-    font-size: .85em;
+    font-size: 1em;
     letter-spacing: 0.03em;
     text-decoration: none;
     font-variant: small-caps;

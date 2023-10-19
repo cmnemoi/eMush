0dbad25884e92c625b6072d586b56e9245996e59
Evian <icdk5b4zf@relay.firefox.com>
2023-10-19 13:25:08 +0200

    refactor tests

---
+++ Api/tests/_support/AbstractTurnDaedalusActionCest.php
@@ -0,0 +1,165 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Mush\Tests;
+
+use Mush\Action\Actions\AbstractTurnDaedalusAction;
+use Mush\Action\Entity\Action;
+use Mush\Action\Enum\ActionEnum;
+use Mush\Action\Enum\ActionImpossibleCauseEnum;
+use Mush\Communication\Entity\Message;
+use Mush\Communication\Enum\NeronMessageEnum;
+use Mush\Equipment\Entity\Config\EquipmentConfig;
+use Mush\Equipment\Entity\GameEquipment;
+use Mush\Equipment\Enum\EquipmentEnum;
+use Mush\Place\Enum\RoomEnum;
+use Mush\Status\Enum\DaedalusStatusEnum;
+use Mush\Status\Enum\EquipmentStatusEnum;
+use Mush\Status\Enum\PlayerStatusEnum;
+use Mush\Status\Service\StatusServiceInterface;
+use Mush\Tests\AbstractFunctionalTest;
+use Mush\Tests\FunctionalTester;
+
+abstract class AbstractTurnDaedalusActionCest extends AbstractFunctionalTest
+{
+    protected Action $turnDaedalusConfig;
+    protected AbstractTurnDaedalusAction $turnDaedalusAction;
+    protected GameEquipment $commandTerminal;
+    protected GameEquipment $alphaLateralReactor;
+    protected GameEquipment $bravoLateralReactor;
+    protected StatusServiceInterface $statusService;
+
+    public function _before(FunctionalTester $I): void
+    {
+        parent::_before($I);
+
+        $this->turnDaedalusConfig = $I->grabEntityFromRepository(Action::class, ['name' => ActionEnum::TURN_DAEDALUS_LEFT]);
+        $engineRoom = $this->createExtraPlace(RoomEnum::ENGINE_ROOM, $I, $this->daedalus);
+        
+        // given there is a command terminal in player's room
+        $commandTerminalConfig = $I->grabEntityFromRepository(EquipmentConfig::class, ['equipmentName' => EquipmentEnum::COMMAND_TERMINAL]);
+        $this->commandTerminal = new GameEquipment($this->daedalus->getPlaceByName(RoomEnum::LABORATORY));
+        $this->commandTerminal
+            ->setName(EquipmentEnum::COMMAND_TERMINAL)
+            ->setEquipment($commandTerminalConfig)
+        ;
+        $I->haveInRepository($this->commandTerminal);
+
+        // given there is bravo lateral reactor in engine room
+        $leftLateralReactorConfig = $I->grabEntityFromRepository(EquipmentConfig::class, ['equipmentName' => EquipmentEnum::REACTOR_LATERAL_BRAVO]);
+        $this->bravoLateralReactor = new GameEquipment($engineRoom);
+        $this->bravoLateralReactor
+            ->setName(EquipmentEnum::REACTOR_LATERAL_BRAVO)
+            ->setEquipment($leftLateralReactorConfig)
+        ;
+        $I->haveInRepository($this->bravoLateralReactor);
+
+        // given there is alpha lateral reactor in engine room
+        $rightLateralReactorConfig = $I->grabEntityFromRepository(EquipmentConfig::class, ['equipmentName' => EquipmentEnum::REACTOR_LATERAL_ALPHA]);
+        $this->alphaLateralReactor = new GameEquipment($engineRoom);
+        $this->alphaLateralReactor
+            ->setName(EquipmentEnum::REACTOR_LATERAL_ALPHA)
+            ->setEquipment($rightLateralReactorConfig)
+        ;
+        $I->haveInRepository($this->alphaLateralReactor);
+
+        $this->statusService = $I->grabService(StatusServiceInterface::class);
+
+        // given player is focused on the astro terminal
+        $this->statusService->createStatusFromName(
+            statusName: PlayerStatusEnum::FOCUSED,
+            holder: $this->player,
+            tags: [],
+            time: new \DateTime(),
+            target: $this->commandTerminal,
+        );
+    }
+
+    public function testTurnDaedalusActionNotVisibleIfPlayerNotFocusedOnCommandTerminal(FunctionalTester $I): void
+    {
+        // given player is not focused on the astro terminal
+        $this->statusService->removeStatus(
+            statusName: PlayerStatusEnum::FOCUSED,
+            holder: $this->player,
+            tags: [],
+            time: new \DateTime(),
+        );
+
+        // when player turns daedalus left
+        $this->turnDaedalusAction->loadParameters($this->turnDaedalusConfig, $this->player, $this->commandTerminal);
+        $this->turnDaedalusAction->execute();
+
+        // then the action is not visible
+        $I->assertFalse($this->turnDaedalusAction->isVisible());
+    }
+
+    public function testTurnDaedalusActionNotExecutableIfDaedalusIsTraveling(FunctionalTester $I): void
+    {
+        // given daedalus is traveling
+        $this->statusService->createStatusFromName(
+            statusName: DaedalusStatusEnum::TRAVELING,
+            holder: $this->daedalus,
+            tags: [],
+            time: new \DateTime(),
+        );
+
+        // when player turns daedalus left
+        $this->turnDaedalusAction->loadParameters(
+            action: $this->turnDaedalusConfig,
+            player: $this->player,
+            target: $this->commandTerminal
+        );
+        $this->turnDaedalusAction->execute();
+
+        // then the action is not executable
+        $I->assertEquals(ActionImpossibleCauseEnum::DAEDALUS_TRAVELING, $this->turnDaedalusAction->cannotExecuteReason());
+    }
+
+    public function testTurnDaedalusActionNotExecutableIfCommandTerminalIsBroken(FunctionalTester $I): void
+    {
+        // given daedalus is traveling
+        $this->statusService->createStatusFromName(
+            statusName: EquipmentStatusEnum::BROKEN,
+            holder: $this->commandTerminal,
+            tags: [],
+            time: new \DateTime(),
+        );
+
+        // when player turns daedalus left
+        $this->turnDaedalusAction->loadParameters(
+            action: $this->turnDaedalusConfig,
+            player: $this->player,
+            target: $this->commandTerminal
+        );
+        $this->turnDaedalusAction->execute();
+
+        // then the action is not executable
+        $I->assertEquals(ActionImpossibleCauseEnum::BROKEN_EQUIPMENT, $this->turnDaedalusAction->cannotExecuteReason());
+    }
+
+    public function testTurnDaedalusActionSuccessTriggersANeronAnnouncement(FunctionalTester $I): void
+    {
+        // when player turns daedalus left
+        $this->turnDaedalusAction->loadParameters(
+            action: $this->turnDaedalusConfig,
+            player: $this->player,
+            target: $this->commandTerminal
+        );
+        $this->turnDaedalusAction->execute();
+
+        // then a neron announcement is triggered
+        $I->seeInRepository(
+            Message::class, 
+            [
+                'neron' => $this->daedalus->getDaedalusInfo()->getNeron(),
+                'message' => NeronMessageEnum::CHANGE_HEADING,
+            ]
+        );
+    }
+
+    abstract public function testTurnDaedalusActionNotExecutableIfLateralReactorIsBroken(FunctionalTester $I): void;
+
+    abstract public function testTurnDaedalusActionSuccessChangesCorrectlyDaedalusOrientation(FunctionalTester $I): void;
+
+}
--- Api/tests/functional/Action/Actions/TurnDaedalusLeftCest.php
+++ Api/tests/functional/Action/Actions/TurnDaedalusLeftCest.php
@@ -5,132 +5,22 @@ declare(strict_types=1);
 namespace Mush\Tests\Functional\Action\Actions;
 
 use Mush\Action\Actions\TurnDaedalusLeft;
-use Mush\Action\Entity\Action;
-use Mush\Action\Enum\ActionEnum;
 use Mush\Action\Enum\ActionImpossibleCauseEnum;
-use Mush\Communication\Entity\Message;
-use Mush\Communication\Enum\NeronMessageEnum;
-use Mush\Equipment\Entity\Config\EquipmentConfig;
-use Mush\Equipment\Entity\GameEquipment;
-use Mush\Equipment\Enum\EquipmentEnum;
 use Mush\Exploration\Enum\SpaceOrientationEnum;
-use Mush\Place\Enum\RoomEnum;
-use Mush\Status\Enum\DaedalusStatusEnum;
 use Mush\Status\Enum\EquipmentStatusEnum;
-use Mush\Status\Enum\PlayerStatusEnum;
-use Mush\Status\Service\StatusServiceInterface;
-use Mush\Tests\AbstractFunctionalTest;
+use Mush\Tests\AbstractTurnDaedalusActionCest;
 use Mush\Tests\FunctionalTester;
 
-final class TurnDaedalusLeftCest extends AbstractFunctionalTest
+final class TurnDaedalusLeftCest extends AbstractTurnDaedalusActionCest
 {
-    private Action $turnDaedalusLeftConfig;
-    private TurnDaedalusLeft $turnDaedalusLeftAction;
-    private GameEquipment $commandTerminal;
-    private GameEquipment $bravoLateralReactor;
-    private StatusServiceInterface $statusService;
-
     public function _before(FunctionalTester $I): void
     {
         parent::_before($I);
 
-        $this->turnDaedalusLeftConfig = $I->grabEntityFromRepository(Action::class, ['name' => ActionEnum::TURN_DAEDALUS_LEFT]);
-        $this->turnDaedalusLeftAction = $I->grabService(TurnDaedalusLeft::class);
-        $engineRoom = $this->createExtraPlace(RoomEnum::ENGINE_ROOM, $I, $this->daedalus);
-        
-        // given there is a command terminal in player's room
-        $commandTerminalConfig = $I->grabEntityFromRepository(EquipmentConfig::class, ['equipmentName' => EquipmentEnum::COMMAND_TERMINAL]);
-        $this->commandTerminal = new GameEquipment($this->daedalus->getPlaceByName(RoomEnum::LABORATORY));
-        $this->commandTerminal
-            ->setName(EquipmentEnum::COMMAND_TERMINAL)
-            ->setEquipment($commandTerminalConfig)
-        ;
-        $I->haveInRepository($this->commandTerminal);
-
-        // given there is bravo lateral reactor in engine room
-        $leftLateralReactorConfig = $I->grabEntityFromRepository(EquipmentConfig::class, ['equipmentName' => EquipmentEnum::REACTOR_LATERAL_BRAVO]);
-        $this->bravoLateralReactor = new GameEquipment($engineRoom);
-        $this->bravoLateralReactor
-            ->setName(EquipmentEnum::REACTOR_LATERAL_BRAVO)
-            ->setEquipment($leftLateralReactorConfig)
-        ;
-        $I->haveInRepository($this->bravoLateralReactor);
-
-        $this->statusService = $I->grabService(StatusServiceInterface::class);
-
-        // given player is focused on the astro terminal
-        $this->statusService->createStatusFromName(
-            statusName: PlayerStatusEnum::FOCUSED,
-            holder: $this->player,
-            tags: [],
-            time: new \DateTime(),
-            target: $this->commandTerminal,
-        );
+        $this->turnDaedalusAction = $I->grabService(TurnDaedalusLeft::class);
     }
 
-    public function testTurnDaedalusLeftNotVisibleIfPlayerNotFocusedOnCommandTerminal(FunctionalTester $I): void
-    {
-        // given player is not focused on the astro terminal
-        $this->statusService->removeStatus(
-            statusName: PlayerStatusEnum::FOCUSED,
-            holder: $this->player,
-            tags: [],
-            time: new \DateTime(),
-        );
-
-        // when player turns daedalus left
-        $this->turnDaedalusLeftAction->loadParameters($this->turnDaedalusLeftConfig, $this->player, $this->commandTerminal);
-        $this->turnDaedalusLeftAction->execute();
-
-        // then the action is not visible
-        $I->assertFalse($this->turnDaedalusLeftAction->isVisible());
-    }
-
-    public function testTurnDaedalusLeftNotExecutableIfDaedalusIsTraveling(FunctionalTester $I): void
-    {
-        // given daedalus is traveling
-        $this->statusService->createStatusFromName(
-            statusName: DaedalusStatusEnum::TRAVELING,
-            holder: $this->daedalus,
-            tags: [],
-            time: new \DateTime(),
-        );
-
-        // when player turns daedalus left
-        $this->turnDaedalusLeftAction->loadParameters(
-            action: $this->turnDaedalusLeftConfig,
-            player: $this->player,
-            target: $this->commandTerminal
-        );
-        $this->turnDaedalusLeftAction->execute();
-
-        // then the action is not executable
-        $I->assertEquals(ActionImpossibleCauseEnum::DAEDALUS_TRAVELING, $this->turnDaedalusLeftAction->cannotExecuteReason());
-    }
-
-    public function testTurnDaedalusLeftNotExecutableIfCommandTerminalIsBroken(FunctionalTester $I): void
-    {
-        // given daedalus is traveling
-        $this->statusService->createStatusFromName(
-            statusName: EquipmentStatusEnum::BROKEN,
-            holder: $this->commandTerminal,
-            tags: [],
-            time: new \DateTime(),
-        );
-
-        // when player turns daedalus left
-        $this->turnDaedalusLeftAction->loadParameters(
-            action: $this->turnDaedalusLeftConfig,
-            player: $this->player,
-            target: $this->commandTerminal
-        );
-        $this->turnDaedalusLeftAction->execute();
-
-        // then the action is not executable
-        $I->assertEquals(ActionImpossibleCauseEnum::BROKEN_EQUIPMENT, $this->turnDaedalusLeftAction->cannotExecuteReason());
-    }
-
-    public function testTurnDaedalusLeftNotExecutableIfBravoLateralReactorIsBroken(FunctionalTester $I): void
+    public function testTurnDaedalusActionNotExecutableIfLateralReactorIsBroken(FunctionalTester $I): void
     {
         // given daedalus is traveling
         $this->statusService->createStatusFromName(
@@ -141,63 +31,42 @@ final class TurnDaedalusLeftCest extends AbstractFunctionalTest
         );
 
         // when player turns daedalus left
-        $this->turnDaedalusLeftAction->loadParameters(
-            action: $this->turnDaedalusLeftConfig,
+        $this->turnDaedalusAction->loadParameters(
+            action: $this->turnDaedalusConfig,
             player: $this->player,
             target: $this->commandTerminal
         );
-        $this->turnDaedalusLeftAction->execute();
+        $this->turnDaedalusAction->execute();
 
         // then the action is not executable
-        $I->assertEquals(ActionImpossibleCauseEnum::LATERAL_REACTOR_BROKEN, $this->turnDaedalusLeftAction->cannotExecuteReason());
+        $I->assertEquals(ActionImpossibleCauseEnum::LATERAL_REACTOR_BROKEN, $this->turnDaedalusAction->cannotExecuteReason());
     }
 
-    public function testTurnDaedalusLeftSuccessChangesCorrectlyDaedalusOrientation(FunctionalTester $I): void
+    public function testTurnDaedalusActionSuccessChangesCorrectlyDaedalusOrientation(FunctionalTester $I): void
     {
         // given daedalus is facing north
         $I->assertEquals(expected: SpaceOrientationEnum::NORTH, actual: $this->daedalus->getOrientation());
 
         // when player turns daedalus left
-        $this->turnDaedalusLeftAction->loadParameters(
-            action: $this->turnDaedalusLeftConfig,
+        $this->turnDaedalusAction->loadParameters(
+            action: $this->turnDaedalusConfig,
             player: $this->player,
             target: $this->commandTerminal
         );
-        $this->turnDaedalusLeftAction->execute();
+        $this->turnDaedalusAction->execute();
 
         // then daedalus is facing west
         $I->assertEquals(expected: SpaceOrientationEnum::WEST, actual: $this->daedalus->getOrientation());
 
         // when player turns daedalus left again
-        $this->turnDaedalusLeftAction->loadParameters(
-            action: $this->turnDaedalusLeftConfig,
+        $this->turnDaedalusAction->loadParameters(
+            action: $this->turnDaedalusConfig,
             player: $this->player,
             target: $this->commandTerminal
         );
-        $this->turnDaedalusLeftAction->execute();
+        $this->turnDaedalusAction->execute();
 
         // then daedalus is facing south
         $I->assertEquals(expected: SpaceOrientationEnum::SOUTH, actual: $this->daedalus->getOrientation());
     }
-
-    public function testTurnDaedalusLeftSuccessTriggersANeronAnnouncement(FunctionalTester $I): void
-    {
-        // when player turns daedalus left
-        $this->turnDaedalusLeftAction->loadParameters(
-            action: $this->turnDaedalusLeftConfig,
-            player: $this->player,
-            target: $this->commandTerminal
-        );
-        $this->turnDaedalusLeftAction->execute();
-
-        // then a neron announcement is triggered
-        $I->seeInRepository(
-            Message::class, 
-            [
-                'neron' => $this->daedalus->getDaedalusInfo()->getNeron(),
-                'message' => NeronMessageEnum::CHANGE_HEADING,
-            ]
-        );
-    }
-
 }

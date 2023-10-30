8c9db61765d3331d5bd617096822d03f803268ab
Sylvain Breton <sylvain.breton.6@gmail.com>
2023-10-30 19:45:53 +0100

    chore: changed how the team is displayed. Thay are now split between active and inactive. Role are indicated by an image. Dev are sorted according to their implication in the project

--- App/src/enums/footer.ts
+++ App/src/enums/footer.ts
@@ -1,5 +1,17 @@
-export const developers = ['Simpkin', 'Breut', 'Charles-Meldhine "Evian" Madi Mnemoi', 'zasfree'];
-
-export const helpers = ['A7', 'rackhaml', 'Haux49', 'Demurgos', 'Biosha', 'Tishwa', 'Gowoons', 'Sami / EaCOS', 'MoUoA'];
+export const team: ({active: boolean, name: string, role: string})[] = [
+    { name: 'Charles-Meldhine "Evian" Madi Mnemoi', role: 'developer', active: true },
+    { name: 'Breut', role: 'developer', active: true },
+    { name: 'Gowoons', role: 'developer', active: true },
+    { name: 'A7', role: 'developer', active: true },
+    { name: 'zasfree', role: 'admin', active: true },
+    { name: 'Simpkin', role: 'developer', active: false },
+    { name: 'rackhaml', role: 'developer', active: false },
+    { name: 'Haux49', role: 'developer', active: false },
+    { name: 'Demurgos', role: 'developer', active: false },
+    { name: 'Biosha', role: 'developer', active: false },
+    { name: 'Tishwa', role: 'developer', active: false },
+    { name: 'Sami / EaCOS', role: 'developer', active: false },
+    { name: 'MoUoA', role: 'helper', active: false },
+];
 
 export const crowdin = "https://eternaltwin.crowdin.com/multilingual/c9f4ef84da7d855637e201101992f6ed/all?languages=fr,es,en&filter=basic&value=0";

4dccd787f415f428fe752186e594ae50e8dea604
Evian <icdk5b4zf@relay.firefox.com>
2023-11-12 10:48:28 +0100

    fix migrations

--- Api/migrations/Version20231111150300.php
+++ Api/migrations/Version20231111150300.php
@@ -21,13 +21,11 @@ final class Version20231111150300 extends AbstractMigration
     {
         // this up() migration is auto-generated, please modify it to your needs
         $this->addSql('ALTER TABLE equipment_mechanic ADD number_of_exploration_steps INT DEFAULT 0');
-        $this->addSql('ALTER TABLE exploration DROP cycle_started_at');
     }
 
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
         $this->addSql('ALTER TABLE equipment_mechanic DROP number_of_exploration_steps');
-        $this->addSql('ALTER TABLE exploration ADD cycle_started_at TIMESTAMP(0) WITHOUT TIME ZONE DEFAULT NULL');
     }
 }

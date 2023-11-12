b8168dc37a43e45be65c1e0354b889ec4895f906
Evian <icdk5b4zf@relay.firefox.com>
2023-11-12 10:14:44 +0000

    fix: Fix a crash while displaying space battle if patrol ship does not have armor (why does this happen?)
    (cherry picked from commit 3bcb3f3eeb3bf46b3604e9244898dde973e31ceb)

--- App/src/entities/SpaceBattlePatrolShip.ts
+++ App/src/entities/SpaceBattlePatrolShip.ts
@@ -2,11 +2,12 @@ export class SpaceBattlePatrolShip {
     public id!: number;
     public key!: string;
     public name!: string;
-    public armor!: integer;
+    public armor: integer|null;
     public charges: integer|null;
     public pilot: string|null;
 
-    public constructor() {
+    public constructor() {  
+        this.armor = null;
         this.charges = null;
         this.pilot = null;
     }

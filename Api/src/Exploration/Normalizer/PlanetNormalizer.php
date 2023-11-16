64e8bb378a1a073a9c0fe797f629d9efa91bb048
Evian <icdk5b4zf@relay.firefox.com>
2023-11-16 22:43:48 +0100

    fix: Do not leak planet sectors in player response for hackers

--- Api/src/Exploration/Normalizer/PlanetNormalizer.php
+++ Api/src/Exploration/Normalizer/PlanetNormalizer.php
@@ -5,9 +5,11 @@ declare(strict_types=1);
 namespace Mush\Exploration\Normalizer;
 
 use Mush\Action\Enum\ActionScopeEnum;
+use Mush\Equipment\Enum\EquipmentEnum;
 use Mush\Equipment\Service\GearToolServiceInterface;
 use Mush\Exploration\Entity\Planet;
 use Mush\Game\Service\TranslationServiceInterface;
+use Mush\Player\Entity\Player;
 use Symfony\Component\Serializer\Normalizer\NormalizerAwareInterface;
 use Symfony\Component\Serializer\Normalizer\NormalizerAwareTrait;
 use Symfony\Component\Serializer\Normalizer\NormalizerInterface;
@@ -33,10 +35,27 @@ final class PlanetNormalizer implements NormalizerInterface, NormalizerAwareInte
     }
 
     public function normalize(mixed $object, string $format = null, array $context = []): array
-    {
+    {   
+        /** @var Player $currentPlayer */
+        $currentPlayer = $context['currentPlayer'];
+
         /** @var Planet $planet */
         $planet = $object;
         $daedalus = $planet->getDaedalus();
+        
+        // Do not leak planet sections if player is not focused on astro terminal or in exploration (TODO)
+        // as the planet is also normalized to display it in Phaser scene
+        if ($currentPlayer->getFocusedTerminal()?->getName() !== EquipmentEnum::ASTRO_TERMINAL) {
+            return [
+                'id' => $planet->getId(),
+                'name' => $this->translationService->translate(
+                    key: 'planet_name',
+                    parameters: $planet->getName()->toArray(),
+                    domain: 'planet',
+                    language: $daedalus->getLanguage()
+                ),
+            ];
+        }
 
         return [
             'id' => $planet->getId(),

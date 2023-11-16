9b89f386b10b75b5f15c47541b8371ebe13468b2
Evian <icdk5b4zf@relay.firefox.com>
2023-11-16 23:54:11 +0100

    test: Add tests on day spawn of advanced hunters

--- Api/tests/functional/Hunter/Event/HunterPoolEventCest.php
+++ Api/tests/functional/Hunter/Event/HunterPoolEventCest.php
@@ -1,15 +1,16 @@
 <?php
 
-namespace Mush\Tests\functional\Hunter\Listener;
+namespace Mush\Tests\functional\Hunter\Event;
 
 use Mush\Alert\Entity\Alert;
 use Mush\Alert\Enum\AlertEnum;
 use Mush\Game\Service\EventServiceInterface;
+use Mush\Hunter\Enum\HunterEnum;
 use Mush\Hunter\Event\HunterPoolEvent;
 use Mush\Tests\AbstractFunctionalTest;
 use Mush\Tests\FunctionalTester;
 
-class HunterSubscriberCest extends AbstractFunctionalTest
+class HunterPoolEventCest extends AbstractFunctionalTest
 {
     private EventServiceInterface $eventService;
 
@@ -19,7 +20,7 @@ class HunterSubscriberCest extends AbstractFunctionalTest
         $this->eventService = $I->grabService(EventServiceInterface::class);
     }
 
-    public function testUnpoolHuntersEvent(FunctionalTester $I)
+    public function testUnpoolHuntersEvent(FunctionalTester $I): void
     {
         $unpoolEvent = new HunterPoolEvent($this->daedalus, ['test'], new \DateTime());
         $this->eventService->callEvent($unpoolEvent, HunterPoolEvent::UNPOOL_HUNTERS);
@@ -28,7 +29,7 @@ class HunterSubscriberCest extends AbstractFunctionalTest
         $I->assertCount(0, $this->daedalus->getHunterPool());
     }
 
-    public function testUnpoolHuntersEventWithExistingWave(FunctionalTester $I)
+    public function testUnpoolHuntersEventWithExistingWave(FunctionalTester $I): void
     {
         $this->daedalus->setHunterPoints(40); // should be enough to unpool 4 hunters
         $unpoolEvent1 = new HunterPoolEvent($this->daedalus, ['test'], new \DateTime());
@@ -48,5 +49,47 @@ class HunterSubscriberCest extends AbstractFunctionalTest
 
         // then we should have a hunter alert
         $I->seeInRepository(Alert::class, ['daedalus' => $this->daedalus, 'name' => AlertEnum::HUNTER]);
+    }
+
+    public function testUnpoolHuntersD1000DoesNotAppearBeforeDayTen(FunctionalTester $I): void
+    {
+        // given D1000s are the only hunters available
+        $gameConfig = $this->daedalus->getGameConfig();
+        $gameConfig->setHunterConfigs(
+            $gameConfig->getHunterConfigs()->filter(fn ($hunterConfig) => $hunterConfig->getHunterName() === HunterEnum::DICE)
+        );
+
+        // given Daedalus is Day 9
+        $this->daedalus->setDay(9);
+        // given Daedalus has enough points to spawn D1000s
+        $this->daedalus->setHunterPoints(100);
+
+        // when we spawn hunters
+        $unpoolEvent = new HunterPoolEvent($this->daedalus, ['test'], new \DateTime());
+        $this->eventService->callEvent($unpoolEvent, HunterPoolEvent::UNPOOL_HUNTERS);
+
+        // then we should not have any attacking D1000s
+        $I->assertEmpty($this->daedalus->getAttackingHunters());
+    }
+
+    public function testUnpoolHuntersD1000AppearAfterDayTen(FunctionalTester $I): void
+    {
+        // given D1000s are the only hunters available
+        $gameConfig = $this->daedalus->getGameConfig();
+        $gameConfig->setHunterConfigs(
+            $gameConfig->getHunterConfigs()->filter(fn ($hunterConfig) => $hunterConfig->getHunterName() === HunterEnum::DICE)
+        );
+
+        // given Daedalus is Day 10
+        $this->daedalus->setDay(10);
+        // given Daedalus has enough points to spawn D1000s
+        $this->daedalus->setHunterPoints(100);
+
+        // when we spawn hunters
+        $unpoolEvent = new HunterPoolEvent($this->daedalus, ['test'], new \DateTime());
+        $this->eventService->callEvent($unpoolEvent, HunterPoolEvent::UNPOOL_HUNTERS);
+
+        // then we should have attacking D1000s
+        $I->assertNotEmpty($this->daedalus->getAttackingHunters());
     }
 }

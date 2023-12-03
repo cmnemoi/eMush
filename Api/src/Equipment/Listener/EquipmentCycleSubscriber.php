c02b4225d563c05650e7740c1a9ea9c7267ae4b8
Sylvain Breton <sylvain.breton.6@gmail.com>
2023-12-03 18:33:16 +0100

    fix: effect of equipment that trigger on day change are displayed before cycle ones

--- Api/src/Equipment/Listener/EquipmentCycleSubscriber.php
+++ Api/src/Equipment/Listener/EquipmentCycleSubscriber.php
@@ -33,11 +33,12 @@ class EquipmentCycleSubscriber implements EventSubscriberInterface
         foreach ($equipment->getEquipment()->getMechanics() as $mechanics) {
             foreach ($mechanics->getMechanics() as $mechanicName) {
                 if ($cycleHandler = $this->equipmentCycleHandler->getEquipmentCycleHandler($mechanicName)) {
-                    $cycleHandler->handleNewCycle($equipment, $event->getTime());
-
+                    // first let apply day changes
                     if ($event->hasTag(EventEnum::NEW_DAY)) {
                         $cycleHandler->handleNewDay($equipment, $event->getTime());
                     }
+
+                    $cycleHandler->handleNewCycle($equipment, $event->getTime());
                 }
             }
         }

3b5070ed6b046e59ac373f5fa5c2518c193ff6f8
Evian <icdk5b4zf@relay.firefox.com>
2023-12-23 19:50:49 +0100

    test: add a test to exhibit a bug when there is no player to bite

---
+++ Api/tests/functional/Disease/SymptomHandler/BitingCest.php
@@ -0,0 +1,49 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Mush\Tests\functional\Disease\SymptomHandler;
+
+use Mush\Disease\SymptomHandler\Biting;
+use Mush\Modifier\Enum\ModifierPriorityEnum;
+use Mush\Place\Enum\RoomEnum;
+use Mush\RoomLog\Entity\RoomLog;
+use Mush\Tests\AbstractFunctionalTest;
+use Mush\Tests\FunctionalTester;
+
+final class BitingCest extends AbstractFunctionalTest
+{
+    private Biting $bitingSymptom;
+
+    public function _before(FunctionalTester $I)
+    {
+        parent::_before($I);
+        $this->bitingSymptom = $I->grabService(Biting::class);
+    }
+
+    public function testBitingDoNotThrowExceptionIfPlayerIsAlone(FunctionalTester $I)
+    {
+        // given I have only one player in laboratory
+        $this->player2->changePlace($this->daedalus->getPlaceByName(RoomEnum::SPACE));
+        $I->assertEquals(1, $this->daedalus->getPlaceByName(RoomEnum::LABORATORY)->getNumberOfPlayersAlive());
+
+        // when I apply biting symptom to player1
+        $this->bitingSymptom->applyEffects(
+            player: $this->player1,
+            priority: ModifierPriorityEnum::getPriorityAsInteger(ModifierPriorityEnum::INITIAL_EVENT),
+            tags: ['test'],
+            time: new \DateTime()
+        );
+
+        // then no exception is thrown
+        $I->expect('no exception is thrown');
+
+        // then no biting log is created
+        $I->dontSeeInRepository(RoomLog::class, [
+            'place' => $this->daedalus->getPlaceByName(RoomEnum::LABORATORY),
+            'log' => 'biting',
+        ]);
+    }
+
+    pu
+}

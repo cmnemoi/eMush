48988329bc26b4295535060fe043fc34ecd941b0
Evian <icdk5b4zf@relay.firefox.com>
2023-12-24 00:26:20 +0100

    test: Add tests to specify how we want hunters to be normalized

---
+++ Api/tests/functional/Hunter/Service/HunterNormalizerHelperCest.php
@@ -0,0 +1,139 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Mush\Tests\functional\Hunter\Service;
+
+use Mush\Daedalus\Entity\Daedalus;
+use Mush\Hunter\Entity\Hunter;
+use Mush\Hunter\Entity\HunterConfig;
+use Mush\Hunter\Enum\HunterEnum;
+use Mush\Hunter\Service\HunterNormalizerHelperInterface;
+use Mush\Tests\AbstractFunctionalTest;
+use Mush\Tests\FunctionalTester;
+
+final class HunterNormalizerHelperCest extends AbstractFunctionalTest
+{
+    private HunterNormalizerHelperInterface $hunterNormalizerHelper;
+
+    public function _before(FunctionalTester $I): void
+    {
+        parent::_before($I);
+        $this->hunterNormalizerHelper = $I->grabService(HunterNormalizerHelperInterface::class);
+    }
+
+    public function testGetHuntersToNormalizeWithSeventeenHuntersReturnSeventeenHunters(FunctionalTester $I): void
+    {
+        // given 17 simple hunters are attacking
+        for ($i = 0; $i < 17; ++$i) {
+            $hunter = $this->createHunterFromName($this->daedalus, HunterEnum::HUNTER);
+            $I->haveInRepository($hunter);
+        }
+
+        // when I call getHuntersToNormalize
+        $hunters = $this->hunterNormalizerHelper->getHuntersToNormalize($this->daedalus);
+
+        // then I get 17 hunters
+        $I->assertCount(17, $hunters->getAllHuntersByType(HunterEnum::HUNTER));
+    }
+
+    public function testGetHuntersToNormalizeWithEighteenHunterReturnSeventeenHunters(FunctionalTester $I): void
+    {
+        // given 17 simple hunters are attacking
+        for ($i = 0; $i < 18; ++$i) {
+            $hunter = $this->createHunterFromName($this->daedalus, HunterEnum::HUNTER);
+            $I->haveInRepository($hunter);
+        }
+
+        // when I call getHuntersToNormalize
+        $hunters = $this->hunterNormalizerHelper->getHuntersToNormalize($this->daedalus);
+
+        // then I get 17 hunters
+        $I->assertCount(17, $hunters->getAllHuntersByType(HunterEnum::HUNTER));
+    }
+
+    public function testGetHuntersToNormalizeWithSeventeenHuntersAndOneTypeOfEachAdvancedHunterReturnsThirteenHuntersAndOneAdvancedHunterByType(FunctionalTester $I): void
+    {
+        // given 17 simple hunters are attacking
+        for ($i = 0; $i < 17; ++$i) {
+            $hunter = $this->createHunterFromName($this->daedalus, HunterEnum::HUNTER);
+            $I->haveInRepository($hunter);
+        }
+
+        // given one type of each advanced hunter is attacking
+        foreach (HunterEnum::getAdvancedHunters() as $advancedHunterName) {
+            $hunter = $this->createHunterFromName($this->daedalus, $advancedHunterName);
+            $I->haveInRepository($hunter);
+        }
+
+        // when I call getHuntersToNormalize
+        $hunters = $this->hunterNormalizerHelper->getHuntersToNormalize($this->daedalus);
+
+        // then I get 13 hunters
+        $I->assertCount(13, $hunters->getAllHuntersByType(HunterEnum::HUNTER));
+
+        // then I get 1 asteroid
+        $I->assertCount(1, $hunters->getAllHuntersByType(HunterEnum::ASTEROID));
+
+        // then I get 1 spider
+        $I->assertCount(1, $hunters->getAllHuntersByType(HunterEnum::SPIDER));
+
+        // then I get 1 trax
+        $I->assertCount(1, $hunters->getAllHuntersByType(HunterEnum::TRAX));
+
+        // then I get 1 dice
+        $I->assertCount(1, $hunters->getAllHuntersByType(HunterEnum::DICE));
+    }
+
+    public function testGetHuntersToNormalizeWithFiveHuntersAndTwoTypeOfEachAdvancedHunterReturnsFiveHuntersAndTwoAdvancedHunterByType(FunctionalTester $I): void
+    {
+        // given 5 simple hunters are attacking
+        for ($i = 0; $i < 5; ++$i) {
+            $hunter = $this->createHunterFromName($this->daedalus, HunterEnum::HUNTER);
+            $I->haveInRepository($hunter);
+        }
+
+        $I->assertCount(5, $this->daedalus->getAttackingHunters());
+
+        // given two type of each advanced hunter is attacking
+        foreach (HunterEnum::getAdvancedHunters() as $advancedHunterName) {
+            for ($i = 0; $i < 2; ++$i) {
+                $hunter = $this->createHunterFromName($this->daedalus, $advancedHunterName);
+                $I->haveInRepository($hunter);
+            }
+        }
+
+        // when I call getHuntersToNormalize
+        $hunters = $this->hunterNormalizerHelper->getHuntersToNormalize($this->daedalus);
+
+        // then I get 5 hunters
+        $I->assertCount(5, $hunters->getAllHuntersByType(HunterEnum::HUNTER));
+
+        // then I get 2 asteroids
+        $I->assertCount(2, $hunters->getAllHuntersByType(HunterEnum::ASTEROID));
+
+        // then I get 2 spiders
+        $I->assertCount(2, $hunters->getAllHuntersByType(HunterEnum::SPIDER));
+
+        // then I get 2 trax
+        $I->assertCount(2, $hunters->getAllHuntersByType(HunterEnum::TRAX));
+
+        // then I get 2 dices
+        $I->assertCount(2, $hunters->getAllHuntersByType(HunterEnum::DICE));
+    }
+
+    private function createHunterFromName(Daedalus $daedalus, string $hunterName): Hunter
+    {
+        /** @var HunterConfig $hunterConfig */
+        $hunterConfig = $daedalus->getGameConfig()->getHunterConfigs()->getHunter($hunterName);
+        if (!$hunterConfig) {
+            throw new \Exception("Hunter config not found for hunter name $hunterName");
+        }
+
+        $hunter = new Hunter($hunterConfig, $daedalus);
+        $hunter->setHunterVariables($hunterConfig);
+        $daedalus->addHunter($hunter);
+
+        return $hunter;
+    }
+}

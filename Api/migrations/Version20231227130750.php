b9d4e9a7c8e94b448952cd6b351dead9f30bb492
Evian <icdk5b4zf@relay.firefox.com>
2023-12-27 14:08:11 +0100

    Fix migrations to update database

--- Api/migrations/Version20231227130750.php
+++ Api/migrations/Version20231227130750.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20231227092810 extends AbstractMigration
+final class Version20231227130750 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -20,11 +20,14 @@ final class Version20231227092810 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE news ADD is_pinned BOOLEAN NOT NULL');
+        $this->addSql('ALTER TABLE news ADD publication_date TIMESTAMP(0) WITHOUT TIME ZONE DEFAULT NULL');
     }
 
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
-        $this->addSql('CREATE SCHEMA public');
+        $this->addSql('ALTER TABLE news DROP is_pinned');
+        $this->addSql('ALTER TABLE news DROP publication_date');
     }
 }

02ec74ce6a9b71a811d5912363201e9108bbc26c
Evian <icdk5b4zf@relay.firefox.com>
2023-12-29 22:07:20 +0100

    style: apply linter fixes

--- Api/src/MetaGame/Service/JwtDecorator.php
+++ Api/src/MetaGame/Service/JwtDecorator.php
@@ -4,24 +4,29 @@ declare(strict_types=1);
 
 namespace Mush\MetaGame\Service;
 
-use ApiPlatform\Core\OpenApi\OpenApi;
 use ApiPlatform\OpenApi\Factory\OpenApiFactoryInterface;
+use ApiPlatform\OpenApi\OpenApi;
 
 class JwtDecorator implements OpenApiFactoryInterface
 {
     public function __construct(
         private OpenApiFactoryInterface $decorated
-    ) {}
+    ) {
+    }
 
     public function __invoke(array $context = []): OpenApi
     {
         $openApi = ($this->decorated)($context);
 
         $schemas = $openApi->getComponents()->getSecuritySchemes();
+        if (!$schemas) {
+            throw new \RuntimeException('No security schemes defined');
+        }
+
         $schemas['Bearer'] = new \ArrayObject([
             'type' => 'http',
             'scheme' => 'bearer',
-            'bearerFormat' => 'JWT'
+            'bearerFormat' => 'JWT',
         ]);
 
         return $openApi;

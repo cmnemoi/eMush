c138c2282021c3ea564a5581e8b91fffd584595b
Evian <icdk5b4zf@relay.firefox.com>
2024-01-08 01:12:23 +0100

    refactor: put all logic to finish an exploration in an entity method

--- Api/src/Exploration/Entity/ClosedExploration.php
+++ Api/src/Exploration/Entity/ClosedExploration.php
@@ -106,6 +106,15 @@ class ClosedExploration
 
     public function finishExploration(): void
     {
+        if (!$this->exploration) {
+            throw new \LogicException('Exploration should not be null to be finished');
+        }
+
+        foreach ($this->exploration->getExplorators() as $explorator) {
+            $explorator->setExploration(null);
+        }
+        $this->exploration->getPlanet()->setExploration(null);
+        $this->exploration->getDaedalus()->setExploration(null);
         $this->exploration = null;
         $this->isExplorationFinished = true;
     }

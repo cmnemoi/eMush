3be4de3a98fed629a06985d72b26452bf4744785
Charles-Meldhine Madi Mnemoi <icdk5b4zf@relay.firefox.com>
2024-01-14 01:32:07 +0000

    test: add a test for the fixed bug

---
+++ Api/tests/functional/Disease/Service/PlayerDiseaseServiceCest.php
@@ -0,0 +1,63 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Mush\Tests\functional\Disease\Service;
+
+use Mush\Disease\Enum\DiseaseEnum;
+use Mush\Disease\Service\PlayerDiseaseServiceInterface;
+use Mush\Game\Enum\VisibilityEnum;
+use Mush\RoomLog\Entity\RoomLog;
+use Mush\RoomLog\Enum\LogEnum;
+use Mush\Status\Enum\PlayerStatusEnum;
+use Mush\Status\Service\StatusServiceInterface;
+use Mush\Tests\AbstractFunctionalTest;
+use Mush\Tests\FunctionalTester;
+
+final class PlayerDiseaseServiceCest extends AbstractFunctionalTest
+{
+    private PlayerDiseaseServiceInterface $playerDiseaseService;
+    private StatusServiceInterface $statusService;
+
+    public function _before(FunctionalTester $I): void
+    {
+        parent::_before($I);
+        $this->playerDiseaseService = $I->grabService(PlayerDiseaseServiceInterface::class);
+        $this->statusService = $I->grabService(StatusServiceInterface::class);
+    }
+
+    public function testIncubatingDiseaseHealsSilentlyWhenPlayerTurnsOutMush(FunctionalTester $I): void
+    {
+        // given player has an incubating disease
+        $disease = $this->playerDiseaseService->createDiseaseFromName(
+            diseaseName: DiseaseEnum::COLD,
+            player: $this->player,
+            reasons: [],
+            delayMin: 1,
+        );
+
+        // given player turns out mush
+        $this->statusService->createStatusFromName(
+            statusName: PlayerStatusEnum::MUSH,
+            holder: $this->player,
+            tags: [],
+            time: new \DateTime(),
+        );
+
+        // when I call handleNewCycle on the disease
+        $this->playerDiseaseService->handleNewCycle($disease, new \DateTime());
+
+        // then the disease should heal silently
+        $I->assertNull($this->player->getMedicalConditionByName(DiseaseEnum::COLD));
+
+        $I->grabEntityFromRepository(
+            RoomLog::class,
+            [
+                'place' => $this->player->getPlace()->getLogName(),
+                'playerInfo' => $this->player->getPlayerInfo(),
+                'log' => LogEnum::DISEASE_CURED,
+                'visibility' => VisibilityEnum::HIDDEN,
+            ]
+        );
+    }
+}

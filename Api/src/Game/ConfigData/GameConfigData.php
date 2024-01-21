cd48cd79d5d1c8dd62d5459f6b794a3d9a660d82
Charles-Meldhine Madi Mnemoi <icdk5b4zf@relay.firefox.com>
2024-01-21 14:14:36 +0100

    hotfix: add a missing status config

--- Api/src/Game/ConfigData/GameConfigData.php
+++ Api/src/Game/ConfigData/GameConfigData.php
@@ -411,6 +411,7 @@ class GameConfigData
                 'in_orbit_default',
                 'exploration_oxygen_default',
                 'exploration_fuel_default',
+                'following_hunters_default'
             ],
             'triumphConfigs' => [
                 'alien_science',

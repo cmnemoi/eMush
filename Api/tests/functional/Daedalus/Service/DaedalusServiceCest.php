93dd9567f1216da77a3f24734a6af1ef847777b0
Evian <icdk5b4zf@relay.firefox.com>
2024-01-21 23:23:45 +0100

    test: Add a test which specify that an exploration should be closed at Daedalus end

--- Api/tests/functional/Daedalus/Service/DaedalusServiceCest.php
+++ Api/tests/functional/Daedalus/Service/DaedalusServiceCest.php
@@ -2,29 +2,53 @@
 
 namespace Mush\Tests\functional\Daedalus\Service;
 
+use Doctrine\Common\Collections\ArrayCollection;
+use Mush\Daedalus\Event\DaedalusEvent;
 use Mush\Daedalus\Service\DaedalusService;
+use Mush\Equipment\Enum\EquipmentEnum;
+use Mush\Equipment\Service\GameEquipmentServiceInterface;
+use Mush\Exploration\Entity\Exploration;
+use Mush\Exploration\Entity\Planet;
+use Mush\Exploration\Entity\PlanetName;
+use Mush\Exploration\Entity\PlanetSector;
+use Mush\Exploration\Entity\PlanetSectorConfig;
+use Mush\Exploration\Enum\PlanetSectorEnum;
+use Mush\Exploration\Service\ExplorationServiceInterface;
 use Mush\Game\Enum\CharacterEnum;
 use Mush\Game\Enum\TitleEnum;
+use Mush\Game\Enum\VisibilityEnum;
 use Mush\Game\Service\EventServiceInterface;
 use Mush\Hunter\Entity\Hunter;
 use Mush\Hunter\Entity\HunterTarget;
 use Mush\Hunter\Event\HunterPoolEvent;
+use Mush\Place\Enum\RoomEnum;
+use Mush\Player\Entity\Collection\PlayerCollection;
 use Mush\Player\Entity\Player;
+use Mush\RoomLog\Entity\RoomLog;
+use Mush\RoomLog\Enum\LogEnum;
+use Mush\Status\Enum\DaedalusStatusEnum;
 use Mush\Status\Enum\PlayerStatusEnum;
+use Mush\Status\Service\StatusServiceInterface;
 use Mush\Tests\AbstractFunctionalTest;
 use Mush\Tests\FunctionalTester;
 
 class DaedalusServiceCest extends AbstractFunctionalTest
 {
     private DaedalusService $daedalusService;
+    private ExplorationServiceInterface $explorationService;
     private EventServiceInterface $eventService;
+    private GameEquipmentServiceInterface $gameEquipmentService;
+    private StatusServiceInterface $statusService;
 
     public function _before(FunctionalTester $I)
     {
         parent::_before($I);
 
         $this->daedalusService = $I->grabService(DaedalusService::class);
+        $this->explorationService = $I->grabService(ExplorationServiceInterface::class);
         $this->eventService = $I->grabService(EventServiceInterface::class);
+        $this->gameEquipmentService = $I->grabService(GameEquipmentServiceInterface::class);
+        $this->statusService = $I->grabService(StatusServiceInterface::class);
     }
 
     public function testFindAllFinishedDaedaluses(FunctionalTester $I)
@@ -136,5 +160,102 @@ class DaedalusServiceCest extends AbstractFunctionalTest
         // then hunter targets are deleted
         $I->dontSeeInRepository(HunterTarget::class);
         $I->dontSeeInRepository(Hunter::class);
+    }
+
+    public function testExplorationIsClosedWhenDaedalusIsEnded(FunctionalTester $I)
+    {
+        // given there is an exploration ongoing
+        $exploration = $this->createExploration($I);
+        $closedExploration = $exploration->getClosedExploration();
+
+        // when daedalus is ended
+        $endDaedalusEvent = new DaedalusEvent(
+            $this->daedalus,
+            ['super_nova'],
+            new \DateTime()
+        );
+        $this->eventService->callEvent($endDaedalusEvent, DaedalusEvent::FINISH_DAEDALUS);
+
+        // then I should not see any ongoing exploration
+        $I->dontSeeInRepository(Exploration::class);
+
+        // then exploration should be archived
+        $I->assertTrue($closedExploration->isExplorationFinished());
+
+        // then I should see a log explaining that all explorators are dead
+        $I->seeInRepository(
+            entity: RoomLog::class,
+            params: [
+                'place' => $this->player->getPlace()->getLogName(),
+                'playerInfo' => $this->player->getPlayerInfo(),
+                'visibility' => VisibilityEnum::PRIVATE,
+                'log' => LogEnum::ALL_EXPLORATORS_DEAD,
+            ]
+        );
+    }
+
+    private function createExploration(FunctionalTester $I): Exploration
+    {
+        // given there is Icarus Bay on this Daedalus
+        $icarusBay = $this->createExtraPlace(RoomEnum::ICARUS_BAY, $I, $this->daedalus);
+
+        // given player is in Icarus Bay
+        $this->player->changePlace($icarusBay);
+
+        // given there is the Icarus ship in Icarus Bay
+        /** @var EquipmentConfig $icarusConfig */
+        $icarus = $this->gameEquipmentService->createGameEquipmentFromName(
+            equipmentName: EquipmentEnum::ICARUS,
+            equipmentHolder: $icarusBay,
+            reasons: [],
+            time: new \DateTime(),
+        );
+
+        // given a planet with oxygen is found
+        $planetName = new PlanetName();
+        $planetName->setFirstSyllable(1);
+        $planetName->setFourthSyllable(1);
+        $I->haveInRepository($planetName);
+
+        $planet = new Planet($this->player);
+        $planet
+            ->setName($planetName)
+            ->setSize(3)
+        ;
+        $I->haveInRepository($planet);
+
+        $desertSectorConfig = $I->grabEntityFromRepository(PlanetSectorConfig::class, ['name' => PlanetSectorEnum::DESERT . '_default']);
+        $desertSector = new PlanetSector($desertSectorConfig, $planet);
+        $I->haveInRepository($desertSector);
+
+        $sismicSectorConfig = $I->grabEntityFromRepository(PlanetSectorConfig::class, ['name' => PlanetSectorEnum::SISMIC_ACTIVITY . '_default']);
+        $sismicSector = new PlanetSector($sismicSectorConfig, $planet);
+        $I->haveInRepository($sismicSector);
+
+        $oxygenSectorConfig = $I->grabEntityFromRepository(PlanetSectorConfig::class, ['name' => PlanetSectorEnum::OXYGEN . '_default']);
+        $oxygenSector = new PlanetSector($oxygenSectorConfig, $planet);
+        $I->haveInRepository($oxygenSector);
+
+        $hydroCarbonSectorConfig = $I->grabEntityFromRepository(PlanetSectorConfig::class, ['name' => PlanetSectorEnum::HYDROCARBON . '_default']);
+        $hydroCarbonSector = new PlanetSector($hydroCarbonSectorConfig, $planet);
+        $I->haveInRepository($hydroCarbonSector);
+
+        $planet->setSectors(new ArrayCollection([$desertSector, $sismicSector, $oxygenSector, $hydroCarbonSector]));
+
+        // given the Daedalus is in orbit around the planet
+        $this->statusService->createStatusFromName(
+            statusName: DaedalusStatusEnum::IN_ORBIT,
+            holder: $this->daedalus,
+            tags: [],
+            time: new \DateTime(),
+        );
+
+        // given there is an exploration with an explorator
+        return $this->explorationService->createExploration(
+            players: new PlayerCollection([$this->player]),
+            explorationShip: $icarus,
+            numberOfSectorsToVisit: 2,
+            reasons: ['test'],
+        );
     }
 }

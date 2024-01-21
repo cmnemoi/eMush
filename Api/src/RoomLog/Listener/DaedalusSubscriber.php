1cac5ac3a3f78f6f3f5b87233acfaa839b150df6
Charles-Meldhine Madi Mnemoi <icdk5b4zf@relay.firefox.com>
2024-01-21 16:03:01 +0100

    Hide exploration logs when players leave planet orbit

--- Api/src/RoomLog/Listener/DaedalusSubscriber.php
+++ Api/src/RoomLog/Listener/DaedalusSubscriber.php
@@ -2,9 +2,12 @@
 
 namespace Mush\RoomLog\Listener;
 
+use Mush\Action\Enum\ActionEnum;
+use Mush\Daedalus\Entity\Daedalus;
 use Mush\Daedalus\Event\DaedalusEvent;
 use Mush\Game\Enum\VisibilityEnum;
 use Mush\Place\Enum\RoomEnum;
+use Mush\RoomLog\Entity\RoomLog;
 use Mush\RoomLog\Enum\LogEnum;
 use Mush\RoomLog\Service\RoomLogServiceInterface;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
@@ -23,6 +26,7 @@ class DaedalusSubscriber implements EventSubscriberInterface
     {
         return [
             DaedalusEvent::START_DAEDALUS => 'onDaedalusStart',
+            DaedalusEvent::TRAVEL_LAUNCHED => 'onTravelLaunched',
         ];
     }
 
@@ -34,5 +38,20 @@ class DaedalusSubscriber implements EventSubscriberInterface
         if ($garden) {
             $this->roomLogService->createLog(LogEnum::GARDEN_DESTROYED, $garden, VisibilityEnum::PUBLIC, 'event_log');
         }
+    }
+
+    public function onTravelLaunched(DaedalusEvent $event): void
+    {
+        if ($event->hasTag(ActionEnum::LEAVE_ORBIT)) {
+            $this->hideOldPlanetExplorationLogs($event->getDaedalus());
+        }
+    }
+
+    private function hideOldPlanetExplorationLogs(Daedalus $daedalus): void
+    {
+        $planetLogs = $this->roomLogService->findAllByDaedalusAndPlace($daedalus, $daedalus->getPlanetPlace());
+
+        $planetLogs->map(fn (RoomLog $log) => $log->setVisibility(VisibilityEnum::HIDDEN));
+        $planetLogs->map(fn (RoomLog $log) => $this->roomLogService->persist($log));
     }
 }

8dfd97cb1d86faaf152258f90c9916982cf75535
Charles-Meldhine Madi Mnemoi <icdk5b4zf@relay.firefox.com>
2024-01-26 23:27:19 +0100

    progress: you can't change neron cpu priority if you are not focused on BIOS terminal

--- Api/src/Action/Actions/ChangeNeronCpuPriority.php
+++ Api/src/Action/Actions/ChangeNeronCpuPriority.php
@@ -7,8 +7,14 @@ namespace Mush\Action\Actions;
 use Mush\Action\Entity\ActionResult\ActionResult;
 use Mush\Action\Entity\ActionResult\Success;
 use Mush\Action\Enum\ActionEnum;
+use Mush\Action\Validator\HasStatus;
+use Mush\Action\Validator\Reach;
 use Mush\Equipment\Entity\GameEquipment;
+use Mush\Equipment\Enum\EquipmentEnum;
+use Mush\Equipment\Enum\ReachEnum;
 use Mush\RoomLog\Entity\LogParameterInterface;
+use Mush\Status\Enum\PlayerStatusEnum;
+use Symfony\Component\Validator\Mapping\ClassMetadata;
 
 final class ChangeNeronCpuPriority extends AbstractAction
 {
@@ -17,6 +23,19 @@ final class ChangeNeronCpuPriority extends AbstractAction
     protected function support(?LogParameterInterface $target, array $parameters): bool
     {
         return $target instanceof GameEquipment;
+    }
+
+    public static function loadValidatorMetadata(ClassMetadata $metadata): void
+    {
+        $metadata->addConstraint(new Reach(['reach' => ReachEnum::ROOM, 'groups' => ['visibility']]));
+        $metadata->addConstraint(
+            new HasStatus([
+                'status' => PlayerStatusEnum::FOCUSED,
+                'target' => HasStatus::PLAYER,
+                'statusTargetName' => EquipmentEnum::BIOS_TERMINAL,
+                'groups' => ['visibility'],
+            ])
+        );
     }
 
     protected function checkResult(): ActionResult

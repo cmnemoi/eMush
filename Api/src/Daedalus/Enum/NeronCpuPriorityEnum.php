7a7d2e9975093f8517753375c500776c1ea583a2
Charles-Meldhine Madi Mnemoi <icdk5b4zf@relay.firefox.com>
2024-01-26 23:19:38 +0100

    Add CPU Priority to NERON entity fields

---
+++ Api/src/Daedalus/Enum/NeronCpuPriorityEnum.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Mush\Daedalus\Enum;
+
+/** @codeCoverageIgnore */
+final class NeronCpuPriorityEnum
+{
+    public const NONE = 'none';
+    public const ASTRONAVIGATION = 'astronavigation';
+}

69acb74ed109a0933aa5413d5064b348b3108fc3
Charles-Meldhine Madi Mnemoi <icdk5b4zf@relay.firefox.com>
2024-01-26 16:12:01 +0100

    Revert "Translate closed daedalus and players end causes from back end. Update theEnd page to still allow front end translations, because a rework would be too much work for now"
    This reverts commit 47dbf9e9d3a7616e7e051d43fac730fc5ea08572.

--- App/src/components/Ranking/TheEndPage.vue
+++ App/src/components/Ranking/TheEndPage.vue
@@ -35,7 +35,7 @@
                     </p>
                     <p class="death-cause">
                         <img src="@/assets/images/dead.png" alt="Dead">
-                        {{ $t("theEnd.endCause." + goldNovaPlayer.endCause?.key) }}
+                        {{ $t("theEnd.endCause." + goldNovaPlayer.endCause) }}
                     </p>
                     <p class="nova">
                         <img src="@/assets/images/nova/first.png" alt="First"> {{ $t('theEnd.goldSuperNova') }}
@@ -85,7 +85,7 @@
                         </p>
                         <p class="death-cause">
                             <img src="@/assets/images/dead.png" alt="Dead">
-                            {{ $t('theEnd.endCause.' + player.endCause?.key) }}
+                            {{ $t('theEnd.endCause.' + player.endCause) }}
                         </p>
                         <p class="nova" v-if="key === 0">
                             <img src="@/assets/images/nova/second.png" alt="Second"> {{ $t('theEnd.silverSuperNova') }}
@@ -147,7 +147,7 @@
                         </p>
                         <p class="death-cause">
                             <img src="@/assets/images/dead.png" alt="Dead">
-                            {{ $t('theEnd.endCause.' + player.endCause?.key) }}
+                            {{ $t('theEnd.endCause.' + player.endCause) }}
                         </p>
                         <p class="nova">
                             <img src="@/assets/images/nova/sixth.png" alt="sixth"> {{ $t('theEnd.normalSuperNova') }}

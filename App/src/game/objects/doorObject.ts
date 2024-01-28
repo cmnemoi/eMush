ea3e9878659bd90dc52f9bd27d13eb8655856361
SylvainBreton <sylvain.breton.6@gmail.com>
2024-01-28 10:13:57 +0000

    fix: doors requires now 2 clicks to move

--- App/src/game/objects/doorObject.ts
+++ App/src/game/objects/doorObject.ts
@@ -3,17 +3,13 @@ import Vector2 = Phaser.Math.Vector2;
 import DaedalusScene from "@/game/scenes/daedalusScene";
 import { CartesianCoordinates } from "@/game/types";
 import { Door as DoorEntity } from "@/entities/Door";
-import { Action } from "@/entities/Action";
-import store from "@/store";
-import InteractObject from "@/game/objects/interactObject";
 import IsometricGeom from "@/game/scenes/isometricGeom";
+import DoorGroundObject from "@/game/objects/doorGroundObject";
 
 
-export default class DoorObject extends InteractObject {
+export default class DoorObject extends DoorGroundObject {
     private openFrames: Phaser.Types.Animations.AnimationFrame[];
     private closeFrames: Phaser.Types.Animations.AnimationFrame[];
-    public door : DoorEntity;
-    private particles: Phaser.GameObjects.Particles.ParticleEmitter | null = null;
 
     constructor(
         scene: DaedalusScene,
@@ -22,32 +18,18 @@ export default class DoorObject extends InteractObject {
         tileset: Phaser.Tilemaps.Tileset,
         tiledFrame: number,
         isFlipped: { x: boolean, y: boolean},
-        door: DoorEntity
+        door: DoorEntity,
+        group: Phaser.GameObjects.Group | null = null,
     )
     {
-        super(scene, cart_coords, iso_geom, tileset, tiledFrame, door.key, isFlipped, false, false);
+        super(scene, cart_coords, iso_geom, tileset, tiledFrame, isFlipped, door, group);
 
-        this.door = door;
-        this.handleBroken();
-
+        // create animations
         this.openFrames = this.anims.generateFrameNames('door_object', { start: this.tiledFrame, end: this.tiledFrame + 10 });
-
         this.closeFrames = this.anims.generateFrameNames('door_object', { start: this.tiledFrame + 10, end: this.tiledFrame + 23 });
         this.closeFrames[this.closeFrames.length + 1] = this.openFrames[0];
-
-        // doors are always on the bottom (just in front of the back_wall layer)
-        this.setDepth(0);
-
         this.createAnimations();
     }
-
-    updateDoor(door: DoorEntity | null = null) {
-        if (door !== null) {
-            this.door = door;
-        }
-        this.handleBroken();
-    }
-
 
     createAnimations(): void
     {
@@ -64,7 +46,6 @@ export default class DoorObject extends InteractObject {
             frameRate: 10,
             repeat: 0
         });
-
     }
 
     applyTexture(
@@ -83,34 +64,24 @@ export default class DoorObject extends InteractObject {
         this.setInteractive(this.setInteractBox(), Phaser.Geom.Polygon.Contains);
     }
 
-    onDoorClicked(pointer: Phaser.Input.Pointer): void
-    {
-        const objectX = pointer.worldX - (this.x - this.width/2);
-        const objectY = pointer.worldY - (this.y - this.height/2);
+    onClickedOut() {
+        super.onClickedOut();
+        if (this.isOpen()) {
+            this.activateDoor();
+            this.activateOtherPartOfDoor();
+        }
+    }
 
-        if (this.input && this.input.hitArea.contains(objectX, objectY)){
-            if(
-                String(this.frame.name) === String(this.tiledFrame)  &&
-                !this.door.isBroken
-            )
-            {
-                //if player click on the door AND the door is closed
-                this.anims.play('door_open');
-                store.dispatch('room/selectTarget', { target: null });
-            } else if (!this.door.isBroken && this.getMoveAction().canExecute) {
-                //if player click on the door AND the door is open AND player can move
-                const moveAction = this.getMoveAction();
-                store.dispatch('action/executeAction', { target: this.door, action: moveAction });
-                store.dispatch('room/selectTarget', { target: null });
-                store.dispatch('room/closeInventory');
-            } else {
-                //If the door is broken propose the repair action
-                const door = this.door;
-                store.dispatch('room/selectTarget', { target: this.door });
-            }
-        } else if (String(this.frame.name) ===  String(this.tiledFrame + 10))
-        {
-            //if player click outside the door AND the door is open
+    isOpen(): boolean
+    {
+        return String(this.frame.name) ===  String(this.tiledFrame + 10);
+    }
+
+    activateDoor(): void
+    {
+        if (!this.isOpen()) {
+            this.anims.play('door_open');
+        } else {
             this.anims.play('door_close');
         }
     }
@@ -141,17 +112,6 @@ export default class DoorObject extends InteractObject {
         if (this.particles !== null) {
             this.particles.setDepth(this.depth + 1);
         }
-    }
-
-    getMoveAction(): Action
-    {
-        const moveAction = this.door.actions.filter((action: Action) => {return action.key === 'move';});
-
-        if (moveAction.length !==1 ) {
-            throw new Error("this door should have exactly one move action");
-        }
-
-        return moveAction[0];
     }
 
     setInteractBox() : Phaser.Geom.Polygon
@@ -198,11 +158,5 @@ export default class DoorObject extends InteractObject {
         } else {
             super.setSelectedOutline();
         }
-    }
-
-    delete()
-    {
-        this.particles?.destroy();
-        super.delete();
     }
 }

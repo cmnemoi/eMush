65a64c839728cc134b5ddf2d69489c301a79ba49
A7 <atar.inscription@gmail.com>
2024-02-05 00:31:16 +0100

    Toast poc

--- App/src/components/Utils/Toast.vue
+++ App/src/components/Utils/Toast.vue
@@ -1,12 +1,10 @@
 <template>
     <template v-if="isOpen">
         <div class="modal-toast" :class="type">
-            <div class="icon">
-                <img v-if="type == 'warning'" src="@/assets/images/att.png" alt="warning" />
-                <img v-else-if="type == 'error'" src="@/assets/images/neron_eye.gif" alt="error" />
-                <img v-else-if="type == 'success'" src="@/assets/images/att.png" alt="warning" />
-                <img v-else src="@/assets/images/info.png" alt="info" />
-            </div>
+            <img v-if="type == 'warning'" src="@/assets/images/att.png" alt="warning" />
+            <img v-else-if="type == 'error'" src="@/assets/images/neron_eye.gif" alt="error" />
+            <img v-else-if="type == 'success'" src="@/assets/images/att.png" alt="warning" />
+            <img v-else src="@/assets/images/info.png" alt="info" />
             <div class="content">
                 <h1 v-if="title">
                     {{ title }}
@@ -24,8 +22,6 @@
 
 <script lang="ts">
 import { defineComponent } from "vue";
-
-export type TToastStatus = "success" | "warning" | "error" | "info";
 
 export default defineComponent ({
     props: {
@@ -56,12 +52,7 @@ export default defineComponent ({
     transition: all 0.3s;
 }
 
-$info-color: #3965fb;
-$success-color: #16b64b;
-$warning-color: #e7b719;
-$error-color: #e72719;
 .modal-toast {
-
     position: fixed;
     z-index: 999;
     bottom: 24px;
@@ -70,54 +61,29 @@ $error-color: #e72719;
     left: 50%;
     transform: translatex(-50%);
     flex-direction: row;
-    // align-items: center;
-    // column-gap: 1.2em;
-    // padding: 0.9em 1.4em;
+    align-items: center;
+    column-gap: 1.2em;
+    padding: 0.9em 1.4em;
     background-color: rgba(35, 37, 100, .9);
-    border: 2px solid $info-color;
-    border-radius: 5px;
     box-shadow:
-        // inset 0 0 12px 3px $info-color,
-        // inset 0 0 0 2px #3965fb,
-        0 0 22px 8px transparentize(#09092d, 0.2);
-
-    /* &.warning {
-        border-color: $warning-color;
-        box-shadow:
-        inset 0 0 12px 3px $warning-color,
+        inset 0 0 12px 3px #3965fb,
+        inset 0 0 0 2px #3965fb,
         0 0 22px 8px transparentize(#09092d, 0.4);
-    } */
 
-    .icon {
-        align-items: center;
-        justify-content: center;
-        background: transparentize($info-color, 0.4); 
-        padding: 1em;
+        &::after { // angle blue triangles decoration
+        content: "";
+        position: absolute;
+        z-index: -1;
+        left: 0;
+        right: 0;
+        top: 0;
+        bottom: 0;
+        margin: 3px;
+        border: 5px solid transparent;
+        border-image: url('~@/assets/images/ToolTip-corners.gif') 50% round;
+    };
 
-        img { max-width: 36px; }
-    }
-
-    &.success {
-        border-color: $success-color;
-
-        .icon { background: transparentize($success-color, 0.4); }
-    }
-
-    &.warning {
-        border-color: $warning-color;
-
-        .icon { background: transparentize($warning-color, 0.4); }
-    }
-
-    &.error {
-        border-color: $error-color;
-        .icon { background: transparentize($error-color, 0.4); }
-    }
-
-    .content {
-        display: block;
-        margin: 0.5em 1.2em 0.8em;
-    }
+    .content { display: block; }
 }
 
 h1 {

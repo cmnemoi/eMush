15fd0184a4c0bb48aed506099630d0bf5195fc3f
Charles-Meldhine Madi Mnemoi <icdk5b4zf@relay.firefox.com>
2024-02-04 16:29:50 +0100

    style: apply linter fixes

--- App/src/components/Moderation/ModerationPage.vue
+++ App/src/components/Moderation/ModerationPage.vue
@@ -7,13 +7,9 @@
 <script lang="ts">
 
 import { defineComponent } from "vue";
-import ModerationPlayerListPage from "./ModerationPlayerListPage.vue";
 
 export default defineComponent ({
     name: "ModerationPage",
-    components: {
-        ModerationPlayerListPage
-    }
 });
 
 </script>

8bcbd9643bbb4cdff7ea8aad7d229ef17fd296d8
A7 <atar.inscription@gmail.com>
2024-02-09 22:45:18 +0100

    Revert "Add unbreakable space to plant name to avoid overflows"
    This reverts commit 1e0e7441d5e5cbe71fa8ef21b2a256869945d0c3.

--- Api/tests/unit/Equipment/Normalizer/EquipmentNormalizerTest.php
+++ Api/tests/unit/Equipment/Normalizer/EquipmentNormalizerTest.php
@@ -411,7 +411,7 @@ class EquipmentNormalizerTest extends TestCase
             'id' => 1,
             'key' => GamePlantEnum::BANANA_TREE,
             'updatedAt' => $time,
-            'name' => 'Jeune Bananier',
+            'name' => 'Jeune Bananier',
             'description' => 'Un bananier',
             'statuses' => [],
             'actions' => [],

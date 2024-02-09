11b20f46c129f5c12fbfe220ea122589fa66e2e2
Evian <icdk5b4zf@relay.firefox.com>
2024-02-09 20:48:11 +0100

    fix broken test

--- Api/tests/unit/Equipment/Normalizer/EquipmentNormalizerTest.php
+++ Api/tests/unit/Equipment/Normalizer/EquipmentNormalizerTest.php
@@ -387,7 +387,7 @@ class EquipmentNormalizerTest extends TestCase
         $this->translationService
             ->shouldReceive('translate')
             ->with('banana_tree.name', ['age' => 'young'], 'items', LanguageEnum::FRENCH)
-            ->andReturn('Jeune Bananier')
+            ->andReturn('Jeune Bananier')
             ->once()
         ;
 

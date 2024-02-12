684ae72885186e82c925b8fe09fb483cfdcf670f
Evian <icdk5b4zf@relay.firefox.com>
2024-02-12 17:31:24 +0100

    refactor: We now normalize channels and messages in separate normalizers to respect Single Responsability Principle

--- Api/src/Communication/Normalizer/ModerationChannelNormalizer.php
+++ Api/src/Communication/Normalizer/ModerationChannelNormalizer.php
@@ -4,49 +4,33 @@ namespace Mush\Communication\Normalizer;
 
 use Mush\Communication\Entity\Channel;
 use Mush\Communication\Entity\ChannelPlayer;
-use Mush\Communication\Services\MessageServiceInterface;
+use Mush\Game\Enum\LanguageEnum;
 use Mush\Game\Service\TranslationServiceInterface;
-use Mush\Player\Entity\Player;
 use Symfony\Component\Serializer\Normalizer\NormalizerInterface;
 
-class ChannelNormalizer implements NormalizerInterface
+class ModerationChannelNormalizer implements NormalizerInterface
 {
     private TranslationServiceInterface $translationService;
-    private MessageServiceInterface $messageService;
 
     public function __construct(
         TranslationServiceInterface $translationService,
-        MessageServiceInterface $messageService
     ) {
         $this->translationService = $translationService;
-        $this->messageService = $messageService;
     }
 
     public function supportsNormalization($data, string $format = null, array $context = []): bool
     {
-        return $data instanceof Channel && !in_array('moderation_read', $context['groups'] ?? []);
+        return $data instanceof Channel && in_array('moderation_read', $context['groups'] ?? []);
     }
 
     public function normalize($object, string $format = null, array $context = []): array
     {
-        return $this->normalizeForCurrentPlayer($object, $context);
+        return $this->normalizeForModerators($object);
     }
 
-    private function normalizeForCurrentPlayer(Channel $channel, array $context): array
+    private function normalizeForModerators(Channel $channel): array
     {
-        /** @var Player $currentPlayer */
-        $currentPlayer = $context['currentPlayer'];
-
-        $language = $currentPlayer->getDaedalus()->getLanguage();
-
-        if (key_exists('piratedPlayer', $context)) {
-            /** @var Player $piratedPlayer */
-            $piratedPlayer = $context['piratedPlayer'];
-            $piratedPlayerId = $piratedPlayer->getId();
-        } else {
-            $piratedPlayerId = null;
-        }
-
+        $language = LanguageEnum::FRENCH;
         $participants = [];
         /** @var ChannelPlayer $participant */
         foreach ($channel->getParticipants() as $participant) {
@@ -68,11 +52,7 @@ class ChannelNormalizer implements NormalizerInterface
             'id' => $channel->getId(),
             'scope' => $channel->getScope(),
             'name' => $this->translationService->translate($channel->getScope() . '.name', [], 'chat', $language),
-            'description' => $this->translationService->translate($channel->getScope() . '.description', [], 'chat', $language),
             'participants' => $participants,
-            'createdAt' => $channel->getCreatedAt()?->format(\DateTimeInterface::ATOM),
-            'newMessageAllowed' => $this->messageService->canPlayerPostMessage($currentPlayer, $channel),
-            'piratedPlayer' => $piratedPlayerId,
         ];
     }
 }

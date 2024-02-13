f37787b24a30e66b0f2e0fec60209735f6e50e41
Evian <icdk5b4zf@relay.firefox.com>
2024-02-13 19:17:14 +0100

    Refactor how we load event config fixtures to delete duplication

--- Api/src/Game/DataFixtures/EventConfigFixtures.php
+++ Api/src/Game/DataFixtures/EventConfigFixtures.php
@@ -5,11 +5,8 @@ namespace Mush\Game\DataFixtures;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 use Mush\Exploration\Entity\PlanetSectorEventConfig;
-use Mush\Exploration\Event\PlanetSectorEvent;
+use Mush\Game\ConfigData\EventConfigData;
 use Mush\Game\Entity\VariableEventConfig;
-use Mush\Game\Event\VariableEventInterface;
-use Mush\Modifier\Enum\ModifierHolderClassEnum;
-use Mush\Player\Enum\PlayerVariableEnum;
 
 /** @codeCoverageIgnore */
 class EventConfigFixtures extends Fixture
@@ -42,347 +39,34 @@ class EventConfigFixtures extends Fixture
 
     public function load(ObjectManager $manager): void
     {
-        $eventConfig = new VariableEventConfig();
-        $eventConfig
-            ->setQuantity(-1)
-            ->setTargetVariable(PlayerVariableEnum::HEALTH_POINT)
-            ->setVariableHolderClass(ModifierHolderClassEnum::PLAYER)
-            ->setEventName(VariableEventInterface::CHANGE_VALUE_MAX)
-            ->setName('change.value.max_player_-1_healthPoint')
-        ;
-        $manager->persist($eventConfig);
-        $this->addReference(self::MAX_HEALTH_REDUCE_1, $eventConfig);
-
-        $eventConfig = new VariableEventConfig();
-        $eventConfig
-            ->setQuantity(-2)
-            ->setTargetVariable(PlayerVariableEnum::HEALTH_POINT)
-            ->setVariableHolderClass(ModifierHolderClassEnum::PLAYER)
-            ->setEventName(VariableEventInterface::CHANGE_VALUE_MAX)
-            ->setName('change.value.max_player_-2_healthPoint')
-        ;
-        $manager->persist($eventConfig);
-        $this->addReference(self::MAX_HEALTH_REDUCE_2, $eventConfig);
-
-        $eventConfig = new VariableEventConfig();
-        $eventConfig
-            ->setQuantity(-4)
-            ->setTargetVariable(PlayerVariableEnum::HEALTH_POINT)
-            ->setVariableHolderClass(ModifierHolderClassEnum::PLAYER)
-            ->setEventName(VariableEventInterface::CHANGE_VALUE_MAX)
-            ->setName('change.value.max_player_-4_healthPoint')
-        ;
-        $manager->persist($eventConfig);
-        $this->addReference(self::MAX_HEALTH_REDUCE_4, $eventConfig);
-
-        $eventConfig = new VariableEventConfig();
-        $eventConfig
-            ->setQuantity(-1)
-            ->setTargetVariable(PlayerVariableEnum::MORAL_POINT)
-            ->setVariableHolderClass(ModifierHolderClassEnum::PLAYER)
-            ->setEventName(VariableEventInterface::CHANGE_VALUE_MAX)
-            ->setName('change.value.max_player_-1_moralPoint')
-        ;
-        $manager->persist($eventConfig);
-        $this->addReference(self::MAX_MORAL_REDUCE_1, $eventConfig);
-
-        $eventConfig = new VariableEventConfig();
-        $eventConfig
-            ->setQuantity(-2)
-            ->setTargetVariable(PlayerVariableEnum::MORAL_POINT)
-            ->setVariableHolderClass(ModifierHolderClassEnum::PLAYER)
-            ->setEventName(VariableEventInterface::CHANGE_VALUE_MAX)
-            ->setName('change.value.max_player_-2_moralPoint')
-        ;
-        $manager->persist($eventConfig);
-        $this->addReference(self::MAX_MORAL_REDUCE_2, $eventConfig);
-
-        $eventConfig = new VariableEventConfig();
-        $eventConfig
-            ->setQuantity(-3)
-            ->setTargetVariable(PlayerVariableEnum::MORAL_POINT)
-            ->setVariableHolderClass(ModifierHolderClassEnum::PLAYER)
-            ->setEventName(VariableEventInterface::CHANGE_VALUE_MAX)
-            ->setName('change.value.max_player_-3_moralPoint')
-        ;
-        $manager->persist($eventConfig);
-        $this->addReference(self::MAX_MORAL_REDUCE_3, $eventConfig);
-
-        $eventConfig = new VariableEventConfig();
-        $eventConfig
-            ->setQuantity(-4)
-            ->setTargetVariable(PlayerVariableEnum::MORAL_POINT)
-            ->setVariableHolderClass(ModifierHolderClassEnum::PLAYER)
-            ->setEventName(VariableEventInterface::CHANGE_VALUE_MAX)
-            ->setName('change.value.max_player_-4_moralPoint')
-        ;
-        $manager->persist($eventConfig);
-        $this->addReference(self::MAX_MORAL_REDUCE_4, $eventConfig);
-
-        $eventConfig = new VariableEventConfig();
-        $eventConfig
-            ->setQuantity(-2)
-            ->setTargetVariable(PlayerVariableEnum::ACTION_POINT)
-            ->setVariableHolderClass(ModifierHolderClassEnum::PLAYER)
-            ->setEventName(VariableEventInterface::CHANGE_VALUE_MAX)
-            ->setName('change.value.max_player_-2_actionPoint')
-        ;
-        $manager->persist($eventConfig);
-        $this->addReference(self::MAX_ACTION_REDUCE_2, $eventConfig);
-
-        $eventConfig = new VariableEventConfig();
-        $eventConfig
-            ->setQuantity(-3)
-            ->setTargetVariable(PlayerVariableEnum::MOVEMENT_POINT)
-            ->setVariableHolderClass(ModifierHolderClassEnum::PLAYER)
-            ->setEventName(VariableEventInterface::CHANGE_VALUE_MAX)
-            ->setName('change.value.max_player_-3_movementPoint')
-        ;
-        $manager->persist($eventConfig);
-        $this->addReference(self::MAX_MOVEMENT_REDUCE_3, $eventConfig);
-
-        $eventConfig = new VariableEventConfig();
-        $eventConfig
-            ->setQuantity(-5)
-            ->setTargetVariable(PlayerVariableEnum::MOVEMENT_POINT)
-            ->setVariableHolderClass(ModifierHolderClassEnum::PLAYER)
-            ->setEventName(VariableEventInterface::CHANGE_VALUE_MAX)
-            ->setName('change.value.max_player_-5_movementPoint')
-        ;
-        $manager->persist($eventConfig);
-        $this->addReference(self::MAX_MOVEMENT_REDUCE_5, $eventConfig);
-
-        $eventConfig = new VariableEventConfig();
-        $eventConfig
-            ->setQuantity(-12)
-            ->setTargetVariable(PlayerVariableEnum::MOVEMENT_POINT)
-            ->setVariableHolderClass(ModifierHolderClassEnum::PLAYER)
-            ->setEventName(VariableEventInterface::CHANGE_VALUE_MAX)
-            ->setName('change.value.max_player_-12_movementPoint')
-        ;
-        $manager->persist($eventConfig);
-        $this->addReference(self::MAX_MOVEMENT_REDUCE_12, $eventConfig);
-
-        $eventConfig = new VariableEventConfig();
-        $eventConfig
-            ->setQuantity(-1)
-            ->setTargetVariable(PlayerVariableEnum::HEALTH_POINT)
-            ->setVariableHolderClass(ModifierHolderClassEnum::PLAYER)
-            ->setEventName(VariableEventInterface::CHANGE_VARIABLE)
-            ->setName('change.variable_player_-1_healthPoint')
-        ;
-        $manager->persist($eventConfig);
-        $this->addReference(self::HEALTH_REDUCE_1, $eventConfig);
-
-        $eventConfig = new VariableEventConfig();
-        $eventConfig
-            ->setQuantity(-2)
-            ->setTargetVariable(PlayerVariableEnum::HEALTH_POINT)
-            ->setVariableHolderClass(ModifierHolderClassEnum::PLAYER)
-            ->setEventName(VariableEventInterface::CHANGE_VARIABLE)
-            ->setName('change.variable_player_-2_healthPoint')
-        ;
-        $manager->persist($eventConfig);
-        $this->addReference(self::HEALTH_REDUCE_2, $eventConfig);
-
-        $eventConfig = new VariableEventConfig();
-        $eventConfig
-            ->setQuantity(-3)
-            ->setTargetVariable(PlayerVariableEnum::HEALTH_POINT)
-            ->setVariableHolderClass(ModifierHolderClassEnum::PLAYER)
-            ->setEventName(VariableEventInterface::CHANGE_VARIABLE)
-            ->setName('change.variable_player_-3_healthPoint')
-        ;
-        $manager->persist($eventConfig);
-        $this->addReference(self::HEALTH_REDUCE_3, $eventConfig);
-
-        $eventConfig = new VariableEventConfig();
-        $eventConfig
-            ->setQuantity(-4)
-            ->setTargetVariable(PlayerVariableEnum::HEALTH_POINT)
-            ->setVariableHolderClass(ModifierHolderClassEnum::PLAYER)
-            ->setEventName(VariableEventInterface::CHANGE_VARIABLE)
-            ->setName('change.variable_player_-4_healthPoint')
-        ;
-        $manager->persist($eventConfig);
-        $this->addReference(self::HEALTH_REDUCE_4, $eventConfig);
-
-        $eventConfig = new VariableEventConfig();
-        $eventConfig
-            ->setQuantity(-6)
-            ->setTargetVariable(PlayerVariableEnum::HEALTH_POINT)
-            ->setVariableHolderClass(ModifierHolderClassEnum::PLAYER)
-            ->setEventName(VariableEventInterface::CHANGE_VARIABLE)
-            ->setName('change.variable_player_-6_healthPoint')
-        ;
-        $manager->persist($eventConfig);
-        $this->addReference(self::HEALTH_REDUCE_6, $eventConfig);
-
-        $eventConfig = new VariableEventConfig();
-        $eventConfig
-            ->setQuantity(-1)
-            ->setTargetVariable(PlayerVariableEnum::MORAL_POINT)
-            ->setVariableHolderClass(ModifierHolderClassEnum::PLAYER)
-            ->setEventName(VariableEventInterface::CHANGE_VARIABLE)
-            ->setName('change.variable_player_-1_moralPoint')
-        ;
-        $manager->persist($eventConfig);
-        $this->addReference(self::MORAL_REDUCE_1, $eventConfig);
-
-        $eventConfig = new VariableEventConfig();
-        $eventConfig
-            ->setQuantity(1)
-            ->setTargetVariable(PlayerVariableEnum::ACTION_POINT)
-            ->setVariableHolderClass(ModifierHolderClassEnum::PLAYER)
-            ->setEventName(VariableEventInterface::CHANGE_VARIABLE)
-            ->setName('change.variable_player_1_actionPoint')
-        ;
-        $manager->persist($eventConfig);
-        $this->addReference(self::ACTION_INCREASE_1, $eventConfig);
-
-        $eventConfig = new VariableEventConfig();
-        $eventConfig
-            ->setQuantity(-1)
-            ->setTargetVariable(PlayerVariableEnum::ACTION_POINT)
-            ->setVariableHolderClass(ModifierHolderClassEnum::PLAYER)
-            ->setEventName(VariableEventInterface::CHANGE_VARIABLE)
-            ->setName('change.variable_player_-1_actionPoint')
-        ;
-        $manager->persist($eventConfig);
-        $this->addReference(self::ACTION_REDUCE_1, $eventConfig);
-
-        $eventConfig = new VariableEventConfig();
-        $eventConfig
-            ->setQuantity(-2)
-            ->setTargetVariable(PlayerVariableEnum::ACTION_POINT)
-            ->setVariableHolderClass(ModifierHolderClassEnum::PLAYER)
-            ->setEventName(VariableEventInterface::CHANGE_VARIABLE)
-            ->setName('change.variable_player_-2_actionPoint')
-        ;
-        $manager->persist($eventConfig);
-        $this->addReference(self::ACTION_REDUCE_2, $eventConfig);
-
-        $eventConfig = new VariableEventConfig();
-        $eventConfig
-            ->setQuantity(-1)
-            ->setTargetVariable(PlayerVariableEnum::MOVEMENT_POINT)
-            ->setVariableHolderClass(ModifierHolderClassEnum::PLAYER)
-            ->setEventName(VariableEventInterface::CHANGE_VARIABLE)
-            ->setName('change.variable_player_-1_movementPoint')
-        ;
-        $manager->persist($eventConfig);
-        $this->addReference(self::MOVEMENT_REDUCE_1, $eventConfig);
-
-        $eventConfig = new VariableEventConfig();
-        $eventConfig
-            ->setQuantity(-2)
-            ->setTargetVariable(PlayerVariableEnum::MOVEMENT_POINT)
-            ->setVariableHolderClass(ModifierHolderClassEnum::PLAYER)
-            ->setEventName(VariableEventInterface::CHANGE_VARIABLE)
-            ->setName('change.variable_player_-2_movementPoint')
-        ;
-        $manager->persist($eventConfig);
-        $this->addReference(self::MOVEMENT_REDUCE_2, $eventConfig);
-
-        $eventConfig = new VariableEventConfig();
-        $eventConfig
-            ->setQuantity(1)
-            ->setTargetVariable(PlayerVariableEnum::MOVEMENT_POINT)
-            ->setVariableHolderClass(ModifierHolderClassEnum::PLAYER)
-            ->setEventName(VariableEventInterface::CHANGE_VARIABLE)
-            ->setName('change.variable_player_1_movementPoint')
-        ;
-        $manager->persist($eventConfig);
-        $this->addReference(self::MOVEMENT_INCREASE_1, $eventConfig);
-
-        $eventConfig = new VariableEventConfig();
-        $eventConfig
-            ->setQuantity(-1)
-            ->setTargetVariable(PlayerVariableEnum::SATIETY)
-            ->setVariableHolderClass(ModifierHolderClassEnum::PLAYER)
-            ->setEventName(VariableEventInterface::CHANGE_VARIABLE)
-            ->setName('change.variable_player_-1_satiety')
-        ;
-        $manager->persist($eventConfig);
-        $this->addReference(self::SATIETY_REDUCE_1, $eventConfig);
-
-        $eventConfig = new VariableEventConfig();
-        $eventConfig
-            ->setQuantity(1)
-            ->setTargetVariable(PlayerVariableEnum::SATIETY)
-            ->setVariableHolderClass(ModifierHolderClassEnum::PLAYER)
-            ->setEventName(VariableEventInterface::CHANGE_VARIABLE)
-            ->setName('change.variable_player_1_satiety')
-        ;
-        $manager->persist($eventConfig);
-        $this->addReference(self::SATIETY_INCREASE_1, $eventConfig);
-
-        $eventConfig = new PlanetSectorEventConfig();
-        $eventConfig
-            ->setOutputQuantityTable([
-                3 => 1,
-                4 => 1,
-                5 => 1,
-            ])
-            ->setName(PlanetSectorEvent::DISASTER . '_3_5')
-            ->setEventName(PlanetSectorEvent::DISASTER)
-        ;
-        $manager->persist($eventConfig);
-
-        $eventConfig = new PlanetSectorEventConfig();
-        $eventConfig
-            ->setOutputQuantityTable([
-                3 => 1,
-                4 => 1,
-                5 => 1,
-            ])
-            ->setName(PlanetSectorEvent::ACCIDENT . '_3_5')
-            ->setEventName(PlanetSectorEvent::ACCIDENT)
-
-        ;
-        $manager->persist($eventConfig);
-
-        $eventConfig = new PlanetSectorEventConfig();
-        $eventConfig
-            ->setName(PlanetSectorEvent::NOTHING_TO_REPORT)
-            ->setEventName(PlanetSectorEvent::NOTHING_TO_REPORT)
-        ;
-        $manager->persist($eventConfig);
-
-        $eventConfig = new PlanetSectorEventConfig();
-        $eventConfig
-            ->setOutputQuantityTable([
-                2 => 1,
-            ])
-            ->setName(PlanetSectorEvent::TIRED . '_2')
-            ->setEventName(PlanetSectorEvent::TIRED)
-        ;
-        $manager->persist($eventConfig);
-
-        $eventConfig = new PlanetSectorEventConfig();
-        $eventConfig
-            ->setOutputQuantityTable([
-                8 => 1,
-                16 => 1,
-                24 => 1,
-            ])
-            ->setName(PlanetSectorEvent::OXYGEN . '_8_16_24')
-            ->setEventName(PlanetSectorEvent::OXYGEN)
-        ;
-        $manager->persist($eventConfig);
-
-        $eventConfig = new PlanetSectorEventConfig();
-        $eventConfig
-            ->setOutputQuantityTable([
-                3 => 4,
-                4 => 3,
-                5 => 2,
-                6 => 1,
-            ])
-            ->setName(PlanetSectorEvent::FUEL . '_3_6')
-            ->setEventName(PlanetSectorEvent::FUEL)
-        ;
-        $manager->persist($eventConfig);
+        foreach (EventConfigData::$dataArray as $rawEventConfig) {
+            switch ($rawEventConfig['type']) {
+                case 'variable_event_config':
+                    $eventConfig = new VariableEventConfig();
+                    $eventConfig
+                        ->setQuantity($rawEventConfig['quantity'])
+                        ->setTargetVariable($rawEventConfig['targetVariable'])
+                        ->setVariableHolderClass($rawEventConfig['variableHolderClass'])
+                        ->setEventName($rawEventConfig['eventName'])
+                        ->setName($rawEventConfig['name'])
+                    ;
+                    break;
+                case 'planet_sector_event_config':
+                    $eventConfig = new PlanetSectorEventConfig();
+                    if (isset($rawEventConfig['outputQuantityTable'])) {
+                        $eventConfig->setOutputQuantityTable($rawEventConfig['outputQuantityTable']);
+                    }
+                    $eventConfig
+                        ->setEventName($rawEventConfig['eventName'])
+                        ->setName($rawEventConfig['name'])
+                    ;
+                    break;
+                default:
+                    throw new \Exception('Unknown event config type');
+            }
+            $this->addReference($eventConfig->getName(), $eventConfig);
+            $manager->persist($eventConfig);
+        }
 
         $manager->flush();
     }

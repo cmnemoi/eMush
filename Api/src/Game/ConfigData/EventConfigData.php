07d7ccd4c4d656e9d4627f96709a598914ee61ec
Evian <icdk5b4zf@relay.firefox.com>
2024-02-13 19:17:20 +0100

    Add artefact event config

--- Api/src/Game/ConfigData/EventConfigData.php
+++ Api/src/Game/ConfigData/EventConfigData.php
@@ -332,15 +332,5 @@ class EventConfigData
                 'water_stick' => 1,
             ],
         ],
-        [
-            'type' => 'planet_sector_event_config',
-            'name' => 'kill_random',
-            'eventName' => 'kill_random',
-        ],
-        [
-            'type' => 'planet_sector_event_config',
-            'name' => 'kill_all',
-            'eventName' => 'kill_all',
-        ],
     ];
 }

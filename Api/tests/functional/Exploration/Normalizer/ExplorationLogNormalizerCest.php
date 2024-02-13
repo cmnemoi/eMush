9ee78f7d5767fdfe7bb641b402c83658be4d20cb
Evian <icdk5b4zf@relay.firefox.com>
2024-02-13 19:15:57 +0100

    Add a first test for Artefact event

--- Api/tests/functional/Exploration/Normalizer/ExplorationLogNormalizerCest.php
+++ Api/tests/functional/Exploration/Normalizer/ExplorationLogNormalizerCest.php
@@ -7,7 +7,6 @@ namespace Mush\tests\functional\Exploration\Normalizer;
 use Doctrine\Common\Collections\ArrayCollection;
 use Mush\Exploration\Entity\Exploration;
 use Mush\Exploration\Entity\ExplorationLog;
-use Mush\Exploration\Entity\PlanetSectorConfig;
 use Mush\Exploration\Enum\PlanetSectorEnum;
 use Mush\Exploration\Event\PlanetSectorEvent;
 use Mush\Exploration\Normalizer\ExplorationLogNormalizer;
@@ -104,14 +103,16 @@ final class ExplorationLogNormalizerCest extends AbstractExplorationTester
     public function testNormalizeTiredEvent(FunctionalTester $I): void
     {
         // given desert sector has only tired event
-        /** @var PlanetSectorConfig $landingSectorConfig */
-        $landingSectorConfig = $this->daedalus->getGameConfig()->getPlanetSectorConfigs()->filter(
-            fn (PlanetSectorConfig $planetSectorConfig) => $planetSectorConfig->getSectorName() === PlanetSectorEnum::DESERT,
-        )->first();
-        $landingSectorConfig->setExplorationEvents([PlanetSectorEvent::TIRED_2 => 1]);
+        $this->setupPlanetSectorEvents(
+            sectorName: PlanetSectorEnum::DESERT,
+            events: [PlanetSectorEvent::TIRED_2 => 1]
+        );
 
         // given exploration is created
-        $this->exploration = $this->createExploration($this->createPlanet([PlanetSectorEnum::DESERT, PlanetSectorEnum::OXYGEN], $I));
+        $this->exploration = $this->createExploration(
+            planet: $this->createPlanet([PlanetSectorEnum::DESERT, PlanetSectorEnum::OXYGEN], $I),
+            explorators: $this->players,
+        );
 
         // given two extra steps are made to trigger the tired event
         $this->explorationService->dispatchExplorationEvent($this->exploration);
@@ -135,5 +136,68 @@ final class ExplorationLogNormalizerCest extends AbstractExplorationTester
             ],
             actual: $normalizedExplorationLog,
         );
+    }
+
+    public function testNormalizeArtefactEvent(FunctionalTester $I): void
+    {
+        // given intelligent life sector has only artefact event
+        $this->setupPlanetSectorEvents(
+            sectorName: PlanetSectorEnum::INTELLIGENT,
+            events: [PlanetSectorEvent::ARTEFACT => 1]
+        );
+
+        // given exploration is created
+        $this->exploration = $this->createExploration(
+            planet: $this->createPlanet([PlanetSectorEnum::INTELLIGENT, PlanetSectorEnum::OXYGEN], $I),
+            explorators: $this->players,
+        );
+
+        // given two extra steps are made to trigger the artefact event
+        $this->explorationService->dispatchExplorationEvent($this->exploration);
+        $this->explorationService->dispatchExplorationEvent($this->exploration);
+
+        // when artefact event exploration log is normalized
+        /** @var ExplorationLog $explorationLog */
+        $explorationLog = $this->exploration->getClosedExploration()->getLogs()->filter(
+            fn (ExplorationLog $explorationLog) => $explorationLog->getPlanetSectorName() === PlanetSectorEnum::INTELLIGENT,
+        )->first();
+        $normalizedExplorationLog = $this->explorationLogNormalizer->normalize($explorationLog);
+        
+        $lootedArtefact = $this->translationService->translate(
+            key: $explorationLog->getParameters()['target_equipment'],
+            parameters: [],
+            domain: 'items',
+            language: $this->exploration->getDaedalus()->getLanguage(),
+        );
+
+        $maleExpectedEventDescription = "Derrière un rocher, vous trouvez une créature étrange très affaiblie. Vous lui donnez un peu d'eau afin qu'elle reprenne connaissance. La créature vous offre un {$lootedArtefact} avant de reprendre sa route.";
+        $femaleExpectedEventDescription = "Derrière un rocher, vous trouvez une créature étrange très affaiblie. Vous lui donnez un peu d'eau afin qu'elle reprenne connaissance. La créature vous offre une {$lootedArtefact} avant de reprendre sa route.";
+
+        // then exploration log is normalized as expected
+        try {
+            $I->assertEquals(
+                expected: [
+                    'id' => $explorationLog->getId(),
+                    'planetSectorKey' => PlanetSectorEnum::INTELLIGENT,
+                    'planetSectorName' => 'Vie intelligente',
+                    'eventName' => 'Artefact',
+                    'eventDescription' => $maleExpectedEventDescription,
+                    'eventOutcome' => 'Vous trouvez un artefact.',
+                ],
+                actual: $normalizedExplorationLog,
+            );
+        } catch (\Exception $e) {
+            $I->assertEquals(
+                expected: [
+                    'id' => $explorationLog->getId(),
+                    'planetSectorKey' => PlanetSectorEnum::INTELLIGENT,
+                    'planetSectorName' => 'Vie intelligente',
+                    'eventName' => 'Artefact',
+                    'eventDescription' => $femaleExpectedEventDescription,
+                    'eventOutcome' => 'Vous trouvez un artefact.',
+                ],
+                actual: $normalizedExplorationLog,
+            );
+        }
     }
 }

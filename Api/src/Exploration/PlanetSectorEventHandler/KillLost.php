786a6d1ccb4dbb6f5fb0d301d38d08042d92f7c2
Charles-Meldhine Madi Mnemoi <icdk5b4zf@relay.firefox.com>
2024-02-18 16:59:11 +0100

    feat: New exploration event : Kill lost

---
+++ Api/src/Exploration/PlanetSectorEventHandler/KillLost.php
@@ -0,0 +1,42 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Mush\Exploration\PlanetSectorEventHandler;
+
+use Mush\Exploration\Entity\ExplorationLog;
+use Mush\Exploration\Event\PlanetSectorEvent;
+use Mush\Player\Event\PlayerEvent;
+
+final class KillLost extends AbstractPlanetSectorEventHandler
+{
+    private const NUMBER_OF_DESCRIPTIONS = 2;
+
+    public function getName(): string
+    {
+        return PlanetSectorEvent::KILL_LOST;
+    }
+
+    public function handle(PlanetSectorEvent $event): ExplorationLog
+    {
+        $playerToKill = $this->randomService->getRandomPlayer($event->getExploration()->getDaedalus()->getLostPlayers());
+
+        $playerEvent = new PlayerEvent(
+            $playerToKill,
+            $event->getTags(),
+            $event->getTime()
+        );
+        $this->eventService->callEvent($playerEvent, PlayerEvent::DEATH_PLAYER);
+
+        // Remove lost planet sector after usage because it's not a "true" sector
+        // (we don't want players to see it in astro terminal for example)
+        $this->entityManager->remove($event->getPlanetSector());
+
+        $logParameters = [
+            $playerToKill->getLogKey() => $playerToKill->getLogName(),
+            'version' => $this->randomService->random(1, self::NUMBER_OF_DESCRIPTIONS),
+        ];
+
+        return $this->createExplorationLog($event, $logParameters);
+    }
+}

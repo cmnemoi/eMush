458d2ca6f1b8f25d54abbecf020793d108896cd2
Charles-Meldhine Madi Mnemoi <icdk5b4zf@relay.firefox.com>
2024-02-18 15:43:57 +0100

    Add FInd lost event handler

---
+++ Api/src/Exploration/PlanetSectorEventHandler/FindLost.php
@@ -0,0 +1,53 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Mush\Exploration\PlanetSectorEventHandler;
+
+use Doctrine\ORM\EntityManagerInterface;
+use Mush\Exploration\Entity\ExplorationLog;
+use Mush\Exploration\Event\PlanetSectorEvent;
+use Mush\Game\Enum\VisibilityEnum;
+use Mush\Game\Service\EventServiceInterface;
+use Mush\Game\Service\RandomServiceInterface;
+use Mush\Status\Enum\PlayerStatusEnum;
+use Mush\Status\Service\StatusServiceInterface;
+
+final class FindLost extends AbstractPlanetSectorEventHandler
+{
+    private StatusServiceInterface $statusService;
+
+    public function __construct(
+        EntityManagerInterface $entityManager,
+        EventServiceInterface $eventService,
+        RandomServiceInterface $randomService,
+        StatusServiceInterface $statusService
+    ) {
+        parent::__construct($entityManager, $eventService, $randomService);
+        $this->statusService = $statusService;
+    }
+
+    public function getName(): string
+    {
+        return PlanetSectorEvent::FIND_LOST;
+    }
+
+    public function handle(PlanetSectorEvent $event): ExplorationLog
+    {
+        $foundPlayer = $this->randomService->getRandomPlayer($event->getExploration()->getLostExplorators());
+
+        $this->statusService->removeStatus(
+            statusName: PlayerStatusEnum::LOST,
+            holder: $foundPlayer,
+            tags: $event->getTags(),
+            time: $event->getTime(),
+            visibility: VisibilityEnum::PRIVATE
+        );
+
+        // Remove lost planet sector after usage because it's not a "true" sector 
+        // (we don't want players to see it in astro terminal for example)
+        $this->entityManager->remove($event->getPlanetSector());
+
+        return $this->createExplorationLog($event, parameters: [$foundPlayer->getLogKey() => $foundPlayer->getLogName()]);
+    }
+}

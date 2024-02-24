16c2c97d9814a630c819ec9322d0899948c91fc0
Evian <icdk5b4zf@relay.firefox.com>
2024-02-24 11:08:53 +0100

    Revert "Allow exploration to be launched if players are lost, even if there are no sectors left to visit"
    This reverts commit 6b950a96283bd49c2a96ae06e911d9cf460d063c.

--- Api/src/Exploration/Service/ExplorationService.php
+++ Api/src/Exploration/Service/ExplorationService.php
@@ -7,7 +7,6 @@ namespace Mush\Exploration\Service;
 use Doctrine\ORM\EntityManagerInterface;
 use Mush\Equipment\Entity\GameEquipment;
 use Mush\Exploration\Entity\Exploration;
-use Mush\Exploration\Entity\Planet;
 use Mush\Exploration\Entity\PlanetSector;
 use Mush\Exploration\Entity\PlanetSectorConfig;
 use Mush\Exploration\Entity\PlanetSectorEventConfig;
@@ -54,7 +53,7 @@ final class ExplorationService implements ExplorationServiceInterface
         $exploration = new Exploration($planet);
         $exploration->setExplorators($players);
         $exploration->getClosedExploration()->setClosedExplorators($players->map(fn (Player $player) => $player->getPlayerInfo()->getClosedPlayer())->toArray());
-        $exploration->setNumberOfSectionsToVisit($this->getNumberOfSectorsToVisit($numberOfSectorsToVisit, $planet));
+        $exploration->setNumberOfSectionsToVisit(min($numberOfSectorsToVisit, $planet->getUnvisitedSectors()->count()));
 
         if ($exploration->getNumberOfSectionsToVisit() < 1) {
             throw new \RuntimeException('You cannot visit less than 1 sector');
@@ -165,14 +164,6 @@ final class ExplorationService implements ExplorationServiceInterface
         }
 
         $this->entityManager->flush();
-    }
-
-    private function getNumberOfSectorsToVisit(int $numberOfSectorsToVisit, Planet $planet): int
-    {
-        $lostPlayersCount = $planet->getDaedalus()->getLostPlayers()->count();
-        $unvisitedSectorsCount = $planet->getUnvisitedSectors()->count();
-
-        return min($numberOfSectorsToVisit, max($lostPlayersCount, $unvisitedSectorsCount));
     }
 
     private function drawPlanetSectorEvent(PlanetSector $sector): string

ceaa74520b976158114f8489880e09122bb4620a
Charles-Meldhine Madi Mnemoi <icdk5b4zf@relay.firefox.com>
2024-02-27 23:26:45 +0100

    fix endpoint method

--- Api/src/MetaGame/Controller/AdminController.php
+++ Api/src/MetaGame/Controller/AdminController.php
@@ -393,7 +393,7 @@ class AdminController extends AbstractFOSRestController
      *
      * @Security(name="Bearer")
      *
-     * @Rest\Get(path="close-exploration/{id}")
+     * @Rest\Patch(path="close-exploration/{id}")
      */
     public function closeExploration(Exploration $exploration): View
     {

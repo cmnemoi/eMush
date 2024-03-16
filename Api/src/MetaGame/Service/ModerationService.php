64869ed5a9b596ea9e90fe9b610950c9b67bca0d
Charles-Meldhine Madi Mnemoi <icdk5b4zf@relay.firefox.com>
2024-03-16 13:36:25 +0100

    refactor: move new function in ModerationService

--- Api/src/MetaGame/Service/ModerationService.php
+++ Api/src/MetaGame/Service/ModerationService.php
@@ -6,8 +6,10 @@ namespace Mush\MetaGame\Service;
 
 use Doctrine\ORM\EntityManagerInterface;
 use Mush\Game\Service\EventServiceInterface;
+use Mush\Game\Service\TranslationServiceInterface;
+use Mush\Player\Entity\ClosedPlayer;
 use Mush\Player\Entity\Player;
-use Mush\Player\Enum\EndCauseEnum;
+use Mush\Player\Enum\ClosedPlayerCauseEnum;
 use Mush\Player\Event\PlayerEvent;
 use Mush\User\Entity\User;
 
@@ -15,11 +17,16 @@ final class ModerationService implements ModerationServiceInterface
 {
     private EntityManagerInterface $entityManager;
     private EventServiceInterface $eventService;
+    private TranslationServiceInterface $translationService;
 
-    public function __construct(EntityManagerInterface $entityManager, EventServiceInterface $eventService)
-    {
+    public function __construct(
+        EntityManagerInterface $entityManager, 
+        EventServiceInterface $eventService, 
+        TranslationServiceInterface $translationService
+    ) {
         $this->entityManager = $entityManager;
         $this->eventService = $eventService;
+        $this->translationService = $translationService;
     }
 
     public function banUser(User $user): User
@@ -28,6 +35,27 @@ final class ModerationService implements ModerationServiceInterface
         $this->entityManager->persist($bannedUser);
 
         return $bannedUser;
+    }
+
+    public function editClosedPlayerMessage(ClosedPlayer $closedPlayer): void
+    {
+        $message = $this->translationService->translate(
+            key: 'edited_by_neron',
+            parameters: [],
+            domain: 'moderation',
+            language: $closedPlayer->getClosedDaedalus()->getLanguage(),
+        );
+
+        $closedPlayer->setMessage($message);
+        $this->entityManager->persist($closedPlayer);
+        $this->entityManager->flush();
+    }
+
+    public function hideClosedPlayerEndMessage(ClosedPlayer $closedPlayer): void
+    {
+        $closedPlayer->hideMessage();
+        $this->entityManager->persist($closedPlayer);
+        $this->entityManager->flush();
     }
 
     public function unbanUser(User $user): User
@@ -40,7 +68,7 @@ final class ModerationService implements ModerationServiceInterface
 
     public function quarantinePlayer(Player $player): Player
     {
-        $deathEvent = new PlayerEvent($player, [EndCauseEnum::QUARANTINE], new \DateTime());
+        $deathEvent = new PlayerEvent($player, [ClosedPlayerCauseEnum::QUARANTINE], new \DateTime());
         $this->eventService->callEvent($deathEvent, PlayerEvent::DEATH_PLAYER);
 
         return $player;

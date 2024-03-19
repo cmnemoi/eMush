f4f51e997e884630b1aa6f5295c5deec6b42f3a7
Sylvain Breton <sylvain.breton.6@gmail.com>
2024-03-19 13:11:37 +0100

    chore: add moderationAction entity

---
+++ Api/src/MetaGame/Entity/ModerationAction.php
@@ -0,0 +1,129 @@
+<?php
+
+namespace Mush\MetaGame\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use Gedmo\Timestampable\Traits\TimestampableEntity;
+use Mush\User\Entity\User;
+
+#[ORM\Entity]
+#[ORM\Table(name: 'moderationAction')]
+class ModerationAction
+{
+    use TimestampableEntity;
+
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column(type: 'integer', length: 255, nullable: false)]
+    private int $id;
+
+    #[ORM\ManyToOne(targetEntity: User::class)]
+    private User $user;
+
+    #[ORM\Column(type: 'string', length: 255, nullable: false)]
+    private string $moderationAction;
+
+    #[ORM\Column(type: 'string', length: 255, nullable: false)]
+    private string $reason;
+
+    #[ORM\Column(type: 'string', length: 255, nullable: false)]
+    private ?string $message;
+
+    #[ORM\Column(type: 'boolean', nullable: false, options: ['default' => false])]
+    private bool $isVisibleByUser = false;
+
+    #[ORM\Column(type: 'datetime', nullable: true)]
+    private \DateTime $startDate;
+
+    #[ORM\Column(type: 'datetime', nullable: true)]
+    private ?\DateTime $endDate = null;
+
+    public function __construct(User $user, \DateTime $startDate)
+    {
+        $this->startDate = $startDate;
+        $this->user = $user;
+    }
+
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    public function getModerationAction(): string
+    {
+        return $this->moderationAction;
+    }
+
+    public function setModerationAction(string $moderationAction): void
+    {
+        $this->moderationAction = $moderationAction;
+    }
+
+    public function getUser(): User
+    {
+        return $this->user;
+    }
+
+    public function getReason(): string
+    {
+        return $this->reason;
+    }
+
+    public function setReason(string $reason): void
+    {
+        $this->reason = $reason;
+    }
+
+    public function getMessage(): ?string
+    {
+        return $this->message;
+    }
+
+    public function setMessage(string $message): void
+    {
+        $this->message = $message;
+    }
+
+    public function getIsVisibleByUser(): bool
+    {
+        return $this->isVisibleByUser;
+    }
+
+    public function setIsVisibleByUser(bool $isVisibleByUser): void
+    {
+        $this->isVisibleByUser = $isVisibleByUser;
+    }
+
+    public function getStartDate(): \DateTime
+    {
+        return $this->startDate;
+    }
+
+    public function setStartDate(\DateTime $startDate): void
+    {
+        $this->startDate = $startDate;
+    }
+
+    public function getEndDate(): ?\DateTime
+    {
+        return $this->endDate;
+    }
+
+    public function setEndDate(\DateTime $endDate): void
+    {
+        $this->startDate = $endDate;
+    }
+
+    public function isSanctionActive(): bool
+    {
+        $currentTime = new \DateTime();
+        if (
+            $this->endDate === null
+            || ($this->endDate > $currentTime && $this->startDate < $currentTime)
+        ) {
+            return true;
+        }
+
+        return false;
+    }
+}

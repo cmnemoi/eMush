16473715a03d59438b968734f952f887edd64d70
Evian <icdk5b4zf@relay.firefox.com>
2024-03-20 17:52:00 +0000

    chore: Activate player lost event as we can now find lost players

--- Api/src/Exploration/ConfigData/PlanetSectorConfigData.php
+++ Api/src/Exploration/ConfigData/PlanetSectorConfigData.php
@@ -89,6 +89,7 @@ final class PlanetSectorConfigData
             'explorationEvents' => [
                 PlanetSectorEvent::NOTHING_TO_REPORT => 7,
                 PlanetSectorEvent::PROVISION_3 => 2,
+                PlanetSectorEvent::PLAYER_LOST => 1,
             ],
         ],
         [
@@ -213,6 +214,7 @@ final class PlanetSectorConfigData
             'explorationEvents' => [
                 PlanetSectorEvent::NOTHING_TO_REPORT => 4,
                 PlanetSectorEvent::TIRED_2 => 3,
+                PlanetSectorEvent::PLAYER_LOST => 2,
                 PlanetSectorEvent::ACCIDENT_3_5 => 1,
             ],
         ],
@@ -335,6 +337,7 @@ final class PlanetSectorConfigData
                 PlanetSectorEvent::MUSH_TRAP => 4,
                 PlanetSectorEvent::STARMAP => 3,
                 PlanetSectorEvent::FIGHT_18 => 2,
+                PlanetSectorEvent::PLAYER_LOST => 1,
             ],
         ],
     ];

cae789fa563db9b89b63b22a835cada0b5675bff
Charles-Meldhine Madi Mnemoi <icdk5b4zf@relay.firefox.com>
2024-03-23 12:36:19 +0100

    fix test

--- Api/tests/functional/Status/Event/CycleEventCest.php
+++ Api/tests/functional/Status/Event/CycleEventCest.php
@@ -30,7 +30,6 @@ use Mush\Status\Entity\ChargeStatus;
 use Mush\Status\Entity\Config\ChargeStatusConfig;
 use Mush\Status\Enum\ChargeStrategyTypeEnum;
 use Mush\Status\Enum\EquipmentStatusEnum;
-use Mush\Status\Enum\PlayerStatusEnum;
 use Mush\Status\Enum\StatusEnum;
 use Mush\Status\Event\StatusCycleEvent;
 use Mush\Status\Listener\StatusCycleSubscriber;
@@ -40,7 +39,7 @@ use Mush\Tests\FunctionalTester;
 use Mush\User\Entity\User;
 
 final class CycleEventCest extends AbstractFunctionalTest
-{   
+{
     private EventServiceInterface $eventService;
     private StatusCycleSubscriber $cycleSubscriber;
     private StatusServiceInterface $statusService;
@@ -271,8 +270,8 @@ final class CycleEventCest extends AbstractFunctionalTest
 
     public function testStarvingDoesNotTriggerTheCycleItAppears(FunctionalTester $I): void
     {
-        // given Chun has -24 satiety, so they will start starving at next cycle
-        $this->chun->setSatiety(-24);
+        // given Chun has -23 satiety, so they will start starving at next cycle
+        $this->chun->setSatiety(-23);
 
         // given a new cycle passes
         $cycleEvent = new PlayerCycleEvent($this->chun, [EventEnum::NEW_CYCLE], new \DateTime());
@@ -281,7 +280,7 @@ final class CycleEventCest extends AbstractFunctionalTest
         // when a new cycle passes
         $cycleEvent = new PlayerCycleEvent($this->chun, [EventEnum::NEW_CYCLE], new \DateTime());
         $this->eventService->callEvent($cycleEvent, PlayerCycleEvent::PLAYER_NEW_CYCLE);
-        
+
         // then Chun should not have lost any health point
         $I->assertEquals(14, $this->chun->getHealthPoint());
 

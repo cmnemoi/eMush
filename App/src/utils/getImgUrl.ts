8d834ee8ab2a7956bfb403c247988dcac8f07c5a
Charles-Meldhine Madi Mnemoi <icdk5b4zf@relay.firefox.com>
2024-03-30 14:02:01 +0000

    trying something else

--- App/src/utils/getImgUrl.ts
+++ App/src/utils/getImgUrl.ts
@@ -1,5 +1,3 @@
 export function getImgUrl(imgName: string): string {
-    const assetsPath = import.meta.env.DEV ? '/src/assets/images/' : '/assets/';
-
-    return new URL(`${assetsPath}${imgName}`, import.meta.url).href;
+    return new URL(`/src/assets/images/${imgName}`, import.meta.url).href;
 }
--- App/src/utils/getVideoUrl.ts
+++ App/src/utils/getVideoUrl.ts
@@ -1,5 +1,3 @@
 export function getVideoUrl(videoName: string): string {
-    const assetsPath = import.meta.env.DEV ? '/src/assets/videos/' : '/assets/';
-
-    return new URL(`${assetsPath}${videoName}`, import.meta.url).href;
+    return new URL(`/src/assets/videos/${videoName}`, import.meta.url).href;
 }

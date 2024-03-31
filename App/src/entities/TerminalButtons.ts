bf273496b93ffa87a10fc3c584ad29256d670001
Sickenberg Romain <r.sickenberg@gmail.com>
2024-03-31 16:03:31 +0200

    style(code): enhanced code liniting

--- App/src/entities/TerminalButtons.ts
+++ App/src/entities/TerminalButtons.ts
@@ -1,6 +1,6 @@
 export class TerminalButtons {
     public sharePlanet!: TerminalButton;
-    
+
     public load(object: any): TerminalButtons {
         if (object) {
             this.sharePlanet = object.share_planet;

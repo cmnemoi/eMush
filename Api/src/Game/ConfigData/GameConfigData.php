86cbb02e1434b83cbb13ba8f3cbb56b839389ec0
Evian <icdk5b4zf@relay.firefox.com>
2024-03-31 23:09:15 +0200

    fix: Add missing status for new defense CPU

--- Api/src/Game/ConfigData/GameConfigData.php
+++ Api/src/Game/ConfigData/GameConfigData.php
@@ -418,6 +418,7 @@ class GameConfigData
                 PlayerStatusEnum::POC_SHOOTER_SKILL . '_alpha',
                 'astronavigation_neron_cpu_priority_default',
                 'changed_cpu_priority_default',
+                'defence_neron_cpu_priority',
             ],
             'triumphConfigs' => [
                 'alien_science',

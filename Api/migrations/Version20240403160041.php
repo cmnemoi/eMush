6129a499e79b06c3ea6e5f71cafe252d852f05a8
Evian <icdk5b4zf@relay.firefox.com>
2024-04-04 08:11:02 +0200

    FIx broken migration

--- Api/migrations/Version20240403160041.php
+++ Api/migrations/Version20240403160041.php
@@ -24,17 +24,15 @@ final class Version20240403160041 extends AbstractMigration
         $this->addSql('CREATE TABLE moderationSanction (id INT NOT NULL, user_id INT DEFAULT NULL, moderation_action VARCHAR(255) NOT NULL, reason VARCHAR(255) NOT NULL, message VARCHAR(255) DEFAULT NULL, is_visible_by_user BOOLEAN DEFAULT false NOT NULL, start_date TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL, end_date TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL, created_at TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL, updated_at TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL, PRIMARY KEY(id))');
         $this->addSql('CREATE INDEX IDX_CB19D91BA76ED395 ON moderationSanction (user_id)');
         $this->addSql('ALTER TABLE moderationSanction ADD CONSTRAINT FK_CB19D91BA76ED395 FOREIGN KEY (user_id) REFERENCES users (id) NOT DEFERRABLE INITIALLY IMMEDIATE');
-        $this->addSql('ALTER TABLE config_difficulty DROP maximum_allowed_spreading_fires');
+        $this->addSql('ALTER TABLE users DROP banned');
     }
 
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
-        $this->addSql('CREATE SCHEMA public');
         $this->addSql('DROP SEQUENCE moderationSanction_id_seq CASCADE');
         $this->addSql('ALTER TABLE moderationSanction DROP CONSTRAINT FK_CB19D91BA76ED395');
         $this->addSql('DROP TABLE moderationSanction');
-        $this->addSql('ALTER TABLE config_difficulty ADD maximum_allowed_spreading_fires INT NOT NULL');
         $this->addSql('ALTER TABLE users ADD banned BOOLEAN DEFAULT false NOT NULL');
     }
 }

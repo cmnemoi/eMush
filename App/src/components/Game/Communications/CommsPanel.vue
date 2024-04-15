76cb7a7015a8e281f331c9890618cedf05a6cd19
Charles-Meldhine Madi Mnemoi <icdk5b4zf@relay.firefox.com>
2024-04-15 14:44:54 +0200

    style: remove comma

--- App/src/components/Game/Communications/CommsPanel.vue
+++ App/src/components/Game/Communications/CommsPanel.vue
@@ -125,7 +125,7 @@ export default defineComponent ({
             } else {
                 await this.markChannelAsRead(this.currentChannel);
             }
-        },
+        }
     }
 });
 </script>

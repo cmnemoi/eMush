f677b378a30c40315f4a58ceabfbdd6b27465f6f
Charles-Meldhine Madi Mnemoi <icdk5b4zf@relay.firefox.com>
2024-04-19 11:40:13 +0200

    Add default values for DB tables to avoid problem in the future

--- Api/migrations/Version20240419083304.php
+++ Api/migrations/Version20240419083304.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20240419002456 extends AbstractMigration
+final class Version20240419083304 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -25,7 +25,7 @@ final class Version20240419002456 extends AbstractMigration
         $this->addSql('CREATE TABLE project (id INT NOT NULL, config_id INT DEFAULT NULL, daedalus_id INT DEFAULT NULL, PRIMARY KEY(id))');
         $this->addSql('CREATE INDEX IDX_2FB3D0EE24DB0683 ON project (config_id)');
         $this->addSql('CREATE INDEX IDX_2FB3D0EE74B5A52D ON project (daedalus_id)');
-        $this->addSql('CREATE TABLE project_config (id INT NOT NULL, name VARCHAR(255) NOT NULL, type VARCHAR(255) NOT NULL, efficiency INT NOT NULL, bonus_skills TEXT NOT NULL, PRIMARY KEY(id))');
+        $this->addSql('CREATE TABLE project_config (id INT NOT NULL, name VARCHAR(255) DEFAULT \'\' NOT NULL, type VARCHAR(255) DEFAULT \'\' NOT NULL, efficiency INT DEFAULT 0 NOT NULL, bonus_skills TEXT DEFAULT \'a:0:{}\' NOT NULL, PRIMARY KEY(id))');
         $this->addSql('COMMENT ON COLUMN project_config.bonus_skills IS \'(DC2Type:array)\'');
         $this->addSql('ALTER TABLE project ADD CONSTRAINT FK_2FB3D0EE24DB0683 FOREIGN KEY (config_id) REFERENCES project_config (id) NOT DEFERRABLE INITIALLY IMMEDIATE');
         $this->addSql('ALTER TABLE project ADD CONSTRAINT FK_2FB3D0EE74B5A52D FOREIGN KEY (daedalus_id) REFERENCES daedalus (id) NOT DEFERRABLE INITIALLY IMMEDIATE');
--- Api/src/Project/Enum/ProjectType.php
+++ Api/src/Project/Enum/ProjectType.php
@@ -5,7 +5,8 @@ declare(strict_types=1);
 namespace Mush\Project\Enum;
 
 enum ProjectType: string
-{
+{   
+    case NULL = '';
     case NERON_PROJECT = 'neron_project';
     case PILGRED = 'pilgred';
     case RESEARCH = 'research';

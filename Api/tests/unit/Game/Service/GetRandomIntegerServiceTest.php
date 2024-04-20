5e9c0b7cadc771145c3e77efa2b230f7d65dd84f
Charles-Meldhine Madi Mnemoi <icdk5b4zf@relay.firefox.com>
2024-04-20 12:05:58 +0200

    test: add tests for new created services

---
+++ Api/tests/unit/Game/Service/GetRandomIntegerServiceTest.php
@@ -0,0 +1,32 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Mush\Tests\unit\Game\Service;
+
+use Mush\Game\Service\GetRandomIntegerService;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * @internal
+ */
+final class GetRandomIntegerServiceTest extends TestCase
+{
+    private GetRandomIntegerService $service;
+
+    /**
+     * @before
+     */
+    public function _before(): void
+    {
+        $this->service = new GetRandomIntegerService();
+    }
+
+    public function testExecute(): void
+    {
+        $result = $this->service->execute(1, 10);
+
+        self::assertGreaterThanOrEqual(1, $result);
+        self::assertLessThanOrEqual(10, $result);
+    }
+}

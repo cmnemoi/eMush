aefb24e206ddd613780d8fd6dbab88581613812e
Charles-Meldhine Madi Mnemoi <icdk5b4zf@relay.firefox.com>
2024-04-20 11:54:52 +0200

    fix action name translation and make projectnormalizer test more exhaustive

--- Api/tests/functional/Project/Normalizer/ProjectNormalizerCest.php
+++ Api/tests/functional/Project/Normalizer/ProjectNormalizerCest.php
@@ -4,8 +4,14 @@ declare(strict_types=1);
 
 namespace Mush\Tests\functional\Project\Normalizer;
 
+use Mush\Action\Enum\ActionEnum;
+use Mush\Equipment\Enum\EquipmentEnum;
+use Mush\Equipment\Enum\EquipmentMechanicEnum;
+use Mush\Equipment\Service\GameEquipmentServiceInterface;
 use Mush\Project\Enum\ProjectName;
 use Mush\Project\Normalizer\ProjectNormalizer;
+use Mush\Status\Enum\PlayerStatusEnum;
+use Mush\Status\Service\StatusServiceInterface;
 use Mush\Tests\AbstractFunctionalTest;
 use Mush\Tests\FunctionalTester;
 use Symfony\Component\Serializer\Normalizer\NormalizerInterface;
@@ -17,12 +23,37 @@ final class ProjectNormalizerCest extends AbstractFunctionalTest
 {
     private ProjectNormalizer $projectNormalizer;
 
+    private GameEquipmentServiceInterface $gameEquipmentService;
+    private StatusServiceInterface $statusService;
+    private int $repairActionId;
+
     public function _before(FunctionalTester $I): void
     {
         parent::_before($I);
 
         $this->projectNormalizer = $I->grabService(ProjectNormalizer::class);
         $this->projectNormalizer->setNormalizer($I->grabService(NormalizerInterface::class));
+
+        $this->gameEquipmentService = $I->grabService(GameEquipmentServiceInterface::class);
+        $this->statusService = $I->grabService(StatusServiceInterface::class);
+
+        // given pilgred terminal in the room
+        $pilgredTerminal = $this->gameEquipmentService->createGameEquipmentFromName(
+            equipmentName: EquipmentEnum::PILGRED,
+            equipmentHolder: $this->chun->getPlace(),
+            reasons: [],
+            time: new \DateTime()
+        );
+
+        // given Chun is focused on the terminal
+        $this->statusService->createStatusFromName(
+            statusName: PlayerStatusEnum::FOCUSED,
+            holder: $this->chun,
+            tags: [],
+            time: new \DateTime()
+        );
+
+        $this->repairActionId = $pilgredTerminal->getEquipment()->getMechanicByName(EquipmentMechanicEnum::TOOL)->getActions()->filter(static fn ($action) => $action->getName() === ActionEnum::REPAIR_PILGRED)->first()->getId();
     }
 
     public function shouldNormalizeProject(FunctionalTester $I): void
@@ -54,7 +85,21 @@ final class ProjectNormalizerCest extends AbstractFunctionalTest
                         'description' => 'Le Technicien est qualifié pour réparer le matériel, les équipements et la coque du Daedalus.//:point: +1 :pa_eng: (point d\'action **Réparation**) par jour.//:point: Chances de réussites doublées pour les **Réparations**.//:point: Chances de réussites doublées pour les **Rénovations**.//:point: Bonus pour développer certains **Projets NERON**.',
                     ],
                 ],
-                'actions' => [],
+                'actions' => [
+                    [
+                        'id' => $this->repairActionId,
+                        'key' => ActionEnum::REPAIR_PILGRED,
+                        'name' => 'Participer',
+                        'actionPointCost' => 2,
+                        'movementPointCost' => 0,
+                        'moralPointCost' => 0,
+                        'shootPointCost' => null,
+                        'successRate' => 100,
+                        'description' => 'Réparer PILGRED vous permettra de revenir sur Sol.',
+                        'canExecute' => true,
+                        'confirmation' => null,
+                    ],
+                ],
             ],
             actual: $normalizedProject
         );

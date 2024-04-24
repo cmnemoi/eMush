f313b7bea55923362d6eafd3fab15a66e59cd060
Evian <icdk5b4zf@relay.firefox.com>
2024-04-24 04:40:06 +0200

    test: Add tests for player efficiency with CPU priority

--- Api/tests/unit/Player/Entity/PlayerTest.php
+++ Api/tests/unit/Player/Entity/PlayerTest.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace Mush\Tests\unit\Player\Entity;
 
+use Mush\Daedalus\Enum\NeronCpuPriorityEnum;
 use Mush\Daedalus\Factory\DaedalusFactory;
 use Mush\Player\Entity\Player;
 use Mush\Player\Factory\PlayerFactory;
@@ -44,6 +45,34 @@ final class PlayerTest extends TestCase
     }
 
     /**
+     * @dataProvider provideShouldReturnPlayerEfficiencyWithCpuPriorityCases
+     */
+    public function testShouldReturnPlayerEfficiencyWithCpuPriority(
+        Project $project,
+        int $numberOfParticipations,
+        PlayerEfficiency $expectedEfficiency
+    ): void {
+        // Given I have a player
+        $daedalus = DaedalusFactory::createDaedalus();
+        $player = PlayerFactory::createPlayerWithDaedalus($daedalus);
+
+        // Given this player has a certain number of participations in a project
+        $this->setPlayerNumberOfParticipations($player, $project, $numberOfParticipations);
+
+        // Given CPU priority is set to projects
+        $daedalus->getDaedalusInfo()->getNeron()->setCpuPriority(NeronCpuPriorityEnum::PROJECTS);
+
+        // When I ask for the player's efficiency in this project
+        $actualEfficiency = $player->getEfficiencyForProject($project);
+
+        // Then I should get the expected efficiency
+        self::assertEquals(
+            expected: $expectedEfficiency,
+            actual: $actualEfficiency
+        );
+    }
+
+    /**
      * Test cases for the getEfficiencyForProject method from Twinpedia: http://twin.tithom.fr/mush/stats/efficacite/.
      *
      * Example of a test case:
@@ -69,6 +98,36 @@ final class PlayerTest extends TestCase
             [$autoWatering, 0, new PlayerEfficiency(3, 4)],
             [$autoWatering, 1, new PlayerEfficiency(1, 1)],
             [$autoWatering, 2, new PlayerEfficiency(0, 0)],
+        ];
+    }
+
+    /**
+     * Test cases for the getEfficiencyForProject with CPU priority method from Twinpedia: http://twin.tithom.fr/mush/stats/efficacite/.
+     *
+     * Example of a test case:
+     *
+     * [$traileReducer, 0, new PlayerEfficiency(7, 10)] => For Trail Reducuer project, with 0 participations,
+     * the efficiency should be 7%-10%.
+     */
+    public static function provideShouldReturnPlayerEfficiencyWithCpuPriorityCases(): iterable
+    {
+        $plasmaShield = ProjectFactory::createPilgredProject();
+        $trailReducer = ProjectFactory::createTrailReducerProject();
+        $autoWatering = ProjectFactory::createAutoWateringProject();
+
+        return [
+            [$plasmaShield, 0, new PlayerEfficiency(2, 3)],
+            [$plasmaShield, 1, new PlayerEfficiency(1, 1)],
+            [$plasmaShield, 2, new PlayerEfficiency(1, 1)],
+            [$trailReducer, 0, new PlayerEfficiency(7, 10)],
+            [$trailReducer, 1, new PlayerEfficiency(5, 7)],
+            [$trailReducer, 2, new PlayerEfficiency(3, 4)],
+            [$trailReducer, 3, new PlayerEfficiency(1, 1)],
+            [$trailReducer, 4, new PlayerEfficiency(1, 1)],
+            [$autoWatering, 0, new PlayerEfficiency(4, 6)],
+            [$autoWatering, 1, new PlayerEfficiency(2, 3)],
+            [$autoWatering, 2, new PlayerEfficiency(1, 1)],
+            [$autoWatering, 3, new PlayerEfficiency(1, 1)],
         ];
     }
 

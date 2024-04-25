c307a38c4a1f4b8c8c1a0b56dd9a0680d780c78f
Charles-Meldhine Madi Mnemoi <icdk5b4zf@relay.firefox.com>
2024-04-25 19:36:41 +0200

    refactor: move common code to an AbstractParticipateAction

---
+++ Api/src/Action/Actions/AbstractParticipateAction.php
@@ -0,0 +1,76 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Mush\Action\Actions;
+
+use Mush\Action\Entity\ActionResult\ActionResult;
+use Mush\Action\Entity\ActionResult\Success;
+use Mush\Action\Enum\ActionImpossibleCauseEnum;
+use Mush\Action\Service\ActionServiceInterface;
+use Mush\Action\Validator\NoEfficiency;
+use Mush\Action\Validator\Reach;
+use Mush\Action\Validator\TargetProjectFinished;
+use Mush\Equipment\Enum\ReachEnum;
+use Mush\Game\Service\EventServiceInterface;
+use Mush\Project\Entity\Project;
+use Mush\Project\Event\ProjectEvent;
+use Mush\Project\UseCase\AdvanceProjectUseCase;
+use Mush\RoomLog\Entity\LogParameterInterface;
+use Symfony\Component\Validator\Mapping\ClassMetadata;
+use Symfony\Component\Validator\Validator\ValidatorInterface;
+
+abstract class AbstractParticipateAction extends AbstractAction
+{
+    protected string $name;
+
+    protected AdvanceProjectUseCase $advanceProjectUseCase;
+
+    public function __construct(
+        EventServiceInterface $eventService,
+        ActionServiceInterface $actionService,
+        ValidatorInterface $validator,
+        AdvanceProjectUseCase $advanceProjectUseCase
+    ) {
+        parent::__construct(
+            $eventService,
+            $actionService,
+            $validator
+        );
+
+        $this->advanceProjectUseCase = $advanceProjectUseCase;
+    }
+
+    public static function loadValidatorMetadata(ClassMetadata $metadata): void
+    {
+        $metadata->addConstraint(new Reach(['reach' => ReachEnum::ROOM, 'groups' => ['visibility']]));
+        $metadata->addConstraint(new TargetProjectFinished(['groups' => ['visibility']]));
+        $metadata->addConstraint(new NoEfficiency(['groups' => ['execute'], 'message' => ActionImpossibleCauseEnum::NO_EFFICIENCY]));
+    }
+
+    protected function support(?LogParameterInterface $target, array $parameters): bool
+    {
+        return $target instanceof Project;
+    }
+
+    protected function checkResult(): ActionResult
+    {
+        return new Success();
+    }
+
+    protected function applyEffect(ActionResult $result): void
+    {
+        /** @var Project $project */
+        $project = $this->target;
+
+        $this->advanceProjectUseCase->execute($this->player, $project);
+
+        $projectEvent = new ProjectEvent(
+            $project,
+            author: $this->player,
+            tags: $this->getAction()->getActionTags(),
+        );
+
+        $this->eventService->callEvent($projectEvent, ProjectEvent::PROJECT_ADVANCED);
+    }
+}
--- Api/src/Action/Actions/Participate.php
+++ Api/src/Action/Actions/Participate.php
@@ -4,50 +4,18 @@ declare(strict_types=1);
 
 namespace Mush\Action\Actions;
 
-use Mush\Action\Entity\ActionResult\ActionResult;
-use Mush\Action\Entity\ActionResult\Success;
 use Mush\Action\Enum\ActionEnum;
 use Mush\Action\Enum\ActionImpossibleCauseEnum;
-use Mush\Action\Service\ActionServiceInterface;
 use Mush\Action\Validator\HasStatus;
-use Mush\Action\Validator\NoEfficiency;
-use Mush\Action\Validator\Reach;
-use Mush\Action\Validator\TargetProjectFinished;
-use Mush\Equipment\Enum\ReachEnum;
-use Mush\Game\Service\EventServiceInterface;
-use Mush\Project\Entity\Project;
-use Mush\Project\Event\ProjectEvent;
-use Mush\Project\UseCase\AdvanceProjectUseCase;
-use Mush\RoomLog\Entity\LogParameterInterface;
 use Mush\Status\Enum\PlayerStatusEnum;
 use Symfony\Component\Validator\Mapping\ClassMetadata;
-use Symfony\Component\Validator\Validator\ValidatorInterface;
 
-final class Participate extends AbstractAction
+final class Participate extends AbstractParticipateAction
 {
     protected string $name = ActionEnum::PARTICIPATE;
 
-    private AdvanceProjectUseCase $advanceProjectUseCase;
-
-    public function __construct(
-        EventServiceInterface $eventService,
-        ActionServiceInterface $actionService,
-        ValidatorInterface $validator,
-        AdvanceProjectUseCase $advanceProjectUseCase
-    ) {
-        parent::__construct(
-            $eventService,
-            $actionService,
-            $validator
-        );
-
-        $this->advanceProjectUseCase = $advanceProjectUseCase;
-    }
-
     public static function loadValidatorMetadata(ClassMetadata $metadata): void
     {
-        $metadata->addConstraint(new Reach(['reach' => ReachEnum::ROOM, 'groups' => ['visibility']]));
-        $metadata->addConstraint(new TargetProjectFinished(['groups' => ['visibility']]));
         $metadata->addConstraint(new HasStatus([
             'status' => PlayerStatusEnum::DIRTY,
             'target' => HasStatus::PLAYER,
@@ -55,32 +23,5 @@ final class Participate extends AbstractAction
             'groups' => ['execute'],
             'message' => ActionImpossibleCauseEnum::DIRTY_RESTRICTION,
         ]));
-        $metadata->addConstraint(new NoEfficiency(['groups' => ['execute'], 'message' => ActionImpossibleCauseEnum::NO_EFFICIENCY]));
-    }
-
-    protected function support(?LogParameterInterface $target, array $parameters): bool
-    {
-        return $target instanceof Project;
-    }
-
-    protected function checkResult(): ActionResult
-    {
-        return new Success();
-    }
-
-    protected function applyEffect(ActionResult $result): void
-    {
-        /** @var Project $project */
-        $project = $this->target;
-
-        $this->advanceProjectUseCase->execute($this->player, $project);
-
-        $projectEvent = new ProjectEvent(
-            $project,
-            author: $this->player,
-            tags: $this->getAction()->getActionTags(),
-        );
-
-        $this->eventService->callEvent($projectEvent, ProjectEvent::PROJECT_ADVANCED);
     }
 }

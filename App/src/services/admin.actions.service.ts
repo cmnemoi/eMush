1b34022727a244506109b4168666247c39ff425a
Evian <icdk5b4zf@relay.firefox.com>
2024-04-28 19:14:09 +0200

    fix HTTP method for the endpoint used

--- App/src/services/admin.actions.service.ts
+++ App/src/services/admin.actions.service.ts
@@ -9,7 +9,7 @@ const AdminActionsService = {
         return await ApiService.post(ADMIN_ACTIONS_ENDPOINT + '/create-all-projects-for-on-going-daedaluses');
     },
     proposeNewNeronProjectsForOnGoingDaedaluses: async(): Promise<any> => {
-        return await ApiService.post(ADMIN_ACTIONS_ENDPOINT + '/propose-new-neron-projects-for-on-going-daedaluses');
+        return await ApiService.put(ADMIN_ACTIONS_ENDPOINT + '/propose-new-neron-projects-for-on-going-daedaluses');
     }
 };
 export default AdminActionsService;

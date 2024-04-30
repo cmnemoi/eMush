3c229bba00bc0632d117decb7b6fca1c922fab89
Sylvain Breton <sylvain.breton.6@gmail.com>
2024-04-30 11:08:51 +0200

    internal: reworked the action target and scope enum

--- Api/src/Action/Enum/ActionScopeEnum.php
+++ Api/src/Action/Enum/ActionScopeEnum.php
@@ -4,23 +4,17 @@ namespace Mush\Action\Enum;
 
 /**
  * Class enumerating the Action scopes
- * Scope describe the relation between the active player and the entity that provide the action.
+ * Scope describes how "far" an action provider can provide an action
  *
- * SELF: the action is provided by the active player
- * CURRENT: the action is provided by a game equipment (e.g. take action is proposed when targeting an item)
- * OTHERPLAYER: the action is provided by a targeted character (different from the active player)
- * INVENTORY: the action is provided by a tool (tool effect only apply on hold item)
- * ROOM: the action is provided by a tool (either in inventory or in the room)
- * SHELVE: the action is provided by a tool (tool effect only apply on equipment on the shelf)
- * TERMINAL: the action is provided if player is focused on a terminal
+ * SELF: the action provider is the action target
+ * INVENTORY: the action provider is in inventory
+ * ROOM: the action provider is in room
+ * TERMINAL: the action is provided if player is focussed on a terminal
  */
 abstract class ActionScopeEnum
 {
     public const string SELF = 'self';
-    public const string CURRENT = 'current';
-    public const string OTHER_PLAYER = 'other_player';
     public const string INVENTORY = 'inventory';
     public const string ROOM = 'room';
-    public const string SHELVE = 'shelve';
     public const string TERMINAL = 'terminal';
 }
--- Api/src/Action/Enum/ActionTargetName.php
+++ Api/src/Action/Enum/ActionTargetName.php
@@ -4,14 +4,15 @@ declare(strict_types=1);
 
 namespace Mush\Action\Enum;
 
-enum ActionTargetName: string
+abstract class ActionTargetName: string
 {
-    case PLAYER = 'player';
-    case DOOR = 'door';
-    case ITEM = 'item';
-    case EQUIPMENT = 'equipment';
-    case HUNTER = 'hunter';
-    case TERMINAL = 'terminal';
-    case PLANET = 'planet';
-    case PROJECT = 'project';
+    public const string PLAYER = 'player';
+    public const string OTHER_PLAYER = 'other_player';
+    public const string DOOR = 'door';
+    public const string ITEM = 'item';
+    public const string EQUIPMENT = 'equipment';
+    public const string HUNTER = 'hunter';
+    public const string TERMINAL = 'terminal';
+    public const string PLANET = 'planet';
+    public const string PROJECT = 'project';
 }

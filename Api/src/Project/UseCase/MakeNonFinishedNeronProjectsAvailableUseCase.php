46eefdb7f5ea7d76b5ab540ef3ed02e4905ff585
Evian <icdk5b4zf@relay.firefox.com>
2024-05-13 22:51:28 +0200

    Revert "fix bug"
    This reverts commit ac45e44a47bafa0fc27a57b0b16b43a00b01927f.

--- Api/src/Project/UseCase/MakeNonFinishedNeronProjectsAvailableUseCase.php
+++ Api/src/Project/UseCase/MakeNonFinishedNeronProjectsAvailableUseCase.php
@@ -14,9 +14,9 @@ final class MakeNonFinishedNeronProjectsAvailableUseCase
 
     public function execute(Daedalus $daedalus): void
     {
-        $projects = $daedalus->getFinishedNeronProjects();
+        $projects = $daedalus->getNonFinishedNeronProjects();
 
-        $projects->map(static fn (Project $project) => $project->unpropose());
+        $projects->map(static fn (Project $project) => $project->makeAvailable());
         $projects->map(fn (Project $project) => $this->projectRepository->save($project));
     }
 }

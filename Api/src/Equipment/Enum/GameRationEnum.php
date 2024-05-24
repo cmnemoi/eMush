fcfdd8bfdd4af9d05f857d13ca6847a171290d14
Evian <icdk5b4zf@relay.firefox.com>
2024-05-24 18:57:19 +0200

    refactor: remove more dead code

--- Api/src/Equipment/Enum/GameRationEnum.php
+++ Api/src/Equipment/Enum/GameRationEnum.php
@@ -4,8 +4,6 @@ declare(strict_types=1);
 
 namespace Mush\Equipment\Enum;
 
-use Doctrine\Common\Collections\ArrayCollection;
-
 abstract class GameRationEnum
 {
     public const LOMBRICK_BAR = 'lombrick_bar';
@@ -19,20 +17,4 @@ abstract class GameRationEnum
     public const STANDARD_RATION = 'standard_ration';
     public const SUPERVITAMIN_BAR = 'supervitamin_bar';
     public const NULL = '';
-
-    public static function getAll(): ArrayCollection
-    {
-        return new ArrayCollection([
-            self::LOMBRICK_BAR,
-            self::ORGANIC_WASTE,
-            self::PROACTIVE_PUFFED_RICE,
-            self::ALIEN_STEAK,
-            self::ANABOLIC,
-            self::COFFEE,
-            self::COOKED_RATION,
-            self::SPACE_POTATO,
-            self::STANDARD_RATION,
-            self::SUPERVITAMIN_BAR,
-        ]);
-    }
 }

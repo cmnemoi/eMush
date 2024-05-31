d3c82f73827e04ca0a6143faecdfd097d81be516
Evian <icdk5b4zf@relay.firefox.com>
2024-06-01 01:12:02 +0200

    Add french translations

---
+++ Api/tests/functional/Status/Normalizer/StatusNormalizerCest.php
@@ -0,0 +1,48 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Mush\Tests\Functional\Status\Normalizer;
+
+use Mush\Game\Enum\SkillEnum;
+use Mush\Status\Normalizer\StatusNormalizer;
+use Mush\Status\Service\StatusServiceInterface;
+use Mush\Tests\AbstractFunctionalTest;
+use Mush\Tests\FunctionalTester;
+
+final class StatusNormalizerCest extends AbstractFunctionalTest
+{
+    private StatusNormalizer $normalizer;
+
+    private StatusServiceInterface $statusService;
+
+    public function _before(FunctionalTester $I): void
+    {   
+        parent::_before($I);
+        $this->normalizer = $I->grabService(StatusNormalizer::class);
+
+        $this->statusService = $I->grabService(StatusServiceInterface::class);
+    }
+
+    public function shouldNormalizeShrinkSkill(FunctionalTester $I): void
+    {
+        $status = $this->statusService->createStatusFromName(
+            statusName: SkillEnum::SHRINK,
+            holder: $this->chun,
+            tags: [],
+            time: new \DateTime(),
+        );
+
+        $normalizedStatus = $this->normalizer->normalize($status, format: null, context: ['currentPlayer' => $this->chun]);
+
+        $I->assertEquals(
+            expected: [
+                'key' => SkillEnum::SHRINK,
+                'name' => 'Psy',
+                'description' => ':point: A chaque cycle, **1 Point de Moral (:pmo:) est régénéré** à chaque personnage allongé dans sa pièce.//:point: **Soigne les maladies Psy**.//:point: Accorde l\'action **Réconforter**, laquelle améliore le moral.//:point: Bonus pour développer certains **Projets NERON**.',
+                'isPrivate' => false,
+            ],
+            actual: $normalizedStatus,
+        );
+    }
+}

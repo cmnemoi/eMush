276e24d345c3e7fc3e5bd49a2b29b8cca91424e6
Evian <icdk5b4zf@relay.firefox.com>
2024-05-31 22:54:57 +0200

    fix migration

--- Api/migrations/Version20240531205330.php
+++ Api/migrations/Version20240531205330.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20240526160823 extends AbstractMigration
+final class Version20240531205330 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -20,17 +20,22 @@ final class Version20240526160823 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE moderationsanction ADD player_id INT DEFAULT NULL');
         $this->addSql('ALTER TABLE moderationsanction ADD author_id INT DEFAULT NULL');
+        $this->addSql('ALTER TABLE moderationsanction ADD CONSTRAINT FK_CB19D91B99E6F5DF FOREIGN KEY (player_id) REFERENCES player_info (id) NOT DEFERRABLE INITIALLY IMMEDIATE');
         $this->addSql('ALTER TABLE moderationsanction ADD CONSTRAINT FK_CB19D91BF675F31B FOREIGN KEY (author_id) REFERENCES users (id) NOT DEFERRABLE INITIALLY IMMEDIATE');
+        $this->addSql('CREATE INDEX IDX_CB19D91B99E6F5DF ON moderationsanction (player_id)');
         $this->addSql('CREATE INDEX IDX_CB19D91BF675F31B ON moderationsanction (author_id)');
     }
 
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
-        $this->addSql('CREATE SCHEMA public');
+        $this->addSql('ALTER TABLE moderationSanction DROP CONSTRAINT FK_CB19D91B99E6F5DF');
         $this->addSql('ALTER TABLE moderationSanction DROP CONSTRAINT FK_CB19D91BF675F31B');
+        $this->addSql('DROP INDEX IDX_CB19D91B99E6F5DF');
         $this->addSql('DROP INDEX IDX_CB19D91BF675F31B');
+        $this->addSql('ALTER TABLE moderationSanction DROP player_id');
         $this->addSql('ALTER TABLE moderationSanction DROP author_id');
     }
 }

46342ba42d42a9271b10c25d7d665be8ee621670
Sylvain Breton <sylvain.breton.6@gmail.com>
2024-06-03 16:10:26 +0200

    feat: added a route to get report for a given player

--- Api/src/MetaGame/Controller/ModerationSanctionController.php
+++ Api/src/MetaGame/Controller/ModerationSanctionController.php
@@ -8,11 +8,14 @@ use FOS\RestBundle\Controller\AbstractFOSRestController;
 use FOS\RestBundle\Controller\Annotations as Rest;
 use FOS\RestBundle\View\View;
 use Mush\MetaGame\Repository\ModerationSanctionRepository;
+use Mush\Player\Entity\Player;
+use Mush\Player\Entity\PlayerInfo;
 use Mush\User\Entity\User;
 use Nelmio\ApiDocBundle\Annotation\Security;
 use OpenApi\Annotations as OA;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Exception\HttpException;
 use Symfony\Component\Routing\Annotation\Route;
 
 /**
@@ -52,5 +55,43 @@ final class ModerationSanctionController extends AbstractFOSRestController
         $warnings = $this->moderationSanctionRepository->findAllUserActiveBansAndWarnings($user);
 
         return $this->view($warnings, Response::HTTP_OK);
+    }
+
+    /**
+     * Get player reports.
+     *
+     * @OA\Parameter(
+     *      name="id",
+     *      in="path",
+     *      description="The player info id",
+     *
+     *       @OA\Schema(type="string")
+     * )
+     *
+     * @OA\Tag(name="moderationSanction")
+     *
+     * @Security(name="Bearer")
+     *
+     * @Rest\Get(path="/{id}/reports")
+     *
+     * @Rest\View()
+     *
+     * @OA\Tag(name="Moderation")
+     *
+     * @Security(name="Bearer")
+ */
+    public function getPlayerActiveReports(PlayerInfo $player): View
+    {
+        $moderator = $this->getUser();
+        if (!$moderator instanceof User) {
+            throw new HttpException(Response::HTTP_UNAUTHORIZED, 'Request author user not found');
+        }
+        if (!$moderator->isModerator()) {
+            throw new HttpException(Response::HTTP_FORBIDDEN, 'Only moderators can use this endpoint!');
+        }
+
+        $reports = $this->moderationSanctionRepository->findAllPlayerReports($player);
+
+        return $this->view($reports, Response::HTTP_OK);
     }
 }

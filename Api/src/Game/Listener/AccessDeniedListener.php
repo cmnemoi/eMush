17d3a9d3d9a22341ef716113ebbf0f8160835e4b
Charles-Meldhine Madi Mnemoi <icdk5b4zf@relay.firefox.com>
2024-07-11 09:08:08 +0000

    Trying to get the real error message for 500 errors in production

--- Api/src/Game/Listener/AccessDeniedListener.php
+++ Api/src/Game/Listener/AccessDeniedListener.php
@@ -7,6 +7,8 @@ namespace Mush\Game\Listener;
 use Mush\Game\Enum\EventPriorityEnum;
 use Mush\Game\Security\AccessDeniedExceptionDto;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Event\ExceptionEvent;
 use Symfony\Component\HttpKernel\KernelEvents;
 use Symfony\Component\Security\Core\Exception\AccessDeniedException;
@@ -23,12 +25,49 @@ final class AccessDeniedListener implements EventSubscriberInterface
     public function onKernelException(ExceptionEvent $event): void
     {
         $exception = $event->getThrowable();
-        if (!$exception instanceof AccessDeniedException) {
-            return;
+        if ($exception instanceof AccessDeniedException) {
+            $exceptionDto = new AccessDeniedExceptionDto($exception);
+        } elseif ($event->getResponse()->getStatusCode() === 500) {
+            $exceptionDto = new InternalServerErrorExceptionDto($exception);
         }
 
-        $exceptionDto = new AccessDeniedExceptionDto($exception);
+        if (isset($exceptionDto)) {
+            $event->setResponse($exceptionDto->toJsonResponse());
+        }
+    }
+}
 
-        $event->setResponse($exceptionDto->toJsonResponse());
+final readonly class InternalServerErrorExceptionDto
+{
+    public string $class;
+    public string $detail;
+    public int $status;
+    public string $title;
+    public string $trace;
+
+    public function __construct(
+        private \Throwable $exception,
+    ) {
+        $this->class = $this->exception::class;
+        $this->detail = $this->exception->getMessage();
+        $this->status = Response::HTTP_INTERNAL_SERVER_ERROR;
+        $this->title = 'Internal Server Error';
+        $this->trace = $this->exception->getTraceAsString();
+    }
+
+    public function toJsonResponse(): JsonResponse
+    {
+        return new JsonResponse($this->toArray(), $this->status);
+    }
+
+    private function toArray(): array
+    {
+        return [
+            'class' => $this->class,
+            'detail' => $this->detail,
+            'status' => $this->status,
+            'title' => $this->title,
+            'trace' => $this->trace,
+        ];
     }
 }

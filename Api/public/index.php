e75e5d16e757b344c9a115055e4bb1cfd6cc5023
Evian <icdk5b4zf@relay.firefox.com>
2024-07-29 22:48:39 +0200

    revert debug changes

--- Api/public/index.php
+++ Api/public/index.php
@@ -7,8 +7,6 @@ use Symfony\Component\HttpFoundation\Request;
 
 require dirname(__DIR__) . '/vendor/autoload.php';
 
-// hello, world!
-
 (new Dotenv())->bootEnv(dirname(__DIR__) . '/.env');
 
 if ($_SERVER['APP_DEBUG']) {

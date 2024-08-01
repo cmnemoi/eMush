d30905f5a7da0bb0705b8603ff8608df0a7ac8e1
Evian <icdk5b4zf@relay.firefox.com>
2024-08-01 21:11:49 +0200

    feat(skill): Firefighter

--- Api/src/Player/DataFixtures/CharacterConfigFixtures.php
+++ Api/src/Player/DataFixtures/CharacterConfigFixtures.php
@@ -66,6 +66,9 @@ class CharacterConfigFixtures extends Fixture implements DependentFixtureInterfa
         /** @var SkillConfig $leaderSkillConfig */
         $leaderSkillConfig = $this->getReference(SkillEnum::LEADER->value);
 
+        /** @var SkillConfig $firefighterSkillConfig */
+        $firefighterSkillConfig = $this->getReference(SkillEnum::FIREFIGHTER->value);
+
         $andie = $this->buildDefaultCharacterConfig();
         $andie
             ->setName(CharacterEnum::ANDIE)
@@ -109,6 +112,7 @@ class CharacterConfigFixtures extends Fixture implements DependentFixtureInterfa
             ->setCharacterName(CharacterEnum::DEREK)
             ->setSkillConfigs([
                 $shooterSkillConfig,
+                $firefighterSkillConfig,
             ])
             ->setInitStatuses([$firstTimeStatus])
             ->setStartingItems($trackerTalkieCollection);
@@ -172,6 +176,9 @@ class CharacterConfigFixtures extends Fixture implements DependentFixtureInterfa
         $ian
             ->setName(CharacterEnum::IAN)
             ->setCharacterName(CharacterEnum::IAN)
+            ->setSkillConfigs([
+                $firefighterSkillConfig,
+            ])
             ->setInitStatuses([$pacifistStatus])
             ->setStartingItems($trackerTalkieCollection);
         $manager->persist($ian);
@@ -241,6 +248,7 @@ class CharacterConfigFixtures extends Fixture implements DependentFixtureInterfa
             ->setSkillConfigs([
                 $pilotSkillConfig,
                 $shooterSkillConfig,
+                $firefighterSkillConfig,
             ])
             ->setStartingItems($trackerTalkieCollection);
         $manager->persist($roland);
@@ -358,9 +366,6 @@ class CharacterConfigFixtures extends Fixture implements DependentFixtureInterfa
         /** @var ActionConfig $boringSpeechAction */
         $boringSpeechAction = $this->getReference(ActionsFixtures::BORING_SPEECH);
 
-        /** @var ActionConfig $extinguishManuallyAction */
-        $extinguishManuallyAction = $this->getReference(ActionsFixtures::EXTINGUISH_MANUALLY);
-
         /** @var ActionConfig $surgeryAction */
         $surgeryAction = $this->getReference(ActionsFixtures::SURGERY);
 
@@ -382,7 +387,6 @@ class CharacterConfigFixtures extends Fixture implements DependentFixtureInterfa
             $infectAction,
             $extractSporeAction,
             $getUpAction,
-            $extinguishManuallyAction,
             $boringSpeechAction,
             $healAction,
             $selfHealAction,

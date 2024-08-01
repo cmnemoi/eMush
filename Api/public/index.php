9c3ca4efa9d8a4c5ba8e022f5b631ece80df9e84
Evian <icdk5b4zf@relay.firefox.com>
2024-08-01 23:27:02 +0200

    dummy commit to trigger php tests

--- Api/public/index.php
+++ Api/public/index.php
@@ -5,6 +5,7 @@ use Symfony\Component\Dotenv\Dotenv;
 use Symfony\Component\ErrorHandler\Debug;
 use Symfony\Component\HttpFoundation\Request;
 
+// Hello, World!
 require dirname(__DIR__) . '/vendor/autoload.php';
 
 (new Dotenv())->bootEnv(dirname(__DIR__) . '/.env');

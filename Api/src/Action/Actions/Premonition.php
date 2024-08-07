9d50a875712476cbe1c4e63547add63060591d8e
Charles-Meldhine Madi Mnemoi <icdk5b4zf@relay.firefox.com>
2024-08-07 14:46:48 +0000

    refactor: Chitchat and Premonition now uses a common service to generate the revealed action log

--- Api/src/Action/Actions/Premonition.php
+++ Api/src/Action/Actions/Premonition.php
@@ -8,16 +8,13 @@ use Mush\Action\Entity\ActionResult\ActionResult;
 use Mush\Action\Entity\ActionResult\Success;
 use Mush\Action\Enum\ActionEnum;
 use Mush\Action\Enum\ActionImpossibleCauseEnum;
+use Mush\Action\Service\ActionHistoryLogService;
 use Mush\Action\Service\ActionServiceInterface;
 use Mush\Action\Validator\GameVariableLevel;
-use Mush\Game\Enum\VisibilityEnum;
 use Mush\Game\Service\EventServiceInterface;
-use Mush\Game\Service\TranslationServiceInterface;
 use Mush\Player\Entity\Player;
 use Mush\Player\Enum\PlayerVariableEnum;
 use Mush\RoomLog\Entity\LogParameterInterface;
-use Mush\RoomLog\Enum\LogEnum;
-use Mush\RoomLog\Service\RoomLogServiceInterface;
 use Symfony\Component\Validator\Mapping\ClassMetadata;
 use Symfony\Component\Validator\Validator\ValidatorInterface;
 
@@ -29,8 +26,7 @@ final class Premonition extends AbstractAction
         EventServiceInterface $eventService,
         ActionServiceInterface $actionService,
         ValidatorInterface $validator,
-        private RoomLogServiceInterface $roomLogService,
-        private TranslationServiceInterface $translationService,
+        private ActionHistoryLogService $actionHistoryLog,
     ) {
         parent::__construct($eventService, $actionService, $validator);
     }
@@ -61,56 +57,11 @@ final class Premonition extends AbstractAction
 
     protected function applyEffect(ActionResult $result): void
     {
-        $this->createRevealedActionLog();
+        $this->actionHistoryLog->generate($this->numberOfActionsToReveal(), $this);
     }
 
-    private function createRevealedActionLog(): void
-    {
-        $actions = $this->getTranslatedPlayerActions(player: $this->targetPlayer(), limit: $this->numberOfActionsRevealed());
-
-        $this->roomLogService->createLog(
-            logKey: LogEnum::PREMONITION_ACTION,
-            place: $this->player->getPlace(),
-            visibility: VisibilityEnum::PRIVATE,
-            type: 'event_log',
-            player: $this->player,
-            parameters: $this->getLogParametersFromActions($this->targetPlayer(), $actions),
-        );
-    }
-
-    private function getTranslatedPlayerActions(Player $player, int $limit): array
-    {
-        $actions = [];
-        foreach ($player->getActionHistory(limit: $limit) as $action) {
-            $translatedAction = $this->translationService->translate(
-                key: sprintf('%s.name', $action),
-                parameters: [],
-                domain: 'actions',
-                language: $player->getLanguage(),
-            );
-            $actions[] = sprintf('**%s**', $translatedAction);
-        }
-
-        return $actions;
-    }
-
-    private function getLogParametersFromActions(Player $player, array $actions): array
-    {
-        return [
-            sprintf('target_%s', $player->getLogKey()) => $player->getLogName(),
-            'actions' => implode(', ', \array_slice($actions, offset: 0, length: -1)),
-            'lastAction' => end($actions),
-            'quantity' => \count($actions),
-        ];
-    }
-
-    private function numberOfActionsRevealed(): int
+    private function numberOfActionsToReveal(): int
     {
         return $this->getOutputQuantity();
-    }
-
-    private function targetPlayer(): Player
-    {
-        return $this->target;
     }
 }

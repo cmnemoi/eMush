000a077b269695003bee6d8ede875d02fcf55dd8
Evian <icdk5b4zf@relay.firefox.com>
2024-08-08 22:52:58 +0000

    fix typos

--- Api/src/Action/Validator/Guardian.php
+++ Api/src/Action/Validator/Guardian.php
@@ -5,7 +5,7 @@ declare(strict_types=1);
 namespace Mush\Action\Validator;
 
 /**
- * Raises a violation if player wants to fo to a romm different from their previous one under a guardian's presence.
+ * Raises a violation if player wants to go to a room different from their previous one under a guardian's presence.
  */
 final class Guardian extends ClassConstraint
 {

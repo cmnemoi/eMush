1dd2864fe8f1ae5d95ada7eb756bb9a38bdac1ff
Evian <icdk5b4zf@relay.firefox.com>
2024-08-24 23:43:36 +0200

    add a test

--- Api/tests/functional/Action/Normalizer/ActionNormalizerCest.php
+++ Api/tests/functional/Action/Normalizer/ActionNormalizerCest.php
@@ -9,6 +9,9 @@ use Mush\Action\Enum\ActionEnum;
 use Mush\Action\Enum\ActionHolderEnum;
 use Mush\Action\Enum\ActionRangeEnum;
 use Mush\Action\Normalizer\ActionNormalizer;
+use Mush\Equipment\Entity\GameEquipment;
+use Mush\Equipment\Enum\EquipmentEnum;
+use Mush\Equipment\Service\GameEquipmentServiceInterface;
 use Mush\Status\Entity\ChargeStatus;
 use Mush\Status\Enum\PlayerStatusEnum;
 use Mush\Status\Service\StatusServiceInterface;
@@ -22,12 +25,15 @@ final class ActionNormalizerCest extends AbstractFunctionalTest
 {
     private ActionNormalizer $normalizer;
 
+    private GameEquipmentServiceInterface $gameEquipmentService;
     private StatusServiceInterface $statusService;
 
     public function _before(FunctionalTester $I): void
     {
         parent::_before($I);
         $this->normalizer = $I->grabService(ActionNormalizer::class);
+
+        $this->gameEquipmentService = $I->grabService(GameEquipmentServiceInterface::class);
         $this->statusService = $I->grabService(StatusServiceInterface::class);
     }
 
@@ -71,6 +77,67 @@ final class ActionNormalizerCest extends AbstractFunctionalTest
                 'description' => 'Permet de piéger la pièce, toutes les interactions avec les objets et équipements de la pièce déclencheront le piège.//Cette action est **Discrète**. Elle sera révélée par les **Caméras** et les **Équipiers**, y compris ceux de **votre camp**.',
                 'canExecute' => true,
                 'confirmation' => null,
+            ],
+            actual: $normalizedAction,
+        );
+    }
+
+    public function shouldNormalizeDiySurgeryAction(FunctionalTester $I): void
+    {
+        // given a surgery plot in the room
+        $surgeryPlot = $this->gameEquipmentService->createGameEquipmentFromName(
+            equipmentName: EquipmentEnum::SURGERY_PLOT,
+            equipmentHolder: $this->chun->getPlace(),
+            reasons: [],
+            time: new \DateTime(),
+        );
+
+        // given a bed in the room
+        $bed = $this->gameEquipmentService->createGameEquipmentFromName(
+            equipmentName: EquipmentEnum::BED,
+            equipmentHolder: $this->chun->getPlace(),
+            reasons: [],
+            time: new \DateTime(),
+        );
+
+        // given Chun is laid down on the bed
+        $this->statusService->createStatusFromName(
+            statusName: PlayerStatusEnum::LYING_DOWN,
+            holder: $this->chun,
+            tags: [],
+            time: new \DateTime(),
+            target: $bed,
+        );
+
+        // given self surgery action
+        $action = $surgeryPlot
+            ->getActions($this->chun, ActionHolderEnum::EQUIPMENT)
+            ->filter(static fn (Action $action) => $action->getActionConfig()->getActionName() === ActionEnum::SELF_SURGERY)
+            ->first();
+
+        // when I normalize the action
+        $normalizedAction = $this->normalizer->normalize($action, format: null, context: [
+            'currentPlayer' => $this->chun,
+            $surgeryPlot->getClassName() => $surgeryPlot,
+        ]);
+
+        // then I should see the normalized action
+        $I->assertEquals(
+            expected: [
+                'id' => $action->getActionConfig()->getId(),
+                'key' => 'self_surgery',
+                'actionProvider' => [
+                    'id' => $surgeryPlot->getId(),
+                    'class' => GameEquipment::class,
+                ],
+                'actionPointCost' => 4,
+                'moralPointCost' => 0,
+                'movementPointCost' => 0,
+                'skillPointCosts' => [],
+                'successRate' => 100,
+                'name' => 'Chirurgie-Auto',
+                'description' => 'Vous n\'êtes pas blessée, mais ça peut s\'arranger...',
+                'canExecute' => false,
             ],
             actual: $normalizedAction,
         );

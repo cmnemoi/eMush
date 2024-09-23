45b6d68b66c2d0a033495bbf49dd7a744d3b66f0
Evian <icdk5b4zf@relay.firefox.com>
2024-09-23 15:40:46 +0200

    refactor: Move common logic to abstract class

---
+++ Api/src/Action/Actions/AbstractUpgradeDrone.php
@@ -0,0 +1,185 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Mush\Action\Actions;
+
+use Doctrine\Common\Collections\Collection;
+use Mush\Action\Entity\ActionResult\ActionResult;
+use Mush\Action\Entity\ActionResult\Success;
+use Mush\Action\Enum\ActionEnum;
+use Mush\Action\Enum\ActionImpossibleCauseEnum;
+use Mush\Action\Service\ActionServiceInterface;
+use Mush\Action\Validator\ClassConstraint;
+use Mush\Action\Validator\HasEquipment;
+use Mush\Action\Validator\HasSkill;
+use Mush\Equipment\Entity\Drone;
+use Mush\Equipment\Entity\GameEquipment;
+use Mush\Equipment\Enum\ItemEnum;
+use Mush\Equipment\Enum\ReachEnum;
+use Mush\Equipment\Event\EquipmentEvent;
+use Mush\Equipment\Event\InteractWithEquipmentEvent;
+use Mush\Game\Enum\VisibilityEnum;
+use Mush\Game\Service\EventServiceInterface;
+use Mush\Game\Service\TranslationServiceInterface;
+use Mush\RoomLog\Entity\LogParameterInterface;
+use Mush\RoomLog\Enum\ActionLogEnum;
+use Mush\RoomLog\Service\RoomLogServiceInterface;
+use Mush\Skill\Enum\SkillEnum;
+use Mush\Status\Enum\EquipmentStatusEnum;
+use Mush\Status\Service\StatusServiceInterface;
+use Symfony\Component\Validator\Mapping\ClassMetadata;
+use Symfony\Component\Validator\Validator\ValidatorInterface;
+
+abstract class AbstractUpgradeDrone extends AbstractAction
+{
+    public function __construct(
+        EventServiceInterface $eventService,
+        ActionServiceInterface $actionService,
+        ValidatorInterface $validator,
+        protected RoomLogServiceInterface $roomLogService,
+        protected StatusServiceInterface $statusService,
+        protected TranslationServiceInterface $translationService,
+    ) {
+        parent::__construct($eventService, $actionService, $validator);
+    }
+
+    public function support(?LogParameterInterface $target, array $parameters): bool
+    {
+        return $target instanceof Drone;
+    }
+
+    public static function loadValidatorMetadata(ClassMetadata $metadata): void
+    {
+        $metadata->addConstraints([
+            new HasSkill([
+                'skill' => SkillEnum::ROBOTICS_EXPERT,
+                'groups' => [ClassConstraint::VISIBILITY],
+            ]),
+            new HasEquipment([
+                'reach' => ReachEnum::ROOM,
+                'equipments' => [ItemEnum::METAL_SCRAPS],
+                'groups' => [ClassConstraint::EXECUTE],
+                'message' => ActionImpossibleCauseEnum::DRONE_UPGRADE_LACK_RESSOURCES,
+                'number' => 2,
+            ]),
+        ]);
+    }
+
+    protected function upgradeStatus(): string
+    {
+        return match ($this->name) {
+            ActionEnum::UPGRADE_DRONE_TO_TURBO => EquipmentStatusEnum::TURBO_DRONE_UPGRADE,
+            default => throw new \LogicException('Unknown upgrade status'),
+        };
+    }
+
+    protected function upgradeLog(): string
+    {
+        return match ($this->name) {
+            ActionEnum::UPGRADE_DRONE_TO_TURBO => ActionLogEnum::UPGRADE_DRONE_TO_TURBO_SUCCESS,
+            default => throw new \LogicException('Unknown upgrade log'),
+        };
+    }
+
+    protected function checkResult(): ActionResult
+    {
+        return new Success();
+    }
+
+    protected function applyEffect(ActionResult $result): void
+    {
+        $this->createUpgradeStatus();
+        $this->createUpgradeLog();
+        $this->destroyScrapMetal();
+    }
+
+    private function createUpgradeStatus(): void
+    {
+        $this->statusService->createStatusFromName(
+            statusName: $this->upgradeStatus(),
+            holder: $this->drone(),
+            tags: $this->getTags(),
+            time: new \DateTime(),
+        );
+    }
+
+    private function createUpgradeLog(): void
+    {
+        $this->roomLogService->createLog(
+            logKey: $this->upgradeLog(),
+            place: $this->drone()->getPlace(),
+            visibility: VisibilityEnum::PUBLIC,
+            type: 'actions_log',
+            player: $this->player,
+            parameters: [
+                'target_drone' => $this->translatedDroneName(),
+                $this->player->getLogKey() => $this->player->getLogName(),
+            ],
+            dateTime: new \DateTime(),
+        );
+    }
+
+    private function translatedDroneName(): string
+    {
+        return $this->translationService->translate(
+            key: 'drone',
+            parameters: [
+                'drone_nickname' => $this->drone()->getNickname(),
+                'drone_serial_number' => $this->drone()->getSerialNumber(),
+            ],
+            domain: 'event_log',
+            language: $this->drone()->getDaedalus()->getLanguage()
+        );
+    }
+
+    private function destroyScrapMetal(): void
+    {
+        $isScrapMetal = static fn (GameEquipment $gameEquipment) => $gameEquipment->getName() === ItemEnum::METAL_SCRAPS;
+
+        $playerScrapMetal = $this->player->getEquipments()->filter($isScrapMetal);
+        $roomScrapMetal = $this->player->getPlace()->getEquipments()->filter($isScrapMetal);
+
+        $totalScrapDestroyed = $this->destroyFromCollection($playerScrapMetal, quantity: $this->getOutputQuantity());
+
+        if ($totalScrapDestroyed < $this->getOutputQuantity()) {
+            $this->destroyFromCollection($roomScrapMetal, quantity: $this->getOutputQuantity() - $totalScrapDestroyed);
+        }
+    }
+
+    /** @param Collection<array-key, GameEquipment> $scrapCollection */
+    private function destroyFromCollection(Collection $scrapCollection, int $quantity): int
+    {
+        $scrapDestroyed = 0;
+        $scrap = $scrapCollection->slice(0, $quantity);
+
+        foreach ($scrap as $scrapMetal) {
+            $this->triggerDestroyEvent($scrapMetal);
+            ++$scrapDestroyed;
+        }
+
+        return $scrapDestroyed;
+    }
+
+    private function triggerDestroyEvent(GameEquipment $scrapMetal): void
+    {
+        $equipmentEvent = new InteractWithEquipmentEvent(
+            equipment: $scrapMetal,
+            author: $this->player,
+            visibility: VisibilityEnum::HIDDEN,
+            tags: $this->getTags(),
+            time: new \DateTime()
+        );
+        $this->eventService->callEvent($equipmentEvent, EquipmentEvent::EQUIPMENT_DESTROYED);
+    }
+
+    private function drone(): Drone
+    {
+        $drone = $this->gameEquipmentTarget();
+        if ($drone instanceof Drone) {
+            return $drone;
+        }
+
+        throw new \RuntimeException('Target is not a drone');
+    }
+}
--- Api/tests/functional/Action/Actions/UpgradeDroneToTurboCest.php
+++ Api/tests/functional/Action/Actions/UpgradeDroneToTurboCest.php
@@ -7,189 +7,19 @@ namespace Mush\Tests\functional\Action\Actions;
 use Mush\Action\Actions\UpgradeDroneToTurbo;
 use Mush\Action\Entity\ActionConfig;
 use Mush\Action\Enum\ActionEnum;
-use Mush\Action\Enum\ActionImpossibleCauseEnum;
-use Mush\Equipment\Entity\Drone;
-use Mush\Equipment\Entity\GameItem;
-use Mush\Equipment\Enum\ItemEnum;
-use Mush\Equipment\Service\GameEquipmentServiceInterface;
-use Mush\Game\Enum\VisibilityEnum;
-use Mush\RoomLog\Enum\ActionLogEnum;
-use Mush\Skill\Enum\SkillEnum;
-use Mush\Status\Enum\EquipmentStatusEnum;
-use Mush\Tests\AbstractFunctionalTest;
+use Mush\Tests\AbstractUpgradeDroneCest;
 use Mush\Tests\FunctionalTester;
-use Mush\Tests\RoomLogDto;
 
 /**
  * @internal
  */
-final class UpgradeDroneToTurboCest extends AbstractFunctionalTest
+final class UpgradeDroneToTurboCest extends AbstractUpgradeDroneCest
 {
-    private ActionConfig $actionConfig;
-    private UpgradeDroneToTurbo $upgradeDroneToTurbo;
-    private GameEquipmentServiceInterface $gameEquipmentService;
-    private Drone $drone;
-
     public function _before(FunctionalTester $I): void
     {
         parent::_before($I);
 
         $this->actionConfig = $I->grabEntityFromRepository(ActionConfig::class, ['name' => ActionEnum::UPGRADE_DRONE_TO_TURBO->value]);
-        $this->upgradeDroneToTurbo = $I->grabService(UpgradeDroneToTurbo::class);
-        $this->gameEquipmentService = $I->grabService(GameEquipmentServiceInterface::class);
-
-        $this->givenChunHasASupportDrone();
-        $this->setupDroneNicknameAndSerialNumber($this->drone, 0, 0);
-    }
-
-    public function shouldNotBeVisibleIfPlayerIsNotRoboticsExpert(FunctionalTester $I): void
-    {
-        $this->whenChunTriesToUpgradeDroneToTurbo();
-
-        $this->thenActionShouldNotBeVisible($I);
-    }
-
-    public function shouldNotBeVisibleIfDroneAlreadyHasUpgrade(FunctionalTester $I): void
-    {
-        $this->addSkillToPlayer(SkillEnum::ROBOTICS_EXPERT, $I);
-
-        $this->givenChunHasPiecesOfScrapMetalOnReach(4);
-
-        $this->givenChunUpgradesDroneToTurbo();
-
-        $this->whenChunTriesToUpgradeDroneToTurbo();
-
-        $this->thenActionShouldNotBeVisible($I);
-    }
-
-    public function shouldNotBeExecutableIfThereAreNotTwoScrapMetalOnReach(FunctionalTester $I): void
-    {
-        $this->addSkillToPlayer(SkillEnum::ROBOTICS_EXPERT, $I);
-
-        $this->whenChunTriesToUpgradeDroneToTurbo();
-
-        $this->thenActionShouldNotBeExecutableWithMessage(ActionImpossibleCauseEnum::DRONE_UPGRADE_LACK_RESSOURCES, $I);
-    }
-
-    public function shouldCreateTurboStatus(FunctionalTester $I): void
-    {
-        $this->addSkillToPlayer(SkillEnum::ROBOTICS_EXPERT, $I);
-
-        $this->givenChunHasPiecesOfScrapMetalOnReach(2);
-
-        $this->whenChunUpgradesDroneToTurbo();
-
-        $this->thenDroneShouldHaveTurboStatus($I);
-    }
-
-    public function shouldPrintAPublicLog(FunctionalTester $I): void
-    {
-        $this->addSkillToPlayer(SkillEnum::ROBOTICS_EXPERT, $I);
-
-        $this->givenChunHasPiecesOfScrapMetalOnReach(2);
-
-        $this->whenChunUpgradesDroneToTurbo();
-
-        $this->ISeeTranslatedRoomLogInRepository(
-            expectedRoomLog: "**Chun** s'acharne un peu sur ce pauvre **Robo Wheatley #0**. Mais c'est pour son bien. **Robo Wheatley #0** reçoit l'amélioration **Turbo**.",
-            actualRoomLogDto: new RoomLogDto(
-                player: $this->chun,
-                log: ActionLogEnum::UPGRADE_DRONE_TO_TURBO_SUCCESS,
-                visibility: VisibilityEnum::PUBLIC,
-                inPlayerRoom: false,
-            ),
-            I: $I,
-        );
-    }
-
-    public function shouldConsumeScrapMetal(FunctionalTester $I): void
-    {
-        $this->addSkillToPlayer(SkillEnum::ROBOTICS_EXPERT, $I);
-
-        $this->givenChunHasPiecesOfScrapMetalOnReach(3);
-
-        $this->whenChunUpgradesDroneToTurbo();
-
-        $this->thenChunShouldHaveScrapMetalOnReach(1, $I);
-    }
-
-    private function givenChunHasASupportDrone(): void
-    {
-        $this->drone = $this->gameEquipmentService->createGameEquipmentFromName(
-            equipmentName: ItemEnum::SUPPORT_DRONE,
-            equipmentHolder: $this->chun,
-            reasons: [],
-            time: new \DateTime()
-        );
-    }
-
-    private function givenChunUpgradesDroneToTurbo(): void
-    {
-        $this->whenChunUpgradesDroneToTurbo();
-    }
-
-    private function givenChunHasPiecesOfScrapMetalOnReach(int $quantity): void
-    {
-        $this->gameEquipmentService->createGameEquipmentsFromName(
-            equipmentName: ItemEnum::METAL_SCRAPS,
-            equipmentHolder: $this->chun,
-            reasons: [],
-            time: new \DateTime(),
-            quantity: $quantity,
-        );
-    }
-
-    private function whenChunUpgradesDroneToTurbo(): void
-    {
-        $this->upgradeDroneToTurbo->loadParameters(
-            actionConfig: $this->actionConfig,
-            actionProvider: $this->drone,
-            player: $this->chun,
-            target: $this->drone,
-        );
-        $this->upgradeDroneToTurbo->execute();
-    }
-
-    private function whenChunTriesToUpgradeDroneToTurbo(): void
-    {
-        $this->upgradeDroneToTurbo->loadParameters(
-            actionConfig: $this->actionConfig,
-            actionProvider: $this->drone,
-            player: $this->chun,
-            target: $this->drone,
-        );
-    }
-
-    private function thenDroneShouldHaveTurboStatus(FunctionalTester $I): void
-    {
-        $I->assertTrue($this->drone->hasStatus(EquipmentStatusEnum::TURBO_DRONE_UPGRADE));
-    }
-
-    private function thenActionShouldNotBeVisible(FunctionalTester $I): void
-    {
-        $I->assertFalse($this->upgradeDroneToTurbo->isVisible());
-    }
-
-    private function thenActionShouldNotBeExecutableWithMessage(string $message, FunctionalTester $I): void
-    {
-        $I->assertEquals($message, $this->upgradeDroneToTurbo->cannotExecuteReason());
-    }
-
-    private function thenChunShouldHaveScrapMetalOnReach(int $quantity, FunctionalTester $I): void
-    {
-        $chunScrapMetal = $this->chun->getEquipments()->filter(static fn (GameItem $item) => $item->getName() === ItemEnum::METAL_SCRAPS);
-        $roomScrapMetal = $this->chun->getPlace()->getEquipments()->filter(static fn (GameItem $item) => $item->getName() === ItemEnum::METAL_SCRAPS);
-
-        $actualQuantity = $chunScrapMetal->count() + $roomScrapMetal->count();
-
-        $I->assertEquals($quantity, $actualQuantity);
-    }
-
-    private function setupDroneNicknameAndSerialNumber(Drone $drone, int $nickName, int $serialNumber): void
-    {
-        $droneInfo = $drone->getDroneInfo();
-        $ref = new \ReflectionClass($droneInfo);
-        $ref->getProperty('nickName')->setValue($droneInfo, $nickName);
-        $ref->getProperty('serialNumber')->setValue($droneInfo, $serialNumber);
+        $this->upgradeDrone = $I->grabService(UpgradeDroneToTurbo::class);
     }
 }

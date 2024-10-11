ee1579a63ee4d72ef17f256e6df835710fbd6673
Evian <icdk5b4zf@relay.firefox.com>
2024-10-11 15:37:49 +0200

    wip PWA

--- App/vite.config.js
+++ App/vite.config.js
@@ -3,6 +3,7 @@ import { defineConfig } from 'vite';
 import { fileURLToPath } from 'url';    
 import typescript from '@rollup/plugin-typescript';
 import vue from '@vitejs/plugin-vue';
+import { VitePWA } from "vite-plugin-pwa";
 import path from 'path';
 
 const filename = fileURLToPath(import.meta.url);
@@ -12,7 +13,47 @@ const STATIC_DIR = 'public';
 export default defineConfig({
   plugins: [
     typescript(),
-    vue()
+    vue(),
+    VitePWA({
+      registerType: "autoUpdate",
+      includeAssets: ["favicon.ico", "apple-touch-icon.png", "masked-icon.svg"],
+      manifest: {
+        name: "eMush - Jeu multijoueur de coopération : trahisons et survie dans l'espace",
+        short_name: "eMush",
+        description: "Vous allez vous réveiller sur le vaisseau spatial Daedalus avec 15 autres joueurs. Seul problème : deux d'entre eux sont infectés par un parasite, le Mush ! 🍄 Luttez pour votre survie dans la plus grande épopée de space opera de l'Humanité, depuis tous vos appareils !",
+        theme_color: "#ffffff",
+        icons: [
+          {
+            "src": "pwa-64x64.png",
+            "sizes": "64x64",
+            "type": "image/png"
+          },
+          {
+            "src": "pwa-192x192.png",
+            "sizes": "192x192",
+            "type": "image/png"
+          },
+          {
+            "src": "pwa-512x512.png",
+            "sizes": "512x512",
+            "type": "image/png"
+          },
+          {
+            "src": "maskable-icon-512x512.png",
+            "sizes": "512x512",
+            "type": "image/png",
+            "purpose": "maskable"
+          }
+        ]
+      },
+      workbox: {
+        cleanupOutdatedCaches: false,
+        maximumFileSizeToCacheInBytes: 3000000
+      },
+      devOptions: {
+        enabled: true
+      }
+    })
   ],
   define: {
     global: 'globalThis'

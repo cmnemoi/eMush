c5d71945d4bdede4670d5754c2cd1c9501b1fb19
Valentin Schiaffino <valenschg@gmail.com>
2024-10-15 21:25:05 +0000

    Apply 2 suggestion(s) to 2 file(s)
    Co-authored-by: Evian <icdk5b4zf@relay.firefox.com>

--- Api/src/Project/Enum/ProjectRequirementName.php
+++ Api/src/Project/Enum/ProjectRequirementName.php
@@ -4,18 +4,16 @@ declare(strict_types=1);
 
 namespace Mush\Project\Enum;
 
-class ProjectRequirementName
+enum ProjectRequirementName: string
 {
-    public const CHUN_IN_LABORATORY = 'chun_in_laboratory';
-    public const MUSH_PLAYER_DEAD = 'mush_player_dead';
-    public const SOAP_IN_LABORATORY = 'soap_in_laboratory';
-    public const MUSH_SAMPLE_IN_LABORATORY = 'mush_sample_in_laboratory';
-    public const MUSH_GENOME_DISK_IN_LABORATORY = 'mush_genome_disk_in_laboratory';
-    public const BLASTER_IN_LABORATORY = 'blaster_in_laboratory';
-
-    public const WATER_STICK_IN_LABORATORY = 'water_stick_in_laboratory';
-
-    public const STARMAP_FRAGMENT_IN_LABORATORY = 'starmap_fragment_in_laboratory';
-    public const MEDIKIT_IN_LABORATORY = 'medikit_in_laboratory';
-    public const SCHRODINGER_IN_PLAYER_INVENTORY = 'schrodinger_in_player_inventory';
+    case CHUN_IN_LABORATORY = 'chun_in_laboratory';
+    case MUSH_PLAYER_DEAD = 'mush_player_dead';
+    case SOAP_IN_LABORATORY = 'soap_in_laboratory';
+    case MUSH_SAMPLE_IN_LABORATORY = 'mush_sample_in_laboratory';
+    case MUSH_GENOME_DISK_IN_LABORATORY = 'mush_genome_disk_in_laboratory';
+    case BLASTER_IN_LABORATORY = 'blaster_in_laboratory';
+    case WATER_STICK_IN_LABORATORY = 'water_stick_in_laboratory';
+    case STARMAP_FRAGMENT_IN_LABORATORY = 'starmap_fragment_in_laboratory';
+    case MEDIKIT_IN_LABORATORY = 'medikit_in_laboratory';
+    case SCHRODINGER_IN_PLAYER_INVENTORY = 'schrodinger_in_player_inventory';
 }
--- Api/src/Project/Enum/ProjectRequirementType.php
+++ Api/src/Project/Enum/ProjectRequirementType.php
@@ -4,10 +4,10 @@ declare(strict_types=1);
 
 namespace Mush\Project\Enum;
 
-class ProjectRequirementType
+enum ProjectRequirementType: string
 {
-    public const CHUN_IN_LABORATORY = 'chun_in_laboratory';
-    public const MUSH_PLAYER_DEAD = 'mush_player_dead';
-    public const ITEM_IN_LABORATORY = 'item_in_laboratory';
-    public const ITEM_IN_PLAYER_INVENTORY = 'item_in_player_inventory';
+    case CHUN_IN_LABORATORY = 'chun_in_laboratory';
+    case MUSH_PLAYER_DEAD = 'mush_player_dead';
+    case ITEM_IN_LABORATORY = 'item_in_laboratory';
+    case ITEM_IN_PLAYER_INVENTORY = 'item_in_player_inventory';
 }

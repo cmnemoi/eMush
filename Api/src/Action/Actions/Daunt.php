39a1d384f4a354d0950c4b950ae524c75123ec70
Evian <icdk5b4zf@relay.firefox.com>
2024-10-21 16:33:52 +0200

    Add reach validator to Daunt action for room visibility checks

--- Api/src/Action/Actions/Daunt.php
+++ Api/src/Action/Actions/Daunt.php
@@ -11,6 +11,8 @@ use Mush\Action\Enum\ActionImpossibleCauseEnum;
 use Mush\Action\Service\ActionServiceInterface;
 use Mush\Action\Validator\ClassConstraint;
 use Mush\Action\Validator\HasStatus;
+use Mush\Action\Validator\Reach;
+use Mush\Equipment\Enum\ReachEnum;
 use Mush\Game\Service\EventServiceInterface;
 use Mush\Player\Entity\Player;
 use Mush\Player\Service\RemoveActionPointsFromPlayerServiceInterface;
@@ -38,15 +40,19 @@ final class Daunt extends AbstractAction
 
     public static function loadValidatorMetadata(ClassMetadata $metadata): void
     {
-        $metadata->addConstraint(
+        $metadata->addConstraints([
+            new Reach([
+                'reach' => ReachEnum::ROOM,
+                'groups' => [ClassConstraint::VISIBILITY],
+            ]),
             new HasStatus([
                 'status' => PlayerStatusEnum::HAS_DAUNTED,
                 'target' => HasStatus::PLAYER,
                 'contain' => false,
                 'groups' => [ClassConstraint::EXECUTE],
                 'message' => ActionImpossibleCauseEnum::DAILY_LIMIT,
-            ])
-        );
+            ]),
+        ]);
     }
 
     public function support(?LogParameterInterface $target, array $parameters): bool

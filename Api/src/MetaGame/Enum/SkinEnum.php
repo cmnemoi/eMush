f34c75b79fb13786361700c3836465a34557e72e
Sylvain Breton <sylvain.breton.6@gmail.com>
2024-11-06 18:44:59 +0100

    feat: added skin enums

---
+++ Api/src/MetaGame/Enum/SkinEnum.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace Mush\MetaGame\Enum;
+
+/**
+ * Class enumerating the different skins for player, equipment and rooms.
+ */
+class SkinEnum
+{
+    public const string JANICE_SEXY = 'janice_sexy';
+    public const string IAN_LABCOAT = 'ian_labcoat';
+    public const string CHAO_LEATHER = 'chao_leather';
+    public const string ROLAND_YULE = 'roland_yule';
+    public const string ELEESHA_CLASSY = 'eleesha_classy';
+    public const string RALUCA_ADDAMS = 'raluca_addams';
+    public const string TERRENCE_BIKER = 'terrence_biker';
+    public const string JIN_SU_GNANGNAN = 'jin_su_gnangnan';
+    public const string HUA_PUPKIN = 'hua_pupkin';
+    public const string JIN_SU_VAMPIRE = 'jin_su_vampire';
+    public const string STEPHEN_SANTA = 'stephen_santa';
+    public const string PAOLA_SANTA = 'paola_santa';
+    public const string KUAN_ALBATOR = 'kuan_albator';
+    public const string FINOLA_SKYWALKER = 'finola_skywalker';
+    public const string GIOELE_BATHROBE = 'gioele_bathrobe';
+    public const string CHUN_DALLAS = 'chun_dallas';
+    public const string FRIEDA_GONADALF = 'frieda_gonadalf';
+
+    public const string REACTOR_BROKEN = 'reactor_broken';
+    public const string BAY_DOOR_EXTRALARGE = 'bay_door_extralarge';
+    public const string GARDEN_INCUBATOR = 'garden_incubator';
+    public const string GARDEN_LAMP = 'garden_lamp';
+    public const string KITCHEN_APERO = 'kitchen_apero';
+    public const string PILGRED_ACTIVE = 'pilgred_active';
+    public const string PLASMA_SHIELD_ACTIVE = 'plasma_shield_active';
+    public const string SCANNER_OVERCLOCKING = 'scanner_overclocking';
+    public const string ANTENNA_SPATIAL_WAVE = 'antenna_spatial_wave';
+    public const string TURRET_TESLA = 'turret_tesla';
+    public const string TURRET_CHARGES = 'turret_charges';
+    public const string COFFEE_MACHINE_FISSION = 'coffee_machine_fission';
+    public const string ICARUS_LARGE = 'icarus_large';
+    public const string ICARUS_THRUSTER = 'icarus_thruster';
+    public const string NERON_CORE_PARTICIPATION = 'neron_core_participation';
+    public const string PATROL_SHIP_BLASTER = 'patrol_ship_blaster';
+    public const string PATROL_SHIP_GOLD = 'patrol_ship_gold';
+    public const string PATROL_SHIP_TELSATRON = 'patrol_ship_telsatron';
+    public const string PATROL_SHIP_RED_BLACK = 'patrol_ship_red_black';
+    public const string SOFA_BROWN = 'sofa_brown';
+    public const string SOFA_GREEN = 'sofa_green';
+    public const string SOFA_PINK = 'sofa_pink';
+    public const string SOFA_WHITE = 'sofa_white';
+
+    public const string ALPHA_POSTER = 'alpha_poster';
+    public const string TABLE_APERO = 'table_apero';
+    public const string MAGNETIC_NET = 'magnetic_net';
+    public const string MAGNETIC_RETURN = 'magnetic_return';
+    public const string QUANTUM_SENSOR = 'quantum_sensor';
+    public const string TAKEOFF_PLATFORM_PROPULSION = 'takeoff_platform_propulsion';
+    public const string WALL_BOX = 'wall_box';
+    public const string PNEUMATIC_DISTRIBUTOR = 'pneumatic_distributor';
+}

4a40dee12e2bd5010602b932547b1dac7b172a49
Evian <icdk5b4zf@relay.firefox.com>
2024-11-08 18:05:32 +0100

    fix: Update PlayerVariableEnum to rename SATIETY constant for clarity from ':pa_cook:' to ':full_stomach:'

--- Api/src/Player/Enum/PlayerVariableEnum.php
+++ Api/src/Player/Enum/PlayerVariableEnum.php
@@ -30,7 +30,7 @@ abstract class PlayerVariableEnum
             self::MOVEMENT_POINT => ':pm:',
             self::HEALTH_POINT => ':hp:',
             self::MORAL_POINT => ':pmo:',
-            self::SATIETY => ':pa_cook:',
+            self::SATIETY => ':full_stomach:',
         ];
     }
 }

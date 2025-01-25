637a8f708248915ddf3ce859faef24178a153802
Evian <icdk5b4zf@relay.firefox.com>
2025-01-25 22:19:31 +0100

    Fix normalization test to assert null for announcement element instead of empty array

--- Api/tests/functional/Communication/Normalizer/TipsChannelNormalizerCest.php
+++ Api/tests/functional/Communication/Normalizer/TipsChannelNormalizerCest.php
@@ -13,6 +13,7 @@ use Mush\Status\Enum\PlayerStatusEnum;
 use Mush\Status\Service\StatusService;
 use Mush\Tests\AbstractFunctionalTest;
 use Mush\Tests\FunctionalTester;
+use Symfony\Component\Serializer\Normalizer\NormalizerInterface;
 
 /**
  * @internal
@@ -26,6 +27,7 @@ final class TipsChannelNormalizerCest extends AbstractFunctionalTest
         parent::_before($I);
 
         $this->normalizer = $I->grabService(TipsChannelNormalizer::class);
+        $this->normalizer->setNormalizer($I->grabService(NormalizerInterface::class));
     }
 
     public function shouldSupportNormaliaztionOnlyForChannel(FunctionalTester $I): void
@@ -89,7 +91,7 @@ final class TipsChannelNormalizerCest extends AbstractFunctionalTest
 
         // Check general announcement
         $I->assertEquals('Annonces', $normalizedChannel['tips']['announcement']['title']);
-        $I->assertEquals([], $normalizedChannel['tips']['announcement']['element']);
+        $I->assertEquals(null, $normalizedChannel['tips']['announcement']['element']);
     }
 
     public function shouldNormalizeMushTipsIfPlayerIsMush(FunctionalTester $I): void

d032feb96ad9e9fa64a1c66803ca4bedc844bf16
Félicie T <taxouck1@live.fr>
2025-03-06 19:57:13 +0000

    Apply 1 suggestion(s) to 1 file(s)
    Co-authored-by: Evian <icdk5b4zf@relay.firefox.com>

--- Api/migrations/Version20250305211253.php
+++ Api/migrations/Version20250305211253.php
@@ -30,7 +30,6 @@ final class Version20250305211253 extends AbstractMigration
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
-        $this->addSql('CREATE SCHEMA public');
         $this->addSql('ALTER TABLE rebel_base_config_status_config DROP CONSTRAINT FK_22DC301438B52EC7');
         $this->addSql('ALTER TABLE rebel_base_config_status_config DROP CONSTRAINT FK_22DC3014AC4E86C2');
         $this->addSql('DROP TABLE rebel_base_config_status_config');

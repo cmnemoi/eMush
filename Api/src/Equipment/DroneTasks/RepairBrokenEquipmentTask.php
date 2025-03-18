43a719d50f581357346c374e88c1b61dcc8bc395
Félicie T <taxouck1@live.fr>
2025-03-18 19:41:24 +0000

    fix: removed debug text on drone repair action

--- Api/src/Equipment/DroneTasks/RepairBrokenEquipmentTask.php
+++ Api/src/Equipment/DroneTasks/RepairBrokenEquipmentTask.php
@@ -7,11 +7,8 @@ namespace Mush\Equipment\DroneTasks;
 use Mush\Equipment\Entity\Drone;
 use Mush\Equipment\Entity\GameEquipment;
 use Mush\Equipment\Event\DroneRepairedEvent;
-use Mush\Game\Enum\VisibilityEnum;
 use Mush\Game\Service\EventServiceInterface;
 use Mush\Game\Service\Random\D100RollServiceInterface;
-use Mush\Game\Service\TranslationServiceInterface;
-use Mush\RoomLog\Service\RoomLogServiceInterface;
 use Mush\Status\Enum\EquipmentStatusEnum;
 use Mush\Status\Service\StatusServiceInterface;
 
@@ -21,73 +18,26 @@ class RepairBrokenEquipmentTask extends AbstractDroneTask
         protected EventServiceInterface $eventService,
         protected StatusServiceInterface $statusService,
         private D100RollServiceInterface $d100Roll,
-        // remove these once bug found
-        private RoomLogServiceInterface $roomLogService,
-        private TranslationServiceInterface $translationService
     ) {
         parent::__construct($this->eventService, $this->statusService);
     }
 
     protected function applyEffect(Drone $drone, \DateTime $time): void
     {
-        $logParameters = $this->getDroneName($drone);
-        $logParameters['charge'] = $drone->getNumberOfActions();
-        $this->roomLogService->createLog(
-            'drone_repair_debug_charges_count',
-            $drone->getPlace(),
-            VisibilityEnum::PUBLIC,
-            'debug',
-            null,
-            $logParameters,
-            $time,
-        );
-
         if ($drone->cannotApplyTask($this)) {
             $this->taskNotApplicable = true;
 
-            // remove this once bug found
-            $this->roomLogService->createLog(
-                'drone_repair_debug_nothing_broken_found',
-                $drone->getPlace(),
-                VisibilityEnum::PUBLIC,
-                'debug',
-                null,
-                $this->getDroneName($drone),
-                $time,
-            );
-
             return;
         }
 
-        // remove $time from function args once bug found
-        $equipmentToRepair = $this->getEquipmentToRepair($drone, $time);
+        $equipmentToRepair = $this->getEquipmentToRepair($drone);
 
-        // remove this once bug found, equipmentToRepair should never be null or false
         if (!$equipmentToRepair) {
-            $this->roomLogService->createLog(
-                'drone_repair_debug_fail_select',
-                $drone->getPlace(),
-                VisibilityEnum::PUBLIC,
-                'debug',
-                null,
-                $this->getDroneName($drone),
-                $time,
-            );
+            // This should never happen, but throwing an exception would stall games if it ever did
             $this->taskNotApplicable = true;
 
             return;
         }
-
-        $logParameters['equipment'] = $equipmentToRepair->getLogName();
-        $this->roomLogService->createLog(
-            'drone_repair_debug_success_select',
-            $drone->getPlace(),
-            VisibilityEnum::PUBLIC,
-            'debug',
-            null,
-            $logParameters,
-            $time,
-        );
 
         // If the repair fails, increase the number of failed repair attempts and abort.
         if ($this->d100Roll->isAFailure($drone->getRepairSuccessRateForEquipment($equipmentToRepair))) {
@@ -104,30 +54,9 @@ class RepairBrokenEquipmentTask extends AbstractDroneTask
         $this->repairEquipment($drone, $equipmentToRepair, $time);
     }
 
-    private function getEquipmentToRepair(Drone $drone, \DateTime $time): ?GameEquipment
+    private function getEquipmentToRepair(Drone $drone): ?GameEquipment
     {
         $brokenRoomEquipment = $drone->getBrokenDoorsAndEquipmentsInRoom();
-
-        // remove this once bug found
-        $brokenEquipmentList = '';
-        foreach ($brokenRoomEquipment as $equipment) {
-            $brokenEquipmentList = $brokenEquipmentList . $equipment->getLogName() . ' ';
-        }
-
-        // remove this once bug found
-        $logParameters = $this->getDroneName($drone);
-        $logParameters['broken_equipments'] = $brokenEquipmentList;
-
-        // remove this once bug found
-        $this->roomLogService->createLog(
-            'drone_repair_debug_broken_item_list',
-            $drone->getPlace(),
-            VisibilityEnum::PUBLIC,
-            'debug',
-            null,
-            $logParameters,
-            $time,
-        );
 
         return $brokenRoomEquipment->first() ?: null;
     }
@@ -147,22 +76,5 @@ class RepairBrokenEquipmentTask extends AbstractDroneTask
             time: $time,
         );
         $this->eventService->callEvent($droneEvent, DroneRepairedEvent::class);
-    }
-
-    // remove this once bug found
-    private function getDroneName(Drone $drone): array
-    {
-        $logParameters = [];
-        $logParameters['drone'] = $this->translationService->translate(
-            key: 'drone',
-            parameters: [
-                'drone_nickname' => $drone->getNickname(),
-                'drone_serial_number' => $drone->getSerialNumber(),
-            ],
-            domain: 'event_log',
-            language: 'fr'
-        );
-
-        return $logParameters;
     }
 }

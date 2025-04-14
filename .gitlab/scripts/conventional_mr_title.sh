1ec016d78bd6c94e2d21534d1c813d0969b80fe8
Evian <icdk5b4zf@relay.firefox.com>
2025-04-14 20:18:36 +0200

    ci: Update CI pipeline to enforce Conventional Commits format for merge request titles

---
+++ .gitlab/scripts/conventional_mr_title.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+TITLE="$CI_MERGE_REQUEST_TITLE"
+REGEX="^(feat|fix|docs|style|refactor|test|chore|ci|internal)(\(.+\))?: .+$"
+
+if [[ ! "$TITLE" =~ $REGEX ]]; then
+    echo -e "ERROR: Merge request title does not follow Conventional Commits format (https://www.conventionalcommits.org/en/v1.0.0/).\n"
+    echo -e "Your change won't appear in the changelog!\n"
+    echo -e "Please follow the format: <type>(<scope>): <description> and relaunch the pipeline to fix this."
+    echo -e "Example - fix: Other player skills are ordered by selection date"
+    exit 1
+fi
+
+echo "Merge request title is valid."
+exit 0

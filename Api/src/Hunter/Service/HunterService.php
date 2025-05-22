f60dfa63bef8739613203d4cc35223ecd581924b
Dzanek1143 <lucker1996@gmail.com>
2025-05-22 16:46:02 +0200

    Rebasing changes: handling linting errors

--- Api/src/Hunter/Service/HunterService.php
+++ Api/src/Hunter/Service/HunterService.php
@@ -38,7 +38,7 @@ use Mush\Status\Entity\ChargeStatus;
 use Mush\Status\Entity\Config\StatusConfig;
 use Mush\Status\Enum\DaedalusStatusEnum;
 use Mush\Status\Enum\EquipmentStatusEnum;
-use Mush\Status\Service\StatusService;
+use Mush\Status\Service\StatusServiceInterface;
 
 final class HunterService implements HunterServiceInterface
 {
@@ -51,7 +51,7 @@ final class HunterService implements HunterServiceInterface
         private HunterRepositoryInterface $hunterRepository,
         private HunterTargetRepositoryInterface $hunterTargetRepository,
         private RandomServiceInterface $randomService,
-        private StatusService $statusService
+        private StatusServiceInterface $statusService
     ) {}
 
     public function delete(array $entities): void

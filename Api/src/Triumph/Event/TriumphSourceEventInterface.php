5dc6a4cb2df4754ec67cf84d2ce2640954327e3e
Evian <icdk5b4zf@relay.firefox.com>
2025-05-29 19:59:14 +0200

    fix: Add getDaedalus method to TriumphSourceEventInterface

--- Api/src/Triumph/Event/TriumphSourceEventInterface.php
+++ Api/src/Triumph/Event/TriumphSourceEventInterface.php
@@ -4,11 +4,14 @@ declare(strict_types=1);
 
 namespace Mush\Triumph\Event;
 
+use Mush\Daedalus\Entity\Daedalus;
 use Mush\Player\Entity\Collection\PlayerCollection;
 use Mush\Triumph\Entity\TriumphConfig;
 
 interface TriumphSourceEventInterface
 {
+    public function getDaedalus(): Daedalus;
+
     public function getEventName(): string;
 
     public function getTargetsForTriumph(TriumphConfig $triumphConfig): PlayerCollection;

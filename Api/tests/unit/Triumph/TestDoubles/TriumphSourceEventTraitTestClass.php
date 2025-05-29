20e365da231370e4f1bf9436915a0b1a7c19e115
Evian <icdk5b4zf@relay.firefox.com>
2025-05-30 00:51:59 +0200

    refactor: Simplify TriumphSourceEventTraitTestClass by extending AbstractGameEvent and removing unused methods

--- Api/tests/unit/Triumph/TestDoubles/TriumphSourceEventTraitTestClass.php
+++ Api/tests/unit/Triumph/TestDoubles/TriumphSourceEventTraitTestClass.php
@@ -5,49 +5,30 @@ declare(strict_types=1);
 namespace Mush\Tests\unit\Triumph\TestDoubles;
 
 use Mush\Daedalus\Entity\Daedalus;
-use Mush\Player\Entity\Collection\PlayerCollection;
-use Mush\Triumph\Entity\TriumphConfig;
+use Mush\Daedalus\Factory\DaedalusFactory;
+use Mush\Game\Event\AbstractGameEvent;
 use Mush\Triumph\Event\TriumphSourceEventInterface;
 use Mush\Triumph\Event\TriumphSourceEventTrait;
 
 /**
  * @internal
  */
-final class TriumphSourceEventTraitTestClass implements TriumphSourceEventInterface
+final class TriumphSourceEventTraitTestClass extends AbstractGameEvent implements TriumphSourceEventInterface
 {
     use TriumphSourceEventTrait;
 
-    private array $tags;
-
     public function __construct(array $tags)
     {
-        $this->tags = $tags;
+        parent::__construct($tags, new \DateTime());
     }
 
     public function getDaedalus(): Daedalus
     {
-        // Return a stub or mock if needed, not used in these tests
-        return new Daedalus();
+        return DaedalusFactory::createDaedalus();
     }
 
     public function getEventName(): string
     {
         return 'test';
-    }
-
-    public function getTargetsForTriumph(TriumphConfig $triumphConfig): PlayerCollection
-    {
-        // Return a stub, not used in these tests
-        return new PlayerCollection();
-    }
-
-    public function hasTag(string $tag): bool
-    {
-        return \in_array($tag, $this->tags, true);
-    }
-
-    public function doesNotHaveTag(string $tag): bool
-    {
-        return !$this->hasTag($tag);
     }
 }

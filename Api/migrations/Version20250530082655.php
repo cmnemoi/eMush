2234a77d68963b870de52402d60fdb98af9b570a
Evian <icdk5b4zf@relay.firefox.com>
2025-05-31 13:58:13 +0200

    refactor(migration): Remove obsolete columns from equipment_mechanic table and update project and triumph_config defaults

--- Api/migrations/Version20250530082655.php
+++ Api/migrations/Version20250530082655.php
@@ -20,9 +20,6 @@ final class Version20250530082655 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE equipment_mechanic DROP critical_success_rate');
-        $this->addSql('ALTER TABLE equipment_mechanic DROP critical_fail_rate');
-        $this->addSql('ALTER TABLE equipment_mechanic DROP one_shot_rate');
         $this->addSql('ALTER TABLE project ADD previous_progress INT DEFAULT 0 NOT NULL');
         $this->addSql('ALTER TABLE triumph_config ALTER quantity SET DEFAULT 0');
         $this->addSql('ALTER TABLE triumph_config ALTER key SET DEFAULT \'\'');
@@ -32,9 +29,6 @@ final class Version20250530082655 extends AbstractMigration
     {
         // this down() migration is auto-generated, please modify it to your needs
         $this->addSql('ALTER TABLE project DROP previous_progress');
-        $this->addSql('ALTER TABLE equipment_mechanic ADD critical_success_rate INT DEFAULT NULL');
-        $this->addSql('ALTER TABLE equipment_mechanic ADD critical_fail_rate INT DEFAULT NULL');
-        $this->addSql('ALTER TABLE equipment_mechanic ADD one_shot_rate INT DEFAULT NULL');
         $this->addSql('ALTER TABLE triumph_config ALTER key DROP DEFAULT');
         $this->addSql('ALTER TABLE triumph_config ALTER quantity DROP DEFAULT');
     }

9be7ac5930b284f334b85bb9573d4fef07e0c194
prRoumanet <david.roumanet@free.fr>
2025-06-01 19:34:04 +0200

    Separated code for each PDF and Clipboard but common createChannelCanvas

--- App/src/services/export-channel-to-pdf.service.ts
+++ App/src/services/export-channel-to-pdf.service.ts
@@ -1,14 +1,18 @@
 import { jsPDF } from 'jspdf';
 import html2canvas from 'html2canvas';
 
-export async function exportChannelToPDF(chatbox: HTMLElement, toPDF: boolean): Promise<void> {
-    const maxHeightCanvas = 32767;
-    const html2canvasScale = 1.5;
-    const jpgCompression = 0.8;
-    const sizeReduction = 1.0;
-    const pageWidth = 148;
-    const pageHeight = 295;
-    const imgWidth = pageWidth * sizeReduction;
+export const page = {
+    maxHeightCanvas: 32767,
+    html2canvasScale: 1.5,
+    jpgCompression: 0.8,
+    sizeReduction: 1.0,
+    pageWidth: 148,
+    pageHeight: 295
+};
+
+
+export async function createChannelCanvas(chatbox: HTMLElement): Promise<HTMLCanvasElement> {
+    const imgWidth = page.pageWidth * page.sizeReduction;
 
     const originalStyle = {
         height: chatbox.style.height,
@@ -20,43 +24,59 @@ export async function exportChannelToPDF(chatbox: HTMLElement, toPDF: boolean): 
     chatbox.style.maxHeight = 'none';
     chatbox.style.overflow = 'visible';
 
-    html2canvas(chatbox, {
-        scale: html2canvasScale,
-        useCORS: true
-    }).then(canvas => {
-        const imgData = canvas.toDataURL('image/jpeg', jpgCompression);
-        if (!toPDF) {
-            copyCanvasToClipboard(canvas);
-        } else {
-            const pdf = new jsPDF({
-                orientation: 'p',
-                unit: 'mm',
-                format: [pageWidth, pageHeight],
-                compress: true
-            });
+    try {
+        const canvas = await html2canvas(chatbox, {
+            scale: page.html2canvasScale,
+            useCORS: true
+        });
+        return canvas;
+    } catch (error) {
+        console.error('Error creating canvas:', error);
+        // Créer un canvas vide en cas d'erreur
+        const canvas = document.createElement('canvas');
+        return canvas;
+    } finally {
+        // Restaurer le style original de l'élément chatbox
+        chatbox.style.height = originalStyle.height;
+        chatbox.style.maxHeight = originalStyle.maxHeight;
+        chatbox.style.overflow = originalStyle.overflow;
+    }
+}
 
-            const imgHeight = canvas.height * imgWidth / canvas.width;
-            let heightLeft = imgHeight;
-            let position = 0;
-
-            pdf.addImage(imgData, 'JPEG', (pageWidth-imgWidth)/2, position, imgWidth, imgHeight);
-            heightLeft -= pageHeight;
-
-            while (heightLeft >= 0) {
-                position = heightLeft - imgHeight;
-                pdf.addPage();
-                pdf.addImage(imgData, 'JPEG', (pageWidth-imgWidth)/2, position, imgWidth, imgHeight);
-                heightLeft -= pageHeight;
-            }
-            chatbox.style.height = originalStyle.height;
-            chatbox.style.maxHeight = originalStyle.maxHeight;
-            chatbox.style.overflow = originalStyle.overflow;
-            const now = new Date();
-            const dateStr = now.toISOString().slice(0,10);
-            const timeStr = now.toTimeString().slice(0,8).replace(/:/g, '-');
-            pdf.save(`emush-comm-channel-${dateStr}_${timeStr}.pdf`);
-        }
+export async function exportChannelToPDF(chatbox: HTMLElement): Promise<void> {
+    const canvas = await createChannelCanvas(chatbox);
+    const imgData = canvas.toDataURL('image/jpeg', page.jpgCompression);
+    const pdf = new jsPDF({
+        orientation: 'p',
+        unit: 'mm',
+        format: [page.pageWidth, page.pageHeight],
+        compress: true
     });
+
+    const imgWidth = page.pageWidth * page.sizeReduction;
+    const imgHeight = canvas.height * imgWidth / canvas.width;
+    let heightLeft = imgHeight;
+    let position = 0;
+
+    pdf.addImage(imgData, 'JPEG', (page.pageWidth-imgWidth)/2, position, imgWidth, imgHeight);
+    heightLeft -= page.pageHeight;
+
+    while (heightLeft >= 0) {
+        position = heightLeft - imgHeight;
+        pdf.addPage();
+        pdf.addImage(imgData, 'JPEG', (page.pageWidth-imgWidth)/2, position, imgWidth, imgHeight);
+        heightLeft -= page.pageHeight;
+    }
+
+    const now = new Date();
+    const dateStr = now.toISOString().slice(0,10);
+    const timeStr = now.toTimeString().slice(0,8).replace(/:/g, '-');
+    pdf.save(`emush-comm-channel-${dateStr}_${timeStr}.pdf`);
+}
+
+
+export async function exportChannelToClipboard(chatbox: HTMLElement): Promise<void> {
+    copyCanvasToClipboard(await createChannelCanvas(chatbox));
 }
 
 function copyCanvasToClipboard(canvas: HTMLCanvasElement) {

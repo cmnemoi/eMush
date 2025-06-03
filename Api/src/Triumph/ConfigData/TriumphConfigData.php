3995c51ac235f967e3342b18dd19f980046415bb
Dzanek <lucker1996@gmail.com>
2025-06-04 00:16:22 +0200

    Rebasing fix

--- Api/src/Triumph/ConfigData/TriumphConfigData.php
+++ Api/src/Triumph/ConfigData/TriumphConfigData.php
@@ -6,7 +6,6 @@ namespace Mush\Triumph\ConfigData;
 
 use Mush\Action\Enum\ActionEnum;
 use Mush\Action\Event\ActionEvent;
-use Mush\Action\Event\ActionEvent;
 use Mush\Communications\Event\LinkWithSolEstablishedEvent;
 use Mush\Daedalus\Event\DaedalusCycleEvent;
 use Mush\Daedalus\Event\DaedalusEvent;

cb25eb6ec5bc5d8692fa46314e4a97566c3a7b7e
Evian <icdk5b4zf@relay.firefox.com>
2025-06-05 18:28:26 +0200

    fix: "New cycle" button will appear without a page refresh

--- App/src/components/Utils/CountdownTimer.vue
+++ App/src/components/Utils/CountdownTimer.vue
@@ -1,66 +1,132 @@
 <template>
     <div>
-        <slot :hour="hour" :min="min" :sec="sec" />
+        <slot
+            :hour="formattedHours"
+            :min="formattedMinutes"
+            :sec="formattedSeconds"
+            :is-finished="isCountdownFinished"
+        />
     </div>
 </template>
 
 <script>
+const MILLISECONDS_PER_SECOND = 1000;
+const SECONDS_PER_MINUTE = 60;
+const SECONDS_PER_HOUR = 3600;
+const TIMER_UPDATE_INTERVAL_MS = 1000;
+const MINIMUM_DOUBLE_DIGIT_NUMBER = 10;
+
 export default {
-    props : {
-        endDate : {  // pass date object till when you want to run the timer
-            type : Date,
-            default(){
+    name: 'CountdownTimer',
+
+    props: {
+        endDate: {
+            type: Date,
+            default() {
                 return new Date();
             }
         },
-        negative : {  // optional, should countdown after 0 to negative
-            type : Boolean,
-            default : false
+        shouldAllowNegativeCountdown: {
+            type: Boolean,
+            default: false
         }
     },
+
     emits: ['end-time'],
-    data(){
-        return{
-            now : new Date(),
-            timer : null
+
+    data() {
+        return {
+            currentTime: new Date(),
+            timerIntervalId: null,
+            isCountdownFinished: false
         };
     },
-    computed:{
-        hour(){
-            const h = Math.trunc((this.endDate - this.now) / 1000 / 3600);
-            return h;
+
+    computed: {
+        remainingTimeInSeconds() {
+            return Math.trunc((this.endDate - this.currentTime) / MILLISECONDS_PER_SECOND);
         },
-        min(){
-            const m = Math.trunc((this.endDate - this.now) / 1000 / 60) % 60;
-            return m>9?m:'0'+m;
+
+        formattedHours() {
+            const hours = Math.trunc(this.remainingTimeInSeconds / SECONDS_PER_HOUR);
+            return hours;
         },
-        sec(){
-            const s = Math.trunc((this.endDate - this.now)/1000) % 60;
-            return s>9?s:'0'+s;
+
+        formattedMinutes() {
+            const minutes = Math.trunc(this.remainingTimeInSeconds / SECONDS_PER_MINUTE) % SECONDS_PER_MINUTE;
+            return this.formatTimeUnit(minutes);
+        },
+
+        formattedSeconds() {
+            const seconds = this.remainingTimeInSeconds % SECONDS_PER_MINUTE;
+            return this.formatTimeUnit(seconds);
         }
     },
-    watch : {
-        endDate : {
-            immediate : true,
-            handler(newVal){
-                if(this.timer){
-                    clearInterval(this.timer);
-                }
-                this.timer = setInterval(()=>{
-                    this.now = new Date();
-                    if(this.negative)
-                        return;
-                    if(this.now > newVal){
-                        this.now = newVal;
-                        this.$emit('end-time');
-                        clearInterval(this.timer);
-                    }
-                }, 1000);
+
+    watch: {
+        endDate: {
+            immediate: true,
+            handler(newEndDate) {
+                this.resetTimer();
+                this.startCountdown(newEndDate);
             }
         }
     },
-    beforeUnmount(){
-        clearInterval(this.timer);
+
+    beforeUnmount() {
+        this.clearTimerInterval();
+    },
+
+    methods: {
+        formatTimeUnit(timeUnit) {
+            const isDoubleDigit = timeUnit >= MINIMUM_DOUBLE_DIGIT_NUMBER;
+            return isDoubleDigit ? timeUnit : '0' + timeUnit;
+        },
+
+        resetTimer() {
+            this.clearTimerInterval();
+            this.isCountdownFinished = false;
+        },
+
+        startCountdown(targetEndDate) {
+            this.timerIntervalId = setInterval(() => {
+                this.updateCurrentTime();
+                this.checkIfCountdownShouldFinish(targetEndDate);
+            }, TIMER_UPDATE_INTERVAL_MS);
+        },
+
+        updateCurrentTime() {
+            this.currentTime = new Date();
+        },
+
+        checkIfCountdownShouldFinish(targetEndDate) {
+            if (this.shouldAllowNegativeCountdown) {
+                return;
+            }
+
+            const hasTimeExpired = this.currentTime > targetEndDate;
+            if (hasTimeExpired) {
+                this.finishCountdown(targetEndDate);
+            }
+        },
+
+        finishCountdown(targetEndDate) {
+            this.currentTime = targetEndDate;
+            this.isCountdownFinished = true;
+            this.emitEndTimeEvent();
+            this.clearTimerInterval();
+        },
+
+        emitEndTimeEvent() {
+            this.$emit('end-time');
+        },
+
+        clearTimerInterval() {
+            if (this.timerIntervalId) {
+                clearInterval(this.timerIntervalId);
+                this.timerIntervalId = null;
+            }
+        }
     }
 };
 </script>

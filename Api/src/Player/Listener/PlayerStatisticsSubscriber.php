23bdf91089129b7704e850a07aa4499ae96c34cf
Dzanek <lucker1996@gmail.com>
2025-06-11 22:01:52 +0200

    refactor: Extracting methods and simplifying computations

--- Api/src/Player/Listener/PlayerStatisticsSubscriber.php
+++ Api/src/Player/Listener/PlayerStatisticsSubscriber.php
@@ -66,7 +66,6 @@ class PlayerStatisticsSubscriber implements EventSubscriberInterface
 
         // Waste AP spent on movement
         if ($event->getActionName() === ActionEnum::CONVERT_ACTION_TO_MOVEMENT) {
-            $playerStatistics->incrementActionPointsUsed($apSpent);
             $playerStatistics->incrementActionPointsWasted($apSpent);
         }
 

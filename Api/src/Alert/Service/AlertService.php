cbca7daae5b25da9367772ca346c4471178882b5
Evian <icdk5b4zf@relay.firefox.com>
2025-06-27 00:03:09 +0200

    fix: Fix duplicate alerts with safer object comparison

--- Api/src/Alert/Service/AlertService.php
+++ Api/src/Alert/Service/AlertService.php
@@ -238,7 +238,7 @@ class AlertService implements AlertServiceInterface
             throw $exception;
         }
 
-        $filteredList = $alert->getAlertElements()->filter(static fn (AlertElement $element) => $element->getEquipment() === $equipment);
+        $filteredList = $alert->getAlertElements()->filter(static fn (AlertElement $element) => $element->getEquipment()?->equals($equipment));
         $alertEquipment = $filteredList->first();
 
         if (!$alertEquipment) {
@@ -294,7 +294,9 @@ class AlertService implements AlertServiceInterface
 
     public function getAlertFireElement(Alert $alert, Place $place): AlertElement
     {
-        $filteredList = $alert->getAlertElements()->filter(static fn (AlertElement $element) => $element->getPlace() === $place);
+        $filteredList = $alert->getAlertElements()->filter(
+            static fn (AlertElement $element) => $element->getPlace()?->equals($place)
+        );
         $fireAlert = $filteredList->first();
 
         if (!$fireAlert) {

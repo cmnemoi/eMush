245bb9a172c327956f4cb0d5275bfa1e2212654d
Dzanek <lucker1996@gmail.com>
2025-07-02 22:39:13 +0000

    feat: Research finished highlight

--- Api/tests/functional/Player/Event/ProjectFinishedEventCest.php
+++ Api/tests/functional/Player/Event/ProjectFinishedEventCest.php
@@ -4,8 +4,11 @@ declare(strict_types=1);
 
 namespace Mush\tests\functional\Player\Event;
 
+use Mush\Game\Service\EventServiceInterface;
 use Mush\Player\Enum\PlayerVariableEnum;
+use Mush\Player\ValueObject\PlayerHighlight;
 use Mush\Project\Enum\ProjectName;
+use Mush\Project\Event\ProjectEvent;
 use Mush\Tests\AbstractFunctionalTest;
 use Mush\Tests\FunctionalTester;
 
@@ -14,6 +17,15 @@ use Mush\Tests\FunctionalTester;
  */
 final class ProjectFinishedEventCest extends AbstractFunctionalTest
 {
+    private EventServiceInterface $eventService;
+
+    public function _before(FunctionalTester $I): void
+    {
+        parent::_before($I);
+
+        $this->eventService = $I->grabService(EventServiceInterface::class);
+    }
+
     public function shouldIncreasePlayerMaxActionPointsWhenNoiseReducerIsFinished(FunctionalTester $I): void
     {
         $this->givenPlayerHasMaxActionPoints(12, $I);
@@ -21,6 +33,58 @@ final class ProjectFinishedEventCest extends AbstractFunctionalTest
         $this->whenNoiseReducerProjectIsFinished($I);
 
         $this->thenPlayerMaxActionPointsShouldBe(14, $I);
+    }
+
+    public function shouldCreatePublicLogAndHighlightWhenResearchIsFinished(FunctionalTester $I): void
+    {
+        // given research project is finished
+        $research = $this->daedalus->getProjectByName(ProjectName::RETRO_FUNGAL_SERUM);
+        $research->finish();
+
+        // given I have a project finished event for a research project
+        $projectEvent = new ProjectEvent(
+            project: $research,
+            author: $this->chun,
+        );
+
+        // when I dispatch the event
+        $this->eventService->callEvent($projectEvent, ProjectEvent::PROJECT_FINISHED);
+
+        // then highlight about research finished should be created
+        $I->assertEquals(
+            expected: [
+                'name' => 'project.finished_research',
+                'result' => PlayerHighlight::SUCCESS,
+                'parameters' => ['target_project' => $research->getLogName()],
+            ],
+            actual: $this->chun->getPlayerInfo()->getPlayerHighlights()[0]->toArray(),
+        );
+    }
+
+    public function shouldNotCreateMessageOrHighlightWhenNeronProjectIsFinished(FunctionalTester $I): void
+    {
+        // given neron project is finished
+        $neronProject = $this->daedalus->getProjectByName(ProjectName::PLASMA_SHIELD);
+        $neronProject->finish();
+
+        // when I dispatch the event
+        $this->eventService->callEvent(
+            event: new ProjectEvent(
+                project: $neronProject,
+                author: $this->chun,
+            ),
+            name: ProjectEvent::PROJECT_FINISHED,
+        );
+
+        // then highlight about research finished not should be created
+        $I->assertNotEquals(
+            expected: [
+                'name' => 'project.finished_research',
+                'result' => PlayerHighlight::SUCCESS,
+                'parameters' => ['target_project' => $neronProject->getLogName()],
+            ],
+            actual: $this->chun->getPlayerInfo()->getPlayerHighlights()[0]->toArray(),
+        );
     }
 
     private function givenPlayerHasMaxActionPoints(int $maxActionPoints, FunctionalTester $I): void

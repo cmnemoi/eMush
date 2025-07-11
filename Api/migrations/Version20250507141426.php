bf9084ecc17a470995379db2aa238964f970ea50
Noctis-S <lolakurai@gmail.com>
2025-07-11 21:35:51 +0000

    Apply 1 suggestion(s) to 1 file(s)
    Co-authored-by: Evian <icdk5b4zf@relay.firefox.com>

--- Api/migrations/Version20250507141426.php
+++ Api/migrations/Version20250507141426.php
@@ -50,7 +50,6 @@ final class Version20250507141426 extends AbstractMigration
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
-        $this->addSql('CREATE SCHEMA public');
         $this->addSql('ALTER TABLE place_config DROP patrol_ship_names');
         $this->addSql('ALTER TABLE game_equipment DROP docking_place');
         $this->addSql('ALTER TABLE game_equipment DROP patrol_ship_name');

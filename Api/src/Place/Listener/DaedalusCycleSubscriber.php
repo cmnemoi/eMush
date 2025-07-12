e88ca9ada89dade5587803ae0fc739eff5c440d8
PrincessFelicie <taxouck1@live.fr>
2025-07-12 16:57:49 +0200

    Moved fire propagation logic to DaedalusCycleSubscriber so that it is only called once per cycle instead of once per fire. Reimplemented limit of 2 spreading fires per cycle, as per Mush code from MT

--- Api/src/Place/Listener/DaedalusCycleSubscriber.php
+++ Api/src/Place/Listener/DaedalusCycleSubscriber.php
@@ -5,21 +5,32 @@ namespace Mush\Place\Listener;
 use Mush\Daedalus\Event\DaedalusCycleEvent;
 use Mush\Game\Enum\EventPriorityEnum;
 use Mush\Game\Service\EventServiceInterface;
+use Mush\Game\Service\RandomServiceInterface;
 use Mush\Place\Entity\Place;
+use Mush\Place\Enum\RoomEventEnum;
 use Mush\Place\Event\PlaceCycleEvent;
+use Mush\Status\Enum\StatusEnum;
+use Mush\Status\Service\StatusServiceInterface;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 use Symfony\Component\Lock\LockFactory;
 
 class DaedalusCycleSubscriber implements EventSubscriberInterface
 {
     private EventServiceInterface $eventService;
+    private RandomServiceInterface $randomService;
+
+    private StatusServiceInterface $statusService;
     private LockFactory $lockFactory;
 
     public function __construct(
         EventServiceInterface $eventService,
+        RandomServiceInterface $randomService,
+        StatusServiceInterface $statusService,
         LockFactory $lockFactory
     ) {
         $this->eventService = $eventService;
+        $this->randomService = $randomService;
+        $this->statusService = $statusService;
         $this->lockFactory = $lockFactory;
     }
 
@@ -37,6 +48,7 @@ class DaedalusCycleSubscriber implements EventSubscriberInterface
 
         try {
             $this->handlePlacesNewCycle($event);
+            $this->handleFirePropagation($event);
         } finally {
             $lock->release();
         }
@@ -52,6 +64,37 @@ class DaedalusCycleSubscriber implements EventSubscriberInterface
                 $event->getTime()
             );
             $this->eventService->callEvent($newRoomCycle, PlaceCycleEvent::PLACE_NEW_CYCLE);
+        }
+    }
+
+    private function handleFirePropagation(DaedalusCycleEvent $event): void
+    {
+        $daedalus = $event->getDaedalus();
+        $difficultyConfig = $daedalus->getGameConfig()->getDifficultyConfig();
+
+        $roomsOnFire = $daedalus->getRooms()->getAllWithStatus(StatusEnum::FIRE);
+        $maxFireSpread = min($difficultyConfig->getMaximumAllowedSpreadingFires(), $roomsOnFire->count());
+
+        /** @var Place $room */
+        foreach ($this->randomService->getRandomElements($roomsOnFire->toArray(), $roomsOnFire->count()) as $room) {
+            if ($maxFireSpread >= 1) {
+                if ($this->randomService->isSuccessful($difficultyConfig->getPropagatingFireRate())) {
+                    $adjacentRoomsNotOnFire = $room->getAdjacentRoomsAsPlaceCollection()->getAllWithoutStatus(StatusEnum::FIRE);
+                    if (!$adjacentRoomsNotOnFire->isEmpty()) {
+                        /** @var Place $spreadingRoom */
+                        $spreadingRoom = $this->randomService->getRandomElement($adjacentRoomsNotOnFire->toArray());
+
+                        $this->statusService->createStatusFromName(
+                            StatusEnum::FIRE,
+                            $spreadingRoom,
+                            [RoomEventEnum::PROPAGATING_FIRE],
+                            $event->getTime()
+                        );
+
+                        --$maxFireSpread;
+                    }
+                }
+            }
         }
     }
 }

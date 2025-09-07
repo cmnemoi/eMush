b4af4c48024d1585e8ead73fdeb68edd40663e1e
Evian <icdk5b4zf@relay.firefox.com>
2025-09-07 21:21:16 +0200

    feat: Update team in the footer

--- App/src/enums/footer.ts
+++ App/src/enums/footer.ts
@@ -11,7 +11,7 @@ export const team: (Contributor)[] = [
     { name: 'gowoons', role: 'developer', active: true, coreTeam: false },
     { name: 'A7', role: 'developer', active: true, coreTeam: false },
     { name: 'zasfree', role: 'admin', active: true, coreTeam: true },
-    { name: 'Simpkin', role: 'admin', active: true, coreTeam: true },
+    { name: 'Simpkin', role: 'developer', active: false, coreTeam: false },
     { name: 'rackhaml', role: 'helper', active: false, coreTeam: false },
     { name: 'RSickenberg', role: 'developer', active: true, coreTeam: false },
     { name: 'Demurgos', role: 'developer', active: false, coreTeam: false },
@@ -27,13 +27,16 @@ export const team: (Contributor)[] = [
     { name: 'Bibni', role: 'developer', active: true, coreTeam: false },
     { name: 'Joker', role: 'developer', active: true, coreTeam: false },
     { name: 'Le Camarade Savva', role: 'developer', active: true, coreTeam: false },
-    { name: 'Princess Félicie', role: 'developer', active: true, coreTeam: false },
+    { name: 'Princess Félicie', role: 'developer', active: true, coreTeam: true },
     { name: 'valen', role: 'developer', active: true, coreTeam: false },
     { name: 'Fendi', role: 'developer', active: true, coreTeam: false },
     { name: 'Dżanek', role: 'developer', active: true, coreTeam: false },
     { name: 'Golfy038', role: 'developer', active: true, coreTeam: false },
-    { name: 'Noctis', role: 'developer', active: true, coreTeam: false },
-    { name: 'Valedres', role: 'developer', active: true, coreTeam: false }
+    { name: 'Noctis', role: 'developer', active: true, coreTeam: true },
+    { name: 'Valedres', role: 'developer', active: true, coreTeam: false },
+    { name: 'Shadowombat', role: 'helper', active: true, coreTeam: true },
+    { name: 'Brute Epaisse', role: 'helper', active: true, coreTeam: true },
+    { name: 'Monoxyd', role: 'developer', active: true, coreTeam: false }
 ];
 
 export const crowdin = "https://eternaltwin.crowdin.com/multilingual/c9f4ef84da7d855637e201101992f6ed/all?languages=fr,es,en&filter=basic&value=0";

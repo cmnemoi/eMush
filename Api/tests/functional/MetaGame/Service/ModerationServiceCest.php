e38d8ef08740adc253483f3d1e9531e3fc7d1553
Evian <icdk5b4zf@relay.firefox.com>
2025-10-09 19:08:55 +0200

    fix(moderation): Fix a crash when banning multiple users

--- Api/tests/functional/MetaGame/Service/ModerationServiceCest.php
+++ Api/tests/functional/MetaGame/Service/ModerationServiceCest.php
@@ -6,6 +6,7 @@ use Mush\MetaGame\Service\ModerationServiceInterface;
 use Mush\Tests\AbstractFunctionalTest;
 use Mush\Tests\FunctionalTester;
 use Mush\User\Entity\BannedIp;
+use Mush\User\Entity\User;
 
 /**
  * @internal
@@ -14,47 +15,98 @@ final class ModerationServiceCest extends AbstractFunctionalTest
 {
     private ModerationServiceInterface $moderationService;
 
+    private User $user;
+    private User $author;
+    private string $hashedIp;
+
     public function _before(FunctionalTester $I): void
     {
         parent::_before($I);
         $this->moderationService = $I->grabService(ModerationServiceInterface::class);
+        $this->user = $this->player1->getUser();
+        $this->author = $this->player2->getUser();
+        $this->hashedIp = hash_hmac('sha256', '127.0.0.1', 'my_secret');
     }
 
-    public function testBan(FunctionalTester $I): void
+    public function shouldBanUser(FunctionalTester $I): void
     {
-        $user = $this->player1->getUser();
+        $this->whenBanUser();
+
+        $this->thenUserIsBanned($I);
+    }
+
+    public function shouldBanUserByIp(FunctionalTester $I): void
+    {
+        $this->givenUserHasHashedIp();
+
+        $this->whenBanUserByIp();
+
+        $this->thenUserIsBanned($I);
+        $this->thenIpIsBanned($I);
+    }
+
+    public function shouldNotSaveAlreadyBannedIp(FunctionalTester $I): void
+    {
+        $this->givenUserHasHashedIp();
+        $this->givenIpIsAlreadyBanned($I);
+
+        $this->whenBanUserByIp();
+
+        $this->thenUserIsBanned($I);
+        $this->thenOnlyOneBannedIpRecordExists($I);
+    }
+
+    private function givenUserHasHashedIp(): void
+    {
+        $this->user->addHashedIp($this->hashedIp);
+    }
+
+    private function givenIpIsAlreadyBanned(FunctionalTester $I): void
+    {
+        $bannedIp = new BannedIp($this->hashedIp);
+        $I->haveInRepository($bannedIp);
+    }
+
+    private function whenBanUser(): void
+    {
         $this->moderationService->banUser(
-            user: $user,
-            author: $this->player2->getUser(),
+            user: $this->user,
+            author: $this->author,
             reason: 'because',
             message: 'adminMessage',
             duration: new \DateInterval('P1D'),
         );
-
-        $I->assertTrue($user->isBanned());
     }
 
-    public function shouldBanByIp(FunctionalTester $I): void
+    private function whenBanUserByIp(): void
     {
-        $user = $this->player1->getUser();
-        $hashedIp = hash_hmac('sha256', '127.0.0.1', 'my_secret');
-        $user->addHashedIp($hashedIp);
-
         $this->moderationService->banUser(
-            user: $user,
-            author: $this->player2->getUser(),
+            user: $this->user,
+            author: $this->author,
             reason: 'because',
             message: 'adminMessage',
             duration: new \DateInterval('P1D'),
             byIp: true,
         );
+    }
 
-        $I->assertTrue($user->isBanned());
+    private function thenUserIsBanned(FunctionalTester $I): void
+    {
+        $I->assertTrue($this->user->isBanned());
+    }
+
+    private function thenIpIsBanned(FunctionalTester $I): void
+    {
         $I->seeInRepository(
             entity: BannedIp::class,
             params: [
-                'value' => $hashedIp,
+                'value' => $this->hashedIp,
             ]
         );
+    }
+
+    private function thenOnlyOneBannedIpRecordExists(FunctionalTester $I): void
+    {
+        $I->seeNumRecords(1, BannedIp::class, ['value' => $this->hashedIp]);
     }
 }

0d014da592097b119f9b1c57bceea779ed6b7244
Evian <icdk5b4zf@relay.firefox.com>
2025-10-20 23:34:06 +0200

    fix: Do not ban all users by IP definitively
    (cherry picked from commit 39e1e7d870ee311294c7fda5a0fb6314b3edd811)

---
+++ Api/tests/api/MetaGame/ModerationControllerCest.php
@@ -0,0 +1,47 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Mush\Tests\api\MetaGame;
+
+use Mush\Tests\ApiTester;
+use Mush\User\Entity\User;
+use Mush\User\Repository\BannedIpRepository;
+
+final class ModerationControllerCest
+{
+    private User $moderator;
+    private User $user;
+
+    private BannedIpRepository $bannedIpRepository;
+
+    public function _before(ApiTester $I): void
+    {
+        $this->user = $I->loginUser('user');
+        $this->moderator = $I->loginUser('moderator');
+
+        $this->bannedIpRepository = $I->grabService(BannedIpRepository::class);
+    }
+
+    public function shouldBanUser(ApiTester $I): void
+    {
+        $this->user->addHashedIp('127.0.0.1');
+
+        $I->sendPostRequest("moderation/ban-user/{$this->user->getId()}?duration=P1D&startDate=2022-01-01&byIp=false&adminMessage=test&reason=multi_account");
+
+        $I->seeResponseCodeIs(200);
+        $I->seeResponseContainsJson(['detail' => 'User banned successfully']);
+        \assert($this->bannedIpRepository->exists('127.0.0.1') === false);
+    }
+
+    public function shouldBanUserByIp(ApiTester $I): void
+    {
+        $this->user->addHashedIp('127.0.0.1');
+
+        $I->sendPostRequest("moderation/ban-user/{$this->user->getId()}?duration=P1D&startDate=2022-01-01&byIp=true&adminMessage=test&reason=multi_account");
+
+        $I->seeResponseCodeIs(200);
+        $I->seeResponseContainsJson(['detail' => 'User banned successfully']);
+        \assert($this->bannedIpRepository->exists('127.0.0.1') === true);
+    }
+}

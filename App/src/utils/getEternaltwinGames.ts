2d0a64df2c30a4766f2e79d3848a707ec10e540f
Evian <icdk5b4zf@relay.firefox.com>
2025-10-23 18:30:38 +0200

    fix: Add Direct Quiz favicon

--- App/src/utils/getEternaltwinGames.ts
+++ App/src/utils/getEternaltwinGames.ts
@@ -9,7 +9,7 @@ export const getEternaltwinGames = (): EternaltwinGame[] => {
         { key: 'alphabounce', link: 'https://alphabounce.eternaltwin.org/', icon: 'https://alphabounce.eternaltwin.org/favicon.ico' },
         { key: 'dinocard', link: 'https://dinocard.eternaltwin.org/', icon: 'https://dinocard.eternaltwin.org/favicon.ico' },
         { key: 'dinorpg', link: 'https://dinorpg.eternaltwin.org/', icon: 'https://dinorpg.eternaltwin.org/favicon.ico' },
-        { key: 'directquiz', link: 'https://directquiz.org/', icon: 'https://eternaltwin.org/favicon.ico' },
+        { key: 'directquiz', link: 'https://directquiz.org/', icon: 'https://directquiz.org/images/favicon.ico' },
         { key: 'epopotamo', link: 'https://epopotamo.eternaltwin.org/', icon: 'https://epopotamo.eternaltwin.org/favicon.ico' },
         { key: 'eternalfest', link: 'https://eternalfest.net/', icon: 'https://eternalfest.net/favicon.ico' },
         { key: 'kingdom', link: 'https://kingdom.eternaltwin.org/', icon: 'https://kingdom.eternaltwin.org/favicon.ico' },

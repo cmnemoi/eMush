ac48daa8764ba30f668077b2bea689357d91678f
AeonPSR <floriandevigne1@hotmail.fr>
2026-05-30 15:40:31 +0000

    fix: fixed discord URL

--- App/src/utils/links.ts
+++ App/src/utils/links.ts
@@ -1,7 +1,7 @@
 import { GameLocales } from "@/i18n";
 import urlJoin from "url-join";
 
-export const getDiscordLink = (): string => "https://discord.gg/eternaltwin-693082011484684348";
+export const getDiscordLink = (): string => "https://discord.gg/eternaltwin";
 export const getForumLink = (): string => urlJoin(import.meta.env.VITE_ETERNALTWIN_URL, "forum", "sections", "b5ddc792-0738-4289-9818-c2f1f029c8b1");
 export const getWikiLink = (locale: string): string => {
     switch (locale) {

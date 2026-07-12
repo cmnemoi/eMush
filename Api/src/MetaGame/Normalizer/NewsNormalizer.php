0f1fe52de014a2f4fb334aa9eed55793e3902c15
Noctis_S <lolakurai@gmail.com>
2026-07-12 21:43:22 +0200

    fix: news with poll don't break the front page

--- Api/src/MetaGame/Normalizer/NewsNormalizer.php
+++ Api/src/MetaGame/Normalizer/NewsNormalizer.php
@@ -5,8 +5,6 @@ declare(strict_types=1);
 namespace Mush\MetaGame\Normalizer;
 
 use Mush\MetaGame\Entity\News;
-use Mush\MetaGame\Entity\Poll\Poll;
-use Mush\User\Entity\User;
 use Symfony\Component\Serializer\Normalizer\NormalizerAwareInterface;
 use Symfony\Component\Serializer\Normalizer\NormalizerAwareTrait;
 use Symfony\Component\Serializer\Normalizer\NormalizerInterface;
@@ -54,27 +52,13 @@ class NewsNormalizer implements NormalizerInterface, NormalizerAwareInterface
         $poll = $news->getPoll();
         if ($poll) {
             $normalizedPoll = $this->normalizer->normalize($news->getPoll(), $format, $context);
-            $normalizedNews['poll'] = $normalizedPoll;
-        }
-
-        return $normalizedNews;
-    }
-
-    private function normalizeOptions(Poll $poll, User $user): array
-    {
-        $normalizedOptions = [];
-
-        foreach ($poll->getOptions() as $option) {
-            if ($option !== null) {
-                $normalizedOptions[] = [
-                    'id' => $option->getId(),
-                    'name' => $option->getName(),
-                    'votes' => $option->getVotes()->count(),
-                    'voted' => $option->userAlreadyVoted($user),
-                ];
+            if ($normalizedPoll) {
+                $normalizedNews['poll'] = $normalizedPoll;
+            } else {
+                unset($normalizedNews['poll']);
             }
         }
 
-        return $normalizedOptions;
+        return $normalizedNews;
     }
 }
